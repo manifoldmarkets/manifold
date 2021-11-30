d5511da4d450f4fdf005c32c3c0c207581059c71
Austin Chen <akrolsmir@gmail.com>
2021-11-30 14:50:50 -0800

    Parse example bets from spreadsheet

---
+++ market-simulator/src/components/Simulator.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="overflow-x-auto px-12">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Order #</th>
+          <th>Yes bid</th>
+          <th>No Bid</th>
+          <th>Implied Probability</th>
+          <th>Payout</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(entry, i) in entries">
+          <th>{{ i }}</th>
+          <td>{{ entry.yesBid || '' }}</td>
+          <td>{{ entry.noBid || '' }}</td>
+          <td>{{ entry.prob }}</td>
+          <td>{{ entry.payout }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { bids } from './orders'
+
+const entries = bids.map((bid) => ({
+  ...bid,
+  prob: 0.646,
+  payout: 1.23,
+}))
+</script>
