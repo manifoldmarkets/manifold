a1163bfcfbff8176c66285c25f8b9fa482e8a728
jahooma <jahooma@gmail.com>
2021-11-30 22:20:13 -0600

    Create Nextjs template app under web directory.

---
+++ web/README.md
@@ -0,0 +1,1 @@
+Mantic Markets web
---
+++ web/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Mantic Markets</title>
+        <meta name="description" content="Create and bet" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    Welcome to Mantic Markets
+    </div>
+  )
+}
+
+export default Home
