078c47068f53f9fa794732ef5d46301cfce141fa
jahooma <jahooma@gmail.com>
2021-12-01 17:23:48 -0600

    Change color and background image of landing page

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,12 +3,16 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'world-trading': "url('/world-trading-background.jpg')",
+      }
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [
-      require('@tailwindcss/forms'),
+    require('@tailwindcss/forms'),
   ],
 }
