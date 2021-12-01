844a908118cb6485ba0a19be5124e7fc1bde5840
jahooma <jahooma@gmail.com>
2021-12-01 13:58:00 -0600

    Add tailwind. Add stripped down hero component.

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import React from 'react'
+import { LandingPage } from './landing-page'
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +12,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    Welcome to Mantic Markets
+      <LandingPage />
     </div>
   )
 }
