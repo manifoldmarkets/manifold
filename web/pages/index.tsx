75b648cc1570633a4290cc38760aabb2bbed6c6d
jahooma <jahooma@gmail.com>
2021-12-01 18:22:45 -0600

    Fix build error

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import React from 'react'
-import { LandingPage } from './landing-page'
+import LandingPage from './landing-page'
 
 const Home: NextPage = () => {
   return (
