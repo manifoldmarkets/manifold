36aec9d553cfc995b921e48f49a998af7cdf6913
Austin Chen <akrolsmir@gmail.com>
2021-12-02 17:21:43 -0600

    Use Major Mono for the header font

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -10,6 +10,10 @@ const Home: NextPage = () => {
         <title>Mantic Markets</title>
         <meta name="description" content="Create and bet" />
         <link rel="icon" href="/favicon.ico" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap" rel="stylesheet" />
       </Head>
 
       <LandingPage />
