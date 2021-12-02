6c6d1ba55f7b83a8b07b626bdc4e4721febc0533
Austin Chen <akrolsmir@gmail.com>
2021-12-02 17:53:21 -0600

    Fix NextJs build

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -12,7 +12,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap"
           rel="stylesheet"
