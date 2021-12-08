2a3d9e62726724cb51e58e240e19284fdbf025bd
Austin Chen <akrolsmir@gmail.com>
2021-12-08 08:42:19 -0800

    Add Firestore package and config

---
+++ web/lib/firebase/init.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app'
+const firebaseConfig = {
+  apiKey: 'AIzaSyDp3J57vLeAZCzxLD-vcPaGIkAmBoGOSYw',
+  authDomain: 'mantic-markets.firebaseapp.com',
+  projectId: 'mantic-markets',
+  storageBucket: 'mantic-markets.appspot.com',
+  messagingSenderId: '128925704902',
+  appId: '1:128925704902:web:f61f86944d8ffa2a642dc7',
+  measurementId: '${config.measurementId}',
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
+try {
+  // Note: this is still throwing a console error atm...
+  import('firebase/analytics').then((analytics) => {
+    analytics.getAnalytics(app)
+  })
+} catch (e) {
+  console.warn('Analytics were blocked')
+}
