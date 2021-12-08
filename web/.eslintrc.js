0d77a66503a59e17549d947e7662c81c23e32ce3
jahooma <jahooma@gmail.com>
2021-12-08 12:25:37 -0600

    Add eslint for react hooks and nextjs.

---
+++ web/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+	  'plugin:react-hooks/recommended',
+	  'plugin:@next/next/recommended',
+	],
+  rules: {
+	  // Add or disable rules here.
+  }
+}
