025e49458f1fd6215b692fe7b0f232f26904947b
Austin Chen <akrolsmir@gmail.com>
2021-12-09 15:37:26 -0800

    Standardize on useUser hook

--- web/hooks/use-user.ts
+++ web/hooks/use-user.ts
@@ -3,10 +3,6 @@ import { listenForLogin, User } from '../lib/firebase/users'
 
 export const useUser = () => {
   const [user, setUser] = useState<User | null>(null)
-
-  useEffect(() => {
-    return listenForLogin(setUser)
-  }, [])
-
+  useEffect(() => listenForLogin(setUser), [])
   return user
 }
