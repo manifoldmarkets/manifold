74de283f55bf6d431b660a1ec3c6374f272e06d4
jahooma <jahooma@gmail.com>
2021-12-09 16:05:55 -0600

    Add template contract page

---
+++ web/hooks/use-contract.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react'
+import { Contract, listenForContract } from '../lib/firebase/contracts'
+
+export const useContract = (contractId: string) => {
+	const [contract, setContract] = useState<Contract | null | 'loading'>('loading')
+
+	useEffect(() => {
+		if (contractId)
+			return listenForContract(contractId, setContract)
+	}, [contractId])
+
+	return contract
+}
