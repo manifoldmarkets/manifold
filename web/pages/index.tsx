c791ecc1e41e835cfd2627cc61b9d38e21ad2dd6
Austin Chen <akrolsmir@gmail.com>
2021-12-08 23:39:20 -0800

    Move html head content to Next's _document

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,55 +1,11 @@
 import React from 'react'
 
 import type { NextPage } from 'next'
-import Head from 'next/head'
 
 import { Hero } from '../components/hero'
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Mantic Markets</title>
-
-        <meta property="og:title" name="twitter:title" content="Mantic Markets" />
-        <meta name="description" content="Decentralized user-created prediction markets on Solana" />
-        <meta property="og:description" name="twitter:description" content="Decentralized user-created prediction markets on Solana" />
-        <meta property="og:url" content="https://mantic.markets" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@manticmarkets" />
-        <meta property="og:image" name="twitter:image" content="https://mantic.markets/logo-cover.png" />
-
-        <link rel="icon" href="/favicon.ico" />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap"
-          rel="stylesheet"
-        />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-SSFK1Q138D"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-SSFK1Q138D');
-          `,
-          }}
-        />
-      </Head>
-
-      <Hero />
-    </div>
-  )
+  return <Hero />
 }
 
 export default Home
