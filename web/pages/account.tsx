b309e5961de3cabb7b512fdcf4d276adc816cec7
Austin Chen <akrolsmir@gmail.com>
2021-12-10 09:54:16 -0800

    Allow users to create new contracts (#4)
    * Add Firestore package and config
    
    * Upload basic Firebase Auth code
    
    * Basic ability to sign in and view profile
    
    * Move html head content to Next's _document
    
    * Apply dark theme to all DaisyUI components
    
    * Add contract page
    
    * Smaller width bet input
    
    * Allow users to create new contracts
    
    * Add back listenForContract
    
    * Add some buttons
    
    * Add Row, Col, and Spacer components
    
    * Implement skeleton ContractPage
    
    * Apply dark theme to all DaisyUI components
    
    * Fix hooks lints (#3)
    
    * Add background to bet panel
    
    * Sort contracts by creation time
    
    * Link to market creation from header
    
    * List your markets on account page
    
    * Set fullscreen black background
    
    * Correctly set seeds on new contracts
    
    * Code cleanups
    
    * Gratuitously cool font
    
    * Add creator name, fix ordering
    
    * Use Readex Pro as body font
    
    * Fixes according to code review
    
    Co-authored-by: jahooma <jahooma@gmail.com>

--- web/pages/account.tsx
+++ web/pages/account.tsx
@@ -2,112 +2,52 @@ import { useRouter } from 'next/router'
 import { firebaseLogout } from '../lib/firebase/users'
 import { Header } from '../components/header'
 import { useUser } from '../hooks/use-user'
+import { useState, useEffect } from 'react'
+import { Contract, listContracts } from '../lib/firebase/contracts'
+import { ContractsList } from '../components/contracts-list'
 
 export default function Account() {
   const user = useUser()
   const router = useRouter()
+  const [contracts, setContracts] = useState<Contract[]>([])
+  useEffect(() => {
+    if (user?.id) {
+      listContracts(user?.id).then(setContracts)
+    }
+  }, [user?.id])
 
   return (
     <div className="relative overflow-hidden h-screen bg-cover bg-gray-900">
       <Header />
-      <div className="flex items-center w-full h-max px-4 py-10 bg-cover card">
-        <div className="card glass lg:card-side text-neutral-content bg-gray-800 m-10 transition-all">
-          <figure className="p-6">
-            <img src={user?.avatarUrl} className="rounded-lg shadow-lg" />
-          </figure>
-          <div className="max-w-md card-body">
-            <h2 className="card-title">{user?.name}</h2>
-            <p>{user?.email}</p>
-            <p>${user?.balanceUsd} USD</p>
-            <div className="card-actions">
-              <button
-                className="btn glass rounded-full"
-                onClick={() => {
-                  firebaseLogout()
-                  router.push('/')
-                }}
-              >
-                Sign Out
-              </button>
+      <div className="max-w-4xl my-20 mx-auto">
+        <div>
+          <div className="card glass lg:card-side text-neutral-content bg-gray-800 transition-all max-w-sm mx-auto my-20">
+            <figure className="p-6">
+              <img src={user?.avatarUrl} className="rounded-lg shadow-lg" />
+            </figure>
+            <div className="max-w-md card-body">
+              <h2 className="card-title font-major-mono">{user?.name}</h2>
+              <p>{user?.email}</p>
+              <p>${user?.balanceUsd} USD</p>
+              <div className="card-actions">
+                <button
+                  className="btn glass rounded-full"
+                  onClick={() => {
+                    firebaseLogout()
+                    router.push('/')
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
             </div>
           </div>
         </div>
 
-        {/* Lorem ipsum table. TODO: fill in user's bets and markets */}
-        <h1 className="text-4xl text-neutral-content m-4">
-          {user?.username}'s Bets
+        <h1 className="text-2xl font-major-mono text-indigo-300 font-bold mt-6 mb-4">
+          Your markets
         </h1>
-        <div className="overflow-x-auto">
-          <table className="table table-compact">
-            <thead>
-              <tr>
-                <th></th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>company</th>
-                <th>location</th>
-                <th>Last Login</th>
-                <th>Favorite Color</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th>1</th>
-                <td>Cy Ganderton</td>
-                <td>Quality Control Specialist</td>
-                <td>Littel, Schaden and Vandervort</td>
-                <td>Canada</td>
-                <td>12/16/2020</td>
-                <td>Blue</td>
-              </tr>
-              <tr>
-                <th>2</th>
-                <td>Hart Hagerty</td>
-                <td>Desktop Support Technician</td>
-                <td>Zemlak, Daniel and Leannon</td>
-                <td>United States</td>
-                <td>12/5/2020</td>
-                <td>Purple</td>
-              </tr>
-              <tr>
-                <th>3</th>
-                <td>Brice Swyre</td>
-                <td>Tax Accountant</td>
-                <td>Carroll Group</td>
-                <td>China</td>
-                <td>8/15/2020</td>
-                <td>Red</td>
-              </tr>
-              <tr>
-                <th>4</th>
-                <td>Marjy Ferencz</td>
-                <td>Office Assistant I</td>
-                <td>Rowe-Schoen</td>
-                <td>Russia</td>
-                <td>3/25/2021</td>
-                <td>Crimson</td>
-              </tr>
-              <tr>
-                <th>5</th>
-                <td>Yancy Tear</td>
-                <td>Community Outreach Specialist</td>
-                <td>Wyman-Ledner</td>
-                <td>Brazil</td>
-                <td>5/22/2020</td>
-                <td>Indigo</td>
-              </tr>
-              <tr>
-                <th>6</th>
-                <td>Irma Vasilik</td>
-                <td>Editor</td>
-                <td>Wiza, Bins and Emard</td>
-                <td>Venezuela</td>
-                <td>12/8/2020</td>
-                <td>Purple</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <ContractsList contracts={contracts} />
       </div>
     </div>
   )
