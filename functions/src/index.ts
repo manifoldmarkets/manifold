d95bef8ab3f0f3e9a2fc477b5406fff8e1b3bb68
mantikoros <sgrugett@gmail.com>
2021-12-10 18:06:17 -0600

    placeBet

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,5 @@
-import * as functions from "firebase-functions"
+import * as admin from 'firebase-admin'
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true })
-  response.send("Hello from Firebase!")
-})
+admin.initializeApp()
+
+export * from './place-bet'
---
+++ functions/src/types/user.ts
@@ -0,0 +1,10 @@
+export type User = {
+  id: string
+  email: string
+  name: string
+  username: string
+  avatarUrl: string
+  balanceUsd: number
+  createdTime: number
+  lastUpdatedTime: number
+}
