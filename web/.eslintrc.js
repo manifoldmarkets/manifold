6b39db9273eee7f2d6051275082313052ac65f1c
jahooma <jahooma@gmail.com>
2021-12-10 18:18:43 -0600

    Turn off nextjs img lint rule

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -3,5 +3,6 @@ module.exports = {
   extends: ['plugin:react-hooks/recommended', 'plugin:@next/next/recommended'],
   rules: {
     // Add or disable rules here.
+    '@next/next/no-img-element': 'off',
   },
 }
