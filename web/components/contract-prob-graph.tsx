e02eba25a1d7fba227fb024720b228a23b93c365
jahooma <jahooma@gmail.com>
2021-12-13 00:55:28 -0600

    Graph time of bets

--- web/components/contract-prob-graph.tsx
+++ web/components/contract-prob-graph.tsx
@@ -1,5 +1,6 @@
 import { DatumValue } from '@nivo/core'
 import { ResponsiveLine } from '@nivo/line'
+import dayjs from 'dayjs'
 import { useBets } from '../hooks/use-bets'
 import { Contract } from '../lib/firebase/contracts'
 
@@ -13,9 +14,17 @@ export function ContractProbGraph(props: { contract: Contract }) {
   const seedProb =
     seedAmounts.YES ** 2 / (seedAmounts.YES ** 2 + seedAmounts.NO ** 2)
 
+  const times = [
+    contract.createdTime,
+    ...bets.map((bet) => bet.createdTime),
+  ].map((time) => new Date(time))
   const probs = [seedProb, ...bets.map((bet) => bet.probAfter)]
-  const points = probs.map((prob, i) => ({ x: i + 1, y: prob * 100 }))
+  const points = probs.map((prob, i) => ({ x: times[i], y: prob * 100 }))
   const data = [{ id: 'Yes', data: points, color: '#11b981' }]
+
+  const lessThanAWeek =
+    times[times.length - 1].getTime() - times[0].getTime() <
+    1000 * 60 * 60 * 24 * 7
 
   const tickValues = [0, 25, 50, 75, 100]
 
@@ -30,17 +39,18 @@ export function ContractProbGraph(props: { contract: Contract }) {
           tickValues,
           format: formatPercent,
         }}
+        xScale={{ type: 'time' }}
+        xFormat={(d) => formatTime(+d.valueOf(), lessThanAWeek)}
         axisBottom={{
-          tickValues: [],
+          format: (time) => formatTime(+time, lessThanAWeek),
         }}
-        enableGridX={false}
         colors={{ datum: 'color' }}
-        pointSize={12}
-        pointBorderWidth={2}
+        pointSize={10}
+        pointBorderWidth={1}
         pointBorderColor="#fff"
         enableSlices="x"
         enableArea
-        margin={{ top: 20, right: 10, bottom: 20, left: 40 }}
+        margin={{ top: 20, right: 22, bottom: 22, left: 40 }}
       />
     </div>
   )
@@ -48,4 +58,14 @@ export function ContractProbGraph(props: { contract: Contract }) {
 
 function formatPercent(y: DatumValue) {
   return `${Math.round(+y.toString())}%`
+}
+
+function formatTime(time: number, includeTime: boolean) {
+  const d = dayjs(time)
+
+  if (d.isSame(Date.now(), 'day')) return d.format('ha')
+
+  if (includeTime) return dayjs(time).format('MMM D, ha')
+
+  return dayjs(time).format('MMM D')
 }
