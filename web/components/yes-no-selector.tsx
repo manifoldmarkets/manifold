62da8ee3a9072f4725334688f0c1cf90ebac9080
Austin Chen <akrolsmir@gmail.com>
2021-12-13 10:26:46 -0800

    Darken the yes/no bg color

--- web/components/yes-no-selector.tsx
+++ web/components/yes-no-selector.tsx
@@ -47,11 +47,10 @@ function Button(props: {
       className={classNames(
         'inline-flex items-center px-8 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white',
         !hideFocusRing && 'focus:outline-none focus:ring-2 focus:ring-offset-2',
-        color === 'green' &&
-          'btn-primary',
+        color === 'green' && 'btn-primary',
         color === 'red' && 'bg-red-400 hover:bg-red-500 focus:ring-red-400',
         color === 'deemphasized' &&
-          'text-gray-700 bg-gray-200 hover:bg-gray-300 focus:ring-gray-300',
+          'text-gray-700 bg-gray-300 hover:bg-gray-400 focus:ring-gray-400',
         className
       )}
       onClick={onClick}
