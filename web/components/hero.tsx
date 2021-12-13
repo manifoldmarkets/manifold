780d25dca1a040156530f548cd7f780e19e2e1cc
jahooma <jahooma@gmail.com>
2021-12-12 22:36:15 -0600

    Improve landing page, especially on mobile!

--- web/components/hero.tsx
+++ web/components/hero.tsx
@@ -3,7 +3,7 @@ import { Header } from './header'
 
 export const Hero = () => {
   return (
-    <div className="relative overflow-hidden h-screen bg-world-trading bg-cover bg-gray-900">
+    <div className="relative overflow-hidden h-screen bg-world-trading bg-cover bg-gray-900 bg-center lg:bg-left">
       <Header darkBackground />
       <main>
         <div className="pt-40 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
@@ -12,14 +12,14 @@ export const Hero = () => {
               <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
                 <div className="lg:py-24">
                   <h1 className="mt-4 text-4xl text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
-                    <span className="block">Create your own</span>
-                    <span className="block text-green-400 font-bold">
+                    <div className="mb-2">Create your own</div>
+                    <div className="text-green-400 font-bold">
                       prediction markets
-                    </span>
+                    </div>
                   </h1>
                   <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                    Forecast the future with play-money prediction markets for
-                    you and your community
+                    Better forecasting with accessible play-money prediction
+                    markets for you and your community
                   </p>
                   <div className="mt-10 sm:mt-12">
                     <ConvertKitEmailForm />
