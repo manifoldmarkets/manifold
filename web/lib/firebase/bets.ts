e14fd8c02b17b57f175a1b76bcc8cb680792a0a9
jahooma <jahooma@gmail.com>
2021-12-13 23:40:38 -0600

    Query for user bets. Template page to show them.

--- web/lib/firebase/bets.ts
+++ web/lib/firebase/bets.ts
@@ -1,4 +1,10 @@
-import { collection, onSnapshot } from 'firebase/firestore'
+import {
+  collection,
+  collectionGroup,
+  query,
+  onSnapshot,
+  where,
+} from 'firebase/firestore'
 import { db } from './init'
 
 export type Bet = {
@@ -27,6 +33,22 @@ export function listenForBets(
 
     bets.sort((bet1, bet2) => bet1.createdTime - bet2.createdTime)
 
+    setBets(bets)
+  })
+}
+
+export function listenForUserBets(
+  userId: string,
+  setBets: (bets: Bet[]) => void
+) {
+  const userQuery = query(
+    collectionGroup(db, 'bets'),
+    where('userId', '==', userId)
+  )
+
+  return onSnapshot(userQuery, (snap) => {
+    const bets = snap.docs.map((doc) => doc.data() as Bet)
+    bets.sort((bet1, bet2) => bet1.createdTime - bet2.createdTime)
     setBets(bets)
   })
 }
