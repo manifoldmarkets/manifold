7b55eeff881b4e93af1a46abe6c74b8756a8d330
Austin Chen <akrolsmir@gmail.com>
2021-12-14 02:27:22 -0800

    Move shared logic into contracts-list

--- web/pages/markets.tsx
+++ web/pages/markets.tsx
@@ -1,81 +1,9 @@
-import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
+import { ContractsGrid } from '../components/contracts-list'
 import { Header } from '../components/header'
-import { Col } from '../components/layout/col'
-import { Row } from '../components/layout/row'
 import { Title } from '../components/title'
 import { compute, listAllContracts } from '../lib/firebase/contracts'
 import { Contract } from '../lib/firebase/contracts'
-import { formatWithCommas } from '../lib/util/format'
-
-export function ContractDetails(props: { contract: Contract }) {
-  const { contract } = props
-  const { volume, createdDate } = compute(contract)
-
-  return (
-    <Row className="flex-wrap text-sm text-gray-500">
-      <div className="whitespace-nowrap">By {contract.creatorName}</div>
-      <div className="mx-2">•</div>
-      <div className="whitespace-nowrap">{createdDate}</div>
-      <div className="mx-2">•</div>
-      <div className="whitespace-nowrap">{formatWithCommas(volume)} vol</div>
-    </Row>
-  )
-}
-
-function ContractCard(props: { contract: Contract }) {
-  const { contract } = props
-  const { probPercent } = compute(contract)
-
-  return (
-    <Link href={`/contract/${contract.id}`}>
-      <a>
-        <li className="col-span-1 bg-white hover:bg-gray-100 shadow-xl rounded-lg divide-y divide-gray-200">
-          <div className="card">
-            <div className="card-body p-6">
-              <div className="flex justify-between gap-2">
-                {/* Left side of card */}
-                <div>
-                  <p className="font-medium text-indigo-700 mb-8">
-                    {contract.question}
-                  </p>
-                  <ContractDetails contract={contract} />
-                </div>
-
-                {/* Right side of card */}
-                <Col>
-                  <Col className="text-4xl mx-auto items-end">
-                    {contract.resolution || (
-                      <div className="text-primary">
-                        {probPercent}
-                        <div className="text-lg">chance</div>
-                      </div>
-                    )}
-                  </Col>
-                </Col>
-              </div>
-            </div>
-          </div>
-        </li>
-      </a>
-    </Link>
-  )
-}
-
-export function ContractsGrid(props: { contracts: Contract[] }) {
-  const { contracts } = props
-  return (
-    <ul
-      role="list"
-      className="grid grid-cols-1 gap-6 sm:grid-cols-1 lg:grid-cols-2"
-    >
-      {contracts.map((contract) => (
-        <ContractCard contract={contract} key={contract.id} />
-      ))}
-      {/* TODO: Show placeholder if empty */}
-    </ul>
-  )
-}
 
 export default function Markets() {
   const [contracts, setContracts] = useState<Contract[]>([])
@@ -94,19 +22,10 @@ export default function Markets() {
     (c) => check(c.question) || check(c.description) || check(c.creatorName)
   )
 
-  function volume(contract: Contract) {
-    return (
-      contract.pot.YES +
-      contract.pot.NO -
-      contract.seedAmounts.YES -
-      contract.seedAmounts.NO
-    )
-  }
-
   if (sort === 'createdTime') {
     matches.sort((a, b) => b.createdTime - a.createdTime)
   } else if (sort === 'volume') {
-    matches.sort((a, b) => volume(b) - volume(a))
+    matches.sort((a, b) => compute(b).volume - compute(a).volume)
   }
 
   return (
