1562565f9a83270cbc7dca763ce9773f596e53ea
jahooma <jahooma@gmail.com>
2021-12-14 01:09:46 -0600

    Try to fix build maximum fraction digits out of range error

--- web/lib/util/format.ts
+++ web/lib/util/format.ts
@@ -2,6 +2,7 @@ const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
   maximumFractionDigits: 0,
+  minimumFractionDigits: 0,
 })
 
 export function formatMoney(amount: number) {
