9b418dacd975fb281c6a58b27d55809e33b16bae
Austin Chen <akrolsmir@gmail.com>
2021-12-14 23:24:55 -0800

    Set all markets to be the logged-in homepage

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 import type { NextPage } from 'next'
 
 import { Hero } from '../components/hero'
+import { useUser } from '../hooks/use-user'
+import Markets from './markets'
 
 const Home: NextPage = () => {
-  return <Hero />
+  const user = useUser()
+  return user ? <Markets /> : <Hero />
 }
 
 export default Home
