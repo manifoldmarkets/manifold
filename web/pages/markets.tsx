f1a96b5320bd5b26efd03fb8c5bca628cab60de6
mantikoros <sgrugett@gmail.com>
2021-12-18 23:59:34 -0600

    static loading of markets

--- web/pages/markets.tsx
+++ web/pages/markets.tsx
@@ -1,14 +1,29 @@
 import { SearchableGrid } from '../components/contracts-list'
 import { Header } from '../components/header'
 import { useContracts } from '../hooks/use-contracts'
+import { Contract, listAllContracts } from '../lib/firebase/contracts'
 
-export default function Markets() {
+export async function getStaticProps() {
+  const contracts = await listAllContracts().catch((_) => [])
+
+  return {
+    props: {
+      contracts,
+    },
+
+    revalidate: 60, // regenerate after a minute
+  }
+}
+
+export default function Markets(props: { contracts: Contract[] }) {
   const contracts = useContracts()
 
   return (
     <div className="max-w-4xl px-4 pb-8 mx-auto">
       <Header />
-      <SearchableGrid contracts={contracts === 'loading' ? [] : contracts} />
+      <SearchableGrid
+        contracts={contracts === 'loading' ? props.contracts || [] : contracts}
+      />
     </div>
   )
 }
