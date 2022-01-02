3ba96028f41ac8e9428e9881d78c6829d55ee85b
jahooma <jahooma@gmail.com>
2022-01-02 16:53:20 -0600

    Parse tags to get unique ignoring case

--- web/lib/util/parse.ts
+++ web/lib/util/parse.ts
@@ -1,8 +1,9 @@
+import _ from 'lodash'
+
 export function parseTags(text: string) {
   const regex = /(?:^|\s)(?:[#][a-z0-9_]+)/gi
-  const matches = text.match(regex) || []
-  return matches.map((match) => {
-    const tag = match.trim().substring(1)
-    return tag
-  })
+  const matches = (text.match(regex) || []).map((match) =>
+    match.trim().substring(1)
+  )
+  return _.uniqBy(matches, (tag) => tag.toLowerCase())
 }
