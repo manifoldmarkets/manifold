4b2412c49c01c9007c6ee32bb9076670f0609649
Austin Chen <akrolsmir@gmail.com>
2022-01-07 12:07:38 -0800

    Set up a custom OpenGraph image generator for social media previews (#20)
    * Copy in og-image code
    
    * Add "yarn start" command in lieu of vercel
    
    * Don't require that images be sourced from vercel
    
    * Load in Major Mono and Readex fonts
    
    * Fix vercel config (?)
    
    * Replace default image with Manifold's
    
    * Add some brief instructions on getting started
    
    * In the UI, use the default image
    
    * Fix typescript errors
    
    * More typescript fixing

---
+++ og-image/api/_lib/parser.ts
@@ -0,0 +1,60 @@
+import { IncomingMessage } from "http";
+import { parse } from "url";
+import { ParsedRequest } from "./types";
+
+export function parseRequest(req: IncomingMessage) {
+  console.log("HTTP " + req.url);
+  const { pathname, query } = parse(req.url || "/", true);
+  const { fontSize, images, widths, heights, theme, md } = query || {};
+
+  if (Array.isArray(fontSize)) {
+    throw new Error("Expected a single fontSize");
+  }
+  if (Array.isArray(theme)) {
+    throw new Error("Expected a single theme");
+  }
+
+  const arr = (pathname || "/").slice(1).split(".");
+  let extension = "";
+  let text = "";
+  if (arr.length === 0) {
+    text = "";
+  } else if (arr.length === 1) {
+    text = arr[0];
+  } else {
+    extension = arr.pop() as string;
+    text = arr.join(".");
+  }
+
+  const parsedRequest: ParsedRequest = {
+    fileType: extension === "jpeg" ? extension : "png",
+    text: decodeURIComponent(text),
+    theme: theme === "dark" ? "dark" : "light",
+    md: md === "1" || md === "true",
+    fontSize: fontSize || "96px",
+    images: getArray(images),
+    widths: getArray(widths),
+    heights: getArray(heights),
+  };
+  parsedRequest.images = getDefaultImages(parsedRequest.images);
+  return parsedRequest;
+}
+
+function getArray(stringOrArray: string[] | string | undefined): string[] {
+  if (typeof stringOrArray === "undefined") {
+    return [];
+  } else if (Array.isArray(stringOrArray)) {
+    return stringOrArray;
+  } else {
+    return [stringOrArray];
+  }
+}
+
+function getDefaultImages(images: string[]): string[] {
+  const defaultImage = "https://manifold.markets/logo.png";
+
+  if (!images || !images[0]) {
+    return [defaultImage];
+  }
+  return images;
+}
---
+++ og-image/api/_lib/sanitizer.ts
@@ -0,0 +1,12 @@
+const entityMap: { [key: string]: string } = {
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  '"': "&quot;",
+  "'": "&#39;",
+  "/": "&#x2F;",
+};
+
+export function sanitizeHtml(html: string) {
+  return String(html).replace(/[&<>"'\/]/g, (key) => entityMap[key]);
+}
