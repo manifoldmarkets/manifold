ca0fca0711fd01efb1b45fc774cac6bb7d5c1f73
Austin Chen <akrolsmir@gmail.com>
2022-01-07 11:27:09 -0800

    Replace default image with Manifold's

--- og-image/api/_lib/parser.ts
+++ og-image/api/_lib/parser.ts
@@ -54,10 +54,7 @@ function getArray(stringOrArray: string[] | string | undefined): string[] {
 }
 
 function getDefaultImages(images: string[], theme: Theme): string[] {
-  const defaultImage =
-    theme === "light"
-      ? "https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg"
-      : "https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg";
+  const defaultImage = "https://manifold.markets/logo.png";
 
   if (!images || !images[0]) {
     return [defaultImage];
