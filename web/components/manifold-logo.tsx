bfd0af1c802bb4337fe64b143c137f24cbeff97e
James Grugett <jahooma@gmail.com>
2022-01-11 00:16:58 -0600

    New logo: oragami crane (#25)
    * new logo
    
    * Switch out mantis for crane in logos
    
    * White svg on dark background. Align correctly on landing page.
    
    * Mobile navbar: hide your name, write out Manifold Markets
    
    Co-authored-by: mantikoros <sgrugett@gmail.com>

---
+++ web/components/manifold-logo.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import clsx from 'clsx'
+
+export function ManifoldLogo(props: { darkBackground?: boolean }) {
+  const { darkBackground } = props
+
+  return (
+    <Link href="/">
+      <a className="flex flex-row gap-4">
+        <img
+          className="hover:rotate-12 transition-all"
+          src={darkBackground ? '/logo-white.svg' : '/logo.svg'}
+          width={45}
+          height={45}
+        />
+        <div
+          className={clsx(
+            'sm:hidden font-major-mono lowercase mt-1 text-lg',
+            darkBackground && 'text-white'
+          )}
+        >
+          Manifold
+          <br />
+          Markets
+        </div>
+        <div
+          className={clsx(
+            'hidden sm:flex font-major-mono lowercase mt-1 sm:text-2xl md:whitespace-nowrap',
+            darkBackground && 'text-white'
+          )}
+        >
+          Manifold Markets
+        </div>
+      </a>
+    </Link>
+  )
+}
