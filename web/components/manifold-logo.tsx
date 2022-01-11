2f2a0030d1ccc3980bb8dc675031af3f987a0598
jahooma <jahooma@gmail.com>
2022-01-11 00:10:05 -0600

    Mobile navbar: hide your name, write out Manifold Markets

--- web/components/manifold-logo.tsx
+++ web/components/manifold-logo.tsx
@@ -10,9 +10,19 @@ export function ManifoldLogo(props: { darkBackground?: boolean }) {
         <img
           className="hover:rotate-12 transition-all"
           src={darkBackground ? '/logo-white.svg' : '/logo.svg'}
-          width={40}
-          height={40}
+          width={45}
+          height={45}
         />
+        <div
+          className={clsx(
+            'sm:hidden font-major-mono lowercase mt-1 text-lg',
+            darkBackground && 'text-white'
+          )}
+        >
+          Manifold
+          <br />
+          Markets
+        </div>
         <div
           className={clsx(
             'hidden sm:flex font-major-mono lowercase mt-1 sm:text-2xl md:whitespace-nowrap',
