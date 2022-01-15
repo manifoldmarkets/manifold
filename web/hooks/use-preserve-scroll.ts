362df71e35c312ff923c401ae32a1bea542a2dd4
jahooma <jahooma@gmail.com>
2022-01-14 18:43:00 -0600

    Preserve scroll on back

---
+++ web/hooks/use-preserve-scroll.ts
@@ -0,0 +1,41 @@
+import { useRouter } from 'next/router'
+import { useEffect, useRef } from 'react'
+
+// From: https://jak-ch-ll.medium.com/next-js-preserve-scroll-history-334cf699802a
+export const usePreserveScroll = () => {
+  const router = useRouter()
+
+  const scrollPositions = useRef<{ [url: string]: number }>({})
+  const isBack = useRef(false)
+
+  useEffect(() => {
+    router.beforePopState(() => {
+      isBack.current = true
+      return true
+    })
+
+    const onRouteChangeStart = () => {
+      const url = router.pathname
+      scrollPositions.current[url] = window.scrollY
+    }
+
+    const onRouteChangeComplete = (url: any) => {
+      if (isBack.current && scrollPositions.current[url]) {
+        window.scroll({
+          top: scrollPositions.current[url],
+          behavior: 'auto',
+        })
+      }
+
+      isBack.current = false
+    }
+
+    router.events.on('routeChangeStart', onRouteChangeStart)
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', onRouteChangeStart)
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [router])
+}
