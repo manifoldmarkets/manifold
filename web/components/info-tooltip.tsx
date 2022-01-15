a36dc0c7aa405580848fd4f13812cb89279e8fb1
jahooma <jahooma@gmail.com>
2022-01-15 15:28:19 -0600

    Show current payout in bet panel! Abolish estimated max payout. Show shares in tooltip

---
+++ web/components/info-tooltip.tsx
@@ -0,0 +1,10 @@
+import { InformationCircleIcon } from '@heroicons/react/solid'
+
+export function InfoTooltip(props: { text: string }) {
+  const { text } = props
+  return (
+    <div className="tooltip" data-tip={text}>
+      <InformationCircleIcon className="h-5 w-5 text-gray-500" />
+    </div>
+  )
+}
