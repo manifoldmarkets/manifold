d32147b53633befa7fbac6261b83d4ddab77c850
Austin Chen <akrolsmir@gmail.com>
2022-01-16 04:16:15 -0500

    Show DateTime tooltips whereever we show dates

---
+++ web/components/datetime-tooltip.tsx
@@ -0,0 +1,23 @@
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+import advanced from 'dayjs/plugin/advancedFormat'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(advanced)
+
+export function DateTimeTooltip(props: {
+  time: number
+  children?: React.ReactNode
+}) {
+  const { time } = props
+  return (
+    <span
+      className="tooltip cursor-default"
+      data-tip={dayjs(time).format('MMM DD, YYYY hh:mm a z')}
+    >
+      {props.children}
+    </span>
+  )
+}
