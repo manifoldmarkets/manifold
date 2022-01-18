f74af0f5b7c95b49b1eee9307050d946d4b673cf
mantikoros <sgrugett@gmail.com>
2022-01-18 16:31:24 -0600

    PrivateUser

--- common/user.ts
+++ common/user.ts
@@ -12,3 +12,11 @@ export type User = {
 }
 
 export const STARTING_BALANCE = 1000
+export const SUS_STARTING_BALANCE = 10 // for sus users, i.e. multiple sign ups for same person
+
+export type PrivateUser = {
+  email?: string
+  unsubscribedFromResolutionEmails?: boolean
+  initialDeviceToken?: string
+  initialIpAddress?: string
+}
