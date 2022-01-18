e6d467764d21b443400e0708bfb5ee7e499d2cca
mantikoros <sgrugett@gmail.com>
2022-01-17 21:36:33 -0600

    Don’t show negative values in feed for sales

---
+++ web/components/join-spans.tsx
@@ -0,0 +1,31 @@
+export const JoinSpans = (props: {
+  children: any[]
+  separator?: JSX.Element | string
+}) => {
+  const { separator } = props
+  const children = props.children.filter((x) => !!x)
+
+  if (children.length === 0) return <></>
+  if (children.length === 1) return children[0]
+  if (children.length === 2)
+    return (
+      <>
+        {children[0]} and {children[1]}
+      </>
+    )
+
+  const head = children.slice(0, -1).map((child) => (
+    <>
+      {child}
+      {separator || ','}{' '}
+    </>
+  ))
+
+  const tail = children[children.length - 1]
+
+  return (
+    <>
+      {head}and {tail}
+    </>
+  )
+}
