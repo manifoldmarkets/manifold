4afc641314d2d4e589ed8b38f042fe7dd6875e47
Austin Chen <akrolsmir@gmail.com>
2022-01-19 13:07:07 -0500

    Move hot & closing markets to All Markets
    Soon to be named "Explore"

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import _ from 'lodash'
-import {
-  Contract,
-  getClosingSoonContracts,
-  getHotContracts,
-  listAllContracts,
-} from '../lib/firebase/contracts'
-import { Spacer } from '../components/layout/spacer'
+import { Contract, listAllContracts } from '../lib/firebase/contracts'
 import { Page } from '../components/page'
-import { Title } from '../components/title'
 import { ActivityFeed, findActiveContracts } from './activity'
 import {
   getRecentComments,
@@ -16,16 +9,12 @@ import {
   listAllComments,
 } from '../lib/firebase/comments'
 import { Bet, listAllBets } from '../lib/firebase/bets'
-import { ContractsGrid } from '../components/contracts-list'
 
 export async function getStaticProps() {
-  const [contracts, hotContracts, closingSoonContracts, recentComments] =
-    await Promise.all([
-      listAllContracts().catch((_) => []),
-      getHotContracts().catch(() => []),
-      getClosingSoonContracts().catch(() => []),
-      getRecentComments().catch(() => []),
-    ])
+  const [contracts, recentComments] = await Promise.all([
+    listAllContracts().catch((_) => []),
+    getRecentComments().catch(() => []),
+  ])
 
   const activeContracts = findActiveContracts(contracts, recentComments)
   const activeContractBets = await Promise.all(
@@ -40,8 +29,6 @@ export async function getStaticProps() {
       activeContracts,
       activeContractBets,
       activeContractComments,
-      hotContracts,
-      closingSoonContracts,
     },
 
     revalidate: 60, // regenerate after a minute
@@ -52,53 +39,17 @@ const Home = (props: {
   activeContracts: Contract[]
   activeContractBets: Bet[][]
   activeContractComments: Comment[][]
-  hotContracts: Contract[]
-  closingSoonContracts: Contract[]
 }) => {
-  const {
-    activeContracts,
-    activeContractBets,
-    activeContractComments,
-    hotContracts,
-    closingSoonContracts,
-  } = props
+  const { activeContracts, activeContractBets, activeContractComments } = props
 
   return (
     <Page>
-      <HotMarkets contracts={hotContracts} />
-      <Spacer h={10} />
-      <ClosingSoonMarkets contracts={closingSoonContracts} />
-      <Spacer h={10} />
       <ActivityFeed
         contracts={activeContracts}
         contractBets={activeContractBets}
         contractComments={activeContractComments}
       />
     </Page>
-  )
-}
-
-const HotMarkets = (props: { contracts: Contract[] }) => {
-  const { contracts } = props
-  if (contracts.length === 0) return <></>
-
-  return (
-    <div className="w-full bg-indigo-50 border-2 border-indigo-100 p-6 rounded-lg shadow-md">
-      <Title className="mt-0" text="🔥 Markets" />
-      <ContractsGrid contracts={contracts} showHotVolume />
-    </div>
-  )
-}
-
-const ClosingSoonMarkets = (props: { contracts: Contract[] }) => {
-  const { contracts } = props
-  if (contracts.length === 0) return <></>
-
-  return (
-    <div className="w-full bg-green-50 border-2 border-green-100 p-6 rounded-lg shadow-md">
-      <Title className="mt-0" text="⏰ Closing soon" />
-      <ContractsGrid contracts={contracts} showCloseTime />
-    </div>
   )
 }
 
