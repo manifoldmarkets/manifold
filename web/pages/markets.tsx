8414120729ebe6a78f79b5f53bfc80c7fc9b7457
jahooma <jahooma@gmail.com>
2022-01-21 18:31:20 -0600

    Change default sort of all markets to most traded

--- web/pages/markets.tsx
+++ web/pages/markets.tsx
@@ -39,7 +39,9 @@ export default function Markets(props: {
 }) {
   const contracts = useContracts() ?? props.contracts
   const { hotContracts, closingSoonContracts } = props
-  const { query, setQuery, sort, setSort } = useQueryAndSortParams()
+  const { query, setQuery, sort, setSort } = useQueryAndSortParams({
+    defaultSort: 'most-traded',
+  })
 
   return (
     <Page>
