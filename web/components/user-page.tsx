a1aa512deb4cd01d60a24a66d7e6f5eb21e661bd
Austin Chen <akrolsmir@gmail.com>
2022-01-23 23:43:07 -0600

    Remove unused UserCard

--- web/components/user-page.tsx
+++ web/components/user-page.tsx
@@ -1,9 +1,7 @@
 import clsx from 'clsx'
-import { firebaseLogout, User } from '../lib/firebase/users'
+import { User } from '../lib/firebase/users'
 import { CreatorContractsList } from './contracts-list'
 import { Title } from './title'
-import { Row } from './layout/row'
-import { formatMoney } from '../lib/util/format'
 import { SEO } from './SEO'
 import { Page } from './page'
 import { SiteLink } from './site-link'
@@ -24,41 +22,6 @@ export function UserLink(props: {
   )
 }
 
-function UserCard(props: { user: User; showPrivateInfo?: boolean }) {
-  const { user, showPrivateInfo } = props
-  return (
-    <Row className="card glass lg:card-side shadow-xl hover:shadow-xl text-neutral-content bg-green-600 hover:bg-green-600 transition-all max-w-sm my-12 mx-auto">
-      <div className="p-4">
-        {user?.avatarUrl && (
-          <img
-            src={user.avatarUrl}
-            className="rounded-lg shadow-lg"
-            width={96}
-            height={96}
-          />
-        )}
-      </div>
-      <div className="max-w-md card-body">
-        <div className="card-title font-major-mono">{user.name}</div>
-
-        {showPrivateInfo && (
-          <>
-            <p>{formatMoney(user?.balance)}</p>
-            <div className="card-actions">
-              <button
-                className="btn glass rounded-full hover:bg-green-500"
-                onClick={firebaseLogout}
-              >
-                Sign Out
-              </button>
-            </div>
-          </>
-        )}
-      </div>
-    </Row>
-  )
-}
-
 export function UserPage(props: { user: User; currentUser?: User }) {
   const { user, currentUser } = props
 
@@ -73,8 +36,6 @@ export function UserPage(props: { user: User; currentUser?: User }) {
         description={possesive + 'markets'}
         url={`/@${user.username}`}
       />
-
-      {/* <UserCard user={user} showPrivateInfo={isCurrentUser} /> */}
 
       <Title text={possesive + 'markets'} />
 
