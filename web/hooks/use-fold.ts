419b356cc134e7e2c21fe1a8ab736c7f9aba6e50
jahooma <jahooma@gmail.com>
2022-01-23 23:47:20 -0600

    Listen for new folds on /folds

--- web/hooks/use-fold.ts
+++ web/hooks/use-fold.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Fold } from '../../common/fold'
-import { listenForFold } from '../lib/firebase/folds'
+import { listenForFold, listenForFolds } from '../lib/firebase/folds'
 
 export const useFold = (foldId: string) => {
   const [fold, setFold] = useState<Fold | null | undefined>()
@@ -10,4 +10,14 @@ export const useFold = (foldId: string) => {
   }, [foldId])
 
   return fold
+}
+
+export const useFolds = () => {
+  const [folds, setFolds] = useState<Fold[] | undefined>()
+
+  useEffect(() => {
+    return listenForFolds(setFolds)
+  }, [])
+
+  return folds
 }
