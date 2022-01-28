a0c7dd3de4f318af0d3c942646f7aa0a47fe98c9
jahooma <jahooma@gmail.com>
2022-01-28 12:43:35 -0600

    Hide negative earners on leaderboard. Show empty message if none on leaderboard.

--- web/components/leaderboard.tsx
+++ web/components/leaderboard.tsx
@@ -17,43 +17,47 @@ export function Leaderboard(props: {
   return (
     <div className={clsx('w-full px-1', className)}>
       <Title text={title} />
-      <div className="overflow-x-auto">
-        <table className="table table-zebra table-compact text-gray-500 w-full">
-          <thead>
-            <tr className="p-2">
-              <th>#</th>
-              <th>Name</th>
-              {columns.map((column) => (
-                <th key={column.header}>{column.header}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user, index) => (
-              <tr key={user.id}>
-                <td>{index + 1}</td>
-                <td>
-                  <SiteLink className="relative" href={`/${user.username}`}>
-                    <Row className="items-center gap-4">
-                      <img
-                        className="rounded-full bg-gray-400 flex-shrink-0 ring-8 ring-gray-50"
-                        src={user.avatarUrl || ''}
-                        alt=""
-                        width={32}
-                        height={32}
-                      />
-                      <div className="truncate">{user.name}</div>
-                    </Row>
-                  </SiteLink>
-                </td>
+      {users.length === 0 ? (
+        <div className="text-gray-500 ml-2">None yet</div>
+      ) : (
+        <div className="overflow-x-auto">
+          <table className="table table-zebra table-compact text-gray-500 w-full">
+            <thead>
+              <tr className="p-2">
+                <th>#</th>
+                <th>Name</th>
                 {columns.map((column) => (
-                  <td key={column.header}>{column.renderCell(user)}</td>
+                  <th key={column.header}>{column.header}</th>
                 ))}
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {users.map((user, index) => (
+                <tr key={user.id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <SiteLink className="relative" href={`/${user.username}`}>
+                      <Row className="items-center gap-4">
+                        <img
+                          className="rounded-full bg-gray-400 flex-shrink-0 ring-8 ring-gray-50"
+                          src={user.avatarUrl || ''}
+                          alt=""
+                          width={32}
+                          height={32}
+                        />
+                        <div className="truncate">{user.name}</div>
+                      </Row>
+                    </SiteLink>
+                  </td>
+                  {columns.map((column) => (
+                    <td key={column.header}>{column.renderCell(user)}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   )
 }
