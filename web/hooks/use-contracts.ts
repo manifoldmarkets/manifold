c1b615d745f6bfec07a6b5f4265bb909b2cc1c05
James Grugett <jahooma@gmail.com>
2022-01-30 22:12:12 -0600

    Listen for contract updates in folds feed

--- web/hooks/use-contracts.ts
+++ web/hooks/use-contracts.ts
@@ -5,6 +5,7 @@ import {
   listenForContracts,
   listenForHotContracts,
 } from '../lib/firebase/contracts'
+import { listenForTaggedContracts } from '../lib/firebase/folds'
 
 export const useContracts = () => {
   const [contracts, setContracts] = useState<Contract[] | undefined>()
@@ -12,6 +13,21 @@ export const useContracts = () => {
   useEffect(() => {
     return listenForContracts(setContracts)
   }, [])
+
+  return contracts
+}
+
+export const useTaggedContracts = (tags: string[] | undefined) => {
+  const [contracts, setContracts] = useState<Contract[] | undefined>(
+    tags && tags.length === 0 ? [] : undefined
+  )
+  const tagsKey = tags?.map((tag) => tag.toLowerCase()).join(',') ?? ''
+
+  useEffect(() => {
+    if (!tags || tags.length === 0) return
+    return listenForTaggedContracts(tags, setContracts)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [tagsKey])
 
   return contracts
 }
