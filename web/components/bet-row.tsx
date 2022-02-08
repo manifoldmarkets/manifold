096ac8f51204423426956de31047e9b7d9b65eb0
James Grugett <jahooma@gmail.com>
2022-02-08 00:57:34 -0600

    Put profile underneath bet popup

--- web/components/bet-row.tsx
+++ web/components/bet-row.tsx
@@ -58,7 +58,7 @@ export function Modal(props: {
     <Transition.Root show={open} as={Fragment}>
       <Dialog
         as="div"
-        className="fixed z-40 inset-0 overflow-y-auto"
+        className="fixed z-50 inset-0 overflow-y-auto"
         onClose={setOpen}
       >
         <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
