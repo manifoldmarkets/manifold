b012b098cdaf3588e744b7987c0b97e561aa64e9
Austin Chen <akrolsmir@gmail.com>
2022-02-15 15:58:16 -0800

    Show bio, banner, and social links on user page

--- web/components/user-page.tsx
+++ web/components/user-page.tsx
@@ -1,10 +1,14 @@
 import clsx from 'clsx'
 import { User } from '../lib/firebase/users'
 import { CreatorContractsList } from './contracts-list'
-import { Title } from './title'
 import { SEO } from './SEO'
 import { Page } from './page'
 import { SiteLink } from './site-link'
+import { Avatar } from './avatar'
+import { Col } from './layout/col'
+import { Linkify } from './linkify'
+import { Spacer } from './layout/spacer'
+import { Row } from './layout/row'
 
 export function UserLink(props: {
   name: string
@@ -29,6 +33,11 @@ export function UserPage(props: { user: User; currentUser?: User }) {
 
   const possesive = isCurrentUser ? 'Your ' : `${user.name}'s `
 
+  const bannerImageUrl =
+    'https://images.unsplash.com/photo-1548197253-652ffe79752c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1975&q=80'
+
+  const placeholderBio = `Hi! Always happy to chat; reach out at akrolsmir@gmail.com, or find a time on https://calendly.com/austinchen/manifold !`
+
   return (
     <Page>
       <SEO
@@ -37,9 +46,48 @@ export function UserPage(props: { user: User; currentUser?: User }) {
         url={`/@${user.username}`}
       />
 
-      <Title className="mx-4 md:mx-0" text={possesive + 'markets'} />
+      <div
+        className="h-32 w-full bg-cover bg-center sm:h-40"
+        style={{
+          backgroundImage: `url(${bannerImageUrl})`,
+        }}
+      />
 
-      <CreatorContractsList creator={user} />
+      <div className="relative -top-10 left-4">
+        <Avatar username={user.username} avatarUrl={user.avatarUrl} size={20} />
+      </div>
+
+      {/* Then the user's name, username, brief bio, and link to their twitter */}
+      <Col className="mx-4 -mt-6">
+        <span className="text-2xl font-bold">{user.name}</span>
+        <span className="text-gray-500">@{user.username}</span>
+        <Spacer h={4} />
+
+        <div>
+          <Linkify text={placeholderBio}></Linkify>
+        </div>
+        <Spacer h={4} />
+
+        <Row className="gap-4">
+          <a href={`https://twitter.com/akrolsmir`}>
+            <Row className="items-center gap-1">
+              <img src="/twitter-logo.svg" className="h-4 w-4" alt="Twitter" />
+              <span className="text-sm text-gray-500">akrolsmir</span>
+            </Row>
+          </a>
+
+          <a href="https://discord.com/invite/eHQBNBqXuh">
+            <Row className="items-center gap-1">
+              <img src="/discord-logo.svg" className="h-4 w-4" alt="Discord" />
+              <span className="text-sm text-gray-500">akrolsmir#4125</span>
+            </Row>
+          </a>
+        </Row>
+
+        <Spacer h={10} />
+
+        <CreatorContractsList creator={user} />
+      </Col>
     </Page>
   )
 }
