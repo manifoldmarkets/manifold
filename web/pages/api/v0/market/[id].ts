8f40350a756369e8d704e240368f1227c8dd5d1e
Austin Chen <akrolsmir@gmail.com>
2022-02-17 15:22:51 -0800

    API: Rename to LiteMarket/FullMarket; include creator avatar url

--- web/pages/api/v0/market/[id].ts
+++ web/pages/api/v0/market/[id].ts
@@ -2,11 +2,11 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { listAllBets } from '../../../../lib/firebase/bets'
 import { listAllComments } from '../../../../lib/firebase/comments'
 import { getContractFromId } from '../../../../lib/firebase/contracts'
-import { FullContract, ApiError, toLiteContract } from '../_types'
+import { FullMarket, ApiError, toLiteMarket } from '../_types'
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<FullContract | ApiError>
+  res: NextApiResponse<FullMarket | ApiError>
 ) {
   const { id } = req.query
   const contractId = id as string
@@ -25,7 +25,7 @@ export default async function handler(
   // Cache on Vercel edge servers for 2min
   res.setHeader('Cache-Control', 'max-age=0, s-maxage=120')
   return res.status(200).json({
-    ...toLiteContract(contract),
+    ...toLiteMarket(contract),
     bets,
     comments,
   })
