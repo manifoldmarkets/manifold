03b4febedcb80ff6380f2099adb03ee004e638a8
Austin Chen <akrolsmir@gmail.com>
2022-02-21 10:50:05 -0800

    Reroute /api and /api/v0 to our docs page

--- web/next.config.js
+++ web/next.config.js
@@ -1,3 +1,6 @@
+const API_DOCS_URL =
+  'https://manifoldmarkets.notion.site/Manifold-Markets-API-5e7d0aef4dcf452bb04b319e178fabc5'
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
@@ -7,5 +10,19 @@ module.exports = {
   },
   images: {
     domains: ['lh3.googleusercontent.com'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/api',
+        destination: API_DOCS_URL,
+        permanent: false,
+      },
+      {
+        source: '/api/v0',
+        destination: API_DOCS_URL,
+        permanent: false,
+      },
+    ]
   },
 }
