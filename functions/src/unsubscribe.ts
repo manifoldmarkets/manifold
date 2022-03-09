f9b881a017872a871740c266e329fba6d7ac6389
James Grugett <jahooma@gmail.com>
2022-03-09 12:39:21 -0600

    Send better error responses for unsubscribe.

--- functions/src/unsubscribe.ts
+++ functions/src/unsubscribe.ts
@@ -7,8 +7,18 @@ import { PrivateUser } from '../../common/user'
 export const unsubscribe = functions
   .runWith({ minInstances: 1 })
   .https.onRequest(async (req, res) => {
-    const { id, type } = req.query as { id: string; type: string }
-    if (!id || !type) return
+    let { id, type } = req.query as { id: string; type: string }
+    if (!id || !type) {
+      res.status(400).send('Empty id or type parameter.')
+      return
+    }
+
+    if (type === 'market-resolved') type = 'market-resolve'
+
+    if (!['market-resolve', 'market-comment', 'market-answer'].includes(type)) {
+      res.status(400).send('Invalid type parameter.')
+      return
+    }
 
     const user = await getUser(id)
 
@@ -16,7 +26,7 @@ export const unsubscribe = functions
       const { name } = user
 
       const update: Partial<PrivateUser> = {
-        ...((type === 'market-resolve' || type === 'market-resolved') && {
+        ...(type === 'market-resolve' && {
           unsubscribedFromResolutionEmails: true,
         }),
         ...(type === 'market-comment' && {
