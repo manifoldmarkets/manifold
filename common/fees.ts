93f790019c0ec0aa1938abf1677ea9b36eecbf7e
mantikoros <sgrugett@gmail.com>
2022-03-18 00:39:21 -0500

    lower fees

--- common/fees.ts
+++ common/fees.ts
@@ -1,6 +1,6 @@
-export const PLATFORM_FEE = 0.02
-export const CREATOR_FEE = 0.08
-export const LIQUIDITY_FEE = 0.08
+export const PLATFORM_FEE = 0.01
+export const CREATOR_FEE = 0.06
+export const LIQUIDITY_FEE = 0.06
 
 export const DPM_PLATFORM_FEE = 0.01
 export const DPM_CREATOR_FEE = 0.04
