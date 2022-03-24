d19c01d9ee9432c5bfbd62dbf515861db6daae54
James Grugett <jahooma@gmail.com>
2022-03-24 12:03:08 -0500

    Embed market: dynamically adjust graph height

--- web/components/layout/col.tsx
+++ web/components/layout/col.tsx
@@ -1,15 +1,16 @@
 import clsx from 'clsx'
-import { CSSProperties } from 'react'
+import { CSSProperties, Ref } from 'react'
 
 export function Col(props: {
   children?: any
   className?: string
   style?: CSSProperties
+  ref?: Ref<HTMLDivElement>
 }) {
-  const { children, className, style } = props
+  const { children, className, style, ref } = props
 
   return (
-    <div className={clsx(className, 'flex flex-col')} style={style}>
+    <div className={clsx(className, 'flex flex-col')} style={style} ref={ref}>
       {children}
     </div>
   )
---
+++ web/hooks/use-measure-size.ts
@@ -0,0 +1,63 @@
+import _ from 'lodash'
+import { RefObject, useMemo, useLayoutEffect, useRef, useState } from 'react'
+
+type elem_size =
+  | { width: number; height: number }
+  | { width: undefined; height: undefined }
+
+const getSize = (elem: HTMLElement | null) =>
+  elem
+    ? { width: elem.offsetWidth, height: elem.offsetHeight }
+    : { width: undefined, height: undefined }
+
+export function useListenElemSize<T extends HTMLElement>(
+  elemRef: RefObject<T | null>,
+  callback: (size: elem_size) => void,
+  debounceMs: number | undefined = undefined
+) {
+  const handleResize = useMemo(() => {
+    let updateSize = () => {
+      if (elemRef.current) callback(getSize(elemRef.current))
+    }
+
+    return debounceMs
+      ? _.debounce(updateSize, debounceMs, { leading: false, trailing: true })
+      : updateSize
+  }, [callback, elemRef, debounceMs])
+
+  let elem = elemRef.current
+
+  useLayoutEffect(() => {
+    if (!elemRef.current) return
+
+    const resizeObserver = new ResizeObserver(handleResize)
+    resizeObserver.observe(elemRef.current)
+
+    return () => resizeObserver.disconnect()
+  }, [elemRef, elem, handleResize])
+}
+
+export function useMeasureSize(debounceMs: number | undefined = undefined) {
+  const elemRef = useRef<HTMLElement | null>(null)
+  const [size, setSize] = useState(() => getSize(null))
+  const sizeRef = useRef<elem_size>(size)
+
+  const setSizeIfDifferent = (newSize: typeof size) => {
+    if (newSize?.height !== size?.height || newSize?.width !== size?.width) {
+      sizeRef.current = newSize
+      setSize(newSize)
+    }
+  }
+
+  useListenElemSize(elemRef, setSizeIfDifferent, debounceMs)
+
+  const setElem = (elem: HTMLElement | null) => {
+    elemRef.current = elem
+
+    if (elem) {
+      setSizeIfDifferent(getSize(elem))
+    }
+  }
+
+  return { setElem, elemRef, sizeRef, ...size }
+}
