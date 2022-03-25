cacd1966c560e8c34b546ed759c036dce1fb706c
Austin Chen <akrolsmir@gmail.com>
2022-03-24 22:53:12 -0700

    Tweak copy

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -6,6 +6,7 @@ import { Page } from '../components/page'
 import { FeedPromo } from '../components/feed-create'
 import { Col } from '../components/layout/col'
 import { useUser } from '../hooks/use-user'
+import { SiteLink } from '../components/site-link'
 
 export async function getStaticProps() {
   const hotContracts = (await getHotContracts().catch(() => [])) ?? []
@@ -31,6 +32,12 @@ const Home = (props: { hotContracts: Contract[] }) => {
       <Col className="items-center">
         <Col className="max-w-3xl">
           <FeedPromo hotContracts={hotContracts ?? []} />
+          <p className="mt-6 text-gray-500">
+            View{' '}
+            <SiteLink href="/markets" className="font-bold text-gray-700">
+              all markets
+            </SiteLink>
+          </p>
         </Col>
       </Col>
     </Page>
