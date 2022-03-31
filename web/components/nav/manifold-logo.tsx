ba129a7a736bfb78fd70af7c73f9e35f10eec4e0
Austin Chen <akrolsmir@gmail.com>
2022-03-30 20:55:25 -0700

    Revert "Reverting side navbar for now"
    This reverts commit a90441d9d5df450a2d94adf401f2a0130870fc01.

--- web/components/nav/manifold-logo.tsx
+++ web/components/nav/manifold-logo.tsx
@@ -7,44 +7,47 @@ import { ENV_CONFIG } from '../../../common/envs/constants'
 export function ManifoldLogo(props: {
   className?: string
   darkBackground?: boolean
+  hideText?: boolean
 }) {
-  const { darkBackground, className } = props
+  const { darkBackground, className, hideText } = props
 
   const user = useUser()
 
   return (
     <Link href={user ? '/home' : '/'}>
-      <a className={clsx('flex flex-shrink-0 flex-row gap-4', className)}>
+      <a className={clsx('group flex flex-shrink-0 flex-row gap-4', className)}>
         <img
-          className="transition-all hover:rotate-12"
+          className="transition-all group-hover:rotate-12"
           src={darkBackground ? '/logo-white.svg' : '/logo.svg'}
           width={45}
           height={45}
         />
-        {ENV_CONFIG.navbarLogoPath ? (
-          <img src={ENV_CONFIG.navbarLogoPath} width={245} height={45} />
-        ) : (
-          <>
-            <div
-              className={clsx(
-                'font-major-mono mt-1 text-lg lowercase sm:hidden',
-                darkBackground && 'text-white'
-              )}
-            >
-              Manifold
-              <br />
-              Markets
-            </div>
-            <div
-              className={clsx(
-                'font-major-mono mt-1 hidden lowercase sm:flex sm:text-2xl md:whitespace-nowrap',
-                darkBackground && 'text-white'
-              )}
-            >
-              Manifold Markets
-            </div>
-          </>
-        )}
+
+        {!hideText &&
+          (ENV_CONFIG.navbarLogoPath ? (
+            <img src={ENV_CONFIG.navbarLogoPath} width={245} height={45} />
+          ) : (
+            <>
+              <div
+                className={clsx(
+                  'font-major-mono mt-1 text-lg lowercase sm:hidden',
+                  darkBackground && 'text-white'
+                )}
+              >
+                Manifold
+                <br />
+                Markets
+              </div>
+              <div
+                className={clsx(
+                  'font-major-mono mt-1 hidden lowercase sm:flex sm:text-2xl md:whitespace-nowrap',
+                  darkBackground && 'text-white'
+                )}
+              >
+                Manifold Markets
+              </div>
+            </>
+          ))}
       </a>
     </Link>
   )
--- web/components/page.tsx
+++ web/components/page.tsx
@@ -1,27 +1,42 @@
 import clsx from 'clsx'
-import { NavBar } from './nav/nav-bar'
+import { BottomNavBar } from './nav/nav-bar'
+import Sidebar from './nav/sidebar'
 
 export function Page(props: {
   wide?: boolean
   margin?: boolean
   assertUser?: 'signed-in' | 'signed-out'
+  rightSidebar?: React.ReactNode
   children?: any
 }) {
-  const { wide, margin, assertUser, children } = props
+  const { wide, margin, assertUser, children, rightSidebar } = props
 
   return (
     <div>
-      <NavBar wide={wide} assertUser={assertUser} />
-
       <div
         className={clsx(
-          'mx-auto w-full pb-16',
+          'mx-auto w-full pb-16 lg:grid lg:grid-cols-12 lg:gap-8 xl:max-w-7xl',
           wide ? 'max-w-6xl' : 'max-w-4xl',
           margin && 'px-4'
         )}
       >
-        {children}
+        <div className="hidden lg:col-span-3 lg:block xl:col-span-2">
+          {assertUser !== 'signed-out' && <Sidebar />}
+        </div>
+        <main
+          className={clsx(
+            'mt-6 lg:col-span-9',
+            rightSidebar ? 'xl:col-span-7' : 'xl:col-span-8'
+          )}
+        >
+          {children}
+        </main>
+        <aside className="hidden xl:col-span-3 xl:block">
+          <div className="sticky top-4 space-y-4">{rightSidebar}</div>
+        </aside>
       </div>
+
+      <BottomNavBar />
     </div>
   )
 }
