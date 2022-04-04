f577437a8b5e4207696f09152a2bffd8b16af947
James Grugett <jahooma@gmail.com>
2022-04-04 16:49:14 -0500

    Show your avatar and balance in mobile bottom bar instead of 'More'

--- web/components/nav/nav-bar.tsx
+++ web/components/nav/nav-bar.tsx
@@ -10,10 +10,16 @@ import {
 import { Transition, Dialog } from '@headlessui/react'
 import { useState, Fragment } from 'react'
 import Sidebar from './sidebar'
+import { useUser } from '../../hooks/use-user'
+import { formatMoney } from '../../../common/util/format'
+import { Avatar } from '../avatar'
 
 // From https://codepen.io/chris__sev/pen/QWGvYbL
 export function BottomNavBar() {
   const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  const user = useUser()
+
   return (
     <nav className="fixed inset-x-0 bottom-0 z-20 flex justify-between border-t-2 bg-white text-xs text-gray-700 lg:hidden">
       <Link href="/home">
@@ -37,13 +43,30 @@ export function BottomNavBar() {
         </a>
       </Link>
 
-      <span
-        className="block w-full py-1 px-3 text-center hover:cursor-pointer hover:bg-indigo-200 hover:text-indigo-700"
+      <div
+        className="w-full py-1 px-3 text-center hover:cursor-pointer hover:bg-indigo-200 hover:text-indigo-700"
         onClick={() => setSidebarOpen(true)}
       >
-        <MenuAlt3Icon className="my-1 mx-auto h-6 w-6" aria-hidden="true" />
-        More
-      </span>
+        {user === null ? (
+          <>
+            <MenuAlt3Icon className="my-1 mx-auto h-6 w-6" aria-hidden="true" />
+            More
+          </>
+        ) : user ? (
+          <>
+            <Avatar
+              containerClassName="mx-auto my-1"
+              size="xs"
+              username={user.username}
+              avatarUrl={user.avatarUrl}
+              noLink
+            />
+            {formatMoney(user.balance)}
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
 
       <MobileSidebar
         sidebarOpen={sidebarOpen}
