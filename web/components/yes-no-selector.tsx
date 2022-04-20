3cec7d2c695d24b849ad340c467af2f2687c7edf
Boa <iansphilips@gmail.com>
2022-04-20 08:13:39 -0600

    Sell shares mobile (#86)
    * Abstract sell shares row to component
    
    * Allow sell row to show just a button
    
    This is nice for the feed and on a bet's mobile interface.
    
    * Add and use floor shares
    
    * Allow sell button on the same line as bet button
    
    * Move use save shares to own file
    
    * Make sure to sell non-integer shares
    
    * Create SellButon & sell non-integer shares
    
    * Remove props prefixes
    
    * Break out sell modal and button

--- web/components/yes-no-selector.tsx
+++ web/components/yes-no-selector.tsx
@@ -9,40 +9,57 @@ export function YesNoSelector(props: {
   onSelect: (selected: 'YES' | 'NO') => void
   className?: string
   btnClassName?: string
+  replaceYesButton?: React.ReactNode
+  replaceNoButton?: React.ReactNode
 }) {
-  const { selected, onSelect, className, btnClassName } = props
+  const {
+    selected,
+    onSelect,
+    className,
+    btnClassName,
+    replaceNoButton,
+    replaceYesButton,
+  } = props
 
   const commonClassNames =
     'inline-flex flex-1  items-center justify-center rounded-3xl border-2 p-2'
 
   return (
     <Row className={clsx('space-x-3', className)}>
-      <button
-        className={clsx(
-          commonClassNames,
-          'hover:bg-primary-focus border-primary hover:border-primary-focus hover:text-white',
-          selected == 'YES'
-            ? 'bg-primary text-white'
-            : 'text-primary bg-transparent',
-          btnClassName
-        )}
-        onClick={() => onSelect('YES')}
-      >
-        Bet YES
-      </button>
-      <button
-        className={clsx(
-          commonClassNames,
-          'border-red-400 hover:border-red-500 hover:bg-red-500 hover:text-white',
-          selected == 'NO'
-            ? 'bg-red-400 text-white'
-            : 'bg-transparent text-red-400',
-          btnClassName
-        )}
-        onClick={() => onSelect('NO')}
-      >
-        Bet NO
-      </button>
+      {replaceYesButton ? (
+        replaceYesButton
+      ) : (
+        <button
+          className={clsx(
+            commonClassNames,
+            'hover:bg-primary-focus border-primary hover:border-primary-focus hover:text-white',
+            selected == 'YES'
+              ? 'bg-primary text-white'
+              : 'text-primary bg-transparent',
+            btnClassName
+          )}
+          onClick={() => onSelect('YES')}
+        >
+          Bet YES
+        </button>
+      )}
+      {replaceNoButton ? (
+        replaceNoButton
+      ) : (
+        <button
+          className={clsx(
+            commonClassNames,
+            'border-red-400 hover:border-red-500 hover:bg-red-500 hover:text-white',
+            selected == 'NO'
+              ? 'bg-red-400 text-white'
+              : 'bg-transparent text-red-400',
+            btnClassName
+          )}
+          onClick={() => onSelect('NO')}
+        >
+          Bet NO
+        </button>
+      )}
     </Row>
   )
 }
