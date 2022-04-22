4e4ffe8e20179fb4646483a4f974adbafa295137
Ian Philips <iansphilips@gmail.com>
2022-04-22 04:43:34 -0600

    Sell either type of share for dpm market

--- web/components/sell-button.tsx
+++ web/components/sell-button.tsx
@@ -2,7 +2,6 @@ import { Binary, CPMM, DPM, FullContract } from '../../common/contract'
 import { User } from '../../common/user'
 import { useUserContractBets } from '../hooks/use-user-bets'
 import { useState } from 'react'
-import { useSaveShares } from './use-save-shares'
 import { Col } from './layout/col'
 import clsx from 'clsx'
 import { SellSharesModal } from './sell-modal'
@@ -10,23 +9,13 @@ import { SellSharesModal } from './sell-modal'
 export function SellButton(props: {
   contract: FullContract<DPM | CPMM, Binary>
   user: User | null | undefined
+  sharesOutcome: 'YES' | 'NO' | undefined
+  shares: number
 }) {
-  const { contract, user } = props
-
+  const { contract, user, sharesOutcome, shares } = props
   const userBets = useUserContractBets(user?.id, contract.id)
   const [showSellModal, setShowSellModal] = useState(false)
-
   const { mechanism } = contract
-  const { yesFloorShares, noFloorShares, yesShares, noShares } = useSaveShares(
-    contract,
-    userBets
-  )
-  const floorShares = yesFloorShares || noFloorShares
-  const sharesOutcome = yesFloorShares
-    ? 'YES'
-    : noFloorShares
-    ? 'NO'
-    : undefined
 
   if (sharesOutcome && user && mechanism === 'cpmm-1') {
     return (
@@ -45,14 +34,14 @@ export function SellButton(props: {
           {'Sell ' + sharesOutcome}
         </button>
         <div className={'mt-1 w-24 text-center text-sm text-gray-500'}>
-          {'(' + floorShares + ' shares)'}
+          {'(' + Math.floor(shares) + ' shares)'}
         </div>
         {showSellModal && (
           <SellSharesModal
             contract={contract as FullContract<CPMM, Binary>}
             user={user}
             userBets={userBets ?? []}
-            shares={yesShares || noShares}
+            shares={shares}
             sharesOutcome={sharesOutcome}
             setOpen={setShowSellModal}
           />
