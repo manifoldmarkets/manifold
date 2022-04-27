d639e73284474596828c22d63fa05a37e44121f4
James Grugett <jahooma@gmail.com>
2022-04-27 17:26:33 -0400

    Store view counts & last viewed time

---
+++ functions/src/on-view.ts
@@ -0,0 +1,24 @@
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+import { View } from '../../common/tracking'
+
+const firestore = admin.firestore()
+
+export const onView = functions.firestore
+  .document('private-users/{userId}/views/{viewId}')
+  .onCreate(async (snapshot, context) => {
+    const { userId } = context.params
+
+    const { contractId, timestamp } = snapshot.data() as View
+
+    await firestore
+      .doc(`private-users/${userId}/cached/viewCounts`)
+      .set(
+        { [contractId]: admin.firestore.FieldValue.increment(1) },
+        { merge: true }
+      )
+
+    await firestore
+      .doc(`private-users/${userId}/cached/lastViewed`)
+      .set({ [contractId]: timestamp }, { merge: true })
+  })
