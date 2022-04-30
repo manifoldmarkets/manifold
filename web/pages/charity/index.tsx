5c03f1581af1da988f70f4f60e943c5a0a124816
Austin Chen <akrolsmir@gmail.com>
2022-04-30 08:42:25 -0400

    Fix lowercase search, tweak copy

--- web/pages/charity/index.tsx
+++ web/pages/charity/index.tsx
@@ -16,7 +16,10 @@ export default function Charity() {
   const debouncedQuery = _.debounce(setQuery, 50)
 
   const filterCharities = useMemo(
-    () => charities.filter((charity) => charity.name.includes(query)),
+    () =>
+      charities.filter((charity) =>
+        charity.name.toLowerCase().includes(query.toLowerCase())
+      ),
     [query]
   )
 
@@ -24,10 +27,10 @@ export default function Charity() {
     <Page>
       <Col className="w-full items-center rounded px-4 py-6 sm:px-8 xl:w-[125%]">
         <Col className="max-w-xl gap-2">
-          <Title className="!mt-0" text="Donate your M$ to charity!" />
+          <Title className="!mt-0" text="Manifold for Good" />
           <div className="mb-6 text-gray-500">
-            Throughout the month of May, every M$ 100 you contribute turns into
-            $1 USD to your chosen charity. We'll cover all processing fees!
+            Donate your winnings to charity! Through the month of May, every M$
+            100 you contribute turns into $1 USD sent to your chosen charity.
           </div>
 
           <input
@@ -44,16 +47,22 @@ export default function Charity() {
         </div>
         {filterCharities.length === 0 && (
           <div className="text-center text-gray-500">
-            No charities match your search :(
+            😢 We couldn't find that charity...
           </div>
         )}
 
-        <div className="mt-10 italic text-gray-500">
-          Note: Manifold is not affiliated with any of these charities, other
-          than being fans of their work.
+        <div className="mt-10 text-gray-500">
+          Don't see your favorite charity? Recommend that we add it by emailing
+          <span className="text-indigo-500"> give@manifold.markets</span>~
           <br />
-          As Manifold is a for-profit entity, your contributions will not be tax
-          deductible.
+          <br />
+          <span className="italic">
+            Note: Manifold is not affiliated with any of these charities (other
+            than being fans of their work!)
+            <br />
+            As Manifold is a for-profit entity, your contributions will not be
+            tax deductible.
+          </span>
         </div>
       </Col>
     </Page>
