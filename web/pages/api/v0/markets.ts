7fcec8b341a4b767c8a92d9e0e208e7c831795a6
Marshall Polaris <marshall@pol.rs>
2022-04-30 13:25:18 -0700

    Apply permissive CORS headers to all API routes

--- web/pages/api/v0/markets.ts
+++ web/pages/api/v0/markets.ts
@@ -2,6 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { listAllContracts } from '../../../lib/firebase/contracts'
 import { toLiteMarket } from './_types'
+import { applyCorsHeaders, CORS_UNRESTRICTED } from '../../../lib/api/cors'
 
 type Data = any[]
 
@@ -9,6 +10,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  await applyCorsHeaders(req, res, CORS_UNRESTRICTED)
   const contracts = await listAllContracts()
   // Serve from Vercel cache, then update. see https://vercel.com/docs/concepts/functions/edge-caching
   res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
