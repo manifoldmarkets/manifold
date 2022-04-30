21692a7b75c761f73a6374b79aa84e42c299aaee
James Grugett <jahooma@gmail.com>
2022-04-30 19:02:39 -0400

    Listen for contract updates to feed.

--- web/hooks/use-algo-feed.ts
+++ web/hooks/use-algo-feed.ts
@@ -7,15 +7,16 @@ import { useTimeSinceFirstRender } from './use-time-since-first-render'
 import { trackLatency } from '../lib/firebase/tracking'
 import { User } from '../../common/user'
 import { getUserFeed } from '../lib/firebase/users'
+import { useUpdatedContracts } from './use-contracts'
+
+type feed = {
+  contract: Contract
+  recentBets: Bet[]
+  recentComments: Comment[]
+}[]
 
 export const useAlgoFeed = (user: User | null | undefined) => {
-  const [feed, setFeed] = useState<
-    {
-      contract: Contract
-      recentBets: Bet[]
-      recentComments: Comment[]
-    }[]
-  >()
+  const [feed, setFeed] = useState<feed>()
 
   const getTime = useTimeSinceFirstRender()
 
@@ -28,7 +29,18 @@ export const useAlgoFeed = (user: User | null | undefined) => {
         console.log('feed load time', getTime())
       })
     }
-  }, [user, getTime])
+  }, [user?.id])
 
-  return feed
+  return useUpdateFeed(feed)
+}
+
+const useUpdateFeed = (feed: feed | undefined) => {
+  const contracts = useUpdatedContracts(feed?.map((item) => item.contract))
+
+  return feed && contracts
+    ? feed.map(({ contract, ...other }, i) => ({
+        ...other,
+        contract: contracts[i],
+      }))
+    : undefined
 }
