12e9de058aa9c57ca881dc97b96c32b15e7e4b43
James Grugett <jahooma@gmail.com>
2022-05-02 11:23:51 -0400

    Add share market widget shown if you are the creator

---
+++ web/components/share-market.tsx
@@ -0,0 +1,26 @@
+import clsx from 'clsx'
+import { Contract, contractUrl } from '../lib/firebase/contracts'
+import { CopyLinkButton } from './copy-link-button'
+import { Col } from './layout/col'
+import { Row } from './layout/row'
+
+export function ShareMarket(props: { contract: Contract; className?: string }) {
+  const { contract, className } = props
+
+  return (
+    <Col className={clsx(className, 'gap-3')}>
+      <div>Share your market</div>
+      <Row className="mb-6 items-center">
+        <input
+          className="input input-bordered flex-1 rounded-r-none text-gray-500"
+          type="text"
+          value={contractUrl(contract)}
+        />
+        <CopyLinkButton
+          contract={contract}
+          buttonClassName="btn-md rounded-l-none"
+        />
+      </Row>
+    </Col>
+  )
+}
