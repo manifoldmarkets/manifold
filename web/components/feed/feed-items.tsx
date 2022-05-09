417735f6d6fa42b258de5ca7cc537eb809111f5e
Marshall Polaris <marshall@pol.rs>
2022-05-09 14:56:18 -0700

    Remove wrapper div in user comment feed item

--- web/components/feed/feed-items.tsx
+++ web/components/feed/feed-items.tsx
@@ -166,43 +166,41 @@ export function FeedComment(props: {
         avatarUrl={userAvatarUrl}
       />
       <div className="min-w-0 flex-1">
-        <div>
-          <p className="mt-0.5 text-sm text-gray-500">
-            <UserLink
-              className="text-gray-500"
-              username={userUsername}
-              name={userName}
-            />{' '}
-            {!matchedBet && userPosition > 0 && (
+        <p className="mt-0.5 text-sm text-gray-500">
+          <UserLink
+            className="text-gray-500"
+            username={userUsername}
+            name={userName}
+          />{' '}
+          {!matchedBet && userPosition > 0 && (
+            <>
+              {'had ' + userPositionMoney + ' '}
               <>
-                {'had ' + userPositionMoney + ' '}
-                <>
-                  {' of '}
-                  <OutcomeLabel
-                    outcome={yesFloorShares > noFloorShares ? 'YES' : 'NO'}
-                    contract={contract}
-                    truncate="short"
-                  />
-                </>
+                {' of '}
+                <OutcomeLabel
+                  outcome={yesFloorShares > noFloorShares ? 'YES' : 'NO'}
+                  contract={contract}
+                  truncate="short"
+                />
+              </>
+            </>
+          )}
+          <>
+            {bought} {money}
+            {outcome && !hideOutcome && (
+              <>
+                {' '}
+                of{' '}
+                <OutcomeLabel
+                  outcome={outcome ? outcome : ''}
+                  contract={contract}
+                  truncate="short"
+                />
               </>
             )}
-            <>
-              {bought} {money}
-              {outcome && !hideOutcome && (
-                <>
-                  {' '}
-                  of{' '}
-                  <OutcomeLabel
-                    outcome={outcome ? outcome : ''}
-                    contract={contract}
-                    truncate="short"
-                  />
-                </>
-              )}
-            </>
-            <RelativeTimestamp time={createdTime} />
-          </p>
-        </div>
+          </>
+          <RelativeTimestamp time={createdTime} />
+        </p>
         <TruncatedComment
           comment={text}
           moreHref={contractPath(contract)}
