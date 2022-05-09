9fc9fc0dd69d484ef6b24f32917b5cdc49066de9
mantikoros <sgrugett@gmail.com>
2022-05-09 17:14:00 -0400

    fix import errors

--- functions/src/place-bet.ts
+++ functions/src/place-bet.ts
@@ -7,16 +7,12 @@ import {
   getNewBinaryCpmmBetInfo,
   getNewBinaryDpmBetInfo,
   getNewMultiBetInfo,
-  getLoanAmount,
 } from '../../common/new-bet'
 import { addObjects, removeUndefinedProps } from '../../common/util/object'
 import { Bet } from '../../common/bet'
 import { redeemShares } from './redeem-shares'
-import { Fees } from 'common/fees'
-import {
-  getContractBetMetrics,
-  hasUserHitManaLimit,
-} from 'common/calculate'
+import { Fees } from '../../common/fees'
+import { hasUserHitManaLimit } from '../../common/calculate'
 
 export const placeBet = functions.runWith({ minInstances: 1 }).https.onCall(
   async (
