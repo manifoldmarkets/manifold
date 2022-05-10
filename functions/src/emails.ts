8313930c707602d67b99c21db6adde10ea1f1220
mantikoros <sgrugett@gmail.com>
2022-05-10 11:23:32 -0400

    show correct commission in market close emails

--- functions/src/emails.ts
+++ functions/src/emails.ts
@@ -6,7 +6,7 @@ import { Bet } from '../../common/bet'
 import { getProbability } from '../../common/calculate'
 import { Comment } from '../../common/comment'
 import { Contract, FreeResponseContract } from '../../common/contract'
-import { CREATOR_FEE } from '../../common/fees'
+import { DPM_CREATOR_FEE } from '../../common/fees'
 import { PrivateUser, User } from '../../common/user'
 import { formatMoney, formatPercent } from '../../common/util/format'
 import { sendTemplateEmail, sendTextEmail } from './send-email'
@@ -149,8 +149,8 @@ export const sendMarketCloseEmail = async (
   const { username, name, id: userId } = user
   const firstName = name.split(' ')[0]
 
-  const { question, pool: pools, slug } = contract
-  const pool = formatMoney(_.sum(_.values(pools)))
+  const { question, slug, volume, mechanism, collectedFees } = contract
+
   const url = `https://${DOMAIN}/${username}/${slug}`
 
   await sendTemplateEmail(
@@ -158,12 +158,15 @@ export const sendMarketCloseEmail = async (
     'Your market has closed',
     'market-close',
     {
-      name: firstName,
       question,
-      pool,
       url,
       userId,
-      creatorFee: (CREATOR_FEE * 100).toString(),
+      name: firstName,
+      volume: formatMoney(volume),
+      creatorFee:
+        mechanism === 'dpm-2'
+          ? `${DPM_CREATOR_FEE * 100}% of the profits`
+          : formatMoney(collectedFees.creatorFee),
     }
   )
 }
