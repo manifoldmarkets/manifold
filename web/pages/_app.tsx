c99cf7579b779e943e14c715fd06285e64b0e954
Austin Chen <akrolsmir@gmail.com>
2022-05-13 10:11:57 -0400

    Try out Hotjar

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -39,6 +39,19 @@ function MyApp({ Component, pageProps }: AppProps) {
           gtag('config', 'G-SSFK1Q138D');
         `}
       </Script>
+      {/* Hotjar Tracking Code for https://manifold.markets */}
+      <Script>
+        {`
+          function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:2968940,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+      </Script>
       <Head>
         <title>Manifold Markets — A market for every question</title>
 
