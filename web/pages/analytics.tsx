c94a18bc61dd924d1274a97cf6b0d031f4a54b06
James Grugett <jahooma@gmail.com>
2022-05-15 15:50:28 -0400

    Change analytics to 45 days instead of 90 to fix timeout

--- web/pages/analytics.tsx
+++ web/pages/analytics.tsx
@@ -18,7 +18,7 @@ import { getDailyNewUsers } from 'web/lib/firebase/users'
 
 export const getStaticProps = fromPropz(getStaticPropz)
 export async function getStaticPropz() {
-  const numberOfDays = 90
+  const numberOfDays = 45
   const today = dayjs(dayjs().format('YYYY-MM-DD'))
   const startDate = today.subtract(numberOfDays, 'day')
 
