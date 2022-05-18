f376b17d5cffdfc37f13e270682d898338e841ab
Austin Chen <akrolsmir@gmail.com>
2022-05-18 12:06:41 -0400

    Kill the ugly "Sign in" button

--- web/components/nav/sidebar.tsx
+++ web/components/nav/sidebar.tsx
@@ -137,22 +137,11 @@ export default function Sidebar(props: { className?: string }) {
   return (
     <nav aria-label="Sidebar" className={className}>
       <ManifoldLogo className="pb-6" twoLine />
-      <div className="mb-2" style={{ minHeight: 80 }}>
-        {user ? (
+      {user && (
+        <div className="mb-2" style={{ minHeight: 80 }}>
           <ProfileSummary user={user} />
-        ) : user === null ? (
-          <div className="py-6 text-center">
-            <button
-              className="btn btn-sm border-2 bg-white px-6 font-medium normal-case text-gray-700"
-              onClick={firebaseLogin}
-            >
-              Sign in
-            </button>
-          </div>
-        ) : (
-          <div />
-        )}
-      </div>
+        </div>
+      )}
 
       <div className="space-y-1 lg:hidden">
         {mobileNavigationOptions.map((item) => (
@@ -180,15 +169,22 @@ export default function Sidebar(props: { className?: string }) {
         />
       </div>
 
-      {user && (
-        <div className={'aligncenter flex justify-center'}>
+      <div className={'aligncenter flex justify-center'}>
+        {user ? (
           <Link href={'/create'} passHref>
             <button className="mx-auto mt-4 -ml-1 w-full rounded-md border border-transparent bg-gradient-to-r from-purple-500 via-violet-500 to-indigo-500 py-2.5 text-base font-semibold text-white shadow-sm hover:from-purple-700 hover:via-violet-700 hover:to-indigo-700">
               Ask a question
             </button>
           </Link>
-        </div>
-      )}
+        ) : (
+          <button
+            onClick={firebaseLogin}
+            className="mx-auto mt-4 -ml-1 w-full rounded-md border border-transparent bg-gradient-to-r from-purple-500 via-violet-500 to-indigo-500 py-2.5 text-base font-semibold text-white shadow-sm hover:from-purple-700 hover:via-violet-700 hover:to-indigo-700"
+          >
+            Sign in
+          </button>
+        )}
+      </div>
 
       {user && deservesDailyFreeMarket && (
         <Row className="mt-2 justify-center">
