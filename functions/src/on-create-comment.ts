85b013d36782c1bb96dd311ff5151ba0b22a6961
Boa <iansphilips@gmail.com>
2022-05-18 07:25:38 -0600

    Fix fr comment emails (#247)
    * Get answer outcome from comment
    
    * Avoid db call if possible
    
    * Include general comments note in comment emails
    
    * Send market-comment on general comment

--- functions/src/on-create-comment.ts
+++ functions/src/on-create-comment.ts
@@ -34,7 +34,14 @@ export const onCreateComment = functions.firestore
 
     let bet: Bet | undefined
     let answer: Answer | undefined
-    if (comment.betId) {
+    if (comment.answerOutcome) {
+      answer =
+        contract.outcomeType === 'FREE_RESPONSE' && contract.answers
+          ? contract.answers?.find(
+              (answer) => answer.id === comment.answerOutcome
+            )
+          : undefined
+    } else if (comment.betId) {
       const betSnapshot = await firestore
         .collection('contracts')
         .doc(contractId)
@@ -66,7 +73,8 @@ export const onCreateComment = functions.firestore
           contract,
           comment,
           bet,
-          answer
+          answer?.text,
+          answer?.id
         )
       )
     )
