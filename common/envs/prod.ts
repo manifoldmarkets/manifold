6f1a569797ecf970d1c67fa2816cbd6cf5567a4f
mantikoros <sgrugett@gmail.com>
2022-05-19 22:28:38 -0400

    back to M$ — the worst option except for all the others

--- common/envs/prod.ts
+++ common/envs/prod.ts
@@ -44,7 +44,7 @@ export const PROD_CONFIG: EnvConfig = {
   ],
   visibility: 'PUBLIC',
 
-  moneyMoniker: 'ϻ',
+  moneyMoniker: 'M$',
   navbarLogoPath: '',
   faviconPath: '/favicon.ico',
   newQuestionPlaceholders: [
