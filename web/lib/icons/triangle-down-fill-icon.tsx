92df092ad359a50df6037ea25ced3aabec2aea22
James Grugett <jahooma@gmail.com>
2022-05-25 11:48:28 -0500

    Fix DOM error in console

--- web/lib/icons/triangle-down-fill-icon.tsx
+++ web/lib/icons/triangle-down-fill-icon.tsx
@@ -8,7 +8,7 @@ export default function TriangleDownFillIcon(props: { className?: string }) {
       viewBox="0 0 16 16"
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         transform="rotate(-180 8 8)"
         d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767L7.022 1.566z"
       />
