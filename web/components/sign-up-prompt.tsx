6427ccd8913d144943b7cef989f5fd23cdabf738
Forrest Wolf <forrest.weiswolf@gmail.com>
2022-05-25 09:56:18 -0400

    Return null instead of false when not showing SignUpPrompt

--- web/components/sign-up-prompt.tsx
+++ web/components/sign-up-prompt.tsx
@@ -5,12 +5,11 @@ import { firebaseLogin } from 'web/lib/firebase/users'
 export function SignUpPrompt() {
   const user = useUser()
 
-  return user === null && (
+  return user === null ?
     <button
       className="btn flex-1 whitespace-nowrap border-none bg-gradient-to-r from-teal-500 to-green-500 px-10 text-lg font-medium normal-case hover:from-teal-600 hover:to-green-600"
       onClick={firebaseLogin}
     >
       Sign up to bet!
-    </button>
-  )
+    </button> : null
 }
