13f32adb3fe4c8aaa30ba1d474141de014500f88
Marshall Polaris <marshall@pol.rs>
2022-05-26 14:37:51 -0700

    Serious business API validation & big cleanup of `createContract`, `placeBet` (#302)
    * Add the great Zod as a dependency to help us
    
    * Tweak eslint
    
    * Rewrite a ton of stuff in createContract and placeBet
    
    * Clean up error reporting in API
    
    * Make sure the UI is enforcing validated limits on lengths
    
    * Remove unnecessary Math.abs
    
    * Better type on `BetInfo`
    
    * Kill `manaLimitPerUser`
    
    * Clean up hacky parameters on bet info functions
    
    * Validate `closeTime` as a valid timestamp in the future

--- functions/src/place-bet.ts
+++ functions/src/place-bet.ts
@@ -1,146 +1,112 @@
 import * as admin from 'firebase-admin'
+import { z } from 'zod'
 
-import { APIError, newEndpoint, parseCredentials, lookupUser } from './api'
+import { APIError, newEndpoint, validate } from './api'
 import { Contract } from '../../common/contract'
 import { User } from '../../common/user'
 import {
+  BetInfo,
   getNewBinaryCpmmBetInfo,
   getNewBinaryDpmBetInfo,
   getNewMultiBetInfo,
   getNumericBetsInfo,
 } from '../../common/new-bet'
 import { addObjects, removeUndefinedProps } from '../../common/util/object'
-import { Bet } from '../../common/bet'
 import { redeemShares } from './redeem-shares'
-import { Fees } from '../../common/fees'
 
-export const placeBet = newEndpoint(['POST'], async (req, _res) => {
-  const [bettor, _privateUser] = await lookupUser(await parseCredentials(req))
-  const { amount, outcome, contractId, value } = req.body || {}
+const bodySchema = z.object({
+  contractId: z.string(),
+  amount: z.number().gte(1),
+})
 
-  if (amount < 1 || isNaN(amount) || !isFinite(amount))
-    throw new APIError(400, 'Invalid amount')
+const binarySchema = z.object({
+  outcome: z.enum(['YES', 'NO']),
+})
 
-  if (outcome !== 'YES' && outcome !== 'NO' && isNaN(+outcome))
-    throw new APIError(400, 'Invalid outcome')
+const freeResponseSchema = z.object({
+  outcome: z.string(),
+})
 
-  if (value !== undefined && !isFinite(value))
-    throw new APIError(400, 'Invalid value')
+const numericSchema = z.object({
+  outcome: z.string(),
+  value: z.number(),
+})
 
-  // run as transaction to prevent race conditions
-  return await firestore
-    .runTransaction(async (transaction) => {
-      const userDoc = firestore.doc(`users/${bettor.id}`)
-      const userSnap = await transaction.get(userDoc)
-      if (!userSnap.exists) throw new APIError(400, 'User not found')
-      const user = userSnap.data() as User
+export const placeBet = newEndpoint(['POST'], async (req, [bettor, _]) => {
+  const { amount, contractId } = validate(bodySchema, req.body)
 
-      const contractDoc = firestore.doc(`contracts/${contractId}`)
-      const contractSnap = await transaction.get(contractDoc)
-      if (!contractSnap.exists) throw new APIError(400, 'Invalid contract')
-      const contract = contractSnap.data() as Contract
+  const result = await firestore.runTransaction(async (trans) => {
+    const userDoc = firestore.doc(`users/${bettor.id}`)
+    const userSnap = await trans.get(userDoc)
+    if (!userSnap.exists) throw new APIError(400, 'User not found.')
+    const user = userSnap.data() as User
+    if (user.balance < amount) throw new APIError(400, 'Insufficient balance.')
 
-      const { closeTime, outcomeType, mechanism, collectedFees, volume } =
-        contract
-      if (closeTime && Date.now() > closeTime)
-        throw new APIError(400, 'Trading is closed')
+    const contractDoc = firestore.doc(`contracts/${contractId}`)
+    const contractSnap = await trans.get(contractDoc)
+    if (!contractSnap.exists) throw new APIError(400, 'Contract not found.')
+    const contract = contractSnap.data() as Contract
 
-      const yourBetsSnap = await transaction.get(
-        contractDoc.collection('bets').where('userId', '==', bettor.id)
-      )
-      const yourBets = yourBetsSnap.docs.map((doc) => doc.data() as Bet)
+    const loanAmount = 0
+    const { closeTime, outcomeType, mechanism, collectedFees, volume } =
+      contract
+    if (closeTime && Date.now() > closeTime)
+      throw new APIError(400, 'Trading is closed.')
 
-      const loanAmount = 0 // getLoanAmount(yourBets, amount)
-      if (user.balance < amount) throw new APIError(400, 'Insufficient balance')
-
-      if (outcomeType === 'FREE_RESPONSE') {
-        const answerSnap = await transaction.get(
-          contractDoc.collection('answers').doc(outcome)
-        )
-        if (!answerSnap.exists) throw new APIError(400, 'Invalid contract')
+    const {
+      newBet,
+      newPool,
+      newTotalShares,
+      newTotalBets,
+      newTotalLiquidity,
+      newP,
+    } = await (async (): Promise<BetInfo> => {
+      if (outcomeType == 'BINARY' && mechanism == 'dpm-2') {
+        const { outcome } = validate(binarySchema, req.body)
+        return getNewBinaryDpmBetInfo(outcome, amount, contract, loanAmount)
+      } else if (outcomeType == 'BINARY' && mechanism == 'cpmm-1') {
+        const { outcome } = validate(binarySchema, req.body)
+        return getNewBinaryCpmmBetInfo(outcome, amount, contract, loanAmount)
+      } else if (outcomeType == 'FREE_RESPONSE' && mechanism == 'dpm-2') {
+        const { outcome } = validate(freeResponseSchema, req.body)
+        const answerDoc = contractDoc.collection('answers').doc(outcome)
+        const answerSnap = await trans.get(answerDoc)
+        if (!answerSnap.exists) throw new APIError(400, 'Invalid answer')
+        return getNewMultiBetInfo(outcome, amount, contract, loanAmount)
+      } else if (outcomeType == 'NUMERIC' && mechanism == 'dpm-2') {
+        const { outcome, value } = validate(numericSchema, req.body)
+        return getNumericBetsInfo(value, outcome, amount, contract)
+      } else {
+        throw new APIError(500, 'Contract has invalid type/mechanism.')
       }
+    })()
 
-      const newBetDoc = firestore
-        .collection(`contracts/${contractId}/bets`)
-        .doc()
+    if (newP != null && !isFinite(newP)) {
+      throw new APIError(400, 'Trade rejected due to overflow error.')
+    }
 
-      const {
-        newBet,
-        newPool,
-        newTotalShares,
-        newTotalBets,
-        newBalance,
-        newTotalLiquidity,
-        fees,
-        newP,
-      } =
-        outcomeType === 'BINARY'
-          ? mechanism === 'dpm-2'
-            ? getNewBinaryDpmBetInfo(
-                user,
-                outcome as 'YES' | 'NO',
-                amount,
-                contract,
-                loanAmount,
-                newBetDoc.id
-              )
-            : (getNewBinaryCpmmBetInfo(
-                user,
-                outcome as 'YES' | 'NO',
-                amount,
-                contract,
-                loanAmount,
-                newBetDoc.id
-              ) as any)
-          : outcomeType === 'NUMERIC' && mechanism === 'dpm-2'
-          ? getNumericBetsInfo(
-              user,
-              value,
-              outcome,
-              amount,
-              contract,
-              newBetDoc.id
-            )
-          : getNewMultiBetInfo(
-              user,
-              outcome,
-              amount,
-              contract as any,
-              loanAmount,
-              newBetDoc.id
-            )
+    const newBalance = user.balance - amount - loanAmount
+    const betDoc = contractDoc.collection('bets').doc()
+    trans.create(betDoc, { id: betDoc.id, userId: user.id, ...newBet })
+    trans.update(userDoc, { balance: newBalance })
+    trans.update(
+      contractDoc,
+      removeUndefinedProps({
+        pool: newPool,
+        p: newP,
+        totalShares: newTotalShares,
+        totalBets: newTotalBets,
+        totalLiquidity: newTotalLiquidity,
+        collectedFees: addObjects(newBet.fees, collectedFees),
+        volume: volume + amount,
+      })
+    )
 
-      if (newP !== undefined && !isFinite(newP)) {
-        throw new APIError(400, 'Trade rejected due to overflow error.')
-      }
+    return { betId: betDoc.id }
+  })
 
-      transaction.create(newBetDoc, newBet)
-
-      transaction.update(
-        contractDoc,
-        removeUndefinedProps({
-          pool: newPool,
-          p: newP,
-          totalShares: newTotalShares,
-          totalBets: newTotalBets,
-          totalLiquidity: newTotalLiquidity,
-          collectedFees: addObjects<Fees>(fees ?? {}, collectedFees ?? {}),
-          volume: volume + Math.abs(amount),
-        })
-      )
-
-      if (!isFinite(newBalance)) {
-        throw new APIError(500, 'Invalid user balance for ' + user.username)
-      }
-
-      transaction.update(userDoc, { balance: newBalance })
-
-      return { betId: newBetDoc.id }
-    })
-    .then(async (result) => {
-      await redeemShares(bettor.id, contractId)
-      return result
-    })
+  await redeemShares(bettor.id, contractId)
+  return result
 })
 
 const firestore = admin.firestore()
