6a26dea597d471c44fbc1833c97434e7d269209a
Marshall Polaris <marshall@pol.rs>
2022-05-27 23:38:23 -0700

    Clean up LinksTable an absurd amount

--- web/pages/link.tsx
+++ web/pages/link.tsx
@@ -149,7 +149,12 @@ export default function LinkPage() {
         info={newManalink}
         isClaiming={false}
       />
-      {links.length > 0 && <LinksTable links={links} />}
+      <Title text="Your links" />
+      {links.length > 0 ? (
+        <LinksTable links={links} />
+      ) : (
+        <p>You haven&apos;t yet created any manalinks.</p>
+      )}
 
       {manalinkTxns.length > 0 && (
         <Col className="mt-12">
@@ -199,100 +204,41 @@ export function Claim(props: { txn: Txn }) {
   )
 }
 
+function LinkSummaryRow(props: { link: Manalink }) {
+  const { link } = props
+  return (
+    <tr className="whitespace-nowrap text-sm text-gray-500" key={link.slug}>
+      <td className="px-5 py-4 font-medium text-gray-900">
+        {formatMoney(link.amount)}
+      </td>
+      <td className="px-5 py-4">{getLinkUrl(link.slug)}</td>
+      <td className="px-5 py-4">{link.claimedUserIds.length}</td>
+      <td className="px-5 py-4">{link.maxUses == null ? '∞' : link.maxUses}</td>
+      <td className="px-5 py-4">
+        {link.expiresTime == null ? 'Never' : fromNow(link.expiresTime)}
+      </td>
+    </tr>
+  )
+}
+
 function LinksTable(props: { links: Manalink[] }) {
   const { links } = props
-
   return (
-    <div>
-      <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="text-xl font-semibold text-gray-900">Your links</h1>
-          <p className="mt-2 text-sm text-gray-700">
-            All mana links you&apos;ve created so far~
-          </p>
-        </div>
-      </div>
-      <div className="mt-8 flex flex-col">
-        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-300">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                    >
-                      Amount
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Link
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Uses
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Max Uses
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Expires
-                    </th>
-                    <th
-                      scope="col"
-                      className="relative py-3.5 pl-3 pr-4 sm:pr-6"
-                    >
-                      <span className="sr-only">Edit</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200 bg-white">
-                  {links.map((manalink) => (
-                    <tr key={manalink.slug}>
-                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                        {formatMoney(manalink.amount)}
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        {getLinkUrl(manalink.slug)}
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        {manalink.claimedUserIds.length}
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        {manalink.maxUses == null ? '∞' : manalink.maxUses}
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        {manalink.expiresTime == null
-                          ? 'Never'
-                          : fromNow(manalink.expiresTime)}
-                      </td>
-                      <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                        <a
-                          href="#"
-                          className="text-indigo-600 hover:text-indigo-900"
-                        >
-                          Edit<span className="sr-only">, {manalink.slug}</span>
-                        </a>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <table className="w-full divide-y divide-gray-300 rounded-lg border border-gray-200">
+      <thead className="bg-gray-50 text-left text-sm font-semibold text-gray-900">
+        <tr>
+          <th className="px-5 py-3.5">Amount</th>
+          <th className="px-5 py-3.5">Link</th>
+          <th className="px-5 py-3.5">Uses</th>
+          <th className="px-5 py-3.5">Max Uses</th>
+          <th className="px-5 py-3.5">Expires</th>
+        </tr>
+      </thead>
+      <tbody className="divide-y divide-gray-200 bg-white">
+        {links.map((link) => (
+          <LinkSummaryRow link={link} />
+        ))}
+      </tbody>
+    </table>
   )
 }
