f14bb63393bbd87e2558a18b539570dc740249ff
James Grugett <jahooma@gmail.com>
2022-06-01 18:30:40 -0500

    Check that sold bet is by auth'd user

--- functions/src/sell-bet.ts
+++ functions/src/sell-bet.ts
@@ -50,6 +50,7 @@ export const sellBet = functions.runWith({ minInstances: 1 }).https.onCall(
       if (!betSnap.exists) return { status: 'error', message: 'Invalid bet' }
       const bet = betSnap.data() as Bet
 
+      if (userId !== bet.userId) return { status: 'error', message: 'Not authorized' }
       if (bet.isSold) return { status: 'error', message: 'Bet already sold' }
 
       const newBetDoc = firestore
