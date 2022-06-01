02a37a14656816a65c5ad0c3cf3771733e40691d
Ian Philips <iansphilips@gmail.com>
2022-06-01 07:11:25 -0600

    Notifications (#354)
    * Notifications generating on comment,answer,contract update
    
    * Notifications MVP
    
    * Submitted an answer => answered
    
    * Listen for unseen notifications
    
    * Fix userlink formatting, move page
    
    * Fix links
    
    * Remove redundant code
    
    * Cleanup
    
    * Cleanup
    
    * Refactor name
    
    * Comments
    
    * Cleanup & update notif only after data retrieval
    
    * Find initial new notifs on user change
    
    * Enforce auth rules in db
    
    * eslint update
    
    * Code review changes
    
    * Refactor reason

---
+++ functions/src/on-create-answer.ts
@@ -0,0 +1,32 @@
+import * as functions from 'firebase-functions'
+import { getContract, getUser } from './utils'
+import { createNotification } from './create-notification'
+import { Answer } from '../../common/answer'
+
+export const onCreateAnswer = functions.firestore
+  .document('contracts/{contractId}/answers/{answerNumber}')
+  .onCreate(async (change, context) => {
+    const { contractId } = context.params as {
+      contractId: string
+    }
+    const { eventId } = context
+    const contract = await getContract(contractId)
+    if (!contract)
+      throw new Error('Could not find contract corresponding with answer')
+
+    const answer = change.data() as Answer
+    // Ignore ante answer.
+    if (answer.number === 0) return
+
+    const answerCreator = await getUser(answer.userId)
+    if (!answerCreator) throw new Error('Could not find answer creator')
+
+    await createNotification(
+      answer.id,
+      'answer',
+      'created',
+      contract,
+      answerCreator,
+      eventId
+    )
+  })
