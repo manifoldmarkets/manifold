cfd6af7c72f5d2e9dcfa4337a956a1af5e3dc289
James Grugett <jahooma@gmail.com>
2022-06-04 21:28:27 -0500

    Tweak create page layout

--- web/components/choices-toggle-group.tsx
+++ web/components/choices-toggle-group.tsx
@@ -20,37 +20,31 @@ export function ChoicesToggleGroup(props: {
     children,
   } = props
   return (
-    <Row className={'mt-2 items-center gap-2'}>
-      <RadioGroup
-        value={currentChoice.toString()}
-        onChange={(str) => null}
-        className="mt-2"
-      >
-        <div className={`grid grid-cols-12 gap-3`}>
-          {Object.keys(choicesMap).map((choiceKey) => (
-            <RadioGroup.Option
-              key={choiceKey}
-              value={choicesMap[choiceKey]}
-              onClick={() => setChoice(choicesMap[choiceKey])}
-              className={({ active }) =>
-                clsx(
-                  active ? 'ring-2 ring-indigo-500 ring-offset-2' : '',
-                  currentChoice === choicesMap[choiceKey]
-                    ? 'border-transparent bg-indigo-500 text-white hover:bg-indigo-600'
-                    : 'border-gray-200 bg-white text-gray-900 hover:bg-gray-50',
-                  'flex cursor-pointer items-center justify-center rounded-md border py-3 px-3 text-sm font-medium normal-case',
-                  "hover:ring-offset-2' hover:ring-2 hover:ring-indigo-500",
-                  className
-                )
-              }
-              disabled={isSubmitting}
-            >
-              <RadioGroup.Label as="span">{choiceKey}</RadioGroup.Label>
-            </RadioGroup.Option>
-          ))}
-          {children}
-        </div>
-      </RadioGroup>
-    </Row>
+    <RadioGroup value={currentChoice.toString()} onChange={(str) => null}>
+      <Row className={'items-center flex-wrap gap-3'}>
+        {Object.keys(choicesMap).map((choiceKey) => (
+          <RadioGroup.Option
+            key={choiceKey}
+            value={choicesMap[choiceKey]}
+            onClick={() => setChoice(choicesMap[choiceKey])}
+            className={({ active }) =>
+              clsx(
+                active ? 'ring-2 ring-indigo-500 ring-offset-2' : '',
+                currentChoice === choicesMap[choiceKey]
+                  ? 'border-transparent bg-indigo-500 text-white hover:bg-indigo-600'
+                  : 'border-gray-200 bg-white text-gray-900 hover:bg-gray-50',
+                'flex cursor-pointer items-center justify-center rounded-md border py-3 px-3 text-sm font-medium normal-case',
+                "hover:ring-offset-2' hover:ring-2 hover:ring-indigo-500",
+                className
+              )
+            }
+            disabled={isSubmitting}
+          >
+            <RadioGroup.Label as="span">{choiceKey}</RadioGroup.Label>
+          </RadioGroup.Option>
+        ))}
+        {children}
+      </Row>
+    </RadioGroup>
   )
 }
