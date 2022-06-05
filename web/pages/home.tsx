eb885e38230fb988da7b6720e3f40dfb9af299a0
James Grugett <jahooma@gmail.com>
2022-06-04 19:00:13 -0500

    Quick back from clicking contract card on home! Preserves search state.

--- web/pages/home.tsx
+++ web/pages/home.tsx
@@ -1,13 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Router from 'next/router'
 
 import { Page } from 'web/components/page'
 import { Col } from 'web/components/layout/col'
 import { useUser } from 'web/hooks/use-user'
 import { ContractSearch } from 'web/components/contract-search'
+import { Contract } from 'common/contract'
+import { ContractPageContent } from './[username]/[contractSlug]'
+import { getContractFromSlug } from 'web/lib/firebase/contracts'
 
 const Home = () => {
   const user = useUser()
+
+  const [contract, setContract] = useState<Contract | undefined>()
+
+  useEffect(() => {
+    const onBack = () => {
+      const path = location.pathname.split('/').slice(1)
+      if (path[0] === 'home') setContract(undefined)
+      else {
+        const [username, contractSlug] = path
+        if (!username || !contractSlug) setContract(undefined)
+        else {
+          // Show contract if route is to a contract: '/[username]/[contractSlug]'.
+          getContractFromSlug(contractSlug).then((c) => {
+            setContract(c)
+            window.scrollTo(0, 0)
+          })
+        }
+      }
+    }
+
+    window.addEventListener('popstate', onBack)
+    return () => window.removeEventListener('popstate', onBack)
+  }, [])
 
   if (user === null) {
     Router.replace('/')
@@ -15,17 +41,39 @@ const Home = () => {
   }
 
   return (
-    <Page assertUser="signed-in">
-      <Col className="mx-auto w-full p-2">
-        <ContractSearch
-          querySortOptions={{
-            shouldLoadFromStorage: true,
-            defaultSort: '24-hour-vol',
+    <>
+      <Page assertUser="signed-in" suspend={!!contract}>
+        <Col className="mx-auto w-full p-2">
+          <ContractSearch
+            querySortOptions={{
+              shouldLoadFromStorage: true,
+              defaultSort: '24-hour-vol',
+            }}
+            showCategorySelector
+            onContractClick={(c) => {
+              // Show contract without navigating to contract page.
+              setContract(c)
+              // Update the url without switching pages in Nextjs.
+              history.pushState(null, '', `/${c.creatorUsername}/${c.slug}`)
+              window.scrollTo(0, 0)
+            }}
+          />
+        </Col>
+      </Page>
+
+      {contract && (
+        <ContractPageContent
+          contract={contract}
+          username={contract.creatorUsername}
+          slug={contract.slug}
+          bets={[]}
+          comments={[]}
+          backToHome={() => {
+            history.back()
           }}
-          showCategorySelector
         />
-      </Col>
-    </Page>
+      )}
+    </>
   )
 }
 
