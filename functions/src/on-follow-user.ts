1976bc755e1dbe479c892fd49f8ed295ca352a6e
Ian Philips <iansphilips@gmail.com>
2022-06-06 11:36:59 -0600

    Revert "Revert "Notification detail, grouping, and settings control [wip] (#403)""
    This reverts commit 07f2d390e58c0a0fb90dd0f53b76ab5f1baacda9.

---
+++ functions/src/on-follow-user.ts
@@ -0,0 +1,28 @@
+import * as functions from 'firebase-functions'
+import { getUser } from './utils'
+import { createNotification } from './create-notification'
+
+export const onFollowUser = functions.firestore
+  .document('users/{userId}/follows/{followedUserId}')
+  .onCreate(async (change, context) => {
+    const { userId } = context.params as {
+      userId: string
+    }
+    const { eventId } = context
+
+    const follow = change.data() as { userId: string; timestamp: number }
+
+    const followingUser = await getUser(userId)
+    if (!followingUser) throw new Error('Could not find following user')
+
+    await createNotification(
+      followingUser.id,
+      'follow',
+      'created',
+      followingUser,
+      eventId,
+      undefined,
+      undefined,
+      follow.userId
+    )
+  })
