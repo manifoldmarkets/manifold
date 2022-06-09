6a6373cf31886c923d8f739d3130a349b385bc7e
Marshall Polaris <marshall@pol.rs>
2022-06-08 17:24:46 -0700

    Add really simple paging to markets endpoint

--- web/pages/api/v0/markets.ts
+++ web/pages/api/v0/markets.ts
@@ -4,15 +4,46 @@ import { listAllContracts } from 'web/lib/firebase/contracts'
 import { applyCorsHeaders, CORS_UNRESTRICTED } from 'web/lib/api/cors'
 import { toLiteMarket } from './_types'
 
-type Data = any[]
-
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   await applyCorsHeaders(req, res, CORS_UNRESTRICTED)
-  const contracts = await listAllContracts()
-  // Serve from Vercel cache, then update. see https://vercel.com/docs/concepts/functions/edge-caching
-  res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
-  res.status(200).json(contracts.map(toLiteMarket))
+  let before: string | undefined
+  let n: number | undefined
+  if (req.query.before != null) {
+    if (typeof req.query.before !== 'string') {
+      res.status(400).json({ error: 'before must be null or a market ID.' })
+      return
+    }
+    before = req.query.before
+  }
+  if (req.query.n != null) {
+    if (typeof req.query.n !== 'string') {
+      res
+        .status(400)
+        .json({ error: 'n must be null or a number of markets to return.' })
+      return
+    }
+    n = parseInt(req.query.n)
+  } else {
+    n = 1000
+  }
+  if (n < 1 || n > 1000) {
+    res.status(400).json({ error: 'n must be between 1 and 1000.' })
+    return
+  }
+
+  try {
+    const contracts = await listAllContracts(n, before)
+    // Serve from Vercel cache, then update. see https://vercel.com/docs/concepts/functions/edge-caching
+    res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
+    res.status(200).json(contracts.map(toLiteMarket))
+  } catch (e) {
+    res.status(400).json({
+      error:
+        'Failed to fetch markets (did you pass an invalid ID as the before parameter?)',
+    })
+    return
+  }
 }
