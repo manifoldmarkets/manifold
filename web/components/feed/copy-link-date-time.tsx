a39f76541ac082f7eea0f60e5145c2e7dc65d3d4
Marshall Polaris <marshall@pol.rs>
2022-06-10 14:12:54 -0700

    Fix busted comment permalink copying code

--- web/components/feed/copy-link-date-time.tsx
+++ web/components/feed/copy-link-date-time.tsx
@@ -28,8 +28,7 @@ export function CopyLinkDateTimeComponent(props: {
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
   ) {
     event.preventDefault()
-    const elementLocation = `https://${ENV_CONFIG.domain}/${contractCreatorUsername}/${contractSlug}
-    )}#${elementId}`
+    const elementLocation = `https://${ENV_CONFIG.domain}/${contractCreatorUsername}/${contractSlug}#${elementId}`
 
     copyToClipboard(elementLocation)
     setShowToast(true)
