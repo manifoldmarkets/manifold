152e758a59ba582d367b12b7349097b71edeab6f
Ian Philips <iansphilips@gmail.com>
2022-06-10 17:33:30 -0600

    Rename liveContract

--- web/components/feed/contract-activity.tsx
+++ web/components/feed/contract-activity.tsx
@@ -28,40 +28,38 @@ export function ContractActivity(props: {
   className?: string
   betRowClassName?: string
 }) {
-  const { user, mode, contractPath, className, betRowClassName, contract } =
-    props
+  const { user, mode, contractPath, className, betRowClassName } = props
 
-  const liveContract = useContractWithPreload(contract) ?? props.contract
+  const contract = useContractWithPreload(props.contract) ?? props.contract
 
   const updatedComments =
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    mode === 'only-recent' ? undefined : useComments(liveContract.id)
+    mode === 'only-recent' ? undefined : useComments(contract.id)
   const comments = updatedComments ?? props.comments
 
   const updatedBets =
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    mode === 'only-recent' ? undefined : useBets(liveContract.id)
+    mode === 'only-recent' ? undefined : useBets(contract.id)
   const bets = (updatedBets ?? props.bets).filter((bet) => !bet.isRedemption)
-  // if (!liveContract) return <div />
   const items =
     mode === 'only-recent'
-      ? getRecentContractActivityItems(liveContract, bets, comments, user, {
+      ? getRecentContractActivityItems(contract, bets, comments, user, {
           contractPath,
         })
       : mode === 'comments' ||
         mode === 'bets' ||
         mode === 'free-response-comment-answer-groups'
-      ? getSpecificContractActivityItems(liveContract, bets, comments, user, {
+      ? getSpecificContractActivityItems(contract, bets, comments, user, {
           mode,
         })
       : // only used in abbreviated mode with folds/communities, all mode isn't used
-        getAllContractActivityItems(liveContract, bets, comments, user, {
+        getAllContractActivityItems(contract, bets, comments, user, {
           abbreviated: mode === 'abbreviated',
         })
 
   return (
     <FeedItems
-      contract={liveContract}
+      contract={contract}
       items={items}
       className={className}
       betRowClassName={betRowClassName}
