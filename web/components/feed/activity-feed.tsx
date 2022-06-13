970313e31fbb92ed4c3be2e5dbb8c6b329321f1d
Ian Philips <iansphilips@gmail.com>
2022-06-13 07:36:00 -0600

    Remove unused acitivity items

--- web/components/feed/activity-feed.tsx
+++ web/components/feed/activity-feed.tsx
@@ -2,8 +2,6 @@ import { Contract } from 'web/lib/firebase/contracts'
 import { Comment } from 'web/lib/firebase/comments'
 import { Col } from '../layout/col'
 import { Bet } from 'common/bet'
-import { useUser } from 'web/hooks/use-user'
-import { ContractActivity } from './contract-activity'
 
 export function ActivityFeed(props: {
   feed: {
@@ -14,25 +12,7 @@ export function ActivityFeed(props: {
   mode: 'only-recent' | 'abbreviated' | 'all'
   getContractPath?: (contract: Contract) => string
 }) {
-  const { feed, mode, getContractPath } = props
-  const user = useUser()
+  const {} = props
 
-  return (
-    <Col className="gap-2">
-      {feed.map((item) => (
-        <ContractActivity
-          key={item.contract.id}
-          className="rounded-md bg-white py-6 px-2 sm:px-4"
-          user={user}
-          contract={item.contract}
-          bets={item.recentBets}
-          comments={item.recentComments}
-          mode={mode}
-          contractPath={
-            getContractPath ? getContractPath(item.contract) : undefined
-          }
-        />
-      ))}
-    </Col>
-  )
+  return <Col className="gap-2"></Col>
 }
