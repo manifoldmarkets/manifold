4352141dbf766a9d98ea8e087f30d763a56a8334
mantikoros <sgrugett@gmail.com>
2022-06-12 23:25:57 -0500

    "add funds" => "get M$"

--- web/components/yes-no-selector.tsx
+++ web/components/yes-no-selector.tsx
@@ -168,7 +168,7 @@ export function FundsSelector(props: {
       {fundAmounts.map((amount) => (
         <Button
           key={amount}
-          color={selected === amount ? 'green' : 'gray'}
+          color={selected === amount ? 'indigo' : 'gray'}
           onClick={() => onSelect(amount as any)}
           className={btnClassName}
         >
@@ -230,7 +230,7 @@ export function NumberCancelSelector(props: {
 function Button(props: {
   className?: string
   onClick?: () => void
-  color: 'green' | 'red' | 'blue' | 'yellow' | 'gray'
+  color: 'green' | 'red' | 'blue' | 'indigo' | 'yellow' | 'gray'
   children?: ReactNode
 }) {
   const { className, onClick, children, color } = props
@@ -244,6 +244,7 @@ function Button(props: {
         color === 'red' && 'bg-red-400 text-white hover:bg-red-500',
         color === 'yellow' && 'bg-yellow-400 text-white hover:bg-yellow-500',
         color === 'blue' && 'bg-blue-400 text-white hover:bg-blue-500',
+        color === 'indigo' && 'bg-indigo-500 text-white hover:bg-indigo-600',
         color === 'gray' && 'bg-gray-200 text-gray-700 hover:bg-gray-300',
         className
       )}
