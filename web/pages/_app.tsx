0ecff1b581e0e02b13aa9fa7891f588a13aef48c
mantikoros <95266179+mantikoros@users.noreply.github.com>
2022-06-14 11:54:58 -0500

    Heap analytics (#500)
    * add heap
    
    * remove hotjar
    
    * prettier formatting conflicting with es lint
    
    * stop weird prettier/eslint conflict

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -5,6 +5,7 @@ import Head from 'next/head'
 import Script from 'next/script'
 import { usePreserveScroll } from 'web/hooks/use-preserve-scroll'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { ENV_CONFIG } from 'common/envs/constants'
 
 function firstLine(msg: string) {
   return msg.replace(/\r?\n.*/s, '')
@@ -39,19 +40,12 @@ function MyApp({ Component, pageProps }: AppProps) {
           gtag('config', 'G-SSFK1Q138D');
         `}
       </Script>
-      {/* Hotjar Tracking Code for https://manifold.markets */}
-      <Script id="hotjar">
-        {`
-          (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:2968940,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        `}
-      </Script>
+      {ENV_CONFIG.heapId && (
+        <Script id="heap-analytics">
+          {`window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+        heap.load("${ENV_CONFIG.heapId}");`}
+        </Script>
+      )}
       <Head>
         <title>Manifold Markets — A market for every question</title>
 
