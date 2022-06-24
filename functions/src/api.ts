66334b63e4e2f38e439f51afc7fed1a24876f664
Marshall Polaris <marshall@pol.rs>
2022-06-23 23:55:50 -0700

    Also explicitly specify concurrency

--- functions/src/api.ts
+++ functions/src/api.ts
@@ -110,6 +110,7 @@ export const validate = <T extends z.ZodTypeAny>(schema: T, val: unknown) => {
 
 const DEFAULT_OPTS: HttpsOptions = {
   minInstances: 1,
+  concurrency: 100,
   memory: '2GiB',
   cpu: 1,
   cors: [CORS_ORIGIN_MANIFOLD, CORS_ORIGIN_LOCALHOST],
