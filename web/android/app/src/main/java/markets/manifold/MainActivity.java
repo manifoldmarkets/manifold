6997e123f686099791c972f32f0f5ac3e964180a
Austin Chen <akrolsmir@gmail.com>
2022-06-27 14:39:10 -0500

    Add capacitor-firebase-auth

--- web/android/app/src/main/java/markets/manifold/MainActivity.java
+++ web/android/app/src/main/java/markets/manifold/MainActivity.java
@@ -1,5 +1,13 @@
 package markets.manifold;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.baumblatt.capacitor.firebase.auth.CapacitorFirebaseAuth;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    registerPlugin(CapacitorFirebaseAuth.class);
+  }
+}
--- web/capacitor.config.ts
+++ web/capacitor.config.ts
@@ -5,6 +5,19 @@ const config: CapacitorConfig = {
   appName: 'Manifold Markets',
   webDir: 'out',
   bundledWebRuntime: false,
+  // TODO: Do we need this?
+  // server: {
+  //   allowNavigation: ['oneword-cf74a.firebaseapp.com', 'accounts.google.com'],
+  // },
+  plugins: {
+    CapacitorFirebaseAuth: {
+      providers: ['google.com'],
+      languageCode: 'en',
+      permissions: {
+        google: ['profile'],
+      },
+    },
+  },
 }
 
 export default config
