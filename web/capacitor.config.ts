4ed4f7e2b2285e0de7db9cd050cf58a306d29f7a
Austin Chen <akrolsmir@gmail.com>
2022-06-27 11:51:28 -0500

    Set up Capacitor

---
+++ web/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'markets.manifold',
+  appName: 'Manifold Markets',
+  webDir: 'out',
+  bundledWebRuntime: false,
+}
+
+export default config
