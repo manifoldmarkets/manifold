17a5fc3880c46f73ac3f3b51bef9ae8594236560
James Grugett <jahooma@gmail.com>
2022-07-13 18:23:03 -0500

    Bet panel: Quick vs Limit pill buttons. Also, pill buttons for Yes vs No.

---
+++ web/components/buttons/pill-button.tsx
@@ -0,0 +1,27 @@
+import clsx from 'clsx'
+import { ReactNode } from 'react'
+
+export function PillButton(props: {
+  selected: boolean
+  onSelect: () => void
+  color?: string
+  big?: boolean
+  children: ReactNode
+}) {
+  const { children, selected, onSelect, color, big } = props
+
+  return (
+    <button
+      className={clsx(
+        'cursor-pointer select-none rounded-full',
+        selected
+          ? ['text-white', color ?? 'bg-gray-700']
+          : 'bg-gray-100 hover:bg-gray-200',
+        big ? 'px-8 py-2' : 'px-3 py-1.5 text-sm'
+      )}
+      onClick={onSelect}
+    >
+      {children}
+    </button>
+  )
+}
