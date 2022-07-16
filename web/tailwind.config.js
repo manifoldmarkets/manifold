d6a221efd30e0eaa074255f2b73b68ca5237d652
Austin Chen <akrolsmir@gmail.com>
2022-07-16 09:30:23 -0700

    Disable quotation marks in quotes

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -17,6 +17,14 @@ module.exports = {
       backgroundImage: {
         'world-trading': "url('/world-trading-background.webp')",
       },
+      typography: {
+        quoteless: {
+          css: {
+            'blockquote p:first-of-type::before': { content: 'none' },
+            'blockquote p:first-of-type::after': { content: 'none' },
+          },
+        },
+      },
     },
   },
   plugins: [
