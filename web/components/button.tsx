ae2e7dfe3078bdb81cabdd08f17b107d585010da
ingawei <46611122+ingawei@users.noreply.github.com>
2022-07-30 00:50:03 -0700

    noobify welcome demo (#699)
    * noobify welcome
    * also beginning to add greyscale color scheme

--- web/components/button.tsx
+++ web/components/button.tsx
@@ -39,8 +39,10 @@ export function Button(props: {
         color === 'yellow' && 'bg-yellow-400 text-white hover:bg-yellow-500',
         color === 'blue' && 'bg-blue-400 text-white hover:bg-blue-500',
         color === 'indigo' && 'bg-indigo-500 text-white hover:bg-indigo-600',
-        color === 'gray' && 'bg-gray-100 text-gray-600 hover:bg-gray-200',
-        color === 'gray-white' && 'bg-white text-gray-500 hover:bg-gray-200',
+        color === 'gray' &&
+          'bg-greyscale-1 text-greyscale-7 hover:bg-greyscale-2',
+        color === 'gray-white' &&
+          'text-greyscale-6 hover:bg-greyscale-2 bg-white',
         className
       )}
       disabled={disabled}
