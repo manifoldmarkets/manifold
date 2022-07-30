ea3bdad72a98cec96962ca412704543efb6fb3e7
James Grugett <jahooma@gmail.com>
2022-07-29 19:08:51 -0500

    Refactor search to not use Algolia components (#705)
    * In progress refactor to not use Algolia components
    
    * Cleanup
    
    * Only query when necessary
    
    * Read and update url params for query and sort
    
    * Don't push router
    
    * Don't update url if using default sort
    
    * Add popstate listener to improve home navigation
    
    * Remove console.logs

--- web/hooks/use-sort-and-query-params.tsx
+++ web/hooks/use-sort-and-query-params.tsx
@@ -1,8 +1,6 @@
 import { defaults, debounce } from 'lodash'
 import { useRouter } from 'next/router'
 import { useEffect, useMemo, useState } from 'react'
-import { useSearchBox } from 'react-instantsearch-hooks-web'
-import { track } from 'web/lib/service/analytics'
 import { DEFAULT_SORT } from 'web/components/contract-search'
 
 const MARKETS_SORT = 'markets_sort'
@@ -74,51 +72,69 @@ export function useInitialQueryAndSort(options?: {
   }
 }
 
-export function useUpdateQueryAndSort(props: {
-  shouldLoadFromStorage: boolean
+export function useQueryAndSortParams(options?: {
+  defaultSort?: Sort
+  shouldLoadFromStorage?: boolean
 }) {
-  const { shouldLoadFromStorage } = props
+  const { defaultSort = DEFAULT_SORT, shouldLoadFromStorage = true } =
+    options ?? {}
   const router = useRouter()
 
+  const { s: sort, q: query } = router.query as {
+    q?: string
+    s?: Sort
+  }
+
   const setSort = (sort: Sort | undefined) => {
-    if (sort !== router.query.s) {
-      router.query.s = sort
-      router.replace({ query: { ...router.query, s: sort } }, undefined, {
-        shallow: true,
-      })
-      if (shouldLoadFromStorage) {
-        localStorage.setItem(MARKETS_SORT, sort || '')
-      }
+    router.replace({ query: { ...router.query, s: sort } }, undefined, {
+      shallow: true,
+    })
+    if (shouldLoadFromStorage) {
+      localStorage.setItem(MARKETS_SORT, sort || '')
     }
   }
 
-  const { query, refine } = useSearchBox()
+  const [queryState, setQueryState] = useState(query)
+
+  useEffect(() => {
+    setQueryState(query)
+  }, [query])
 
   // Debounce router query update.
   const pushQuery = useMemo(
     () =>
       debounce((query: string | undefined) => {
-        if (query) {
-          router.query.q = query
-        } else {
-          delete router.query.q
-        }
-        router.replace({ query: router.query }, undefined, {
+        router.replace({ query: { ...router.query, q: query } }, undefined, {
           shallow: true,
         })
-        track('search', { query })
-      }, 500),
+      }, 100),
     [router]
   )
 
   const setQuery = (query: string | undefined) => {
-    refine(query ?? '')
+    setQueryState(query)
     pushQuery(query)
   }
 
+  useEffect(() => {
+    // If there's no sort option, then set the one from localstorage
+    if (router.isReady && !sort && shouldLoadFromStorage) {
+      const localSort = localStorage.getItem(MARKETS_SORT) as Sort
+      if (localSort && localSort !== defaultSort) {
+        // Use replace to not break navigating back.
+        router.replace(
+          { query: { ...router.query, s: localSort } },
+          undefined,
+          { shallow: true }
+        )
+      }
+    }
+  })
+
   return {
+    sort: sort ?? defaultSort,
+    query: queryState ?? '',
     setSort,
     setQuery,
-    query,
   }
 }
