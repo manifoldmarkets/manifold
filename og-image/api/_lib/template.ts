055cf7dd31f342d323e2a6b29bef00f4ebb7b7ba
Ian Philips <iansphilips@gmail.com>
2022-08-01 15:31:09 -0600

    Add emojis

--- og-image/api/_lib/template.ts
+++ og-image/api/_lib/template.ts
@@ -148,7 +148,7 @@ export function getHtml(parsedReq: ParsedRequest) {
 
       <div class="flex flex-row justify-between gap-12 pt-36">
         <div class="flex flex-col text-gray-900 text-6xl">
-          ${challengeAmount ? 'Bet against me on' : ''}
+          ${challengeAmount ? '⚔️ Bet against me ⚔️' : ''}
           <div class="text-indigo-700 leading-tight">
             ${truncatedQuestion}
           </div>
