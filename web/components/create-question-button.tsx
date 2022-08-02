76dd07f82b6ecac5b5e84dbe055e8eab2a383d16
mantikoros <sgrugett@gmail.com>
2022-08-01 17:44:24 -0700

    update question button

--- web/components/create-question-button.tsx
+++ web/components/create-question-button.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import clsx from 'clsx'
 import { firebaseLogin, User } from 'web/lib/firebase/users'
 import React from 'react'
+import { Button } from './button'
 
 export const createButtonStyle =
   'border-w-0 mx-auto mt-4 -ml-1 w-full rounded-md bg-gradient-to-r py-2.5 text-base font-semibold text-white shadow-sm lg:-ml-0 h-11'
@@ -20,9 +21,9 @@ export const CreateQuestionButton = (props: {
     <div className={clsx('flex justify-center', className)}>
       {user ? (
         <Link href={`/create${query ? query : ''}`} passHref>
-          <button className={clsx(gradient, createButtonStyle)}>
+          <Button color="gradient" size='lg'>
             {overrideText ? overrideText : 'Create a question'}
-          </button>
+          </Button>
         </Link>
       ) : (
         <button
