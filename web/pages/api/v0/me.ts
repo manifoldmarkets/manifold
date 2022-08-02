da7dc92b752e7ed9227aee0d1e3269363d96ae81
Marshall Polaris <marshall@pol.rs>
2022-08-02 00:21:51 -0700

    Just make `me` endpoint forward the backend response

--- web/pages/api/v0/me.ts
+++ web/pages/api/v0/me.ts
@@ -1,7 +1,6 @@
-import { User } from 'common/user'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { fetchBackend } from 'web/lib/api/proxy'
-import { LiteUser, ApiError, toLiteUser } from './_types'
+import { fetchBackend, forwardResponse } from 'web/lib/api/proxy'
+import { LiteUser, ApiError } from './_types'
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,15 +8,7 @@ export default async function handler(
 ) {
   try {
     const backendRes = await fetchBackend(req, 'getcurrentuser')
-
-    const user = (await backendRes.json()) as User
-    if (!user) {
-      res.status(404).json({ error: 'User not found' })
-      return
-    }
-    res.setHeader('Cache-Control', 'no-cache')
-    res.status(200).json(toLiteUser(user))
-    return
+    await forwardResponse(res, backendRes)
   } catch (err) {
     console.error('Error talking to cloud function: ', err)
     res.status(500).json({ error: 'Error communicating with backend.' })
