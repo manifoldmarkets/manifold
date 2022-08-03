1f06f24d7a458ee21d7ee9eb7473a8bc29d12652
Marshall Polaris <marshall@pol.rs>
2022-08-03 14:32:29 -0700

    Use custom token to authenticate Firebase SDK on server

--- web/lib/firebase/auth.ts
+++ web/lib/firebase/auth.ts
@@ -2,7 +2,7 @@ import { PROJECT_ID } from 'common/envs/constants'
 import { setCookie, getCookies } from '../util/cookie'
 import { IncomingMessage, ServerResponse } from 'http'
 
-const TOKEN_KINDS = ['refresh', 'id'] as const
+const TOKEN_KINDS = ['refresh', 'id', 'custom'] as const
 type TokenKind = typeof TOKEN_KINDS[number]
 
 const getAuthCookieName = (kind: TokenKind) => {
@@ -12,6 +12,9 @@ const getAuthCookieName = (kind: TokenKind) => {
 
 const ID_COOKIE_NAME = getAuthCookieName('id')
 const REFRESH_COOKIE_NAME = getAuthCookieName('refresh')
+const CUSTOM_COOKIE_NAME = getAuthCookieName('custom')
+const ONE_HOUR_SECS = 60 * 60
+const TEN_YEARS_SECS = 60 * 60 * 24 * 365 * 10
 
 export const getAuthCookies = (request?: IncomingMessage) => {
   const data = request != null ? request.headers.cookie ?? '' : document.cookie
@@ -19,24 +22,31 @@ export const getAuthCookies = (request?: IncomingMessage) => {
   return {
     idToken: cookies[ID_COOKIE_NAME] as string | undefined,
     refreshToken: cookies[REFRESH_COOKIE_NAME] as string | undefined,
+    customToken: cookies[CUSTOM_COOKIE_NAME] as string | undefined,
   }
 }
 
 export const setAuthCookies = (
   idToken?: string,
   refreshToken?: string,
+  customToken?: string,
   response?: ServerResponse
 ) => {
-  // these tokens last an hour
-  const idMaxAge = idToken != null ? 60 * 60 : 0
+  const idMaxAge = idToken != null ? ONE_HOUR_SECS : 0
   const idCookie = setCookie(ID_COOKIE_NAME, idToken ?? '', [
     ['path', '/'],
     ['max-age', idMaxAge.toString()],
     ['samesite', 'lax'],
     ['secure'],
   ])
-  // these tokens don't expire
-  const refreshMaxAge = refreshToken != null ? 60 * 60 * 24 * 365 * 10 : 0
+  const customMaxAge = customToken != null ? ONE_HOUR_SECS : 0
+  const customCookie = setCookie(CUSTOM_COOKIE_NAME, customToken ?? '', [
+    ['path', '/'],
+    ['max-age', customMaxAge.toString()],
+    ['samesite', 'lax'],
+    ['secure'],
+  ])
+  const refreshMaxAge = refreshToken != null ? TEN_YEARS_SECS : 0
   const refreshCookie = setCookie(REFRESH_COOKIE_NAME, refreshToken ?? '', [
     ['path', '/'],
     ['max-age', refreshMaxAge.toString()],
@@ -44,10 +54,11 @@ export const setAuthCookies = (
     ['secure'],
   ])
   if (response != null) {
-    response.setHeader('Set-Cookie', [idCookie, refreshCookie])
+    response.setHeader('Set-Cookie', [idCookie, refreshCookie, customCookie])
   } else {
     document.cookie = idCookie
     document.cookie = refreshCookie
+    document.cookie = customCookie
   }
 }
 
