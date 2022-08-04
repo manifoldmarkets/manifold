edae709f5f25c192e386dded4838182684c9e6d9
Sinclair Chen <abc.sinclair@gmail.com>
2022-08-04 15:35:55 -0700

    Notify mentioned users on market publish (#683)
    * Add function to parse at mentions
    
    * Notify mentioned users on market create
    
    - refactor createNotification to accept list of recipients' ids

--- functions/src/on-create-group.ts
+++ functions/src/on-create-group.ts
@@ -12,19 +12,17 @@ export const onCreateGroup = functions.firestore
     const groupCreator = await getUser(group.creatorId)
     if (!groupCreator) throw new Error('Could not find group creator')
     // create notifications for all members of the group
-    for (const memberId of group.memberIds) {
-      await createNotification(
-        group.id,
-        'group',
-        'created',
-        groupCreator,
-        eventId,
-        group.about,
-        {
-          relatedUserId: memberId,
-          slug: group.slug,
-          title: group.name,
-        }
-      )
-    }
+    await createNotification(
+      group.id,
+      'group',
+      'created',
+      groupCreator,
+      eventId,
+      group.about,
+      {
+        recipients: group.memberIds,
+        slug: group.slug,
+        title: group.name,
+      }
+    )
   })
