6f196c7518221c85bbdbc1da06705f05a03d3828
James Grugett <jahooma@gmail.com>
2022-08-07 08:23:49 -0700

    Use S$

--- web/components/play-money-disclaimer.tsx
+++ web/components/play-money-disclaimer.tsx
@@ -1,9 +1,10 @@
+import { ENV_CONFIG } from 'common/envs/constants'
 import { InfoBox } from './info-box'
 
 export const PlayMoneyDisclaimer = () => (
   <InfoBox
     title="Play-money betting"
     className="mt-4 max-w-md"
-    text="Mana (M$) is the play-money used by our platform to keep track of your bets. It's completely free for you and your friends to get started!"
+    text={`Mana (${ENV_CONFIG.moneyMoniker}) is the play-money used by our platform to keep track of your bets. It's completely free for you and your friends to get started!`}
   />
 )
