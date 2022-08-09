564916134834fab8a9779a58090bda427bdc7a98
Marshall Polaris <marshall@pol.rs>
2022-08-08 22:42:52 -0700

    Pass page props user to auth provider if present (#724)
    * Pass page props user to auth provider if present
    
    * Rename `user` -> `serverUser`
    
    * Don't load from local storage if server told us a user

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -79,7 +79,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
       </Head>
-      <AuthProvider>
+      <AuthProvider serverUser={pageProps.user}>
         <QueryClientProvider client={queryClient}>
           <Welcome {...pageProps} />
           <Component {...pageProps} />
