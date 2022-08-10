dc26db28642fb04d436148f62ba580c8f2807df6
mantikoros <sgrugett@gmail.com>
2022-08-10 12:17:33 -0500

    add salem to sidebar; clean up code

--- common/util/array.ts
+++ common/util/array.ts
@@ -1,3 +1,19 @@
 export function filterDefined<T>(array: (T | null | undefined)[]) {
   return array.filter((item) => item !== null && item !== undefined) as T[]
 }
+
+export function buildArray<T>(
+  ...params: (T | T[] | false | undefined | null)[]
+) {
+  const array: T[] = []
+
+  for (const el of params) {
+    if (Array.isArray(el)) {
+      array.push(...el)
+    } else if (el) {
+      array.push(el)
+    }
+  }
+
+  return array
+}
