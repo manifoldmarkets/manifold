ee8abccb8aa3c3a168c103f578d154bc723670d4
Marshall Polaris <marshall@pol.rs>
2022-08-11 12:54:57 -0700

    Clean up debug logging

--- web/components/fullscreen-confetti.tsx
+++ web/components/fullscreen-confetti.tsx
@@ -3,6 +3,5 @@ import { useWindowSize } from 'web/hooks/use-window-size'
 
 export function FullscreenConfetti(props: ConfettiProps) {
   const { width, height } = useWindowSize()
-  console.log('Width: ', width, 'Height: ', height)
   return <Confetti {...props} width={width} height={height} />
 }
