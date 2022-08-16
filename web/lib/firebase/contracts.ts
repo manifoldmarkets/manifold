e29c3e5f99169ab6b4a037da6083001d3e1db119
Ian Philips <iansphilips@gmail.com>
2022-08-15 19:34:45 -0600

    Signed out home page shows dynamic trending markets

--- web/lib/firebase/contracts.ts
+++ web/lib/firebase/contracts.ts
@@ -266,12 +266,16 @@ export function listenForHotContracts(
   })
 }
 
-export async function getHotContracts() {
-  const data = await getValues<Contract>(hotContractsQuery)
-  return sortBy(
-    chooseRandomSubset(data, 10),
-    (contract) => -1 * contract.volume24Hours
-  )
+const trendingContractsQuery = query(
+  contracts,
+  where('isResolved', '==', false),
+  where('visibility', '==', 'public'),
+  orderBy('popularityScore', 'desc'),
+  limit(10)
+)
+
+export async function getTrendingContracts() {
+  return await getValues<Contract>(trendingContractsQuery)
 }
 
 export async function getContractsBySlugs(slugs: string[]) {
