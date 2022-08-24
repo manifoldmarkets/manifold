78780a92199d233004cfec489bdc122e0a4f9a7c
James Grugett <jahooma@gmail.com>
2022-08-23 19:25:57 -0500

    Dedup contract leaderboards code from contract slug (merge error?)

--- web/components/contract/contract-leaderboard.tsx
+++ web/components/contract/contract-leaderboard.tsx
@@ -49,7 +49,7 @@ export function ContractLeaderboard(props: {
 
   return users && users.length > 0 ? (
     <Leaderboard
-      title="🏅 Top traders"
+      title="🏅 Top bettors"
       users={users || []}
       columns={[
         {
