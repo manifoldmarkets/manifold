d07e642aaf83dcca3a8fb9b5894c58dee87c01b1
James Grugett <jahooma@gmail.com>
2022-08-26 23:10:10 -0500

    Fix floating button to be on top of quick bet arrows. Switch icon.

--- web/pages/home.tsx
+++ web/pages/home.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import { PlusSmIcon } from '@heroicons/react/solid'
+import { PencilAltIcon } from '@heroicons/react/solid'
 
 import { Page } from 'web/components/page'
 import { Col } from 'web/components/layout/col'
@@ -50,13 +50,13 @@ const Home = (props: { auth: { user: User } | null }) => {
         </Col>
         <button
           type="button"
-          className="fixed bottom-[70px] right-3 inline-flex items-center rounded-full border border-transparent bg-indigo-600 p-3 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 lg:hidden"
+          className="fixed bottom-[70px] right-3 z-20 inline-flex items-center rounded-full border border-transparent bg-indigo-600 p-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 lg:hidden"
           onClick={() => {
             router.push('/create')
             track('mobile create button')
           }}
         >
-          <PlusSmIcon className="h-8 w-8" aria-hidden="true" />
+          <PencilAltIcon className="h-7 w-7" aria-hidden="true" />
         </button>
       </Page>
 
