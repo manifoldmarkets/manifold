f83b62cf50aab0a60e7bde567afd8a08a231d3d0
James Grugett <jahooma@gmail.com>
2022-08-30 16:18:39 -0500

    Implement double carousel for /experimental/home

--- web/pages/experimental/home.tsx
+++ web/pages/experimental/home.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useRouter } from 'next/router'
+import Router from 'next/router'
 import { PlusSmIcon } from '@heroicons/react/solid'
 
 import { Page } from 'web/components/page'
@@ -17,7 +17,13 @@ import { Button } from 'web/components/button'
 import { Spacer } from 'web/components/layout/spacer'
 import { useMemberGroups } from 'web/hooks/use-group'
 import { Group } from 'common/group'
-import { Title } from 'web/components/title'
+import { Carousel } from 'web/components/carousel'
+import { LoadingIndicator } from 'web/components/loading-indicator'
+import { ContractCard } from 'web/components/contract/contract-card'
+import { range } from 'lodash'
+import { Subtitle } from 'web/components/subtitle'
+import { Contract } from 'common/contract'
+import { ShowTime } from 'web/components/contract/contract-details'
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const creds = await authenticateOnServer(ctx)
@@ -28,7 +34,6 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 const Home = (props: { auth: { user: User } | null }) => {
   const user = props.auth ? props.auth.user : null
 
-  const router = useRouter()
   useTracking('view home')
 
   useSaveReferral()
@@ -39,7 +44,7 @@ const Home = (props: { auth: { user: User } | null }) => {
 
   return (
     <Page>
-      <Col className="mx-auto mb-8 w-full">
+      <Col className="mx-4 mt-4 gap-2 sm:mx-10 xl:w-[125%]">
         <SearchSection label="Trending" sort="score" user={user} />
         <SearchSection label="Newest" sort="newest" user={user} />
         <SearchSection label="Closing soon" sort="close-date" user={user} />
@@ -51,7 +56,7 @@ const Home = (props: { auth: { user: User } | null }) => {
         type="button"
         className="fixed bottom-[70px] right-3 inline-flex items-center rounded-full border border-transparent bg-indigo-600 p-3 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 lg:hidden"
         onClick={() => {
-          router.push('/create')
+          Router.push('/create')
           track('mobile create button')
         }}
       >
@@ -68,21 +73,25 @@ function SearchSection(props: {
 }) {
   const { label, user, sort } = props
 
-  const router = useRouter()
-
   return (
     <Col>
-      <Title className="mx-2 !text-gray-800 sm:mx-0" text={label} />
-      <Spacer h={2} />
-      <ContractSearch user={user} defaultSort={sort} maxItems={4} noControls />
-      <Button
-        className="self-end"
-        color="blue"
-        size="sm"
-        onClick={() => router.push(`/home?s=${sort}`)}
-      >
-        See more
-      </Button>
+      <Subtitle className="mx-2 !mt-2 !text-gray-800 sm:mx-0" text={label} />
+      <ContractSearch
+        user={user}
+        defaultSort={sort}
+        maxItems={12}
+        noControls
+        renderContracts={(contracts) =>
+          contracts ? (
+            <DoubleCarousel
+              contracts={contracts}
+              seeMoreUrl={`/home?s=${sort}`}
+            />
+          ) : (
+            <LoadingIndicator />
+          )
+        }
+      />
     </Col>
   )
 }
@@ -90,28 +99,73 @@ function SearchSection(props: {
 function GroupSection(props: { group: Group; user: User | null }) {
   const { group, user } = props
 
-  const router = useRouter()
-
   return (
     <Col className="">
-      <Title className="mx-2 !text-gray-800 sm:mx-0" text={group.name} />
+      <Subtitle className="mx-2 !text-gray-800 sm:mx-0" text={group.name} />
       <Spacer h={2} />
       <ContractSearch
         user={user}
         defaultSort={'score'}
         additionalFilter={{ groupSlug: group.slug }}
-        maxItems={4}
+        maxItems={12}
         noControls
+        renderContracts={(contracts) =>
+          contracts ? (
+            <DoubleCarousel
+              contracts={contracts}
+              seeMoreUrl={`/group/${group.slug}`}
+            />
+          ) : (
+            <LoadingIndicator />
+          )
+        }
       />
+    </Col>
+  )
+}
+
+function DoubleCarousel(props: {
+  contracts: Contract[]
+  seeMoreUrl?: string
+  showTime?: ShowTime
+}) {
+  const { contracts, seeMoreUrl, showTime } = props
+  return (
+    <Carousel className="-mx-4 mt-2 sm:-mx-10">
+      <div className="shrink-0 sm:w-6" />
+      {contracts &&
+        range(0, Math.floor(contracts.length / 2)).map((col) => {
+          const i = col * 2
+          return (
+            <Col>
+              <ContractCard
+                key={contracts[i].id}
+                contract={contracts[i]}
+                className="mb-2 max-h-[200px] w-96 shrink-0"
+                questionClass="line-clamp-3"
+                trackingPostfix=" tournament"
+                showTime={showTime}
+              />
+              <ContractCard
+                key={contracts[i + 1].id}
+                contract={contracts[i + 1]}
+                className="mb-2 max-h-[200px] w-96 shrink-0"
+                questionClass="line-clamp-3"
+                trackingPostfix=" tournament"
+                showTime={showTime}
+              />
+            </Col>
+          )
+        })}
       <Button
-        className="mr-2 self-end"
+        className="self-center whitespace-nowrap"
         color="blue"
         size="sm"
-        onClick={() => router.push(`/group/${group.slug}`)}
+        onClick={() => seeMoreUrl && Router.push(seeMoreUrl)}
       >
         See more
       </Button>
-    </Col>
+    </Carousel>
   )
 }
 
