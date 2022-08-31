f6e5fe8a5e6fbebfe077d1e6501fcddcff193830
mantikoros <sgrugett@gmail.com>
2022-08-31 01:10:59 -0500

    use user id instead of bot id, add manifold api endpoint

--- functions/src/save-twitch-credentials.ts
+++ functions/src/save-twitch-credentials.ts
@@ -1,22 +1,20 @@
 import * as admin from 'firebase-admin'
 import { z } from 'zod'
 
-import { APIError, newEndpoint, validate } from './api'
+import { newEndpoint, validate } from './api'
 
 const bodySchema = z.object({
-  userId: z.string(),
   twitchInfo: z.object({
     twitchName: z.string(),
     controlToken: z.string(),
   }),
 })
 
-const BOT_ID = 'BOT_ID'
 
 export const savetwitchcredentials = newEndpoint({}, async (req, auth) => {
-  if (auth.uid !== BOT_ID) throw new APIError(403, 'Invalid access credentials')
+  const { twitchInfo } = validate(bodySchema, req.body)
+  const userId = auth.uid
 
-  const { userId, twitchInfo } = validate(bodySchema, req.body)
   await firestore.doc(`private-users/${userId}`).update({ twitchInfo })
   return { success: true }
 })
