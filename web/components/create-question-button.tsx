4c91c9bf3f1b907f3b396d388b9a3119d4ad55f1
Marshall Polaris <marshall@pol.rs>
2022-09-02 01:22:19 -0700

    Tidy up some dead code and markup in sidebar

--- web/components/create-question-button.tsx
+++ web/components/create-question-button.tsx
@@ -1,27 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
-import clsx from 'clsx'
-
-import { User } from 'web/lib/firebase/users'
 import { Button } from './button'
 
-export const CreateQuestionButton = (props: {
-  user: User | null | undefined
-  overrideText?: string
-  className?: string
-  query?: string
-}) => {
-  const { user, overrideText, className, query } = props
-
-  if (!user || user?.isBannedFromPosting) return <></>
-
+export const CreateQuestionButton = () => {
   return (
-    <div className={clsx('flex justify-center', className)}>
-      <Link href={`/create${query ? query : ''}`} passHref>
-        <Button color="gradient" size="xl" className="mt-4">
-          {overrideText ?? 'Create a market'}
-        </Button>
-      </Link>
-    </div>
+    <Link href="/create" passHref>
+      <Button color="gradient" size="xl" className="mt-4">
+        Create a market
+      </Button>
+    </Link>
   )
 }
