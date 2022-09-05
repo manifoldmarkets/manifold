9a49c0b8fe99435023a16873aea3b6211e42018b
mantikoros <sgrugett@gmail.com>
2022-09-05 13:33:58 -0500

    remove numeric, multiple choice markets from create market page

--- web/pages/create.tsx
+++ web/pages/create.tsx
@@ -290,9 +290,9 @@ export function NewContract(props: {
         }}
         choicesMap={{
           'Yes / No': 'BINARY',
-          'Multiple choice': 'MULTIPLE_CHOICE',
+          // 'Multiple choice': 'MULTIPLE_CHOICE',
           'Free response': 'FREE_RESPONSE',
-          Numeric: 'PSEUDO_NUMERIC',
+          // Numeric: 'PSEUDO_NUMERIC',
         }}
         isSubmitting={isSubmitting}
         className={'col-span-4'}
