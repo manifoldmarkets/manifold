66fa073d0528531c88eecf41d9eed547baf9a0be
James Grugett <jahooma@gmail.com>
2022-09-07 21:51:34 -0500

    Expand option for daily movers

--- web/components/contract/prob-change-table.tsx
+++ web/components/contract/prob-change-table.tsx
@@ -3,20 +3,22 @@ import { contractPath } from 'web/lib/firebase/contracts'
 import { CPMMContract } from 'common/contract'
 import { formatPercent } from 'common/util/format'
 import { useProbChanges } from 'web/hooks/use-prob-changes'
-import { SiteLink } from '../site-link'
+import { linkClass, SiteLink } from '../site-link'
 import { Col } from '../layout/col'
 import { Row } from '../layout/row'
+import { useState } from 'react'
 
 export function ProbChangeTable(props: { userId: string | undefined }) {
   const { userId } = props
 
   const changes = useProbChanges(userId ?? '')
+  const [expanded, setExpanded] = useState(false)
 
   if (!changes) {
     return null
   }
 
-  const count = 4
+  const count = expanded ? 16 : 4
 
   const { positiveChanges, negativeChanges } = changes
   const filteredPositiveChanges = positiveChanges.slice(0, count / 2)
@@ -27,40 +29,48 @@ export function ProbChangeTable(props: { userId: string | undefined }) {
   ]
 
   return (
-    <Row className="mb-4 w-full flex-wrap divide-x-2 rounded bg-white shadow-md">
-      <Col className="min-w-[300px] flex-1 divide-y">
-        {filteredChanges.slice(0, count / 2).map((contract) => (
-          <Row className="items-center hover:bg-gray-100">
-            <ProbChange
-              className="p-4 text-right text-xl"
-              contract={contract}
-            />
-            <SiteLink
-              className="p-4 pl-2 font-semibold text-indigo-700"
-              href={contractPath(contract)}
-            >
-              <span className="line-clamp-2">{contract.question}</span>
-            </SiteLink>
-          </Row>
-        ))}
+    <Col>
+      <Col className="mb-4 w-full divide-x-2 divide-y rounded bg-white shadow-md md:flex-row md:divide-y-0">
+        <Col className="flex-1 divide-y">
+          {filteredChanges.slice(0, count / 2).map((contract) => (
+            <Row className="items-center hover:bg-gray-100">
+              <ProbChange
+                className="p-4 text-right text-xl"
+                contract={contract}
+              />
+              <SiteLink
+                className="p-4 pl-2 font-semibold text-indigo-700"
+                href={contractPath(contract)}
+              >
+                <span className="line-clamp-2">{contract.question}</span>
+              </SiteLink>
+            </Row>
+          ))}
+        </Col>
+        <Col className="flex-1 divide-y">
+          {filteredChanges.slice(count / 2).map((contract) => (
+            <Row className="items-center hover:bg-gray-100">
+              <ProbChange
+                className="p-4 text-right text-xl"
+                contract={contract}
+              />
+              <SiteLink
+                className="p-4 pl-2 font-semibold text-indigo-700"
+                href={contractPath(contract)}
+              >
+                <span className="line-clamp-2">{contract.question}</span>
+              </SiteLink>
+            </Row>
+          ))}
+        </Col>
       </Col>
-      <Col className="justify-content-stretch min-w-[300px] flex-1 divide-y">
-        {filteredChanges.slice(count / 2).map((contract) => (
-          <Row className="items-center hover:bg-gray-100">
-            <ProbChange
-              className="p-4 text-right text-xl"
-              contract={contract}
-            />
-            <SiteLink
-              className="p-4 pl-2 font-semibold text-indigo-700"
-              href={contractPath(contract)}
-            >
-              <span className="line-clamp-2">{contract.question}</span>
-            </SiteLink>
-          </Row>
-        ))}
-      </Col>
-    </Row>
+      <div
+        className={clsx(linkClass, 'cursor-pointer self-end')}
+        onClick={() => setExpanded(!expanded)}
+      >
+        {expanded ? 'Show less' : 'Show more'}
+      </div>
+    </Col>
   )
 }
 
