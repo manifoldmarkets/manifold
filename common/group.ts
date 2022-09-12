8e1ae2b7ffd51bb0ee65d883c740e568945a590c
FRC <pico2x@gmail.com>
2022-09-12 17:32:36 +0100

    Fix "500 internal error" in large groups (#856)
    This reverts commit 28f0c6b1f8f14bd210d02b6032967822d1779b3b.

--- common/group.ts
+++ common/group.ts
@@ -12,7 +12,18 @@ export type Group = {
   aboutPostId?: string
   chatDisabled?: boolean
   mostRecentContractAddedTime?: number
+  cachedLeaderboard?: {
+    topTraders: {
+      userId: string
+      score: number
+    }[]
+    topCreators: {
+      userId: string
+      score: number
+    }[]
+  }
 }
+
 export const MAX_GROUP_NAME_LENGTH = 75
 export const MAX_ABOUT_LENGTH = 140
 export const MAX_ID_LENGTH = 60
