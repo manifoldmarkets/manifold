78661f02425262d26db5ac50beb6e5418feacdec
Ian Philips <iansphilips@gmail.com>
2022-09-15 15:51:39 -0600

    Capitalize

--- web/components/contract/contract-info-dialog.tsx
+++ web/components/contract/contract-info-dialog.tsx
@@ -19,6 +19,7 @@ import ShortToggle from '../widgets/short-toggle'
 import { DuplicateContractButton } from '../copy-contract-button'
 import { Row } from '../layout/row'
 import { BETTORS } from 'common/user'
+import { capitalize } from 'lodash'
 
 export const contractDetailsButtonClassName =
   'group flex items-center rounded-md px-3 py-2 text-sm font-medium cursor-pointer hover:bg-gray-100 text-gray-400 hover:text-gray-500'
@@ -136,7 +137,7 @@ export function ContractInfoDialog(props: {
               </tr> */}
 
               <tr>
-                <td>{BETTORS}</td>
+                <td>{capitalize(BETTORS)}</td>
                 <td>{bettorsCount}</td>
               </tr>
 
