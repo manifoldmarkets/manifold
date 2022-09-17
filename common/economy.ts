1b8fbba40665f8352862ee5d6c75f4d8f82b8037
mantikoros <sgrugett@gmail.com>
2022-09-17 14:37:14 -0500

    halve referral bonus

--- common/economy.ts
+++ common/economy.ts
@@ -7,7 +7,7 @@ export const FIXED_ANTE = econ?.FIXED_ANTE ?? 100
 export const STARTING_BALANCE = econ?.STARTING_BALANCE ?? 1000
 // for sus users, i.e. multiple sign ups for same person
 export const SUS_STARTING_BALANCE = econ?.SUS_STARTING_BALANCE ?? 10
-export const REFERRAL_AMOUNT = econ?.REFERRAL_AMOUNT ?? 500
+export const REFERRAL_AMOUNT = econ?.REFERRAL_AMOUNT ?? 250
 
 export const UNIQUE_BETTOR_BONUS_AMOUNT = econ?.UNIQUE_BETTOR_BONUS_AMOUNT ?? 10
 export const BETTING_STREAK_BONUS_AMOUNT =
