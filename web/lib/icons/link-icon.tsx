f111d6e24fbae91739365fdebdc093c422b9d907
James Grugett <jahooma@gmail.com>
2022-09-18 15:55:39 -0500

    Fix console errors from svg non-camelcase attributes

--- web/lib/icons/link-icon.tsx
+++ web/lib/icons/link-icon.tsx
@@ -8,9 +8,9 @@ export default function LinkIcon(props: React.SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
       <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
