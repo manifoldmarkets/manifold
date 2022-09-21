55ed04dea16289af66c9f22f54b0017d5d58ed1b
Phil <phil.bladen@gmail.com>
2022-09-21 18:30:20 +0100

    Enabled auto organise imports on save. Changed formatter to conform to eslint recommended standards. Added .gitattributes to force repository to use LF line endings.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,21 @@
 module.exports = {
-    root: true,
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        sourceType: "module",
-        ecmaFeatures: {
-            modules: true,
-        },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
     },
-    // plugins: ["@typescript-eslint"],
-    extends: [
-        // 'airbnb-base',
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-    ],
-    env: {
-        browser: true,
-        amd: true,
-        node: true,
-    },
-    rules: {
-        "no-unused-vars": 0,
-        "prefer-const": 1,
-    },
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb-base', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  rules: {
+    'no-unused-vars': 0,
+    'prefer-const': 1,
+  },
 };
