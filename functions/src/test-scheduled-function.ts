21f608b1b36d5d163ab99a0ac6f6654906be2136
Ian Philips <iansphilips@gmail.com>
2022-09-23 16:08:57 -0400

    Comments

--- functions/src/test-scheduled-function.ts
+++ functions/src/test-scheduled-function.ts
@@ -2,13 +2,14 @@ import { APIError, newEndpoint } from './api'
 import { sendPortfolioUpdateEmailsToAllUsers } from './weekly-portfolio-emails'
 import { isProd } from './utils'
 
+// Function for testing scheduled functions locally
 export const testscheduledfunction = newEndpoint(
   { method: 'GET', memory: '4GiB' },
   async (_req) => {
-    // Replace your function here
     if (isProd())
       throw new APIError(400, 'This function is only available in dev mode')
 
+    // Replace your function here
     await sendPortfolioUpdateEmailsToAllUsers()
 
     return { success: true }
