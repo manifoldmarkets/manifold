88873b3c0d198a8fa56ada6e42bd6c423bf66762
Barak Gila <barak@userleap.com>
2022-09-24 10:00:37 -0700

    basic sprig integration with possible page URL events

--- web/lib/service/analytics.ts
+++ web/lib/service/analytics.ts
@@ -12,6 +12,29 @@ init(ENV_CONFIG.amplitudeApiKey ?? '', undefined, { includeReferrer: true })
 
 export { track }
 
+// Integrate Sprig
+
+try {
+  (function(l,e,a,p) {
+    // @ts-expect-error
+    if (window.Sprig) return
+    // @ts-expect-error
+    window.Sprig = function(){S._queue.push(arguments)}
+    // @ts-expect-error
+    var S = window.Sprig;S.appId = a;S._queue = [];window.UserLeap=S
+    // @ts-expect-error
+    a=l.createElement('script')
+    // @ts-expect-error
+    a.async=1;a.src=e+'?id='+S.appId
+    // @ts-expect-error
+    p=l.getElementsByTagName('script')[0]
+    // @ts-expect-error
+    p.parentNode.insertBefore(a, p)
+  })(document, 'https://cdn.sprig.com/shim.js', ENV_CONFIG.sprigEnvironmentId)
+} catch (error) {
+  console.log('Error initializing Sprig, please complain to Barak', error)
+}
+
 // Convenience functions:
 
 export const trackCallback =
