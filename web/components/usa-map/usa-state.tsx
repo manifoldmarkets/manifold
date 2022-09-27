2f25093a51d843c9940af3841932690ed22b7377
mantikoros <sgrugett@gmail.com>
2022-09-27 16:10:43 -0400

    usa map

---
+++ web/components/usa-map/usa-state.tsx
@@ -0,0 +1,36 @@
+import { ClickHandler } from './usa-map'
+
+type USAStateProps = {
+  state: string
+  dimensions: string
+  fill: string
+  onClickState: ClickHandler
+  stateName: string
+  hideStateTitle?: boolean
+}
+export const USAState = ({
+  state,
+  dimensions,
+  fill,
+  onClickState,
+  stateName,
+  hideStateTitle,
+}: USAStateProps) => {
+  return (
+    <path
+      d={dimensions}
+      fill={fill}
+      data-name={state}
+      className={`${state} state hover:cursor-pointer hover:contrast-125`}
+      onClick={onClickState}
+      id={state}
+    >
+      <text>
+        <textPath xlinkHref={`#${state}`}>
+          {stateName}
+        </textPath>
+      </text>
+      {hideStateTitle ? null : <title>{stateName}</title>}
+    </path>
+  )
+}
