a6fc4b91f2476ebb2348a0e72df6d74992b8a58e
Pico2x <pico2x@gmail.com>
2022-09-28 00:59:24 -0400

    Create cowp.tsx

---
+++ web/pages/cowp.tsx
@@ -0,0 +1,11 @@
+import { Page } from 'web/components/page'
+
+const App = () => {
+  return (
+    <Page className="">
+      <img src="https://i.imgur.com/Lt54IiU.png" />
+    </Page>
+  )
+}
+
+export default App
