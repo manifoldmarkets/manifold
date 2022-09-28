4b131fad715bb206911abb3c3764cf4c674cf47a
Pico2x <pico2x@gmail.com>
2022-09-28 01:04:38 -0400

    Cowp SEO friendly

--- web/pages/cowp.tsx
+++ web/pages/cowp.tsx
@@ -1,9 +1,18 @@
+import Link from 'next/link'
 import { Page } from 'web/components/page'
+import { SEO } from 'web/components/SEO'
 
 const App = () => {
   return (
     <Page className="">
-      <img src="https://i.imgur.com/Lt54IiU.png" />
+      <SEO
+        title="COWP"
+        description="A picture of a cowpy cowp copwer cowp saying 'salutations'"
+        url="/cowp"
+      />
+      <Link href="https://en.wikipedia.org/wiki/Earl_Cowper">
+        <img src="https://i.imgur.com/Lt54IiU.png" />
+      </Link>
     </Page>
   )
 }
