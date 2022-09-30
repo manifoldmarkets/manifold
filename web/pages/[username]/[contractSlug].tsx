4178d3fd2269723fb9be509a998752949da6bf83
ingawei <ingawei@gmail.com>
2022-09-29 23:34:48 -0700

    added removal of event listener

--- web/pages/[username]/[contractSlug].tsx
+++ web/pages/[username]/[contractSlug].tsx
@@ -1,4 +1,4 @@
-import React, { memo, useCallback, useEffect, useMemo, useState } from 'react'
+import React, { memo, useEffect, useMemo, useState } from 'react'
 import { ArrowLeftIcon } from '@heroicons/react/outline'
 import dayjs from 'dayjs'
 
@@ -46,7 +46,6 @@ import { BetsSummary } from 'web/components/bet-summary'
 import { listAllComments } from 'web/lib/firebase/comments'
 import { ContractComment } from 'common/comment'
 import { ScrollToTopButton } from 'web/components/scroll-to-top-button'
-import { VisibilityObserver } from 'web/components/visibility-observer'
 
 export const getStaticProps = fromPropz(getStaticPropz)
 export async function getStaticPropz(props: {
