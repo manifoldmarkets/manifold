efc04989a8bc2d6e1e15bebe8c06f43f6c248c44
ingawei <ingawei@gmail.com>
2022-09-29 23:40:48 -0700

    made event listener passive

--- web/components/scroll-to-top-button.tsx
+++ web/components/scroll-to-top-button.tsx
@@ -16,7 +16,7 @@ export function ScrollToTopButton(props: { className?: string }) {
   }
 
   useEffect(() => {
-    window.addEventListener('scroll', onScroll)
+    window.addEventListener('scroll', onScroll, { passive: true })
 
     return () => {
       window.removeEventListener('scroll', onScroll)
