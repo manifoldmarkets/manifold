303b9727d706faef20120a0abe442a9b491de36e
Marshall Polaris <marshall@pol.rs>
2022-09-30 18:01:48 -0700

    Don't render stuff whenever window size changes (#978)

--- web/components/amount-input.tsx
+++ web/components/amount-input.tsx
@@ -5,7 +5,7 @@ import { formatMoney } from 'common/util/format'
 import { Col } from './layout/col'
 import { SiteLink } from './site-link'
 import { ENV_CONFIG } from 'common/envs/constants'
-import { useWindowSize } from 'web/hooks/use-window-size'
+import { useIsMobile } from 'web/hooks/use-is-mobile'
 import { Row } from './layout/row'
 
 export function AmountInput(props: {
@@ -36,8 +36,7 @@ export function AmountInput(props: {
     onChange(isInvalid ? undefined : amount)
   }
 
-  const { width } = useWindowSize()
-  const isMobile = (width ?? 0) < 768
+  const isMobile = useIsMobile(768)
 
   return (
     <>
