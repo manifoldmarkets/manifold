9e27051815d6856a27b93ad6fbbf5b99f2119c89
Ian Philips <iansphilips@gmail.com>
2022-10-03 09:36:49 -0600

    Show whether market is unlisted

--- web/components/widgets/short-toggle.tsx
+++ web/components/widgets/short-toggle.tsx
@@ -3,22 +3,27 @@ import { Switch } from '@headlessui/react'
 import clsx from 'clsx'
 
 export default function ShortToggle(props: {
-  enabled: boolean
-  setEnabled: (enabled: boolean) => void
+  on: boolean
+  setOn: (enabled: boolean) => void
+  disabled?: boolean
   onChange?: (enabled: boolean) => void
 }) {
-  const { enabled, setEnabled } = props
+  const { on, setOn, disabled } = props
 
   return (
     <Switch
-      checked={enabled}
+      disabled={disabled}
+      checked={on}
       onChange={(e: boolean) => {
-        setEnabled(e)
+        setOn(e)
         if (props.onChange) {
           props.onChange(e)
         }
       }}
-      className="group relative inline-flex h-5 w-10 flex-shrink-0 cursor-pointer items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      className={clsx(
+        'group relative inline-flex h-5 w-10 flex-shrink-0 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2',
+        !disabled ? 'cursor-pointer' : 'cursor-not-allowed opacity-50'
+      )}
     >
       <span className="sr-only">Use setting</span>
       <span
@@ -28,14 +33,14 @@ export default function ShortToggle(props: {
       <span
         aria-hidden="true"
         className={clsx(
-          enabled ? 'bg-indigo-600' : 'bg-gray-200',
+          on ? 'bg-indigo-600' : 'bg-gray-200',
           'pointer-events-none absolute mx-auto h-4 w-9 rounded-full transition-colors duration-200 ease-in-out'
         )}
       />
       <span
         aria-hidden="true"
         className={clsx(
-          enabled ? 'translate-x-5' : 'translate-x-0',
+          on ? 'translate-x-5' : 'translate-x-0',
           'pointer-events-none absolute left-0 inline-block h-5 w-5 transform rounded-full border border-gray-200 bg-white shadow ring-0 transition-transform duration-200 ease-in-out'
         )}
       />
