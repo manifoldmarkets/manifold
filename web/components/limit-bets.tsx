2b1ad9eea7172dbe040a0d3626191e722a14c4bc
mantikoros <sgrugett@gmail.com>
2022-10-03 23:56:39 -0500

    order book label

--- web/components/limit-bets.tsx
+++ web/components/limit-bets.tsx
@@ -182,7 +182,7 @@ export function OrderBookButton(props: {
         size="xs"
         color="blue"
       >
-        {limitBets.length} Limit orders
+        Order book ({limitBets.length})
       </Button>
 
       <Modal open={open} setOpen={setOpen} size="lg">
