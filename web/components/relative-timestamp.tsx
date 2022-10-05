db9ffe116cea5f68d67f05699c52022d7bbe62bd
James Grugett <jahooma@gmail.com>
2022-10-05 18:43:51 -0500

    Render timestamp only on client to prevent error of server not matching client

--- web/components/relative-timestamp.tsx
+++ web/components/relative-timestamp.tsx
@@ -1,15 +1,22 @@
 import { DateTimeTooltip } from './datetime-tooltip'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { fromNow } from 'web/lib/util/time'
 
 export function RelativeTimestamp(props: { time: number }) {
   const { time } = props
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    // Only render on client to prevent difference from server.
+    setIsClient(true)
+  }, [])
+
   return (
     <DateTimeTooltip
       className="ml-1 whitespace-nowrap text-gray-400"
       time={time}
     >
-      {fromNow(time)}
+      {isClient ? fromNow(time) : ''}
     </DateTimeTooltip>
   )
 }
