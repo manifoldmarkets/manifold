eba9c71fea6523d3e18a61b92c25f5c5f8c3fb62
James Grugett <jahooma@gmail.com>
2022-10-11 00:32:55 -0500

    Daily profit 💰 (#1023)
    * Daily profit client side
    
    * Filter out those where profit rounds to 0
    
    * Tabs to spaces

--- web/hooks/use-user.ts
+++ web/hooks/use-user.ts
@@ -1,10 +1,18 @@
 import { useContext } from 'react'
-import { useFirestoreDocumentData } from '@react-query-firebase/firestore'
-import { useQueryClient } from 'react-query'
+import {
+  useFirestoreDocumentData,
+  useFirestoreQueryData,
+} from '@react-query-firebase/firestore'
+import { useQuery, useQueryClient } from 'react-query'
 
 import { doc, DocumentData } from 'firebase/firestore'
 import { getUser, User, users } from 'web/lib/firebase/users'
 import { AuthContext } from 'web/components/auth-context'
+import { ContractMetrics } from 'common/calculate-metrics'
+import { getUserContractMetricsQuery } from 'web/lib/firebase/contract-metrics'
+import { getContractFromId } from 'web/lib/firebase/contracts'
+import { buildArray, filterDefined } from 'common/util/array'
+import { CPMMBinaryContract } from 'common/contract'
 
 export const useUser = () => {
   const authUser = useContext(AuthContext)
@@ -37,4 +45,46 @@ export const usePrefetchUsers = (userIds: string[]) => {
   return userIds.map((userId) =>
     queryClient.prefetchQuery(['users', userId], () => getUser(userId))
   )
+}
+
+export const useUserContractMetricsByProfit = (
+  userId: string,
+  count: number
+) => {
+  const positiveResult = useFirestoreQueryData<ContractMetrics>(
+    ['contract-metrics-descending', userId, count],
+    getUserContractMetricsQuery(userId, count, 'desc')
+  )
+  const negativeResult = useFirestoreQueryData<ContractMetrics>(
+    ['contract-metrics-ascending', userId, count],
+    getUserContractMetricsQuery(userId, count, 'asc')
+  )
+
+  const metrics = buildArray(positiveResult.data, negativeResult.data)
+  const contractIds = metrics.map((m) => m.contractId)
+
+  const contractResult = useQuery(['contracts', contractIds], () =>
+    Promise.all(contractIds.map(getContractFromId))
+  )
+  const contracts = contractResult.data
+
+  if (!positiveResult.data || !negativeResult.data || !contracts)
+    return undefined
+
+  const filteredContracts = filterDefined(contracts) as CPMMBinaryContract[]
+  const filteredMetrics = metrics.filter(
+    (m) => m.from && Math.abs(m.from.day.profit) >= 0.5
+  )
+  return { contracts: filteredContracts, metrics: filteredMetrics }
+}
+
+export const useUserContractMetrics = (userId = '_', contractId: string) => {
+  const result = useFirestoreDocumentData<DocumentData, ContractMetrics>(
+    ['user-contract-metrics', userId, contractId],
+    doc(users, userId, 'contract-metrics', contractId)
+  )
+
+  if (userId === '_') return undefined
+
+  return result.data
 }
