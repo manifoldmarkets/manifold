7a0efe22c9406d3754ceb1eea76df1ab0884c33e
marsteralex <bob.masteralex@gmail.com>
2022-10-11 23:33:04 -0400

    update jsons

--- web/public/mtg/importCards.py
+++ web/public/mtg/importCards.py
@@ -366,7 +366,7 @@ def write_image_uris(card_image_uris):
 
 if __name__ == "__main__":
     # uncomment this once in a while, but it's expensive to run
-    # fetch_and_write_initial_artist_query()
+    fetch_and_write_initial_artist_query()
 
     for category in allCategories:
         print(category)
@@ -375,4 +375,4 @@ if __name__ == "__main__":
         print(category)
         fetch_and_write_all_special(
             category, generate_initial_special_query(category))
-    # fetch_and_write_all_artist()
+    fetch_and_write_all_artist()
