b3c50986c4ddac9ca5e23a1c169e08a6903627cc
Ian Philips <iansphilips@gmail.com>
2022-10-14 14:02:40 -0600

    Works so well omg thank you lard

--- native/App.tsx
+++ native/App.tsx
@@ -8,73 +8,49 @@ import {
   signInWithCredential,
 } from 'firebase/auth'
 import { initializeApp } from 'firebase/app'
-import { doc, getFirestore, getDoc, updateDoc } from 'firebase/firestore'
 import Constants, { ExecutionEnvironment } from 'expo-constants'
 import 'expo-dev-client'
 import CookieManager from '@react-native-cookies/cookies'
+import { AUTH_COOKIE_NAME, ENV_CONFIG } from 'common/envs/constants'
 
 const TEN_YEARS_SECS = 60 * 60 * 24 * 365 * 10
 const isExpoClient =
   Constants.ExecutionEnvironment === ExecutionEnvironment.StoreClient
 // Initialize Firebase
-const app = initializeApp({
-  // prod:
-  // apiKey: 'AIzaSyDp3J57vLeAZCzxLD-vcPaGIkAmBoGOSYw',
-  // authDomain: 'mantic-markets.firebaseapp.com',
-  // projectId: 'mantic-markets',
-  // region: 'us-central1',
-  // storageBucket: 'mantic-markets.appspot.com',
-  // messagingSenderId: '128925704902',
-  // appId: '1:128925704902:web:f61f86944d8ffa2a642dc7',
-  // measurementId: 'G-SSFK1Q138D',
-
-  // dev:
-  apiKey: 'AIzaSyBoq3rzUa8Ekyo3ZaTnlycQYPRCA26VpOw',
-  authDomain: 'dev-mantic-markets.firebaseapp.com',
-  projectId: 'dev-mantic-markets',
-  storageBucket: 'dev-mantic-markets.appspot.com',
-  messagingSenderId: '134303100058',
-  appId: '1:134303100058:web:27f9ea8b83347251f80323',
-  measurementId: 'G-YJC9E37P37',
-})
-const firestore = getFirestore(app)
-
+console.log('using ', process.env.NEXT_PUBLIC_FIREBASE_ENV, 'env')
+console.log('env not switching? run `expo start --clear` and then try again')
+const isDev = process.env.NEXT_PUBLIC_FIREBASE_ENV === 'DEV'
+const app = initializeApp(ENV_CONFIG.firebaseConfig)
 // no other uri works for API requests due to CORS
 const uri = 'http://localhost:3000/'
 
 export default function App() {
   const [fbUser, setFbUser] = useState<string | null>()
-  const auth = getAuth(app)
-
-  // way to cache these credentials?
-  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    //prod:
-    // clientId: '128925704902-bpcbnlp2gt73au3rrjjtnup6cskr89p0.apps.googleusercontent.com',
-    //dev:
-    iosClientId:
-      '134303100058-pe0f0oc28cv4u7o3tf3m0021utva0u55.apps.googleusercontent.com',
-    expoClientId:
-      '134303100058-2uvio555s8mnhde20b4old97ptjnji3u.apps.googleusercontent.com',
-  })
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest(
+    isDev
+      ? {
+          //dev:
+          iosClientId:
+            '134303100058-pe0f0oc28cv4u7o3tf3m0021utva0u55.apps.googleusercontent.com',
+          expoClientId:
+            '134303100058-2uvio555s8mnhde20b4old97ptjnji3u.apps.googleusercontent.com',
+        }
+      : {
+          //prod:
+          iosClientId:
+            '128925704902-6ci48vjqud9ddcl436go5ma3m9ceei4k.apps.googleusercontent.com',
+          clientId:
+            '128925704902-bpcbnlp2gt73au3rrjjtnup6cskr89p0.apps.googleusercontent.com',
+        }
+  )
   const webview = useRef<WebView>()
-
-  const testFirestore = async () => {
-    const currentUserId = auth.currentUser
-    console.log('currentUserId', currentUserId)
-    // get user doc:
-    const userDoc = doc(firestore, 'users', currentUserId?.uid)
-    const userDocSnap = (await getDoc(userDoc)).data()
-    console.log('userDocSnap', userDocSnap)
-    await updateDoc(doc(firestore, 'users', '6hHpzvRG0pMq8PNJs7RZj2qlZGn2'), {
-      website: 'Ian ' + Date.now().toString(),
-    })
-  }
   const [hasInjectedVariable, setHasInjectedVariable] = useState(false)
   const useWebKit = true
   // We can't just login to google within the webview: see https://developers.googleblog.com/2021/06/upcoming-security-changes-to-googles-oauth-2.0-authorization-endpoint.html#instructions-ios
   useEffect(() => {
     if (response?.type === 'success') {
       const { id_token } = response.params
+      const auth = getAuth(app)
       const credential = GoogleAuthProvider.credential(id_token)
       signInWithCredential(auth, credential).then((result) => {
         const fbUser = result.user.toJSON()
@@ -95,7 +71,7 @@ export default function App() {
         CookieManager.set(
           uri,
           {
-            name: 'FBUSER_DEV_MANTIC_MARKETS',
+            name: AUTH_COOKIE_NAME,
             value: encodeURIComponent(fbUser),
             path: '/',
             expires: new Date(TEN_YEARS_SECS).toISOString(),
@@ -110,14 +86,7 @@ export default function App() {
   const handleMessage = ({ nativeEvent }) => {
     if (nativeEvent.data === 'googleLoginClicked') {
       console.log('googleLoginClicked')
-      if (!fbUser) {
-        promptAsync()
-      } else {
-        console.log('already logged in', fbUser)
-        webview.current.postMessage(
-          JSON.stringify({ type: 'nativeFbUser', data: fbUser })
-        )
-      }
+      promptAsync()
     } else if (nativeEvent.data.includes('user')) {
       // on reload the fb user from webview cache, set the fb user
       console.log('setting fb user from webciew cache')
--- native/metro.config.js
+++ native/metro.config.js
@@ -1,5 +1,38 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
-const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.assetExts.push("cjs");
-module.exports = defaultConfig;
+const { getDefaultConfig } = require('expo/metro-config')
+const defaultConfig = getDefaultConfig(__dirname)
+
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+const path = require('path')
+const extraNodeModules = {
+  common: path.resolve(__dirname + '/../common'),
+}
+const watchFolders = [path.resolve(__dirname + '/../common')]
+
+module.exports = {
+  ...defaultConfig,
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  resolver: {
+    extraNodeModules: new Proxy(extraNodeModules, {
+      get: (target, name) =>
+        //redirects dependencies referenced from common/ to local node_modules
+        name in target
+          ? target[name]
+          : path.join(process.cwd(), `node_modules/${name}`),
+    }),
+    assetExts: [...defaultConfig.resolver.assetExts, 'cjs'],
+  },
+  watchFolders,
+}
