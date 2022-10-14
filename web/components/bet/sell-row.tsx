1e6bd242649adb09a26820928018ca9d23f2bfdf
James Grugett <jahooma@gmail.com>
2022-10-14 17:22:10 -0500

    Move hook into /hooks

--- web/components/bet/sell-row.tsx
+++ web/components/bet/sell-row.tsx
@@ -6,7 +6,7 @@ import { Row } from '../layout/row'
 import { formatWithCommas } from 'common/util/format'
 import { OutcomeLabel } from '../outcome-label'
 import { useUserContractBets } from 'web/hooks/use-user-bets'
-import { useSaveBinaryShares } from '../use-save-binary-shares'
+import { useSaveBinaryShares } from '../../hooks/use-save-binary-shares'
 import { SellSharesModal } from './sell-modal'
 import { Button } from '../buttons/button'
 
