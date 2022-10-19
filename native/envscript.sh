f58080a0eb09830a196bd424ad314cbe8ca0d93b
Ian Philips <iansphilips@gmail.com>
2022-10-19 11:00:51 -0600

    Try new google oauth ids and .plists from firebase

--- native/envscript.sh
+++ native/envscript.sh
@@ -4,9 +4,9 @@ if [ "$NEXT_PUBLIC_FIREBASE_ENV" == "PROD"  ];
 then
   echo "Switching to Firebase Production environment"
   cp -a "configs/prod/." ./
-#  yes | cp -rf "js/config/firebase_production/GoogleService-Info.plist" ios/appfolder
+  mv "GoogleService-Info.plist" ios/Manifold
 else
   echo "Switching to Firebase Dev environment"
   cp -a "configs/dev/." ./
-#  yes | cp -rf "js/config/firebase_development/GoogleService-Info.plist" ios/appfolder
+  mv "GoogleService-Info.plist" ios/Manifold
 fi
