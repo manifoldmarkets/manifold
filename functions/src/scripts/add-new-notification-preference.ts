3993d01aeb2a91a802a5e678fc2f0804d1813ad5
Ian Philips <iansphilips@gmail.com>
2022-10-19 09:51:00 -0600

    Add mobile destination to notif settings and prefs

--- functions/src/scripts/add-new-notification-preference.ts
+++ functions/src/scripts/add-new-notification-preference.ts
@@ -2,22 +2,29 @@ import * as admin from 'firebase-admin'
 
 import { initAdmin } from './script-init'
 initAdmin()
-import { getAllPrivateUsers } from 'functions/src/utils'
-import { notification_preference } from 'common/user-notification-preferences'
+import { getAllPrivateUsers, isProd } from 'functions/src/utils'
+import {
+  getDefaultNotificationPreferences,
+  notification_preference,
+} from 'common/user-notification-preferences'
+import { filterDefined } from 'common/util/array'
 
 const firestore = admin.firestore()
 async function main() {
-  const privateUsers = await getAllPrivateUsers()
+  const privateUsers = filterDefined(await getAllPrivateUsers())
 
   // const privateUsers = filterDefined([
   //   await getPrivateUser('6hHpzvRG0pMq8PNJs7RZj2qlZGn2'),
   //   await getPrivateUser('eHn5FXMK1leAsVoEjqg6Mh9tGqn2'),
   // ])
+  const defaults = getDefaultNotificationPreferences(!isProd())
 
   await Promise.all(
     privateUsers.map((privateUser) => {
       if (!privateUser.id) return Promise.resolve()
       const prefs = privateUser.notificationPreferences
+        ? privateUser.notificationPreferences
+        : defaults
       const enablePushFor: Array<notification_preference> = [
         'all_replies_to_my_comments_on_watched_markets',
         'all_replies_to_my_answers_on_watched_markets',
@@ -27,11 +34,17 @@ async function main() {
         'probability_updates_on_watched_markets',
       ]
       enablePushFor.forEach((pref) => {
+        if (prefs[pref] === undefined) {
+          prefs[pref] = defaults[pref]
+        }
         const userInterestedInNotifs =
           (prefs[pref].includes('email') || prefs[pref].includes('browser')) &&
           !prefs[pref].includes('mobile')
         if (userInterestedInNotifs) prefs[pref].push('mobile')
       })
+      // They'll opt in when they allow push notifications
+      if (!prefs.opt_out_all.includes('mobile'))
+        prefs.opt_out_all.push('mobile')
 
       return firestore
         .collection('private-users')
