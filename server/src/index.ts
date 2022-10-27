92f41c7193026623ce5aa34cb3c7a220a2a45740
Phil <phil.bladen@gmail.com>
2022-10-27 16:30:28 +0100

    Uncaught exceptions result in a proper error being thrown before the application exits.

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,11 @@ import { exit } from 'process';
 import App from './app';
 import log from './logger';
 
+process.on('uncaughtException', function (e) {
+  log.trace(e);
+  exit(1);
+});
+
 const app = new App();
 app.launch().catch((e) => {
   log.trace(e);
