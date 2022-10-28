00edf3d9c578989a854be5ea28cccf3fed2a4d8f
Sinclair Chen <abc.sinclair@gmail.com>
2022-10-27 22:42:31 -0700

    Get rid of static buttons, replace with percent changes

--- web/pages/swipe/index.tsx
+++ web/pages/swipe/index.tsx
@@ -2,19 +2,9 @@ import clsx from 'clsx'
 import { getOutcomeProbabilityAfterBet } from 'common/calculate'
 import type { BinaryContract, Contract } from 'common/contract'
 import { formatMoney, formatPercent } from 'common/util/format'
-import {
-  Dispatch,
-  forwardRef,
-  SetStateAction,
-  useMemo,
-  useRef,
-  useState,
-} from 'react'
+import { useMemo, useRef, useState } from 'react'
 import TinderCard from 'react-tinder-card'
-import { getColor } from 'web/components/bet/quick-bet'
 import { Button } from 'web/components/buttons/button'
-import { Row } from 'web/components/layout/row'
-import { Spacer } from 'web/components/layout/spacer'
 import { Avatar } from 'web/components/widgets/avatar'
 import { RichContent } from 'web/components/widgets/editor'
 import { useWindowSize } from 'web/hooks/use-window-size'
@@ -39,14 +29,10 @@ export default function Swipe(props: { contracts: BinaryContract[] }) {
   const { contracts } = props
 
   const [index, setIndex] = useState(0)
-  const current = contracts[index]
-  const next = useMemo(
-    () => contracts.slice(index + 1, index + 4).reverse(),
+  const cards = useMemo(
+    () => contracts.slice(index, index + 4).reverse(),
     [contracts, index]
   )
-
-  console.log(current.question)
-  console.log(next.map((c) => c.question))
 
   // resize height manually for iOS
   const { height } = useWindowSize()
@@ -59,97 +45,96 @@ export default function Swipe(props: { contracts: BinaryContract[] }) {
       style={{ height }}
     >
       <div className="relative  mx-auto h-full max-w-lg">
-        {next.map((c) => (
-          <Card contract={c} key={c.id} />
+        {cards.map((c) => (
+          <Card
+            contract={c}
+            onLeave={() => setIndex((i) => i + 1)}
+            key={c.id}
+          />
         ))}
-        <Interface
-          contract={current}
-          onNext={() => setIndex((i) => i + 1)}
-          key={index}
-        />
       </div>
     </main>
   )
 }
 
+const betTapAdd = 10
+// const betHoldAdd = 100
+
 type CardRef = Parameters<typeof TinderCard>[0]['ref']
 
-const Card = forwardRef(
-  (
-    props: {
-      contract: BinaryContract
-      amount?: number
-      setAmount?: Dispatch<SetStateAction<number>>
-      onRight?: () => void
-      onLeft?: () => void
-      onLeave?: () => void
-    },
-    ref: CardRef
-  ) => {
-    const { contract, amount = 10, setAmount, onRight, onLeft, onLeave } = props
-    const { question, description, coverImageUrl } = contract
-    const isTop = !!setAmount
+const Card = (props: { contract: BinaryContract; onLeave?: () => void }) => {
+  const { contract, onLeave } = props
+  const { question, description, coverImageUrl, id } = contract
 
-    const image =
-      coverImageUrl ??
-      `https://picsum.photos/id/${parseInt(contract.id, 36) % 1000}/512`
+  const [amount, setAmount] = useState(10)
 
-    return (
-      <TinderCard
-        onSwipe={(direction) => {
-          if (direction === 'left') onLeft?.()
-          else if (direction === 'right') onRight?.()
-        }}
-        onCardLeftScreen={onLeave}
-        preventSwipe={['down']}
-        className={clsx('absolute cursor-grab', isTop ? 'inset-0' : 'inset-2')}
-        ref={ref}
-      >
-        <div className="h-full overflow-hidden rounded-2xl">
-          {/* background */}
-          <div className="flex h-full flex-col bg-black">
-            <div className="relative mb-24 grow">
-              <img src={image} alt="" className="h-full object-cover" />
-              <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent" />
-            </div>
-          </div>
-          {/* content */}
-          <div className="absolute inset-0 flex select-none flex-col items-center gap-4">
-            <CornerDetails contract={contract} />
-            {/* [text-shadow:red_1px_1px,#10b981_-1px_-1px] */}
-            <div className="line-clamp-4 mx-8 mt-auto mb-4 text-2xl text-white [text-shadow:black_1px_1px_4px] ">
-              {question}
-            </div>
-            <Percent contract={contract} />
-            {/* TODO: exclude embeds, images */}
-            <RichContent
-              content={description}
-              smallImage
-              className="mx-8"
-              proseClassName="prose-invert prose-sm text-greyscale-1 line-clamp-3"
-            />
-            <Button
-              size="2xl"
-              color="yellow"
-              onClick={() => setAmount?.((amount) => amount + betTapAdd)}
-              className="pressable"
-            >
-              {formatMoney(amount)}
-            </Button>
-            <Spacer h={28} />
+  const ref: CardRef = useRef(null)
+
+  const image =
+    coverImageUrl ??
+    `https://picsum.photos/id/${parseInt(contract.id, 36) % 1000}/512`
+
+  return (
+    <TinderCard
+      onSwipe={(direction) => {
+        if (direction === 'left') {
+          placeBet({ amount, outcome: 'NO', contractId: id })
+        } else if (direction === 'right') {
+          placeBet({ amount, outcome: 'YES', contractId: id })
+        }
+      }}
+      onCardLeftScreen={onLeave}
+      preventSwipe={['down']}
+      className={'absolute inset-2 cursor-grab last:inset-0'}
+      ref={ref}
+    >
+      <div className="h-full overflow-hidden rounded-2xl">
+        {/* background */}
+        <div className="flex h-full flex-col bg-black">
+          <div className="relative mb-24 grow">
+            <img src={image} alt="" className="h-full object-cover" />
+            <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent" />
           </div>
         </div>
-      </TinderCard>
-    )
-  }
-)
+        {/* content */}
+        <div className="absolute inset-0 flex select-none flex-col gap-4">
+          <CornerDetails contract={contract} />
+          <div className="line-clamp-4 mx-8 mt-auto mb-4 text-2xl text-white [text-shadow:black_1px_1px_4px] ">
+            {question}
+          </div>
+          <Percents
+            contract={contract}
+            amount={amount}
+            onLeft={() => ref.current?.swipe('left')}
+            onRight={() => ref.current?.swipe('right')}
+          />
+          {/* TODO: exclude embeds, images */}
+          <RichContent
+            content={description}
+            smallImage
+            className="mx-8"
+            proseClassName="prose-invert prose-sm text-greyscale-1 line-clamp-3"
+          />
+          <Button
+            size="2xl"
+            color="yellow"
+            onClick={() => setAmount?.((amount) => amount + betTapAdd)}
+            className="pressable mb-6 self-center"
+          >
+            {formatMoney(amount)}
+          </Button>
+        </div>
+      </div>
+    </TinderCard>
+  )
+}
 
 const CornerDetails = (props: { contract: Contract }) => {
   const { contract } = props
   const { creatorName, creatorAvatarUrl, closeTime } = contract
 
   return (
-    <div className="m-3 flex gap-2 self-start drop-shadow">
+    <div className="m-3 flex gap-2 drop-shadow">
       <Avatar size="sm" avatarUrl={creatorAvatarUrl} noLink />
       <div className="text-xs">
         <div className="text-white">{creatorName} </div>
@@ -163,75 +148,36 @@ const CornerDetails = (props: { contract: Contract }) => {
   )
 }
 
-// TODO: scale based on user's balance so they never run out
-const betTapAdd = 10
-// const betHoldAdd = 100
-
-function Interface(props: { contract: BinaryContract; onNext: () => void }) {
-  const { contract, onNext } = props
-
-  const [amount, setAmount] = useState(10)
-  const cardRef: CardRef = useRef(null)
+const Percents = (props: {
+  contract: BinaryContract
+  amount: number
+  onLeft: () => void
+  onRight: () => void
+}) => {
+  const { contract, amount, onLeft, onRight } = props
+  const percent = getBinaryProbPercent(contract)
 
   return (
-    <>
-      <Card
-        contract={contract}
-        amount={amount}
-        setAmount={setAmount}
-        onLeft={() =>
-          placeBet({ amount, outcome: 'NO', contractId: contract.id })
-        }
-        onRight={() =>
-          placeBet({ amount, outcome: 'YES', contractId: contract.id })
-        }
-        onLeave={onNext}
-        ref={cardRef}
-      />
-      <section className="absolute bottom-0 left-0 right-0 flex flex-col items-center">
-        <Row className="mb-2 items-start justify-evenly self-stretch text-lg text-white">
-          <div className="flex flex-col items-end">
-            <button
-              className="rounded-l-full border-2 border-red-400 py-4 pl-7 pr-5 text-2xl uppercase text-red-400 hover:bg-red-900 focus:bg-red-900"
-              onClick={() => cardRef.current?.swipe('left')}
-            >
-              No
-            </button>
-            {formatPercent(
-              getOutcomeProbabilityAfterBet(contract, 'NO', amount)
-            )}{' '}
-            ←
-          </div>
-
-          <button
-            className="border-2 border-blue-400 py-4 px-5 text-2xl uppercase text-blue-400 hover:bg-blue-900 focus:bg-blue-900"
-            onClick={() => cardRef.current?.swipe('up')}
-          >
-            Skip
-          </button>
-
-          <div className="flex flex-col items-start ">
-            <button
-              className="text-teal rounded-r-full border-2 border-teal-500 py-4 pr-6 pl-5 text-2xl uppercase text-teal-500 hover:bg-teal-900 focus:bg-teal-900"
-              onClick={() => cardRef.current?.swipe('right')}
-            >
-              Yes
-            </button>
-            →{' '}
-            {formatPercent(
-              getOutcomeProbabilityAfterBet(contract, 'YES', amount)
-            )}
-          </div>
-        </Row>
-      </section>
-    </>
+    <div className="flex justify-evenly text-3xl font-semibold">
+      <button
+        className="pressable text-scarlet-200 flex items-center p-4 [text-shadow:#991600_4px_-2px]"
+        onClick={onLeft}
+      >
+        {formatPercent(
+          1 - getOutcomeProbabilityAfterBet(contract, 'NO', amount)
+        )}{' '}
+        ←
+      </button>
+      <div className="text-7xl text-white [text-shadow:#4337c9_0_6px]">
+        {percent}
+      </div>{' '}
+      <button
+        className="pressable flex items-center p-4 text-teal-200 [text-shadow:#0f766e_-4px_2px]"
+        onClick={onRight}
+      >
+        →{' '}
+        {formatPercent(getOutcomeProbabilityAfterBet(contract, 'YES', amount))}
+      </button>
+    </div>
   )
-}
-
-const Percent = (props: { contract: BinaryContract }) => {
-  const { contract } = props
-  const percent = getBinaryProbPercent(contract)
-  const textColor = `text-${getColor(contract)}`
-
-  return <div className={clsx(textColor, 'text-6xl ')}>{percent}</div>
 }
