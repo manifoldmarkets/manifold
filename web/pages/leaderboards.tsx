6350d7763a1da28ee0a3f376d745582d55130fa4
Ian Philips <iansphilips@gmail.com>
2022-10-28 17:16:46 -0600

    Show current user's rank

--- web/pages/leaderboards.tsx
+++ web/pages/leaderboards.tsx
@@ -15,6 +15,7 @@ import { Tabs } from 'web/components/layout/tabs'
 import { useTracking } from 'web/hooks/use-tracking'
 import { SEO } from 'web/components/SEO'
 import { BETTORS } from 'common/user'
+import { useUser } from 'web/hooks/use-user'
 
 export async function getStaticProps() {
   const props = await fetchProps()
@@ -67,27 +68,62 @@ export default function Leaderboards(_props: {
   topFollowed: User[]
 }) {
   const [props, setProps] = useState<Parameters<typeof Leaderboards>[0]>(_props)
+  const user = useUser()
   useEffect(() => {
-    fetchProps().then((props) => setProps(props))
-  }, [])
+    fetchProps().then((props) => {
+      if (!user || !user.profitRankCached) {
+        setProps(props)
+        return
+      }
+      // check if user is in each of leaderboard categories
+      const { allTime, monthly, weekly, daily, topFollowed } = props
+      let leaderboards = [allTime, monthly, weekly, daily]
+      // check each type of leaderboard for user, if they're not in it, add them
+      leaderboards = leaderboards.map((leaderboard) => {
+        const { topTraders, topCreators } = leaderboard
+        if (!topTraders.find((u) => u.id === user?.id)) {
+          topTraders.push(user)
+        }
+        return { topTraders, topCreators }
+      })
+      setProps({
+        allTime: leaderboards[0],
+        monthly: leaderboards[1],
+        weekly: leaderboards[2],
+        daily: leaderboards[3],
+        topFollowed,
+      })
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    user?.profitRankCached?.allTime,
+    user?.profitRankCached?.monthly,
+    user?.profitRankCached?.weekly,
+    user?.profitRankCached?.daily,
+  ])
 
   const { topFollowed } = props
 
   const LeaderboardWithPeriod = (period: Period) => {
     const { topTraders, topCreators } = props[period]
+    const user = useUser()
 
     return (
       <>
         <Col className="mx-4 items-center gap-10 lg:flex-row">
           <Leaderboard
             title={`🏅 Top ${BETTORS}`}
-            entries={topTraders}
+            entries={topTraders.map((user) => ({
+              ...user,
+              rank: user.profitRankCached?.[period],
+            }))}
             columns={[
               {
                 header: 'Total profit',
                 renderCell: (user) => formatMoney(user.profitCached[period]),
               },
             ]}
+            highlightUsername={user?.username}
           />
 
           <Leaderboard
@@ -100,6 +136,7 @@ export default function Leaderboards(_props: {
                   formatMoney(user.creatorVolumeCached[period]),
               },
             ]}
+            highlightUsername={user?.username}
           />
         </Col>
         {period === 'allTime' ? (
@@ -113,6 +150,7 @@ export default function Leaderboards(_props: {
                   renderCell: (user) => user.followerCountCached,
                 },
               ]}
+              highlightUsername={user?.username}
             />
           </Col>
         ) : (
