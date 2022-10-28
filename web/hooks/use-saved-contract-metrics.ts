c88c7409598a7229a3c377fd5685a92c82cc41e4
James Grugett <jahooma@gmail.com>
2022-10-28 15:07:52 -0500

    Also save contract metrics in memory

--- web/hooks/use-saved-contract-metrics.ts
+++ web/hooks/use-saved-contract-metrics.ts
@@ -1,15 +1,18 @@
 import { Bet } from 'common/bet'
 import { getContractBetMetrics } from 'common/calculate'
 import { Contract } from 'common/contract'
-import { useState } from 'react'
 import { safeLocalStorage } from 'web/lib/util/local'
 import { useEffectCheckEquality } from './use-effect-check-equality'
+import { inMemoryStore, usePersistentState } from './use-persistent-state'
 
 export const useSavedContractMetrics = (
   contract: Contract,
   userBets: Bet[] | undefined
 ) => {
-  const [savedMetrics, setSavedMetrics] = useState(undefined)
+  const [savedMetrics, setSavedMetrics] = usePersistentState(undefined, {
+    key: `contract-metrics-${contract.id}`,
+    store: inMemoryStore(),
+  })
 
   const metrics = userBets
     ? getContractBetMetrics(contract, userBets)
