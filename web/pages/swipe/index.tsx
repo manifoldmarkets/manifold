5912ecc31db33edae63b1cca68ba87b219a5191a
Sinclair Chen <abc.sinclair@gmail.com>
2022-10-28 19:22:34 -0700

    Show full question, comments on peek

--- web/pages/swipe/index.tsx
+++ web/pages/swipe/index.tsx
@@ -5,6 +5,7 @@ import { richTextToString } from 'common/util/parse'
 import { useMemo, useState } from 'react'
 import TinderCard from 'react-tinder-card'
 import { Avatar } from 'web/components/widgets/avatar'
+import { RichContent } from 'web/components/widgets/editor'
 
 import { useWindowSize } from 'web/hooks/use-window-size'
 import { placeBet } from 'web/lib/firebase/api'
@@ -41,10 +42,10 @@ export default function Swipe(props: { contracts: BinaryContract[] }) {
 
   return (
     <main
-      className="h-screen overflow-hidden overscroll-none lg:py-6"
+      className="bg-greyscale-1 h-screen overflow-hidden overscroll-none lg:py-6"
       style={{ height }}
     >
-      <div className="relative  mx-auto h-full max-w-lg">
+      <div className="relative mx-auto h-full max-w-lg">
         {cards.map((c) => (
           <Card
             contract={c}
@@ -71,61 +72,69 @@ const Card = (props: { contract: BinaryContract; onLeave?: () => void }) => {
     coverImageUrl ??
     `https://picsum.photos/id/${parseInt(contract.id, 36) % 1000}/512`
 
+  const [peek, setPeek] = useState(false)
+
   return (
-    <TinderCard
-      onSwipe={async (direction) => {
-        if (direction === 'left' || direction === 'right') {
-          const outcome = direction === 'left' ? 'NO' : 'YES'
-          await placeBet({ amount, outcome, contractId })
-          track('bet', {
-            location: 'swipe',
-            outcomeType: 'BINARY',
-            slug: contract.slug,
-            contractId,
-            amount,
-            outcome,
-            isLimitOrder: false,
-          })
-        }
-      }}
-      onCardLeftScreen={onLeave}
-      preventSwipe={['down']}
-      className={'absolute inset-0 cursor-grab [&>*]:last:scale-100'}
-    >
-      <div className="h-full scale-95 overflow-hidden rounded-2xl transition-transform">
-        {/* background */}
-        <div className="flex h-full flex-col bg-black">
-          <div className="relative mb-24 grow">
-            <img src={image} alt="" className="h-full object-cover" />
-            <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent" />
+    <>
+      {peek && <Peek contract={contract} onClose={() => setPeek(false)} />}
+      <TinderCard
+        onSwipe={async (direction) => {
+          if (direction === 'left' || direction === 'right') {
+            const outcome = direction === 'left' ? 'NO' : 'YES'
+            await placeBet({ amount, outcome, contractId })
+            track('bet', {
+              location: 'swipe',
+              outcomeType: 'BINARY',
+              slug: contract.slug,
+              contractId,
+              amount,
+              outcome,
+              isLimitOrder: false,
+            })
+          }
+          if (direction === 'down') {
+            setPeek(true)
+          }
+        }}
+        onCardLeftScreen={onLeave}
+        preventSwipe={['down']}
+        className={'absolute inset-2 cursor-grab [&>*]:last:scale-100'}
+      >
+        <div className="h-full scale-95 overflow-hidden rounded-2xl transition-transform">
+          {/* background */}
+          <div className="flex h-full flex-col bg-black">
+            <div className="relative mb-24 grow">
+              <img src={image} alt="" className="h-full object-cover" />
+              <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent" />
+            </div>
+          </div>
+          {/* content */}
+          <div className="absolute inset-0 flex select-none flex-col gap-4">
+            <CornerDetails contract={contract} />
+            <div className="line-clamp-4 mx-8 mt-auto mb-4 text-2xl text-white [text-shadow:black_1px_1px_4px] ">
+              {question}
+            </div>
+            <Percents contract={contract} amount={amount} />
+            {/* TODO: use editor excluding widgets */}
+            <div className="prose prose-invert prose-sm text-greyscale-1 line-clamp-3 mx-8">
+              {typeof description === 'string'
+                ? description
+                : richTextToString(description)}
+            </div>
+            <div className="mb-4 flex flex-col items-center gap-2 self-center text-yellow-100">
+              Swipe ⭤ to bet
+              <button
+                onClick={onClickMoney}
+                onTouchStart={onClickMoney}
+                className="rounded-full border border-yellow-400 px-12 py-4 font-bold text-yellow-300 transition-colors focus:bg-yellow-200/20 active:bg-yellow-400 active:text-white"
+              >
+                {formatMoney(amount)}
+              </button>
+            </div>
           </div>
         </div>
-        {/* content */}
-        <div className="absolute inset-0 flex select-none flex-col gap-4">
-          <CornerDetails contract={contract} />
-          <div className="line-clamp-4 mx-8 mt-auto mb-4 text-2xl text-white [text-shadow:black_1px_1px_4px] ">
-            {question}
-          </div>
-          <Percents contract={contract} amount={amount} />
-          {/* TODO: use editor excluding widgets */}
-          <div className="prose prose-invert prose-sm text-greyscale-1 line-clamp-3 mx-8">
-            {typeof description === 'string'
-              ? description
-              : richTextToString(description)}
-          </div>
-          <div className="mb-4 flex flex-col items-center gap-2 self-center text-yellow-100">
-            Swipe ⭤ to bet
-            <button
-              onClick={onClickMoney}
-              onTouchStart={onClickMoney}
-              className="rounded-full border border-yellow-400 px-12 py-4 font-bold text-yellow-300 transition-colors focus:bg-yellow-200/20 active:bg-yellow-400 active:text-white"
-            >
-              {formatMoney(amount)}
-            </button>
-          </div>
-        </div>
-      </div>
-    </TinderCard>
+      </TinderCard>
+    </>
   )
 }
 
@@ -168,5 +177,27 @@ const Percents = (props: { contract: BinaryContract; amount: number }) => {
         {formatPercent(getOutcomeProbabilityAfterBet(contract, 'YES', amount))}
       </div>
     </div>
+  )
+}
+
+const Peek = (props: { contract: BinaryContract; onClose: () => void }) => {
+  const { contract, onClose } = props
+  const { question, description } = contract
+  return (
+    <section className="absolute inset-0 z-50 flex flex-col bg-black/40">
+      {/* spacer to close */}
+      <button className="h-40 shrink-0" onClick={onClose} />
+      <div className="h-6 shrink-0 rounded-t-3xl bg-white" />
+      <div className="grow overflow-auto bg-white px-4">
+        <h1 className="mb-8 text-lg font-semibold text-indigo-700">
+          {question}
+        </h1>
+        <RichContent
+          proseClassName="prose-sm"
+          content={description}
+          smallImage
+        />
+      </div>
+    </section>
   )
 }
