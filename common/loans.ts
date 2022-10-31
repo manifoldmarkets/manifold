2050c2df3a09ddd99baceee4dc1d9fd32199d5d3
Marshall Polaris <marshall@pol.rs>
2022-10-31 13:36:44 -0700

    Refactor loan code a little bit (#1121)

--- common/loans.ts
+++ common/loans.ts
@@ -1,4 +1,4 @@
-import { Dictionary, groupBy, sumBy, minBy } from 'lodash'
+import { Dictionary, sumBy, minBy } from 'lodash'
 import { Bet } from './bet'
 import { getContractBetMetrics } from './calculate'
 import {
@@ -7,7 +7,7 @@ import {
   FreeResponseContract,
   MultipleChoiceContract,
 } from './contract'
-import { PortfolioMetrics, User } from './user'
+import { PortfolioMetrics } from './user'
 import { filterDefined } from './util/array'
 
 const LOAN_DAILY_RATE = 0.02
@@ -19,46 +19,18 @@ const calculateNewLoan = (investedValue: number, loanTotal: number) => {
 
 export const getUserLoanUpdates = (
   betsByContractId: { [contractId: string]: Bet[] },
-  contractsById: { [contractId: string]: Contract },
-  portfolio?: PortfolioMetrics | undefined
+  contractsById: { [contractId: string]: Contract }
 ) => {
-  if (isUserEligibleForLoan(portfolio)) {
-    const updates = calculateLoanBetUpdates(
-      betsByContractId,
-      contractsById
-    ).betUpdates
-    return { updates, payout: sumBy(updates, (update) => update.newLoan) }
-  } else {
-    return undefined
-  }
+  const updates = calculateLoanBetUpdates(
+    betsByContractId,
+    contractsById
+  ).betUpdates
+  return { updates, payout: sumBy(updates, (update) => update.newLoan) }
 }
 
-export const getLoanUpdates = (
-  users: User[],
-  contractsById: { [contractId: string]: Contract },
-  portfolioByUser: { [userId: string]: PortfolioMetrics | undefined },
-  betsByUser: { [userId: string]: Bet[] }
+export const isUserEligibleForLoan = (
+  portfolio: PortfolioMetrics | undefined
 ) => {
-  const userUpdates = filterDefined(
-    users.map((user) => {
-      const result = getUserLoanUpdates(
-        groupBy(betsByUser[user.id] ?? [], (b) => b.contractId),
-        contractsById,
-        portfolioByUser[user.id]
-      )
-      return result ? { user, result } : undefined
-    })
-  )
-  return {
-    betUpdates: userUpdates.map((u) => u.result.updates).flat(),
-    userPayouts: userUpdates.map(({ user, result: { payout } }) => ({
-      user,
-      payout,
-    })),
-  }
-}
-
-const isUserEligibleForLoan = (portfolio: PortfolioMetrics | undefined) => {
   if (!portfolio) return true
 
   const { balance, investmentValue } = portfolio
