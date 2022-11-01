b5907405da5e750ce999a6e8a10618e214371722
mantikoros <sgrugett@gmail.com>
2022-11-01 15:30:49 -0500

    larger text on cards

--- web/components/contract/contract-card.tsx
+++ web/components/contract/contract-card.tsx
@@ -135,7 +135,7 @@ export function ContractCard(props: {
           {(!showImage || !contract.coverImageUrl) && (
             <div
               className={clsx(
-                'break-anywhere text-greyscale-7 pb-2 font-medium',
+                'break-anywhere text-greyscale-7 pb-2 font-medium text-lg',
                 questionClass
               )}
             >
