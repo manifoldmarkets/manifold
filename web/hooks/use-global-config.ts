89006e2844a79b464c5a7ecb96a5148cc59fe5b6
James Grugett <jahooma@gmail.com>
2022-11-04 02:21:28 -0500

    Multi-CPMM mechanism (#1153)
    * useStore for global config
    
    * In progress cpmm multi math
    
    * In prog
    
    * Implement short sell
    
    * Cleanup
    
    * Cleanup

--- web/hooks/use-global-config.ts
+++ web/hooks/use-global-config.ts
@@ -1,17 +1,9 @@
 import { GlobalConfig } from 'common/globalConfig'
-import { useEffect } from 'react'
 import { listenForGlobalConfig } from 'web/lib/firebase/globalConfig'
-import { inMemoryStore, usePersistentState } from './use-persistent-state'
+import { useStore } from './use-store'
 
 export const useGlobalConfig = () => {
-  const [globalConfig, setGlobalConfig] =
-    usePersistentState<GlobalConfig | null>(null, {
-      store: inMemoryStore(),
-      key: 'globalConfig',
-    })
-
-  useEffect(() => {
-    return listenForGlobalConfig(setGlobalConfig)
-  }, [setGlobalConfig])
-  return globalConfig
+  return useStore<GlobalConfig | null>('globalConfig', (_, setConfig) =>
+    listenForGlobalConfig(setConfig)
+  )
 }
