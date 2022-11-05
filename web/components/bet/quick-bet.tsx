f78c1468f6df922205cf77b3cfd2838b538cfd00
James Grugett <jahooma@gmail.com>
2022-11-04 23:37:20 -0500

    Fix quick bet selling M$0, selling M$9 instead of M$10.

--- web/components/bet/quick-bet.tsx
+++ web/components/bet/quick-bet.tsx
@@ -30,7 +30,11 @@ import { placeBet } from 'web/lib/firebase/api'
 import { getBinaryProbPercent } from 'web/lib/firebase/contracts'
 import { useSaveBinaryShares } from '../../hooks/use-save-binary-shares'
 import { sellShares } from 'web/lib/firebase/api'
-import { calculateCpmmSale, getCpmmProbability } from 'common/calculate-cpmm'
+import {
+  calculateCpmmSale,
+  getCpmmProbability,
+  getCpmmProbabilityAfterSale,
+} from 'common/calculate-cpmm'
 import { track } from 'web/lib/service/analytics'
 import { formatNumericProbability } from 'common/pseudo-numeric'
 import { useUnfilledBetsAndBalanceByUserId } from 'web/hooks/use-bets'
@@ -64,12 +68,13 @@ export function QuickBet(props: {
     contract.id
   )
 
-  const { yesShares, noShares } = useSaveBinaryShares(contract, userBets)
+  const { yesShares, noShares, hasNoShares, hasYesShares } =
+    useSaveBinaryShares(contract, userBets)
 
   const [upHover, setUpHover] = useState(false)
   const [downHover, setDownHover] = useState(false)
 
-  let previewProb = undefined
+  let previewProb: number | undefined = undefined
   try {
     previewProb = upHover
       ? getOutcomeProbabilityAfterBet(
@@ -92,7 +97,7 @@ export function QuickBet(props: {
   let sharesSold: number | undefined
   let sellOutcome: 'YES' | 'NO' | undefined
   let saleAmount: number | undefined
-  if (isCpmm && (upHover || downHover)) {
+  if (isCpmm && ((upHover && hasNoShares) || (downHover && hasYesShares))) {
     const oppositeShares = upHover ? noShares : yesShares
     if (oppositeShares) {
       sellOutcome = upHover ? 'NO' : 'YES'
@@ -100,6 +105,20 @@ export function QuickBet(props: {
       const prob = getProb(contract)
       const maxSharesSold = BET_SIZE / (sellOutcome === 'YES' ? prob : 1 - prob)
       sharesSold = Math.min(oppositeShares, maxSharesSold)
+      const probAfterSale = getCpmmProbabilityAfterSale(
+        contract,
+        sharesSold,
+        sellOutcome,
+        unfilledBets,
+        balanceByUserId
+      )
+
+      // Recompute max shares sold using prob after selling.
+      // This lower price for your shares means the max is more generous.
+      // Which fixes the issue where you sell 99% of your shares instead of all.
+      const maxSharesSold2 =
+        BET_SIZE / (sellOutcome === 'YES' ? probAfterSale : 1 - probAfterSale)
+      sharesSold = Math.min(oppositeShares, maxSharesSold2)
 
       const { cpmmState, saleValue } = calculateCpmmSale(
         contract,
@@ -133,7 +152,7 @@ export function QuickBet(props: {
     const shortQ = contract.question.slice(0, 20)
     const message =
       sellOutcome && saleAmount
-        ? `${formatMoney(saleAmount)} sold of "${shortQ}"...`
+        ? `${formatMoney(Math.round(saleAmount))} sold of "${shortQ}"...`
         : `${formatMoney(BET_SIZE)} on "${shortQ}"...`
 
     toast.promise(betPromise(), {
