7f0c83967bfec0e23b64e358ad51be2edfc9a833
Ian Philips <iansphilips@gmail.com>
2022-11-07 09:07:09 -0700

    Collapsible content styling

--- web/components/widgets/collapsible-content.tsx
+++ web/components/widgets/collapsible-content.tsx
@@ -15,8 +15,8 @@ import {
 import { safeLocalStorage } from 'web/lib/util/local'
 import { useSafeLayoutEffect } from 'web/hooks/use-safe-layout-effect'
 
-const COLLAPSIBLE_HEIGHT = 350
-const START_COLLAPSED_HEIGHT = 500
+const COLLAPSIBLE_HEIGHT = 370
+const START_COLLAPSED_HEIGHT = 800
 
 export function ShowMoreLessButton(props: {
   onClick: () => void
@@ -30,7 +30,7 @@ export function ShowMoreLessButton(props: {
       className={clsx('z-10 select-none bg-white text-sm', className)}
       onClick={onClick}
     >
-      <Row className="items-center gap-0.5 text-indigo-700">
+      <Row className="items-center gap-0.5 text-indigo-700 drop-shadow-2xl">
         {isCollapsed ? (
           <ChevronDoubleDownIcon className="h-4 w-4" />
         ) : (
@@ -65,41 +65,36 @@ export function CollapsibleContent(props: {
 
   if (shouldAllowCollapseOfContent) {
     return (
-      <div
-        style={{ height: isCollapsed ? COLLAPSIBLE_HEIGHT : 'auto' }}
-        className={clsx('transition-height relative w-full overflow-hidden')}
-      >
-        <div ref={contentRef}>
-          <Content content={content} />
+      <div className="relative">
+        <div
+          style={{ height: isCollapsed ? COLLAPSIBLE_HEIGHT : 'auto' }}
+          className={clsx(
+            'transition-height relative w-full overflow-hidden rounded-b-md'
+          )}
+        >
+          <div ref={contentRef}>
+            <Content content={content} />
+          </div>
+          {isCollapsed && (
+            <>
+              <div className="absolute bottom-0 w-full">
+                <div className="h-16 bg-gradient-to-t from-gray-100" />
+              </div>
+            </>
+          )}
         </div>
-        {isCollapsed && (
-          <>
-            <div className="absolute bottom-0 w-full">
-              <div className="h-2 bg-gradient-to-t from-white" />
-              <div className="h-8 bg-white" />
-            </div>
-            <ShowMoreLessButton
-              className="absolute right-0 bottom-0"
-              onClick={() => setIsCollapsed(false)}
-              isCollapsed={isCollapsed}
-            />
-          </>
-        )}
-        {!isCollapsed && (
-          <Row className="w-full justify-end">
-            <ShowMoreLessButton
-              className="flex"
-              onClick={() => {
-                setIsCollapsed(true)
-                window.scrollTo({
-                  top: 0,
-                  behavior: 'smooth',
-                })
-              }}
-              isCollapsed={isCollapsed}
-            />
-          </Row>
-        )}
+        <ShowMoreLessButton
+          className="absolute right-0 -bottom-8 bg-transparent"
+          onClick={() => {
+            if (!isCollapsed)
+              window.scrollTo({
+                top: 0,
+                behavior: 'smooth',
+              })
+            setIsCollapsed(!isCollapsed)
+          }}
+          isCollapsed={isCollapsed}
+        />
       </div>
     )
   }
