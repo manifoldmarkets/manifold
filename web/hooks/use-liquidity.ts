1ed4e5dced8108cdc8012d7bb62e3f06c960c095
James Grugett <jahooma@gmail.com>
2022-11-09 15:45:57 -0600

    Implement payouts, including liquidity payouts for multiple choice

--- web/hooks/use-liquidity.ts
+++ web/hooks/use-liquidity.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 
-import { CPMMContract } from 'common/contract'
+import { CPMM2Contract, CPMMContract } from 'common/contract'
 import { LiquidityProvision } from 'common/liquidity-provision'
 import { getUserLiquidityShares } from 'common/calculate-cpmm'
 
@@ -18,9 +18,9 @@ export const useLiquidity = (contractId: string) => {
   return liquidities
 }
 
-export const useUserLiquidity = (contract: CPMMContract, userId: string) => {
+export const useUserLiquidity = (contract: CPMMContract | CPMM2Contract, userId: string) => {
   const liquidities = useLiquidity(contract.id)
 
-  const userShares = getUserLiquidityShares(userId, contract, liquidities ?? [])
+  const userShares = getUserLiquidityShares(userId, contract.pool, liquidities ?? [])
   return userShares
 }
