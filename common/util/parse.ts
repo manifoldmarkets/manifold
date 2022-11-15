64731ef1d2560b9bd69afa0e09e5466dd2b0cb00
Sinclair Chen <abc.sinclair@gmail.com>
2022-11-15 23:49:44 +0000

    Use StarterKit instead of individual extensions

--- common/util/parse.ts
+++ common/util/parse.ts
@@ -1,23 +1,6 @@
 import { generateText, JSONContent, Node } from '@tiptap/core'
 import { generateJSON } from '@tiptap/html'
-// Tiptap starter extensions
-import { Blockquote } from '@tiptap/extension-blockquote'
-import { Bold } from '@tiptap/extension-bold'
-import { BulletList } from '@tiptap/extension-bullet-list'
-import { Code } from '@tiptap/extension-code'
-import { CodeBlock } from '@tiptap/extension-code-block'
-import { Document } from '@tiptap/extension-document'
-import { HardBreak } from '@tiptap/extension-hard-break'
-import { Heading } from '@tiptap/extension-heading'
-import { History } from '@tiptap/extension-history'
-import { HorizontalRule } from '@tiptap/extension-horizontal-rule'
-import { Italic } from '@tiptap/extension-italic'
-import { ListItem } from '@tiptap/extension-list-item'
-import { OrderedList } from '@tiptap/extension-ordered-list'
-import { Paragraph } from '@tiptap/extension-paragraph'
-import { Strike } from '@tiptap/extension-strike'
-import { Text } from '@tiptap/extension-text'
-// other tiptap extensions
+import { StarterKit } from '@tiptap/starter-kit'
 import { Image } from '@tiptap/extension-image'
 import { Link } from '@tiptap/extension-link'
 import { Mention } from '@tiptap/extension-mention'
@@ -66,24 +49,7 @@ function skippableComponent(extension: string, label: string): Node<any, any> {
 }
 
 const stringParseExts = [
-  // StarterKit extensions
-  Blockquote,
-  Bold,
-  BulletList,
-  Code,
-  CodeBlock,
-  Document,
-  HardBreak,
-  Heading,
-  History,
-  HorizontalRule,
-  Italic,
-  ListItem,
-  OrderedList,
-  Paragraph,
-  Strike,
-  Text,
-  // other extensions
+  StarterKit,
   Link,
   Image.extend({ renderText: () => '[image]' }),
   Mention, // user @mention
