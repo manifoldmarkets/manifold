ce5fafd19f83e6aff6ac5d1c9625011da93aa9ba
Marshall Polaris <marshall@pol.rs>
2022-11-14 22:36:26 -0800

    Don't randomly not do stuff in `onUpdateContract`

--- functions/src/on-update-contract.ts
+++ functions/src/on-update-contract.ts
@@ -28,21 +28,25 @@ export const onUpdateContract = functions.firestore
     const { eventId } = context
     const { closeTime, question } = contract
 
-    // maybe we should do this more often, but at least if someone bets
-    if (previousContract.volume !== contract.volume) {
-      await revalidateStaticProps(getContractPath(contract))
+    if (previousContract.groupSlugs !== contract.groupSlugs) {
+      await handleContractGroupUpdated(previousContract, contract)
     }
 
-    if (!previousContract.isResolved && contract.isResolved) {
-      // No need to notify users of resolution, that's handled in resolve-market
-      return await handleResolvedContract(contract)
-    } else if (previousContract.groupSlugs !== contract.groupSlugs) {
-      await handleContractGroupUpdated(previousContract, contract)
-    } else if (
+    if (
       previousContract.closeTime !== closeTime ||
       previousContract.question !== question
     ) {
       await handleUpdatedCloseTime(previousContract, contract, eventId)
+    }
+
+    if (!previousContract.isResolved && contract.isResolved) {
+      // No need to notify users of resolution, that's handled in resolve-market
+      await handleResolvedContract(contract)
+    }
+
+    // maybe we should do this more often, but at least if someone bets
+    if (previousContract.volume !== contract.volume) {
+      await revalidateStaticProps(getContractPath(contract))
     }
   })
 
