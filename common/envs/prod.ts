4ad1c55a3928dc3a982fe629f8aad42887a7b14a
Marshall Polaris <marshall@pol.rs>
2022-11-20 18:40:45 -0800

    Add prod Supabase URL

--- common/envs/prod.ts
+++ common/envs/prod.ts
@@ -64,7 +64,7 @@ type FirebaseConfig = {
 export const PROD_CONFIG: EnvConfig = {
   domain: 'manifold.markets',
   amplitudeApiKey: '2d6509fd4185ebb8be29709842752a15',
-  supabaseUrl: 'https://mfodonznyfxllcezufgr.supabase.co',
+  supabaseUrl: 'https://pxidrgkatumlvfqaxcll.supabase.co',
   sprigEnvironmentId: 'sQcrq9TDqkib',
 
   firebaseConfig: {
