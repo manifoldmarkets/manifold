c6398f08b5ecee0478349bb57fe0460b1f9a320f
James Grugett <jahooma@gmail.com>
2022-11-27 16:38:39 -0500

    Fix contract leaderboard calculation

--- web/components/contract/contract-leaderboard.tsx
+++ web/components/contract/contract-leaderboard.tsx
@@ -1,9 +1,9 @@
 import { Bet } from 'common/bet'
-import { resolvedPayout } from 'common/calculate'
+import { getContractBetMetrics } from 'common/calculate'
 import { Contract } from 'common/contract'
 import { formatMoney } from 'common/util/format'
 
-import { groupBy, mapValues, sumBy } from 'lodash'
+import { groupBy, mapValues } from 'lodash'
 import { Leaderboard } from '../leaderboard'
 import { BETTORS } from 'common/user'
 import { memo } from 'react'
@@ -16,14 +16,14 @@ export const ContractLeaderboard = memo(function ContractLeaderboard(props: {
   const { contract, bets } = props
 
   // Create a map of userIds to total profits (including sales)
-  const openBets = bets.filter((bet) => !bet.isSold && !bet.sale)
-  const betsByUser = groupBy(openBets, 'userId')
+  const betsByUser = groupBy(bets, 'userId')
   const userProfits = mapValues(betsByUser, (bets) => {
+    const { profit } = getContractBetMetrics(contract, bets)
     return {
       name: bets[0].userName,
       username: bets[0].userUsername,
       avatarUrl: bets[0].userAvatarUrl,
-      total: sumBy(bets, (bet) => resolvedPayout(contract, bet) - bet.amount),
+      total: profit,
     }
   })
   // Find the 5 users with the most profits
