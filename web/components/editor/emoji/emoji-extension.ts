8676bf3f2fe9b3367220361aded9e650f060da94
Sinclair Chen <abc.sinclair@gmail.com>
2022-11-29 12:37:54 -0800

    Remove unused code

--- web/components/editor/emoji/emoji-extension.ts
+++ web/components/editor/emoji/emoji-extension.ts
@@ -1,16 +1,6 @@
 import { Extension } from '@tiptap/core'
 import Suggestion from '@tiptap/suggestion'
-import { PluginKey } from 'prosemirror-state'
 import { emojiSuggestion } from './emoji-suggestion'
-
-export const EmojiPluginKey = new PluginKey('emoji')
-
-// {
-//   name: 'mana',
-//   shortcodes: ['m$, mana'],
-//   tags: ['money'],
-//   emoji: ENV_CONFIG.moneyMoniker,
-// },
 
 export const EmojiExtension = Extension.create({
   name: 'emoji',
