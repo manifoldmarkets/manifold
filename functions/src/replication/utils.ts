c02eb7e7af0e4433f3ed856b2f5bcb908eda54dc
Marshall Polaris <marshall@pol.rs>
2022-11-30 17:00:08 -0800

    Better handling of Supabase config client creation

--- functions/src/replication/utils.ts
+++ functions/src/replication/utils.ts
@@ -6,7 +6,13 @@ import { isProd } from '../utils'
 
 export function createSupabaseClient() {
   const url = isProd() ? PROD_CONFIG.supabaseUrl : DEV_CONFIG.supabaseUrl
+  if (!url) {
+    throw new Error("Can't connect to Supabase; no supabaseUrl in config.")
+  }
   const key = process.env.SUPABASE_KEY
+  if (!key) {
+    throw new Error("Can't connect to Supabase; no process.env.SUPABASE_KEY.")
+  }
   return createClient(url, key)
 }
 
