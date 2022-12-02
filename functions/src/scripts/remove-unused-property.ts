90dbca0727f476d64a4ad42e94c2ebd6d3553f90
Ian Philips <iansphilips@gmail.com>
2022-12-02 08:46:59 -0700

    Remove unused likeByUserIds prop on contracts

---
+++ functions/src/scripts/remove-unused-property.ts
@@ -0,0 +1,24 @@
+import * as admin from 'firebase-admin'
+
+import { initAdmin } from './script-init'
+import { getValues } from '../utils'
+import { FieldValue } from 'firebase-admin/firestore'
+import { Contract } from 'common/contract'
+
+initAdmin()
+
+const firestore = admin.firestore()
+
+async function main() {
+  const contracts = await getValues<Contract>(firestore.collection('contracts'))
+  await Promise.all(
+    contracts.map(async (contract) => {
+      if (contract.likedByUserIds !== undefined)
+        await firestore.collection('contracts').doc(contract.id).update({
+          likedByUserIds: FieldValue.delete(),
+        })
+    })
+  )
+}
+
+if (require.main === module) main().then(() => process.exit())
