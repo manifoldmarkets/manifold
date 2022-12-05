6be35bf3b1a5126d132cdae25595303ba4a8fdbf
Ian Philips <iansphilips@gmail.com>
2022-12-05 17:35:32 -0600

    Add useLikesOnContent hook for Inga

--- web/hooks/use-likes.ts
+++ web/hooks/use-likes.ts
@@ -1,8 +1,9 @@
-import { collection, query, where } from 'firebase/firestore'
+import { collection, collectionGroup, query, where } from 'firebase/firestore'
 import { users } from 'web/lib/firebase/users'
 import { listenForValues } from 'web/lib/firebase/utils'
 import { Reaction, ReactionContentTypes } from 'common/reaction'
 import { useStore } from 'web/hooks/use-store'
+import { db } from 'web/lib/firebase/init'
 
 export const useUserLikes = (
   userId: string | undefined,
@@ -12,6 +13,18 @@ export const useUserLikes = (
     `user-likes-${userId}-${contentType}`,
     (_, setReactions) => {
       return listenForLikes(userId ?? '_', contentType, setReactions)
+    }
+  )
+}
+
+export const useLikesOnContent = (
+  contentType: ReactionContentTypes,
+  contentId: string
+) => {
+  return useStore<Reaction[] | undefined>(
+    `${contentType}-likes-on-${contentId}`,
+    (_, setReactions) => {
+      return listenForLikesOnContent(contentType, contentId, setReactions)
     }
   )
 }
@@ -35,6 +48,20 @@ function listenForLikes(
     collection(users, userId, 'reactions'),
     where('type', '==', 'like'),
     where('contentType', '==', contentType)
+  )
+  return listenForValues<Reaction>(likes, (docs) => setLikes(docs))
+}
+
+function listenForLikesOnContent(
+  contentType: ReactionContentTypes,
+  contentId: string,
+  setLikes: (likes: Reaction[]) => void
+) {
+  const likes = query(
+    collectionGroup(db, 'reactions'),
+    where('type', '==', 'like'),
+    where('contentType', '==', contentType),
+    where('contentId', '==', contentId)
   )
   return listenForValues<Reaction>(likes, (docs) => setLikes(docs))
 }
