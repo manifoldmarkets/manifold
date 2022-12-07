94c04c7fb2db625b86a83bb89b15b5c5fc85cc64
SirSaltyy <d4vidchee@gmail.com>
2022-12-07 14:14:35 -0600

    Updated Destiny landing page
    changed from destiny to destinygg, removed images, added buttons, change enter username input bar position.

--- web/pages/destinygg.tsx
+++ web/pages/destinygg.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
+import { ReactNode, useState } from 'react'
 import { CheckmarkIcon } from 'react-hot-toast'
-import Image from 'next/image'
-
+import { ENV_CONFIG } from 'common/envs/constants'
 import { Col } from 'web/components/layout/col'
 import { Page } from 'web/components/layout/page'
 import { SEO } from 'web/components/SEO'
@@ -21,6 +20,12 @@ import { LoadingIndicator } from 'web/components/widgets/loading-indicator'
 import { ContractsGrid } from 'web/components/contract/contracts-grid'
 import { usePrivateUser } from 'web/hooks/use-user'
 import { claimDestinySub } from 'web/lib/firebase/api'
+import { ManaExplainer, PredictionMarketExplainer } from '.'
+import { ChartBarIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+import { Modal } from 'web/components/layout/modal'
+import GoToIcon from 'web/lib/icons/go-to-icon'
+import { SiteLink } from 'web/components/widgets/site-link'
 
 export default function DestinyLandingPage() {
   useSaveReferral()
@@ -59,23 +64,12 @@ export default function DestinyLandingPage() {
 
       <Col className="max-w-3xl rounded bg-white p-4 text-gray-600 shadow-md sm:mx-auto sm:p-10">
         <Title>Claim a free Destiny tier-1 subscription!</Title>
-
         <div>
           Support Destiny by trading in our markets to earn {formatMoney(1000)}{' '}
           and claim your sub. It's free and you can sign up with google
           instantly. For each subscription, Manifold Markets will pay Destiny
           $5.00 on your behalf!
         </div>
-        <Row className="hidden items-center px-4 sm:flex">
-          <Image src="/dgg-logo.svg" alt="Destiny" height={50} width={200} />
-          <div className="mx-8 text-4xl">+</div>
-          <Image src="/logo.png" alt="Manifold" width={150} height={100} />
-        </Row>
-        <div className="mt-4 sm:mt-0">
-          Once you have the required balance, simply enter the destiny.gg
-          account name you want the sub gifted to. You may only claim one sub.
-        </div>
-
         {destinySubClaimed ? (
           <Row className="my-4 items-center self-center text-xl">
             <CheckmarkIcon className="mr-2" /> Destiny subscription claimed!
@@ -101,11 +95,36 @@ export default function DestinyLandingPage() {
           </Row>
         )}
         {error && <div className="mt-2 text-sm text-red-500">{error}</div>}
-
-        <Spacer h={8} />
+        <div className="mt-4 pt-6 sm:mt-0">
+          Once you have the required balance, simply enter the destiny.gg
+          account name you want the sub gifted to. You may only claim one sub.
+        </div>
+        <Spacer h={2} />
         <Subtitle text="New to Manifold Markets?" />
-        <LandingPagePanel />
-        <Spacer h={8} />
+        <Col className=" max-w-3xl gap-4 px-4">
+          <LandingPagePanel />
+          <Row className="w-full gap-2 sm:gap-4">
+            <ExternalInfoCard
+              link="https://manifold.markets/post/welcome-explanation-for-newcomers-f"
+              icon={<div className="text-2xl">?</div>}
+              text="Welcome Explanation"
+            />
+            <InfoCard
+              link="https://help.manifold.markets/introduction-to-manifold-markets/what-is-mana-m"
+              icon={<div className="text-2xl">{ENV_CONFIG.moneyMoniker}</div>}
+              text="What is Mana?"
+              modal={<ManaExplainer />}
+            />
+            <ExternalInfoCard
+              link="https://manifold.markets/group/destinygg"
+              icon={<ChartBarIcon className="mx-auto h-8 w-8" />}
+              text="All Dgg Markets"
+              modal={<PredictionMarketExplainer />}
+            />
+          </Row>
+        </Col>
+
+        <Spacer h={4} />
         <Subtitle text="Trending markets" />
         {trendingContracts ? (
           <ContractsGrid
@@ -118,5 +137,77 @@ export default function DestinyLandingPage() {
         <TestimonialsPanel />
       </Col>
     </Page>
+  )
+}
+
+export function ExternalInfoCard(props: {
+  link: string
+  icon: ReactNode
+  text: string
+}) {
+  const { link, icon, text } = props
+  return (
+    <Link
+      className="group flex w-1/3 flex-col items-center gap-1 rounded-xl bg-indigo-700 px-4 py-2 text-center text-sm text-white drop-shadow-sm transition-all hover:drop-shadow-lg"
+      href={link}
+      target="_blank"
+    >
+      <div className="text-indigo-400 transition-colors group-hover:text-white">
+        {icon}
+      </div>
+      <div>
+        {text}
+        <span>
+          <GoToIcon className="mb-1 ml-2 inline h-4 w-4 text-white" />
+        </span>
+      </div>
+    </Link>
+  )
+}
+
+export function InfoCard(props: {
+  link: string
+  icon: ReactNode
+  text: string
+  externalLink?: boolean
+  modal: ReactNode
+}) {
+  const { link, icon, text, externalLink, modal } = props
+  const [open, setOpen] = useState(false)
+  return (
+    <>
+      <Modal open={open} setOpen={setOpen} size="md">
+        <Col className="rounded-md bg-white px-8 pb-6 pt-0 text-sm font-light md:text-lg">
+          <Title text={text} />
+          {modal}
+          <Link
+            href={link}
+            className="mt-2 text-indigo-700 underline"
+            target="_blank"
+          >
+            Learn more{' '}
+            <span>
+              <GoToIcon className="mb-1 ml-1 inline h-4 w-4 text-indigo-700" />
+            </span>
+          </Link>
+        </Col>
+      </Modal>
+      <button
+        className="group flex w-1/3 flex-col items-center gap-1 rounded-xl bg-indigo-700 px-4 py-2 text-center text-sm text-white drop-shadow-sm transition-all hover:drop-shadow-lg"
+        onClick={() => setOpen(true)}
+      >
+        <div className="text-indigo-400 transition-colors group-hover:text-white">
+          {icon}
+        </div>
+        <div>
+          <div>{text}</div>
+          {externalLink && (
+            <span>
+              <GoToIcon className="mb-1 ml-2 inline h-4 w-4 text-indigo-400" />
+            </span>
+          )}
+        </div>
+      </button>
+    </>
   )
 }
