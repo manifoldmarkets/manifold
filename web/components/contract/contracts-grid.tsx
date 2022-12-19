7a720a3e94d193933b8017d6287d4e3c7f9d50a6
Ian Philips <iansphilips@gmail.com>
2022-12-19 12:27:23 -0500

    Add market stats to market tab

--- web/components/contract/contracts-grid.tsx
+++ web/components/contract/contracts-grid.tsx
@@ -1,15 +1,16 @@
 import { Contract } from 'web/lib/firebase/contracts'
-import { User } from 'web/lib/firebase/users'
+import { getTotalContractCreated, User } from 'web/lib/firebase/users'
 import { Col } from '../layout/col'
 import { SiteLink } from '../widgets/site-link'
 import { ContractCard, ContractMetricsFooter } from './contract-card'
 import { ShowTime } from './contract-details'
 import { ContractSearch } from '../contract-search'
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import clsx from 'clsx'
 import { LoadingIndicator } from '../widgets/loading-indicator'
 import { VisibilityObserver } from '../widgets/visibility-observer'
 import Masonry from 'react-masonry-css'
+import { Row } from 'web/components/layout/row'
 
 export function ContractsGrid(props: {
   contracts: Contract[] | undefined
@@ -115,17 +116,65 @@ export function ContractsGrid(props: {
 
 export function CreatorContractsList(props: { creator: User }) {
   const { creator } = props
+  const [marketsCreated, setMarketsCreated] = useState<number | undefined>()
+  useEffect(() => {
+    getTotalContractCreated(creator.id).then(setMarketsCreated)
+  }, [creator.id])
 
+  const { creatorTraders } = creator
+  const MarketStats = (props: {
+    title: string
+    total: number
+    weeklyChange: number | undefined
+  }) => {
+    const { title, total, weeklyChange } = props
+    return (
+      <Col className={clsx('')}>
+        <div className="text-xs text-gray-600 sm:text-sm">{title}</div>
+        <Row className={'items-center  gap-2'}>
+          <span className="text-lg text-indigo-600 sm:text-xl">{total}</span>
+          {weeklyChange ? (
+            <span
+              className={clsx(
+                weeklyChange > 0 ? 'text-teal-500' : 'text-scarlet-500'
+              )}
+            >
+              {weeklyChange > 0 ? '+' : ''}
+              {Math.round((weeklyChange * 100) / total)}% (7d)
+            </span>
+          ) : (
+            <div />
+          )}
+        </Row>
+      </Col>
+    )
+  }
   return (
-    <ContractSearch
-      headerClassName="sticky"
-      defaultSort="newest"
-      defaultFilter="all"
-      additionalFilter={{
-        creatorId: creator.id,
-      }}
-      persistPrefix={`user-${creator.id}`}
-      profile={true}
-    />
+    <Col className={'w-full'}>
+      <Row className={'gap-8 pb-4'}>
+        <MarketStats
+          title={'Total markets'}
+          total={marketsCreated ?? 0}
+          weeklyChange={undefined}
+        />
+        <MarketStats
+          title={'Unique traders'}
+          total={creatorTraders.allTime}
+          weeklyChange={creatorTraders.weekly}
+        />
+      </Row>
+      <Row>
+        <ContractSearch
+          headerClassName="sticky"
+          defaultSort="newest"
+          defaultFilter="all"
+          additionalFilter={{
+            creatorId: creator.id,
+          }}
+          persistPrefix={`user-${creator.id}`}
+          profile={true}
+        />
+      </Row>
+    </Col>
   )
 }
