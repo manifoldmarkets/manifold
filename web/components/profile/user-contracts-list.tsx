2f955145c248821b772f4118c6634d5a2bc92b67
Ian Philips <iansphilips@gmail.com>
2022-12-19 13:19:48 -0500

    Add rank to user page

--- web/components/profile/user-contracts-list.tsx
+++ web/components/profile/user-contracts-list.tsx
@@ -1,42 +1,36 @@
 import { User } from 'common/user'
-import { useEffect, useState } from 'react'
-import { getTotalContractCreated } from 'web/lib/firebase/users'
+import { ReactNode, useEffect, useState } from 'react'
+import { getCreatorRank, getTotalContractCreated } from 'web/lib/firebase/users'
 import { Col } from 'web/components/layout/col'
 import clsx from 'clsx'
 import { Row } from 'web/components/layout/row'
 import { ContractSearch } from 'web/components/contract-search'
+import { Tooltip } from 'web/components/widgets/tooltip'
 
 export function UserContractsList(props: { creator: User }) {
   const { creator } = props
+  const { creatorTraders } = creator
+  const { weekly, allTime } = creatorTraders
   const [marketsCreated, setMarketsCreated] = useState<number | undefined>()
+  const [creatorRank, setCreatorRank] = useState<number | undefined>()
+
   useEffect(() => {
     getTotalContractCreated(creator.id).then(setMarketsCreated)
-  }, [creator.id])
+    getCreatorRank(allTime, 'allTime').then(setCreatorRank)
+  }, [creator.id, allTime])
 
-  const { creatorTraders } = creator
   const MarketStats = (props: {
     title: string
-    total: number
-    weeklyChange: number | undefined
+    total: string
+    subTitle?: ReactNode
   }) => {
-    const { title, total, weeklyChange } = props
+    const { title, total, subTitle } = props
     return (
       <Col className={clsx('')}>
         <div className="text-xs text-gray-600 sm:text-sm">{title}</div>
         <Row className={'items-center  gap-2'}>
           <span className="text-lg text-indigo-600 sm:text-xl">{total}</span>
-          {weeklyChange ? (
-            <span
-              className={clsx(
-                weeklyChange > 0 ? 'text-teal-500' : 'text-scarlet-500'
-              )}
-            >
-              {weeklyChange > 0 ? '+' : ''}
-              {Math.round((weeklyChange * 100) / total)}% (7d)
-            </span>
-          ) : (
-            <div />
-          )}
+          {subTitle}
         </Row>
       </Col>
     )
@@ -44,15 +38,27 @@ export function UserContractsList(props: { creator: User }) {
   return (
     <Col className={'w-full'}>
       <Row className={'gap-8 pb-4'}>
+        <MarketStats title={'Creator rank'} total={`#${creatorRank}` ?? ''} />
         <MarketStats
           title={'Total markets'}
-          total={marketsCreated ?? 0}
-          weeklyChange={undefined}
+          total={marketsCreated?.toString() ?? ''}
         />
         <MarketStats
           title={'Unique traders'}
-          total={creatorTraders.allTime}
-          weeklyChange={creatorTraders.weekly}
+          total={allTime.toString()}
+          subTitle={
+            <span
+              className={clsx(
+                'text-sm',
+                weekly > 0 ? 'text-teal-500' : 'text-scarlet-500'
+              )}
+            >
+              <Tooltip text={'7-day change'}>
+                {weekly > 0 ? '+' : ''}
+                {Math.round((weekly * 100) / allTime)}%
+              </Tooltip>
+            </span>
+          }
         />
       </Row>
       <Row>
