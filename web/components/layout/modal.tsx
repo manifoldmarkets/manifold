d09166bc37ab5cd56ef957ee4118c1b7dd3146a2
Ian Philips <iansphilips@gmail.com>
2022-12-22 11:21:38 -0500

    Revert "Make modals peek on mobile"
    This reverts commit 7b73b990050a27699c4f936801a76b79d636cd48.

--- web/components/layout/modal.tsx
+++ web/components/layout/modal.tsx
@@ -14,71 +14,73 @@ export function Modal(props: {
   position?: 'center' | 'top' | 'bottom'
   className?: string
 }) {
-  const {
-    children,
-    position = 'bottom',
-    open,
-    setOpen,
-    size = 'md',
-    className,
-  } = props
+  const { children, position, open, setOpen, size = 'md', className } = props
 
   const sizeClass = {
-    sm: 'sm:max-w-sm',
-    md: 'sm:max-w-md',
+    sm: 'max-w-sm',
+    md: 'max-w-md',
     lg: 'max-w-2xl',
     xl: 'max-w-5xl',
   }[size]
 
   const positionClass = {
-    center: 'sm:items-center',
-    top: 'sm:items-start',
-    bottom: '',
-  }[position]
+    center: 'items-center',
+    top: 'items-start',
+    bottom: 'items-end',
+  }[position ?? 'bottom']
 
   return (
     <Transition.Root show={open} as={Fragment}>
-      <Dialog className="relative z-50" onClose={setOpen}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-linear duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-linear duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-50 overflow-y-auto"
+        onClose={setOpen}
+      >
+        <div
+          className={clsx(
+            'flex min-h-screen justify-center px-4 pt-4 pb-20 text-center sm:p-0',
+            positionClass
+          )}
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
 
-        <Transition.Child
-          as={Fragment}
-          enter="ease-in sm:ease-out duration-300"
-          enterFrom="sm:opacity-0 translate-y-full sm:translate-y-0 sm:scale-95"
-          enterTo="opacity-100 translate-y-0 sm:scale-100"
-          leave="ease-out sm:ease-in duration-200"
-          leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-          leaveTo="sm:opacity-0 translate-y-full sm:translate-y-0 sm:scale-95"
-        >
-          <div className="fixed inset-0 overflow-y-auto pt-20 sm:p-0">
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="hidden sm:inline-block sm:h-screen sm:align-middle"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
             <div
               className={clsx(
-                'flex min-h-full items-end justify-center overflow-hidden',
-                positionClass
+                'my-8 mx-6 inline-block w-full transform overflow-hidden text-left align-bottom transition-all sm:self-center sm:align-middle',
+                sizeClass,
+                className
               )}
             >
-              <Dialog.Panel
-                className={clsx(
-                  'w-full transform transition-all sm:my-8 sm:mx-6 sm:self-center',
-                  sizeClass,
-                  className
-                )}
-              >
-                {children}
-              </Dialog.Panel>
+              {children}
             </div>
-          </div>
-        </Transition.Child>
+          </Transition.Child>
+        </div>
       </Dialog>
     </Transition.Root>
   )
