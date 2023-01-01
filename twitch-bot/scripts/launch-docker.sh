8401bf6092fdc3b18d08bbdd20c2df085ece62b2
Phil Bladen <phil.bladen@gmail.com>
2023-01-01 15:57:40 +0000

    Optimized Dockerfile performance when dealing with potentially frequently changing common files. Fixed docker build and launch scripts to support running from any CWD.

--- twitch-bot/scripts/launch-docker.sh
+++ twitch-bot/scripts/launch-docker.sh
@@ -1,2 +1,3 @@
-cd ..
-docker run -it --env-file=.env -p 9172:9172 mb
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR/.."
+docker run -it --env-file=.env --env PUBLIC_FACING_URL=http://127.0.0.1:9172 -p 9172:9172 mb
