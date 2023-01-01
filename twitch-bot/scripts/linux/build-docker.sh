bc6cac7a64000c55ca1b157024100c207ecd90fa
Phil Bladen <phil.bladen@gmail.com>
2023-01-01 14:21:33 +0000

    Removed duplicate scripts.

--- twitch-bot/scripts/linux/build-docker.sh
+++
@@ -1,8 +0,0 @@
-cd ../../..
-start=`date +%s`
-docker build -f twitch-bot/Dockerfile -t mb .
-end=`date +%s`
-echo "================================="
-echo "= Build completed in $((end-start)) seconds ="
-echo "================================="
-sleep 3
