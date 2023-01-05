229a16d1eade652e978658109262beb8293a6e57
ingawei <46611122+ingawei@users.noreply.github.com>
2023-01-04 21:10:46 -0600

    right side panel is now individually scrollable (#1393)
    * right side panel is now individually scrollable

--- web/components/layout/page.tsx
+++ web/components/layout/page.tsx
@@ -35,7 +35,7 @@ export function Page(props: {
         />
         <Sidebar
           logoSubheading={logoSubheading}
-          className="sticky top-0 hidden divide-gray-300 self-start pl-2 lg:col-span-2 lg:flex"
+          className="sticky top-0 hidden divide-gray-300 self-start pl-2 lg:col-span-2 lg:flex "
         />
         {/* put right sidebar below main content on small or medium screens */}
         <div className="lg:col-span-8 xl:contents">
@@ -48,7 +48,9 @@ export function Page(props: {
             {children}
           </main>
           <aside className="col-span-3">
-            <div className="top-6 xl:sticky">{rightSidebar}</div>
+            <div className="xl:sticky xl:top-0 xl:bottom-0 xl:h-screen xl:overflow-y-auto xl:py-6">
+              {rightSidebar}
+            </div>
           </aside>
         </div>
       </div>
