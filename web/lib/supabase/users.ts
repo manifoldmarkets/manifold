671254bad866df8554d960e97e9893348e0bf54a
Ian Philips <iansphilips@gmail.com>
2023-01-05 07:46:35 -0700

    Search upper or lower cased exact match too

--- web/lib/supabase/users.ts
+++ web/lib/supabase/users.ts
@@ -1,13 +1,13 @@
 import { db } from './db'
 import { run } from 'common/supabase/utils'
 import { User } from 'common/user'
-import { uniqBy } from 'lodash'
+import { capitalize, lowerCase, uniqBy } from 'lodash'
 
 export type SearchUserInfo = Pick<
   User,
   'id' | 'name' | 'username' | 'avatarUrl'
 >
-
+const isUpperCase = (str: string) => /^[A-Z]*$/.test(str)
 export async function searchUsers(prompt: string, limit: number) {
   const { data } =
     prompt != ''
@@ -25,6 +25,18 @@ export async function searchUsers(prompt: string, limit: number) {
             .order('data->followerCountCached', { ascending: false } as any)
             .limit(limit)
         )
+
+  const casePrompt = isUpperCase(prompt[0])
+    ? lowerCase(prompt)
+    : capitalize(prompt)
+  const { data: caseData } = await run(
+    db
+      .from('users')
+      .select('id, data->name, data->username, data->avatarUrl')
+      .eq('data->>username', casePrompt)
+      .limit(limit)
+  )
+
   const { data: similarData } = await run(
     db
       .from('users')
@@ -36,5 +48,5 @@ export async function searchUsers(prompt: string, limit: number) {
       } as any)
       .limit(limit)
   )
-  return uniqBy([...data, ...similarData], 'id').slice(0, limit)
+  return uniqBy([...data, ...caseData, ...similarData], 'id').slice(0, limit)
 }
