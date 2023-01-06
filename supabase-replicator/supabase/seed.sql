c9c34269a45c67d84197223b831d29c349fb1df9
James Grugett <jahooma@gmail.com>
2023-01-06 14:37:17 -0800

    seed.sql: Create user_recommendation_features and contract_recommendation_features tables

--- supabase-replicator/supabase/seed.sql
+++ supabase-replicator/supabase/seed.sql
@@ -221,6 +221,38 @@ drop policy if exists "public read" on test;
 create policy "public read" on test for select using (true);
 create index if not exists test_data_gin on test using GIN (data);
 
+create table if not exists user_recommendation_features (
+    user_id text not null primary key,
+    f0 real not null,
+    f1 real not null,
+    f2 real not null,
+    f3 real not null,
+    f4 real not null
+);
+alter table user_recommendation_features enable row level security;
+drop policy if exists "public read" on user_recommendation_features;
+create policy "public read" on user_recommendation_features for select using (true);
+drop policy if exists "admin write access" on user_recommendation_features;
+create policy "admin write access" on user_recommendation_features
+  as PERMISSIVE FOR ALL
+  to service_role;
+
+create table if not exists contract_recommendation_features (
+    contract_id text not null primary key,
+    f0 real not null,
+    f1 real not null,
+    f2 real not null,
+    f3 real not null,
+    f4 real not null
+);
+alter table contract_recommendation_features enable row level security;
+drop policy if exists "public read" on contract_recommendation_features;
+create policy "public read" on contract_recommendation_features for select using (true);
+drop policy if exists "admin write access" on contract_recommendation_features;
+create policy "admin write access" on contract_recommendation_features
+  as PERMISSIVE FOR ALL
+  to service_role;
+
 begin;
   drop publication if exists supabase_realtime;
   create publication supabase_realtime;
