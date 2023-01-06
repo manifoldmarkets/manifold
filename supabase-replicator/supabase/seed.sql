a57d13dae6ad6542d0141b55b2e175b9941ea346
Marshall Polaris <marshall@pol.rs>
2023-01-06 12:33:19 -0800

    Log document kind and write kind on replication failures

--- supabase-replicator/supabase/seed.sql
+++ supabase-replicator/supabase/seed.sql
@@ -272,7 +272,7 @@ declare dest_table table_spec;
 begin
   dest_spec = get_document_table_spec(r.doc_kind);
   if dest_spec is null then
-    raise warning 'Invalid document kind.';
+    raise warning 'Invalid document kind: %', r.doc_kind;
     return false;
   end if;
   if r.write_kind = 'create' or r.write_kind = 'update' then
@@ -308,7 +308,7 @@ begin
       );
     end if;
   else
-    raise warning 'Invalid write kind.';
+    raise warning 'Invalid write kind: %', r.write_kind;
     return false;
   end if;
   return true;
