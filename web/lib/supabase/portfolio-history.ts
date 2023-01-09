d22ad54716f007c5b73c3c8bc77fcea7c2996200
Ian Philips <iansphilips@gmail.com>
2023-01-09 10:36:52 -0700

    Sort on client

--- web/lib/supabase/portfolio-history.ts
+++ web/lib/supabase/portfolio-history.ts
@@ -1,6 +1,7 @@
 import { run } from 'common/supabase/utils'
 import { db } from 'web/lib/supabase/db'
 import { PortfolioMetrics } from 'common/user'
+import { sortBy } from 'lodash'
 
 export async function getPortfolioHistory(userId: string, cutoff: number) {
   const { data } = await run(
@@ -12,5 +13,5 @@ export async function getPortfolioHistory(userId: string, cutoff: number) {
       .eq('user_id', userId)
       .gt('data->>timestamp', cutoff)
   )
-  return data as PortfolioMetrics[]
+  return sortBy(data as PortfolioMetrics[], 'timestamp')
 }
