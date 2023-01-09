c9c4ff961f4e183f13276c02e3b1799e2ee9ac6f
Austin Chen <akrolsmir@gmail.com>
2023-01-09 14:41:31 -0800

    Re-insert preview, fix imgur link

--- common/charity.ts
+++ common/charity.ts
@@ -804,11 +804,11 @@ The goal of the project is to clearly present and organize the considerations wh
     tags: ['New'] as const,
   },
   {
-  name: 'MSI Reproductive Choices',
-  website: 'https://www.msichoices.org/',
-  photo: 'https://imgur.com/9cBHwWd',
-  preview:
-    `MSI Reproductive Choices provides contraception and safe abortion services that enable women all over the world to choose their own futures.
+    name: 'MSI Reproductive Choices',
+    website: 'https://www.msichoices.org/',
+    photo: 'https://i.imgur.com/9cBHwWd.png',
+    preview: `MSI Reproductive Choices provides contraception and safe abortion services that enable women all over the world to choose their own futures.`,
+    description: `MSI Reproductive Choices provides contraception and safe abortion services that enable women all over the world to choose their own futures.
 
 MSI Reproductive Choices is an international non-governmental organization providing contraception and safe abortion services in 37 countries around the world. MSI Reproductive Choices as an organisation lobbies in favour of access to abortion, and provides a variety of sexual and reproductive healthcare services including advice, vasectomies, and abortions in the UK and other countries where it is legal to do so. It is based in London and is a registered charity under English law.
 
@@ -817,8 +817,8 @@ Around the world, 32.6 million people are using a method of contraception provid
 The organization's core services include family planning; safe abortion and post-abortion care; maternal and child health care, including safe delivery and obstetrics; diagnosis and treatment of sexually transmitted infections; and HIV/AIDS prevention.
 
 The organization reports for the year 2021 to have prevented 14.1 million unintended pregnancies, averted 6.6 million unsafe abortions, and saved 39,000 women's lives.`,
-  tags: ['New'] as const,
-},
+    tags: ['New'] as const,
+  },
 ].map((charity) => {
   const slug = charity.name.toLowerCase().replace(/\s/g, '-')
   return {
