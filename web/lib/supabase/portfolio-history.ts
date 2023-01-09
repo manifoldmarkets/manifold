4ab613c5ffa00bce13bcc1b13bd2ad230be471ab
Ian Philips <iansphilips@gmail.com>
2023-01-09 09:28:04 -0700

    Query portfolio history via supabase

---
+++ web/lib/supabase/portfolio-history.ts
@@ -0,0 +1,16 @@
+import { run } from 'common/lib/supabase/utils'
+import { db } from 'web/lib/supabase/db'
+import { PortfolioMetrics } from 'common/user'
+
+export async function getPortfolioHistory(userId: string, cutoff: number) {
+  const { data } = await run(
+    db
+      .from('user_portfolio_history')
+      .select(
+        'data->timestamp, data->investmentValue, data->totalDeposits, data->balance'
+      )
+      .eq('user_id', userId)
+      .gt('data->>timestamp', cutoff)
+  )
+  return data as PortfolioMetrics[]
+}
