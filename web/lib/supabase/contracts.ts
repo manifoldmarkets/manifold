f63ebe37bf40e90b28879f1b1888434a4d1840b3
Ian Philips <iansphilips@gmail.com>
2023-01-10 17:09:09 -0700

    Fix import

--- web/lib/supabase/contracts.ts
+++ web/lib/supabase/contracts.ts
@@ -1,4 +1,4 @@
-import { run } from 'common/lib/supabase/utils'
+import { run } from 'common/supabase/utils'
 import { db } from 'web/lib/supabase/db'
 import { Contract } from 'common/contract'
 import { JsonData } from 'web/lib/supabase/json-data'
