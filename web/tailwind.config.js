e912975930e3557688b02383251a21a413df65fa
Ian Philips <iansphilips@gmail.com>
2023-01-12 10:53:35 -0700

    Make daily profit fun if profitable

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -326,6 +326,12 @@ module.exports = {
             display: 'none',
           },
         },
+        '.text-rainbow': {
+          background:
+            'linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #f9a825, #ff5722);',
+          '-webkit-background-clip': 'text',
+          '-webkit-text-fill-color': 'transparent',
+        },
       })
     }),
   ],
