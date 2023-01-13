6d05fb2726960a575f0e72145ee1e6594d197480
mantikoros <mantikoros@users.noreply.github.com>
2023-01-13 18:59:35 +0000

    Auto-prettification

--- web/components/resolution-panel.tsx
+++ web/components/resolution-panel.tsx
@@ -70,83 +70,83 @@ export function ResolutionPanel(props: {
       )}
     >
       <Col className="w-full justify-between rounded bg-white pb-6 pt-4 pl-1 pr-2 sm:px-2 md:px-6 md:py-8">
-      {isAdmin && !isCreator && (
-        <span className="bg-scarlet-50 text-scarlet-500 absolute right-4 top-4 rounded p-1 text-xs">
-          ADMIN
-        </span>
-      )}
-      <div className="mb-6">Resolve your market</div>
-      <YesNoCancelSelector
-        className="mx-auto my-2"
-        selected={outcome}
-        onSelect={setOutcome}
-      />
-
-      <Spacer h={4} />
-      {!!error && <div className="text-scarlet-500">{error}</div>}
-
-      <Row className={'items-center justify-between'}>
-        <div className="text-sm">
-          {outcome === 'YES' ? (
-            <>
-              Winnings will be paid out to {BETTORS} who bought YES.
-              {/* <br />
-            <br />
-            You will earn {earnedFees}. */}
-            </>
-          ) : outcome === 'NO' ? (
-            <>
-              Winnings will be paid out to {BETTORS} who bought NO.
-              {/* <br />
-            <br />
-            You will earn {earnedFees}. */}
-            </>
-          ) : outcome === 'CANCEL' ? (
-            <>Cancel all trades and return money back to {BETTORS}.</>
-          ) : outcome === 'MKT' ? (
-            <>
-              {capitalize(PLURAL_BETS)} will be paid out at the probability you
-              specify:
-            </>
-          ) : (
-            <span className="text-gray-500">
-              Resolving this market will immediately pay out {BETTORS}.
-            </span>
-          )}
-        </div>
-        {outcome === 'MKT' && (
-          <ProbabilityInput
-            prob={prob}
-            onChange={setProb}
-            inputClassName="w-28 mr-3 !h-11"
-          />
+        {isAdmin && !isCreator && (
+          <span className="bg-scarlet-50 text-scarlet-500 absolute right-4 top-4 rounded p-1 text-xs">
+            ADMIN
+          </span>
         )}
-        <ResolveConfirmationButton
-          color={
-            outcome === 'YES'
-              ? 'green'
-              : outcome === 'NO'
-              ? 'red'
-              : outcome === 'CANCEL'
-              ? 'yellow'
-              : outcome === 'MKT'
-              ? 'blue'
-              : 'indigo'
-          }
-          label={
-            outcome === 'CANCEL'
-              ? 'N/A'
-              : outcome === 'MKT'
-              ? `${prob}%`
-              : outcome ?? ''
-          }
-          marketTitle={contract.question}
-          disabled={!outcome}
-          onResolve={resolve}
-          isSubmitting={isSubmitting}
+        <div className="mb-6">Resolve your market</div>
+        <YesNoCancelSelector
+          className="mx-auto my-2"
+          selected={outcome}
+          onSelect={setOutcome}
         />
+
+        <Spacer h={4} />
+        {!!error && <div className="text-scarlet-500">{error}</div>}
+
+        <Row className={'items-center justify-between'}>
+          <div className="text-sm">
+            {outcome === 'YES' ? (
+              <>
+                Winnings will be paid out to {BETTORS} who bought YES.
+                {/* <br />
+            <br />
+            You will earn {earnedFees}. */}
+              </>
+            ) : outcome === 'NO' ? (
+              <>
+                Winnings will be paid out to {BETTORS} who bought NO.
+                {/* <br />
+            <br />
+            You will earn {earnedFees}. */}
+              </>
+            ) : outcome === 'CANCEL' ? (
+              <>Cancel all trades and return money back to {BETTORS}.</>
+            ) : outcome === 'MKT' ? (
+              <>
+                {capitalize(PLURAL_BETS)} will be paid out at the probability
+                you specify:
+              </>
+            ) : (
+              <span className="text-gray-500">
+                Resolving this market will immediately pay out {BETTORS}.
+              </span>
+            )}
+          </div>
+          {outcome === 'MKT' && (
+            <ProbabilityInput
+              prob={prob}
+              onChange={setProb}
+              inputClassName="w-28 mr-3 !h-11"
+            />
+          )}
+          <ResolveConfirmationButton
+            color={
+              outcome === 'YES'
+                ? 'green'
+                : outcome === 'NO'
+                ? 'red'
+                : outcome === 'CANCEL'
+                ? 'yellow'
+                : outcome === 'MKT'
+                ? 'blue'
+                : 'indigo'
+            }
+            label={
+              outcome === 'CANCEL'
+                ? 'N/A'
+                : outcome === 'MKT'
+                ? `${prob}%`
+                : outcome ?? ''
+            }
+            marketTitle={contract.question}
+            disabled={!outcome}
+            onResolve={resolve}
+            isSubmitting={isSubmitting}
+          />
         </Row>
-        </Col>
+      </Col>
     </Col>
   )
 }
