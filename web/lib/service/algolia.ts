c3ba645a32fdee05baf854d4aa649f9d71b868da
James Grugett <jahooma@gmail.com>
2023-01-14 00:47:32 -0800

    Omni search: Show results after all subqueries loaded.

--- web/lib/service/algolia.ts
+++ web/lib/service/algolia.ts
@@ -1,5 +1,6 @@
 import algoliasearch from 'algoliasearch/lite'
 import { ENV } from 'common/envs/constants'
+import { Contract } from '../firebase/contracts'
 
 export const searchClient = algoliasearch(
   'GJQPAYENIF',
@@ -19,3 +20,9 @@ export const newIndex = searchClient.initIndex(getIndexName('newest'))
 export const dailyScoreIndex = searchClient.initIndex(
   getIndexName('daily-score')
 )
+
+const searchIndex = searchClient.initIndex(searchIndexName)
+export const searchContracts = async (query: string, limit: number) => {
+  const { hits } = await searchIndex.search(query, { hitsPerPage: limit })
+  return hits as any as Contract[]
+}
