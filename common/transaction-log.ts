8e48bb14453a3045320393276599c521c598ecb7
Marshall Polaris <marshall@pol.rs>
2023-01-16 15:22:01 -0800

    Log table names for writes to pub/sub

--- common/transaction-log.ts
+++ common/transaction-log.ts
@@ -1,3 +1,5 @@
+import { Database } from './supabase/schema'
+
 export type DocumentKind =
   | 'user'
   | 'userFollow'
@@ -26,6 +28,7 @@ export type WriteDocument = { [k: string]: any }
 
 export type TLEntry<T extends WriteDocument = WriteDocument> = {
   eventId: string
+  tableId: keyof Database['public']['Tables']
   docKind: DocumentKind
   writeKind: WriteKind
   docId: string
--- functions/src/log-writes.ts
+++ functions/src/log-writes.ts
@@ -3,6 +3,9 @@ import * as functions from 'firebase-functions'
 import { Change, EventContext } from 'firebase-functions'
 import { PubSub } from '@google-cloud/pubsub'
 import { DocumentKind, TLEntry } from '../../common/transaction-log'
+import { Database } from '../../common/supabase/schema'
+
+type TableName = keyof Database['public']['Tables']
 
 const pubSubClient = new PubSub()
 
@@ -22,11 +25,13 @@ function getWriteInfo<T>(change: Change<DocumentSnapshot<T>>) {
 function getTLEntry<T extends DocumentData>(
   change: Change<DocumentSnapshot<T>>,
   context: EventContext,
-  docKind: DocumentKind
+  docKind: DocumentKind,
+  tableName: TableName
 ): TLEntry<T> {
   const info = getWriteInfo(change)
   return {
     docKind,
+    tableId: tableName,
     writeKind: info.kind,
     eventId: context.eventId,
     docId: info.ref.id,
@@ -37,69 +42,82 @@ function getTLEntry<T extends DocumentData>(
   }
 }
 
-function logger(path: string, docKind: DocumentKind) {
+function logger(path: string, docKind: DocumentKind, tableName: TableName) {
   return functions.firestore.document(path).onWrite((change, ctx) => {
-    const entry = getTLEntry(change, ctx, docKind)
+    const entry = getTLEntry(change, ctx, docKind, tableName)
     return pubSubClient.topic('firestoreWrite').publishMessage({ json: entry })
   })
 }
 
-export const logUsers = logger('users/{id}', 'user')
+export const logUsers = logger('users/{id}', 'user', 'users')
 export const logUserPortfolioHistories = logger(
   'users/{parent}/portfolioHistory/{id}',
-  'userPortfolioHistory'
+  'userPortfolioHistory',
+  'user_portfolio_history'
 )
 export const logUserContractMetrics = logger(
   'users/{parent}/contract-metrics/{id}',
-  'userContractMetrics'
+  'userContractMetrics',
+  'user_contract_metrics'
 )
 export const logUserFollows = logger(
   'users/{parent}/follows/{id}',
-  'userFollow'
+  'userFollow',
+  'user_follows'
 )
 export const logUserReactions = logger(
   'users/{parent}/reactions/{id}',
-  'userReaction'
+  'userReaction',
+  'user_reactions'
 )
 export const logUserEvents = logger(
   'users/{parent}/events/{id}',
-  'userEvent'
+  'userEvent',
+  'user_events'
 )
 export const logUserSeenMarkets = logger(
   'private-users/{parent}/seenMarkets/{id}',
-  'userSeenMarket'
+  'userSeenMarket',
+  'user_seen_markets'
 )
-export const logContracts = logger('contracts/{id}', 'contract')
+export const logContracts = logger('contracts/{id}', 'contract', 'contracts')
 export const logContractAnswers = logger(
   'contracts/{parent}/answers/{id}',
-  'contractAnswer'
+  'contractAnswer',
+  'contract_answers'
 )
 export const logContractBets = logger(
   'contracts/{parent}/bets/{id}',
-  'contractBet'
+  'contractBet',
+  'contract_bets'
 )
 export const logContractComments = logger(
   'contracts/{parent}/comments/{id}',
-  'contractComment'
+  'contractComment',
+  'contract_comments'
 )
 export const logContractFollows = logger(
   'contracts/{parent}/follows/{id}',
-  'contractFollow'
+  'contractFollow',
+  'contract_follows'
 )
 export const logContractLiquidity = logger(
   'contracts/{parent}/liquidity/{id}',
-  'contractLiquidity'
+  'contractLiquidity',
+  'contract_liquidity'
 )
-export const logGroups = logger('groups/{id}', 'group')
+export const logGroups = logger('groups/{id}', 'group', 'groups')
 export const logGroupContracts = logger(
   'groups/{parent}/groupContracts/{id}',
-  'groupContract'
+  'groupContract',
+  'group_contracts'
 )
 export const logGroupMembers = logger(
   'groups/{parent}/groupMembers/{id}',
-  'groupMember'
+  'groupMember',
+  'group_members'
 )
-export const logTxns = logger('txns/{id}', 'txn')
-export const logManalinks = logger('manalinks/{id}', 'manalink')
-export const logPosts = logger('posts/{id}', 'post')
-export const logTest = logger('test/{id}', 'test')
+export const logTxns = logger('txns/{id}', 'txn', 'txns')
+export const logManalinks = logger('manalinks/{id}', 'manalink', 'manalinks')
+export const logPosts = logger('posts/{id}', 'post', 'posts')
+export const logTest = logger('test/{id}', 'test', 'test')
