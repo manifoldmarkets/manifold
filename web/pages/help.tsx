3bc348cc2e4e5203f1a05d64fda7d8bf8fd5bd6c
James Grugett <jahooma@gmail.com>
2023-01-16 17:09:45 -0800

    Add help page that tests friend's MagicForm product for Manifold.

---
+++ web/pages/help.tsx
@@ -0,0 +1,20 @@
+import { Page } from 'web/components/layout/page'
+
+export default function Help() {
+  return (
+    <Page>
+      <iframe
+        width="100%"
+        height="900"
+        src="https://assets.titanx.ai/?user_id=ab401bde-a675-48fe-a649-bb5e77c01a90&stack_id=stack_7c81aa30-27b9-43c2-85a5-d6b4c24e5118&domain=https://manifold.markets"
+        style={{
+          border: 0,
+          position: 'fixed',
+          bottom: 0,
+          left: '50%',
+          transform: 'translateX(-50%); z-index: 9999;',
+        }}
+      />
+    </Page>
+  )
+}
