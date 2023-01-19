537fa9ec2ee03c69e80af3a5d034136eaccdac55
mantikoros <sgrugett@gmail.com>
2023-01-19 12:21:26 -0600

    generate embeddings script

---
+++ functions/src/scripts/generate-embeddings.ts
@@ -0,0 +1,50 @@
+import * as admin from 'firebase-admin'
+import { initAdmin } from './script-init'
+
+initAdmin()
+
+import { generateEmbeddings } from '../helpers/openai-utils'
+import { createSupabaseClient } from '../supabase/init'
+import { run } from 'common/supabase/utils'
+import { Contract } from 'common/contract'
+
+const firestore = admin.firestore()
+const db = createSupabaseClient()
+
+async function main() {
+  const result = await run(db.from('contract_embeddings').select('contract_id'))
+
+  const contractIds = new Set(result.data.map((row: any) => row.contract_id))
+  console.log('Got', contractIds.size, 'markets with preexisting embeddings')
+
+  const snapshot = await firestore
+    .collection('contracts')
+    .orderBy('popularityScore', 'desc')
+    .get()
+
+  const contracts = snapshot.docs
+    .map((doc) => doc.data() as Contract)
+    .filter((c) => !contractIds.has(c.id))
+
+  console.log('Got', contracts.length, 'markets to process')
+
+  for (const contract of contracts) {
+    const { id, question } = contract
+    const embeddings = await generateEmbeddings(question)
+    if (!embeddings) {
+      console.log('No embeddings for', question)
+      continue
+    }
+
+    console.log('Generated', embeddings?.length, 'embeddings for', question)
+    await run(
+      db
+        .from('contract_embeddings')
+        .upsert({ contract_id: id, embeddings: embeddings })
+    )
+  }
+}
+
+if (require.main === module) {
+  main().then(() => process.exit())
+}
