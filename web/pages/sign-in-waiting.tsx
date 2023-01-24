f2e02f7fe1a0837b9910e6308f24c92eec18ec8e
Ian Philips <iansphilips@gmail.com>
2023-01-24 14:47:15 +0100

    Hide try again button

--- web/pages/sign-in-waiting.tsx
+++ web/pages/sign-in-waiting.tsx
@@ -1,20 +1,8 @@
 import { Col } from 'web/components/layout/col'
 import { useRedirectIfSignedIn } from 'web/hooks/use-redirect-if-signed-in'
-import { useEffect, useState } from 'react'
-import { Button } from 'web/components/buttons/button'
-import { firebaseLogout } from 'web/lib/firebase/users'
 
 export default function SignInWaiting() {
   useRedirectIfSignedIn()
-  const [showSignOutButton, setShowSignOutButton] = useState(false)
-
-  // Allow user to sign out if they've arrived here erroneously
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setShowSignOutButton(true)
-    }, 5000)
-    return () => clearTimeout(timeout)
-  }, [])
   return (
     <Col className="h-[100vh] items-center justify-center">
       <img
@@ -23,11 +11,6 @@ export default function SignInWaiting() {
         width={200}
         height={200}
       />
-      {showSignOutButton && (
-        <Button color={'gray'} onClick={firebaseLogout}>
-          Try again
-        </Button>
-      )}
     </Col>
   )
 }
