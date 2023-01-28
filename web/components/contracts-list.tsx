fb7da7e69f5f7b688142513dac45e6903d020f89
Austin Chen <akrolsmir@gmail.com>
2023-01-27 20:13:13 -0800

    Toggle ContractsGrid to use as list

---
+++ web/components/contracts-list.tsx
@@ -0,0 +1,51 @@
+import { Contract } from 'common/contract'
+import { ContractCard } from './contract/contract-card'
+import { ContractMention } from './contract/contract-mention'
+import { Col } from './layout/col'
+import Tippy from '@tippyjs/react'
+import { VisibilityObserver } from './widgets/visibility-observer'
+import { useCallback } from 'react'
+import { LoadingIndicator } from './widgets/loading-indicator'
+
+export function ContractsList(props: {
+  contracts: Contract[] | undefined
+  loadMore?: () => void
+}) {
+  const { contracts, loadMore } = props
+  const onVisibilityUpdated = useCallback(
+    (visible: boolean) => {
+      if (visible && loadMore) {
+        loadMore()
+      }
+    },
+    [loadMore]
+  )
+
+  if (contracts === undefined) {
+    return <LoadingIndicator />
+  }
+
+  return (
+    <Col className="gap-2">
+      {contracts.map((contract) => (
+        <Tippy
+          interactive
+          duration={0}
+          placement="bottom-end"
+          content={<ContractCard contract={contract} key={contract.id} />}
+        >
+          <span tabIndex={0}>
+            <ContractMention contract={contract} />
+          </span>
+        </Tippy>
+      ))}
+
+      {loadMore && (
+        <VisibilityObserver
+          onVisibilityUpdated={onVisibilityUpdated}
+          className="relative -top-96 h-1"
+        />
+      )}
+    </Col>
+  )
+}
