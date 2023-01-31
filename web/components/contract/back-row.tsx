90692d797c4fcfef73cf69a35be897b1cb07c41b
Sinclair Chen <abc.sinclair@gmail.com>
2023-01-30 16:04:58 -0800

    refactor back button to be rectangle

--- web/components/contract/back-row.tsx
+++ web/components/contract/back-row.tsx
@@ -29,12 +29,13 @@ export function BackRow() {
     <header className="top-2 w-full border-b border-gray-200 lg:hidden">
       <Row className="items-center justify-between gap-2 bg-white">
         <div className="flex flex-1">
-          <Button color={'gray-white'} className={''} onClick={router.back}>
-            <Row className={'items-center text-xs text-indigo-700'}>
-              <ArrowLeftIcon className="mr-2 h-5 w-5" aria-hidden="true" />
-              Back
-            </Row>
-          </Button>
+          <button
+            className="flex px-3 py-2 text-sm text-indigo-700 hover:bg-gray-200 "
+            onClick={router.back}
+          >
+            <ArrowLeftIcon className="mr-2 h-5 w-5" aria-hidden />
+            Back
+          </button>
         </div>
       </Row>
     </header>
