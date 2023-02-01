e7e84937480b81c9d335c77d9690c01db1c8daac
Sinclair Chen <abc.sinclair@gmail.com>
2023-02-01 14:36:38 -0800

    refactor emoji list to use same mention renderer

--- web/components/editor/emoji/emoji-suggestion.ts
+++ web/components/editor/emoji/emoji-suggestion.ts
@@ -1,15 +1,13 @@
-import { ReactRenderer } from '@tiptap/react'
 import { SuggestionOptions } from '@tiptap/suggestion'
 import { beginsWith, searchInAny } from 'common/util/parse'
-import tippy from 'tippy.js'
 import { EmojiList } from './emoji-list'
 import { invertBy, orderBy } from 'lodash'
 import shortcodes from './github-shortcodes.json' // from https://api.github.com/emojis
 import { PluginKey } from 'prosemirror-state'
 import { ENV_CONFIG } from 'common/envs/constants'
+import { makeMentionRender } from '../user-mention/mention-suggestion'
 
 type Suggestion = Omit<SuggestionOptions, 'editor'>
-type Render = Suggestion['render']
 
 export interface EmojiData {
   shortcodes: string[] // ['grin']
@@ -72,60 +70,4 @@ export const emojiSuggestion: Suggestion = {
       .insertContentAt(range, props.character + ' ')
       .run()
   },
-}
-
-export function makeMentionRender(mentionList: any): Render {
-  return () => {
-    let component: ReactRenderer
-    let popup: ReturnType<typeof tippy>
-    return {
-      onStart: (props) => {
-        component = new ReactRenderer(mentionList, {
-          props,
-          editor: props.editor,
-        })
-        if (!props.clientRect) {
-          return
-        }
-
-        popup = tippy('body', {
-          getReferenceClientRect: props.clientRect as any,
-          appendTo: () => document.body,
-          content: component?.element,
-          showOnCreate: true,
-          interactive: true,
-          trigger: 'manual',
-          placement: 'bottom-start',
-        })
-      },
-      onUpdate(props) {
-        component?.updateProps(props)
-
-        if (!props.clientRect) {
-          return
-        }
-
-        popup?.[0].setProps({
-          getReferenceClientRect: props.clientRect as any,
-        })
-      },
-      onKeyDown(props) {
-        if (props.event.key)
-          if (
-            props.event.key === 'Escape' ||
-            // Also break out of the mention if the tooltip isn't visible
-            (props.event.key === 'Enter' && !popup?.[0].state.isShown)
-          ) {
-            popup?.[0].destroy()
-            component?.destroy()
-            return false
-          }
-        return (component?.ref as any)?.onKeyDown(props)
-      },
-      onExit() {
-        popup?.[0].destroy()
-        component?.destroy()
-      },
-    }
-  }
 }
