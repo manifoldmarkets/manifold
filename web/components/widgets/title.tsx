6ed495fa93d796e5669f7c0b7ad32356a1a6b927
Sinclair Chen <abc.sinclair@gmail.com>
2023-02-01 15:04:56 -0800

    Remove textColor prop

--- web/components/widgets/title.tsx
+++ web/components/widgets/title.tsx
@@ -1,18 +1,13 @@
 import clsx from 'clsx'
 import { ReactNode } from 'react'
 
-export function Title(props: {
-  className?: string
-  children?: ReactNode
-  textColor?: string
-}) {
-  const { children, className, textColor } = props
+export function Title(props: { className?: string; children?: ReactNode }) {
+  const { children, className } = props
   return (
     <h1
       className={clsx(
-        'my-4 inline-block text-2xl font-normal sm:my-6 sm:text-3xl',
-        className,
-        textColor || 'text-indigo-700'
+        'my-4 inline-block text-2xl font-normal text-indigo-700 sm:my-6 sm:text-3xl',
+        className
       )}
     >
       {children}
