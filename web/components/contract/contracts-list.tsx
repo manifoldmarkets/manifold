babf11d8af59f741b43557f8030eb18902a68d47
Austin Chen <akrolsmir@gmail.com>
2023-02-01 19:21:59 -0800

    Don't use Tippy cards on mobile

--- web/components/contract/contracts-list.tsx
+++ web/components/contract/contracts-list.tsx
@@ -6,6 +6,7 @@ import { VisibilityObserver } from '../widgets/visibility-observer'
 import { useCallback } from 'react'
 import { LoadingIndicator } from '../widgets/loading-indicator'
 import { ContractsListEntry } from './contracts-list-entry'
+import { useIsMobile } from 'web/hooks/use-is-mobile'
 
 export function ContractsList(props: {
   contracts: Contract[] | undefined
@@ -28,24 +29,7 @@ export function ContractsList(props: {
   return (
     <Col className="gap-2">
       {contracts.map((contract) => (
-        <Tippy
-          interactive
-          duration={0}
-          placement="bottom-end"
-          content={
-            <ContractCard
-              contract={contract}
-              key={contract.id}
-              showImage
-              showDescription
-              className="w-[400px]"
-            />
-          }
-        >
-          <span tabIndex={0}>
-            <ContractsListEntry contract={contract} />
-          </span>
-        </Tippy>
+        <DesktopPopover contract={contract} key={contract.id}></DesktopPopover>
       ))}
 
       {loadMore && (
@@ -55,5 +39,33 @@ export function ContractsList(props: {
         />
       )}
     </Col>
+  )
+}
+
+function DesktopPopover(props: { contract: Contract }) {
+  const { contract } = props
+  const isMobile = useIsMobile()
+  if (isMobile) {
+    return <ContractsListEntry contract={contract} />
+  }
+
+  return (
+    <Tippy
+      interactive
+      duration={0}
+      placement="bottom-end"
+      content={
+        <ContractCard
+          contract={contract}
+          showImage
+          showDescription
+          className="w-[350px]"
+        />
+      }
+    >
+      <span tabIndex={0}>
+        <ContractsListEntry contract={contract} />
+      </span>
+    </Tippy>
   )
 }
