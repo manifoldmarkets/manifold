88802ac461771f101cb953bbaac59282f702e960
mantikoros <sgrugett@gmail.com>
2023-02-01 19:18:51 -0500

    groundhog day easter egg

--- web/components/nav/manifold-logo.tsx
+++ web/components/nav/manifold-logo.tsx
@@ -1,8 +1,9 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import clsx from 'clsx'
 
-import { useUser } from 'web/hooks/use-user'
 import { ENV_CONFIG } from 'common/envs/constants'
+import { useUser } from 'web/hooks/use-user'
 
 export function ManifoldLogo(props: {
   className?: string
@@ -13,15 +14,30 @@ export function ManifoldLogo(props: {
   const { darkBackground, className, hideText, twoLine } = props
 
   const user = useUser()
+  const [showHog, setShowHog] = useState(false)
 
   return (
     <Link
-      href={user ? '/home' : '/'}
+      href={
+        showHog
+          ? '/TaiRuiYang/will-punxsutawney-phil-see-his-shad-e4ee552c2911'
+          : user
+          ? '/home'
+          : '/'
+      }
       className={clsx('group flex flex-shrink-0 flex-row gap-4', className)}
     >
       <img
         className="transition-all group-hover:rotate-12"
-        src={darkBackground ? '/logo-white.svg' : '/logo.svg'}
+        onMouseEnter={() => setShowHog(true)}
+        onMouseOut={() => setShowHog(false)}
+        src={
+          showHog
+            ? '/groundhog.png'
+            : darkBackground
+            ? '/logo-white.svg'
+            : '/logo.svg'
+        }
         width={45}
         height={45}
         alt=""
