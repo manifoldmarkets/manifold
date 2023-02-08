ec00a1c569106650d5b2298e48ec79d4082482cb
Ian Philips <iansphilips@gmail.com>
2023-02-08 11:16:21 -0500

    Protect scheduled function

--- functions/src/test-scheduled-function.ts
+++ functions/src/test-scheduled-function.ts
@@ -1,15 +1,16 @@
-import { newEndpoint } from './api'
+import { APIError, newEndpoint } from './api'
 import {
   saveWeeklyContractMetricsInternal,
   sendWeeklyPortfolioUpdateNotifications,
 } from 'functions/src/weekly-portfolio-updates'
+import { isProd } from 'functions/src/utils'
 
 // Function for testing scheduled functions locally
 export const testscheduledfunction = newEndpoint(
   { method: 'GET', memory: '4GiB' },
   async (_req) => {
-    // if (isProd())
-    //   throw new APIError(400, 'This function is only available in dev mode')
+    if (isProd())
+      throw new APIError(400, 'This function is only available in dev mode')
 
     await saveWeeklyContractMetricsInternal()
     await sendWeeklyPortfolioUpdateNotifications()
