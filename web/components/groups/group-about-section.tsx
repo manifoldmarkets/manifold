64bc398ec2f482f38bbb70ef4a374a0816a43709
ingawei <ingawei@gmail.com>
2023-02-08 17:51:37 -0800

    fixed logged out and about showing twice bug

--- web/components/groups/group-about-section.tsx
+++ web/components/groups/group-about-section.tsx
@@ -44,6 +44,7 @@ export function GroupAboutSection(props: {
   setWritingNewAbout: (writingNewAbout: boolean) => void
 }) {
   const { group, canEdit, post, writingNewAbout, setWritingNewAbout } = props
+  console.log(canEdit)
   if ((post && post.content) || writingNewAbout) {
     return (
       <Col className="group my-2 gap-2 px-4 py-2 lg:px-0">
@@ -56,7 +57,7 @@ export function GroupAboutSection(props: {
             setWritingNewAbout={setWritingNewAbout}
           />
         )}
-        {canEdit && post && post.content && (
+        {!canEdit && post && post.content && (
           <ExpandableContent
             content={post.content}
             modalContent={
