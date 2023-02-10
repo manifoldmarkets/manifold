ec12d73a0eefeb2788901920f3e0cbd504381175
mantikoros <sgrugett@gmail.com>
2023-02-10 17:02:16 -0600

    creator share panel: always open, a/b test title

--- web/components/contract/creator-share-panel.tsx
+++ web/components/contract/creator-share-panel.tsx
@@ -1,7 +1,8 @@
 import { Contract } from 'common/contract'
-import { REFERRAL_AMOUNT } from 'common/economy'
+import { REFERRAL_AMOUNT, UNIQUE_BETTOR_BONUS_AMOUNT } from 'common/economy'
 import { formatMoney } from 'common/util/format'
 import { getShareUrl } from 'common/util/share'
+import { useABTest } from '../../hooks/use-ab-test'
 import { CopyLinkButton } from '../buttons/copy-link-button'
 import { TweetButton } from '../buttons/tweet-button'
 import { Spacer } from '../layout/spacer'
@@ -10,14 +11,19 @@ import { GradientContainer } from '../widgets/gradient-container'
 export function CreatorSharePanel(props: { contract: Contract }) {
   const { contract } = props
 
+  const title = useABTest('creator share panel title', {
+    share: 'Share your market',
+    earn: `Earn up to ${formatMoney(10000)}`,
+  })
+
   return (
     <GradientContainer className="mb-8 p-4">
-      <div className="mb-6">Share your market</div>
+      <div className="mb-6">{title}</div>
 
       <div className="mb-2 text-base text-gray-500">
-        Invite traders to participate in your market! Earn a{' '}
-        {formatMoney(REFERRAL_AMOUNT)} referral bonus for each new trader that
-        signs up.
+        Earn a {formatMoney(REFERRAL_AMOUNT)} referral bonus for each new sign
+        up and {formatMoney(UNIQUE_BETTOR_BONUS_AMOUNT)} for each unique trader
+        that bets in your market using your share link.
       </div>
 
       <CopyLinkButton
