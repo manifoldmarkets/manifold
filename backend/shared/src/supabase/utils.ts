70575caa1684a47e00d717fee37033b118bdc5fd
James Grugett <jahooma@gmail.com>
2023-02-14 13:07:09 -0800

    scoreContracts: bulk update using direct supabase connection

--- backend/shared/src/supabase/utils.ts
+++ backend/shared/src/supabase/utils.ts
@@ -12,3 +12,23 @@ export async function bulkInsert<
     await db.none(query)
   }
 }
+
+export async function bulkUpdate<
+  T extends TableName,
+  ColumnValues extends Tables[T]['Update'],
+  Row extends Tables[T]['Row']
+>(
+  db: SupabaseDirectClient,
+  table: T,
+  idField: keyof Row,
+  values: ColumnValues[]
+) {
+  if (values.length) {
+    const columnNames = Object.keys(values[0])
+    const cs = new pgp.helpers.ColumnSet(columnNames, { table })
+    const query =
+      pgp.helpers.update(values, cs) +
+      ` WHERE v.${String(idField)} = t.${String(idField)}`
+    await db.none(query)
+  }
+}
