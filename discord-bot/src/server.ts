fa5200a4ac755000e459d83f26e545bf12353ce1
Ian Philips <iansphilips@gmail.com>
2023-02-23 10:41:12 -0800

    Comment to response

--- discord-bot/src/server.ts
+++ discord-bot/src/server.ts
@@ -3,11 +3,12 @@ import * as process from 'process'
 
 const hostname = '0.0.0.0'
 const port = process.env.PORT ?? '8080'
-// This server is just to let the GCP Cloud Run service know we're ready, we don't need it if we move off Cloud Run
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World')
+  res.end(
+    `Hello! This server is just to let the GCP Cloud Run service know we're ready, and we don't need it if we move off Cloud Run`
+  )
 })
 
 export const startListener = () =>
