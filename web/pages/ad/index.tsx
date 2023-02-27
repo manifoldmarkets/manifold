f7bee332856c321929878721eb0125e0eb8c6b36
Sinclair Chen <abc.sinclair@gmail.com>
2023-02-26 21:57:33 -0800

    wip

---
+++ web/pages/ad/index.tsx
@@ -0,0 +1,15 @@
+import { Page } from 'web/components/layout/page'
+import { NoSEO } from 'web/components/NoSEO'
+
+// export function getServersideProps() {
+// get user's
+// }
+
+export default function AdsPage() {
+  return (
+    <Page>
+      <NoSEO />
+      <div className="flex flex-col bg-white"></div>
+    </Page>
+  )
+}
