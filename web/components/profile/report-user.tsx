3bc8ab72d2c2fb3279095e750bd29ff086c249db
ingawei <ingawei@gmail.com>
2023-02-28 13:45:29 -0800

    dark mode quick fixes

--- web/components/profile/report-user.tsx
+++ web/components/profile/report-user.tsx
@@ -95,7 +95,9 @@ export const ReportUser = (props: {
                 'Add more context and/or provide a link to the content'
               }
               rows={2}
-              className={'border-ink-300 -ml-2 rounded-md border p-2'}
+              className={
+                'border-ink-300 bg-canvas-0 -ml-2 rounded-md border p-2'
+              }
               value={otherReportType}
               onChange={(e) => setOtherReportType(e.target.value)}
             />
