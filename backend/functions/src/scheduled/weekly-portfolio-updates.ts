a73547998a3c8d635e389b70f2de3c4f938876eb
Ian Philips <iansphilips@gmail.com>
2023-03-03 13:46:37 -0700

    Load weekly profit points in static props

--- backend/functions/src/scheduled/weekly-portfolio-updates.ts
+++ backend/functions/src/scheduled/weekly-portfolio-updates.ts
@@ -6,10 +6,8 @@ import { getUsersContractMetricsOrderedByProfit } from 'common/supabase/contract
 import { createWeeklyPortfolioUpdateNotification } from '../create-notification'
 import { getUsernameById } from 'common/supabase/users'
 import { createSupabaseClient } from 'shared/supabase/init'
-import { log } from 'shared/utils'
+import { getUser, log } from 'shared/utils'
 import { WeeklyPortfolioUpdate } from 'common/weekly-portfolio-update'
-import { DAY_MS } from 'common/util/time'
-import { getPortfolioHistories } from 'common/supabase/portfolio-metrics'
 import { PrivateUser } from 'common/user'
 
 const firestore = admin.firestore()
@@ -17,12 +15,12 @@ const now = new Date()
 const time = now.getTime()
 const getDateSlug = () =>
   `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
-const USERS_TO_SAVE = 100
-// Saving metrics should work until USERS_TO_SAVE * 5*60 users
+const USERS_TO_SAVE = 300
+// Saving metrics should work until our users are greater than USERS_TO_SAVE * 2*60 users
 export const saveWeeklyContractMetrics = functions
   .runWith({ memory: '4GB', secrets: ['SUPABASE_KEY'], timeoutSeconds: 60 })
-  // every minute for 5 hours Friday 1am PT (UTC -08:00)
-  .pubsub.schedule('* 9-14 * * 5')
+  // every minute for 2 hours Friday 4am PT (UTC -08:00)
+  .pubsub.schedule('* 13-14 * * 5')
   .timeZone('Etc/UTC')
   .onRun(async () => {
     await saveWeeklyContractMetricsInternal()
@@ -50,13 +48,17 @@ export const saveWeeklyContractMetricsInternal = async () => {
     )
     .get()
   const privateUsers = users.docs.map((doc) => doc.data() as PrivateUser)
-  const snap = await firestore
+
+  // This is a lot of reads on firebase, we should save these in supabase instead
+  const alreadyUpdatedSnap = await firestore
     .collectionGroup(`weekly-update`)
     .where('rangeEndDateSlug', '==', getDateSlug())
     .get()
 
-  // get the parent ref is the user id
-  const weeklyUpdatedUsers = snap.docs.map((doc) => doc.ref.parent.parent?.id)
+  // The parent ref is the user id
+  const weeklyUpdatedUsers = alreadyUpdatedSnap.docs.map(
+    (doc) => doc.ref.parent.parent?.id
+  )
   log('already updated users', weeklyUpdatedUsers.length, 'at', time)
   // filter out the users who have already had their weekly update saved
   const usersToSave = privateUsers
@@ -77,35 +79,18 @@ export const saveWeeklyContractMetricsInternal = async () => {
     return
   }
 
-  const allPortfolioMetrics = await getPortfolioHistories(
-    usersToSave.map((u) => u.id),
-    time - 7 * DAY_MS,
-    db
-  )
-  if (Object.keys(allPortfolioMetrics).length === 0) {
-    log('Error: no portfolio metrics to save')
-    return
-  }
-
-  // If the function doesn't complete, filter by those who haven't had their weekly update saved yet
   const results = sortBy(
     await Promise.all(
-      usersToSave.map(async (user) => {
-        const portfolioMetrics = (allPortfolioMetrics[user.id] ?? []).map(
-          (p) => ({
-            x: p.timestamp,
-            y: p.balance + p.investmentValue - p.totalDeposits,
-          })
-        )
-        const contractMetrics = usersToContractMetrics[user.id]
+      usersToSave.map(async (privateUser) => {
+        const user = await getUser(privateUser.id)
+        const contractMetrics = usersToContractMetrics[privateUser.id]
         return {
           contractMetrics,
-          userId: user.id,
-          weeklyProfit: sum(
-            contractMetrics.map((m) => m.from?.week.profit ?? 0)
-          ),
+          userId: privateUser.id,
+          weeklyProfit:
+            user?.profitCached.weekly ??
+            sum(contractMetrics.map((m) => m.from?.week.profit ?? 0)),
           rangeEndDateSlug: getDateSlug(),
-          profitPoints: portfolioMetrics,
           createdTime: time,
         } as Omit<WeeklyPortfolioUpdate, 'id'>
       })
@@ -149,13 +134,12 @@ export const sendWeeklyPortfolioUpdateNotifications = async () => {
     privateUsers.map(async (privateUser) => {
       const data = await getUsersWeeklyUpdate(privateUser.id, getDateSlug())
       if (!data) return
-      const { weeklyProfit, rangeEndDateSlug, contractMetrics, profitPoints } =
-        data
+      const { weeklyProfit, rangeEndDateSlug, contractMetrics } = data
       // Don't send update if there are no contracts
       count++
       if (count % 100 === 0)
         log('sent weekly portfolio updates to', count, '/', privateUsers.length)
-      if (contractMetrics.length === 0 || profitPoints.length === 0) return
+      if (contractMetrics.length === 0) return
       await createWeeklyPortfolioUpdateNotification(
         privateUser,
         userData[privateUser.id].username,
--- common/src/supabase/portfolio-metrics.ts
+++ common/src/supabase/portfolio-metrics.ts
@@ -5,21 +5,24 @@ import { chunk, Dictionary, sortBy } from 'lodash'
 
 export async function getPortfolioHistory(
   userId: string,
-  cutoff: number,
-  db: SupabaseClient
+  start: number,
+  db: SupabaseClient,
+  end?: number
 ) {
-  const { data } = await run(
-    selectFrom(
-      db,
-      'user_portfolio_history',
-      'timestamp',
-      'investmentValue',
-      'totalDeposits',
-      'balance'
-    )
-      .eq('user_id', userId)
-      .gt('data->>timestamp', cutoff)
+  let query = selectFrom(
+    db,
+    'user_portfolio_history',
+    'timestamp',
+    'investmentValue',
+    'totalDeposits',
+    'balance'
   )
+    .eq('user_id', userId)
+    .gt('data->>timestamp', start)
+  if (end) {
+    query = query.lt('data->>timestamp', end)
+  }
+  const { data } = await run(query)
   return sortBy(data, 'timestamp')
 }
 
