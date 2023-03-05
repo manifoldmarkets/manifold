4a432d6d87a935162428a29ec3910239ff932622
Marshall Polaris <marshall@pol.rs>
2023-03-04 23:15:52 -0800

    Tidy up `updategroupmetrics`

--- backend/functions/src/scheduled/update-group-metrics.ts
+++ backend/functions/src/scheduled/update-group-metrics.ts
@@ -5,9 +5,8 @@ import { groupBy, mapValues, sortBy } from 'lodash'
 import { log } from 'shared/utils'
 import { newEndpointNoAuth } from '../api/helpers'
 import { invokeFunction } from 'shared/utils'
+import { getIds } from 'shared/supabase/utils'
 import { createSupabaseDirectClient } from 'shared/supabase/init'
-
-const firestore = admin.firestore()
 
 export const scheduleUpdateGroupMetrics = functions.pubsub
   .schedule('every 15 minutes')
@@ -33,24 +32,24 @@ export const updategroupmetrics = newEndpointNoAuth(
 )
 
 export async function updateGroupMetrics() {
+  const firestore = admin.firestore()
   const pg = createSupabaseDirectClient()
-  log('Loading groups...')
-  const groups = await firestore.collection('groups').select().get()
-  log(`Loaded ${groups.size} groups.`)
+  log('Loading group IDs...')
+  const groupIds = await getIds(pg, 'groups')
+  log(`Loaded ${groupIds.length} group IDs.`)
 
   log(`Loading contract creator scores...`)
   const groupCreatorScores = await pg.manyOrNone(
     `with creator_scores as (
-      select gc.group_id, c.data->>'creatorId' as user_id, count(*) as score
+      select
+        gc.group_id, c.data->>'creatorId' as user_id, count(*) as score,
+        rank() over (partition by gc.group_id order by count(*) desc) as nth
       from group_contracts as gc
       join contracts as c on gc.contract_id = c.id
       join user_contract_metrics as ucm on ucm.contract_id = gc.contract_id
       group by gc.group_id, c.data->>'creatorId'
-    ), ranked_scores as (
-      select *, rank() over (partition by group_id order by score desc, user_id) as nth
-      from creator_scores
     )
-    select * from ranked_scores where nth <= 50`
+    select * from creator_scores where nth <= 50`
   )
   const creatorScoresByGroup = mapValues(
     groupBy(groupCreatorScores, (r) => r.group_id as string),
@@ -64,15 +63,14 @@ export async function updateGroupMetrics() {
   log(`Loading contract profit scores...`)
   const groupProfitScores = await pg.manyOrNone(
     `with profit_scores as (
-      select gc.group_id, ucm.user_id, sum((ucm.data->'profit')::numeric) as score
+      select
+        gc.group_id, ucm.user_id, sum((ucm.data->'profit')::numeric) as score,
+        rank() over (partition by gc.group_id order by sum((ucm.data->'profit')::numeric) desc) as nth
       from group_contracts as gc
       join user_contract_metrics as ucm on ucm.contract_id = gc.contract_id
       group by gc.group_id, ucm.user_id
-    ), ranked_scores as (
-      select *, rank() over (partition by group_id order by score desc, user_id) as nth
-      from profit_scores
     )
-    select * from ranked_scores where nth <= 50`
+    select * from profit_scores where nth <= 50`
   )
   const profitScoresByGroup = mapValues(
     groupBy(groupProfitScores, (r) => r.group_id as string),
@@ -85,10 +83,11 @@ export async function updateGroupMetrics() {
 
   log('Updating leaderboards...')
   const writer = firestore.bulkWriter()
-  for (const doc of groups.docs) {
-    const topTraderScores = profitScoresByGroup[doc.id] ?? []
-    const topCreatorScores = creatorScoresByGroup[doc.id] ?? []
-    writer.update(doc.ref, {
+  for (const groupId of groupIds) {
+    const topTraderScores = profitScoresByGroup[groupId] ?? []
+    const topCreatorScores = creatorScoresByGroup[groupId] ?? []
+    const doc = firestore.collection('groups').doc('id')
+    writer.update(doc, {
       cachedLeaderboard: {
         topTraders: sortBy(topTraderScores, (x) => -x.score),
         topCreators: sortBy(topCreatorScores, (x) => -x.score),
