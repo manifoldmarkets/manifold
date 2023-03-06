bb6fc71de2f9f58f5a7d3dce79d4856a849b98eb
Sinclair Chen <abc.sinclair@gmail.com>
2023-03-06 15:19:07 -0800

    remove the rainbow

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -359,12 +359,6 @@ module.exports = {
             display: 'none',
           },
         },
-        '.text-rainbow': {
-          background:
-            'linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #f9a825, #ff5722);',
-          '-webkit-background-clip': 'text',
-          '-webkit-text-fill-color': 'transparent',
-        },
         '.bg-confetti-animated': {
           'background-repeat': 'repeat-x',
           'background-position': 'top -10px center',
