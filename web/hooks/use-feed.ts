76dbba85f69d136b0e22c8cc1097b9cbf18b0ee5
James Grugett <jahooma@gmail.com>
2023-03-06 13:44:21 -0800

    Filter destiny markets from feed if not following one of the groups

--- web/hooks/use-feed.ts
+++ web/hooks/use-feed.ts
@@ -1,13 +1,15 @@
 import { uniqBy } from 'lodash'
 import { CPMMBinaryContract } from 'common/contract'
 import { User } from 'common/user'
-import { useEffect, useMemo } from 'react'
+import { useEffect } from 'react'
 import { usePersistentState, inMemoryStore } from './use-persistent-state'
 import { db } from 'web/lib/supabase/db'
 import { buildArray } from 'common/util/array'
 import { usePrivateUser } from './use-user'
 import { isContractBlocked } from 'web/lib/firebase/users'
 import { useEvent } from './use-event'
+import { useMemberGroupsSubscription } from './use-group'
+import { DESTINY_GROUP_SLUGS } from 'common/envs/constants'
 
 const PAGE_SIZE = 20
 
@@ -43,8 +45,20 @@ export const useFeed = (user: User | null | undefined, key: string) => {
     loadMore()
   }, [loadMore])
 
-  return useMemo(
-    () => ({ contracts: savedContracts, loadMore }),
-    [savedContracts, loadMore]
+  const followedGroups = useMemberGroupsSubscription(user)
+  const shouldFilterDestiny = !followedGroups?.find((g) =>
+    DESTINY_GROUP_SLUGS.includes(g.slug)
   )
+  const filteredContracts = savedContracts
+    ?.filter((c) => !isContractBlocked(privateUser, c))
+    .filter(
+      (c) =>
+        !shouldFilterDestiny ||
+        !c.groupSlugs?.some((s) => DESTINY_GROUP_SLUGS.includes(s))
+    )
+
+  return {
+    contracts: filteredContracts,
+    loadMore,
+  }
 }
