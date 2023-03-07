1ff4b586baaa8ef68ee1cf4325c68eac8d2a8b2e
Ian Philips <iansphilips@gmail.com>
2023-03-07 12:11:34 -0700

    Update dev domain

--- backend/discord-bot/src/constants/dev.ts
+++ backend/discord-bot/src/constants/dev.ts
@@ -2,5 +2,5 @@ export const DEV = {
   clientId: '1074829857537663098',
   // Set a guildId if that's the only server you want to test the bot on
   guildId: '1003693352962773106',
-  domain: 'https://manifold.markets/',
+  domain: 'https://dev.manifold.markets/',
 }
