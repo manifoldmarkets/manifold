8ef4e1b630774385c4482307f24d91a83b7cd3f2
mantikoros <sgrugett@gmail.com>
2023-03-10 15:22:58 -0800

    group share url

--- common/src/util/share.ts
+++ common/src/util/share.ts
@@ -3,10 +3,10 @@ import { ENV_CONFIG } from 'common/envs/constants'
 
 export const getShareUrl = (contract: Contract, username: string | undefined) =>
   `https://${ENV_CONFIG.domain}/${contract.creatorUsername}/${contract.slug}${
-    username ? queryString(username) : ''
+    username ? referralQuery(username) : ''
   }`
 
-const queryString = (username: string) => {
+export const referralQuery = (username: string) => {
   try {
     return '?r=' + btoa(username).replace(/=/g, '')
   } catch (e) {
