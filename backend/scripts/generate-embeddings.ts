7659820e00aa7ba6952ea593077989d4b89d9f54
mantikoros <sgrugett@gmail.com>
2023-03-10 14:52:34 -0800

    embeddings script

--- backend/scripts/generate-embeddings.ts
+++ backend/scripts/generate-embeddings.ts
@@ -1,14 +1,10 @@
-import * as admin from 'firebase-admin'
-
 import { initAdmin } from 'shared/init-admin'
 initAdmin()
 
 import { run } from 'common/supabase/utils'
-import { Contract } from 'common/contract'
 import { createSupabaseClient } from 'shared/supabase/init'
 import { generateEmbeddings } from 'shared/helpers/openai-utils'
 
-const firestore = admin.firestore()
 const db = createSupabaseClient()
 
 async function main() {
@@ -17,30 +13,31 @@ async function main() {
   const contractIds = new Set(result.data.map((row: any) => row.contract_id))
   console.log('Got', contractIds.size, 'markets with preexisting embeddings')
 
-  const snapshot = await firestore
-    .collection('contracts')
-    .orderBy('popularityScore', 'desc')
-    .get()
-
-  const contracts = snapshot.docs
-    .map((doc) => doc.data() as Contract)
-    .filter((c) => !contractIds.has(c.id))
+  const { data: contracts } = await run(
+    db
+      .from('contracts')
+      .select('id, data')
+      .not('id', 'in', '(' + [...contractIds].join(',') + ')')
+  )
 
   console.log('Got', contracts.length, 'markets to process')
 
   for (const contract of contracts) {
-    const { id, question } = contract
+    const { id, data } = contract
+    const { question } = data as { question: string }
     const embedding = await generateEmbeddings(question)
-    if (!embedding) {
+    if (!embedding || embedding.length < 1500) {
       console.log('No embeddings for', question)
       continue
     }
 
-    console.log('Generated', embedding?.length, 'embeddings for', question)
+    console.log('Generated embeddings for', id, ':', question)
 
     await run(
-      db.from('contract_embeddings').upsert({ contract_id: id, embedding })
-    )
+      db
+        .from('contract_embeddings')
+        .upsert({ contract_id: id, embedding, created_at: Date.now() })
+    ).catch((err) => console.error(err))
   }
 }
 
