e313d03ec63e3aa95a521840a08120de046e05df
ingawei <ingawei@gmail.com>
2023-03-14 14:03:46 -0700

    adding index back in

---
+++ backend/functions/src/index.js
@@ -0,0 +1,42 @@
+import * as admin from 'firebase-admin'
+
+admin.initializeApp()
+
+// triggers
+export * from './triggers/log-writes' // Running the emulator? Comment this line out
+export * from './triggers/on-create-user'
+export * from './triggers/on-create-bet'
+export * from './triggers/on-create-comment-on-contract'
+export * from './triggers/on-create-comment-on-post'
+export * from './triggers/on-create-answer'
+export * from './triggers/on-update-contract'
+export * from './triggers/on-create-contract'
+export * from './triggers/on-follow-user'
+export * from './triggers/on-unfollow-user'
+export * from './triggers/on-create-liquidity-provision'
+export * from './triggers/on-update-group'
+export * from './triggers/on-delete-group'
+export * from './triggers/on-update-reaction'
+
+// scheduled functions
+export * from './scheduled/update-loans'
+export * from './scheduled/update-recommended'
+export {
+  sendWeeklyPortfolioUpdate,
+  saveWeeklyContractMetrics,
+} from './scheduled/weekly-portfolio-updates'
+export * from './scheduled/update-contract-metrics'
+export * from './scheduled/update-user-metrics'
+export * from './scheduled/update-group-metrics'
+export * from './scheduled/update-stats'
+export * from './scheduled/backup-db'
+export * from './scheduled/mana-signup-bonus'
+export * from './scheduled/market-close-notifications'
+export * from './scheduled/score-contracts'
+export * from './scheduled/weekly-markets-emails'
+export * from './scheduled/reset-betting-streaks'
+export * from './scheduled/reset-weekly-emails-flags'
+export * from './scheduled/weekly-portfolio-emails'
+export * from './scheduled/drizzle-liquidity'
+export * from './scheduled/check-push-notification-receipts'
+export * from './scheduled/increment-streak-forgiveness'
