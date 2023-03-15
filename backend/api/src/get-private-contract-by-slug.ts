ee37b2747f3a1ceeb0cefe37f0012708a4443288
ingawei <ingawei@gmail.com>
2023-03-14 20:42:15 -0700

    get-private-contract

---
+++ backend/api/src/get-private-contract-by-slug.ts
@@ -0,0 +1,60 @@
+import { createSupabaseDirectClient } from 'shared/supabase/init'
+import { z } from 'zod'
+import { APIError, authEndpoint, validate } from './helpers'
+const bodySchema = z.object({
+  contractSlug: z.string(),
+})
+
+export const getprivatecontractbyslug = authEndpoint(async (req, auth) => {
+  const { contractSlug } = validate(bodySchema, req.body)
+  if (!auth.uid) {
+    return false
+  }
+  const pg = createSupabaseDirectClient()
+
+  const contract = (
+    await pg.one(
+      `select data from contracts where data->>'slug' = '${contractSlug}'`
+    )
+  ).data
+
+  if (!contract) {
+    throw new APIError(400, 'This contract does not exist!')
+  }
+
+  if (!contract.visibility || contract.visibility != 'private') {
+    throw new APIError(
+      400,
+      'How did you get here? This contract is not private...'
+    )
+  }
+
+  if (!contract.groupLinks) {
+    throw new APIError(400, 'No associated group with this private contract.')
+  }
+
+  if (contract.groupLinks.length > 1) {
+    throw new APIError(
+      400,
+      'Too many groups associated with this private contract!'
+    )
+  }
+
+  const groupId = contract.groupLinks[0].groupId
+
+  // checks if user is member
+  const userCanAccess = (
+    await pg.one(
+      `select exists(
+        select * from group_role
+        where group_id = '${groupId}'
+        and member_id='${auth.uid}'
+        )`
+    )
+  ).exists
+
+  if (userCanAccess) {
+    return contract
+  }
+  return null
+})
