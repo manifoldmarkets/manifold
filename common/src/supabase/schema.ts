bfef0448915002f474d491a7d41d350d8a94c7fa
Marshall Polaris <marshall@pol.rs>
2023-03-15 23:48:39 -0700

    Generate schema for new columns

--- common/src/supabase/schema.ts
+++ common/src/supabase/schema.ts
@@ -457,21 +457,33 @@ export interface Database {
       }
       user_portfolio_history: {
         Row: {
-          data: Json
-          fs_updated_time: string
+          balance: number | null
+          data: Json | null
+          fs_updated_time: string | null
+          investment_value: number | null
           portfolio_id: string
+          total_deposits: number | null
+          ts: string | null
           user_id: string
         }
         Insert: {
-          data: Json
-          fs_updated_time: string
+          balance?: number | null
+          data?: Json | null
+          fs_updated_time?: string | null
+          investment_value?: number | null
           portfolio_id: string
+          total_deposits?: number | null
+          ts?: string | null
           user_id: string
         }
         Update: {
-          data?: Json
-          fs_updated_time?: string
+          balance?: number | null
+          data?: Json | null
+          fs_updated_time?: string | null
+          investment_value?: number | null
           portfolio_id?: string
+          total_deposits?: number | null
+          ts?: string | null
           user_id?: string
         }
       }
