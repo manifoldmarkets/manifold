702cf3c55e97e990d322313ea73a8d402105e881
Ian Philips <iansphilips@gmail.com>
2023-03-17 17:16:49 -0600

    Modify quest notifications slightly

--- web/components/notifications/income-summary-notifications.tsx
+++ web/components/notifications/income-summary-notifications.tsx
@@ -87,7 +87,7 @@ export function IncomeNotificationItem(props: {
   setHighlighted: (highlighted: boolean) => void
 }) {
   const { notification, highlighted, setHighlighted } = props
-  const { sourceType, reason } = notification
+  const { sourceType } = notification
 
   if (sourceType === 'tip' || sourceType === 'tip_and_like') {
     return (
@@ -98,22 +98,21 @@ export function IncomeNotificationItem(props: {
       />
     )
   } else if (sourceType === 'bonus') {
-    if (reason === 'quest_payout') {
-      return (
-        <QuestIncomeNotification
-          notification={notification}
-          highlighted={highlighted}
-          setHighlighted={setHighlighted}
-        />
-      )
-    } else
-      return (
-        <BonusIncomeNotification
-          notification={notification}
-          highlighted={highlighted}
-          setHighlighted={setHighlighted}
-        />
-      )
+    return (
+      <BonusIncomeNotification
+        notification={notification}
+        highlighted={highlighted}
+        setHighlighted={setHighlighted}
+      />
+    )
+  } else if (sourceType === 'quest_reward') {
+    return (
+      <QuestIncomeNotification
+        notification={notification}
+        highlighted={highlighted}
+        setHighlighted={setHighlighted}
+      />
+    )
   } else if (sourceType === 'betting_streak_bonus') {
     return (
       <BettingStreakBonusIncomeNotification
@@ -257,7 +256,7 @@ export function QuestIncomeNotification(props: {
       isChildOfGroup={true}
       icon={
         <NotificationIcon
-          symbol={'🎁'}
+          symbol={'🧭'}
           symbolBackgroundClass={
             'bg-gradient-to-br from-primary-500 to-primary-300'
           }
