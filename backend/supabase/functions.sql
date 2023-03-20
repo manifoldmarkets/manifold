ceead32b1b4280d0556959e1141e755293263247
James Grugett <jahooma@gmail.com>
2023-03-20 15:22:42 -0700

    Create views.sql file

--- backend/supabase/functions.sql
+++ backend/supabase/functions.sql
@@ -351,39 +351,6 @@ limit count
 offset start
 $$;
 
-
-create or replace view group_role as(
-  select member_id,
-    gp.id as group_id,
-    gp.data as group_data,
-    gp.data ->> 'name' as group_name,
-    gp.data ->> 'slug' as group_slug,
-    gp.data ->> 'creatorId' as creator_id,
-    users.data ->> 'name' as name,
-    users.data ->> 'username' as username,
-    users.data ->> 'avatarUrl' as avatar_url,
-    (select
-      CASE
-      WHEN (gp.data ->> 'creatorId')::text = member_id THEN 'admin'
-      ELSE (gm.data ->> 'role')
-      END
-    ) as role,
-    (gm.data ->> 'createdTime')::bigint as createdTime
-  from (group_members gm join groups gp on gp.id = gm.group_id) join users on users.id = gm.member_id
-);
-
-create or replace view user_groups as(
-select
-users.id as id,
-users.data->>'name' as name,
-users.data->>'username' as username,
-users.data->>'avatarUrl' as avatarurl,
-(users.data->>'followerCountCached')::integer as follower_count,
-user_groups.groups as groups
-from (users left join
-(select member_id, array_agg(group_id) as groups from group_members group by member_id) user_groups
-on users.id=user_groups.member_id));
-
 create or replace function get_contracts_by_creator_ids(creator_ids text[], created_time bigint)
 returns table(creator_id text, contracts jsonb)
     immutable parallel safe
