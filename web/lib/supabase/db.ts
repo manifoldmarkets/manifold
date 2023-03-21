97194f672b0eaf4ee60c59857822127242567f84
Marshall Polaris <marshall@pol.rs>
2023-03-21 03:29:01 -0700

    Get Supabase token during auth flow, apply it to client

--- web/lib/supabase/db.ts
+++ web/lib/supabase/db.ts
@@ -1,11 +1,26 @@
 import { createClient } from 'common/supabase/utils'
 import { ENV_CONFIG } from 'common/envs/constants'
 
-if (!ENV_CONFIG.supabaseInstanceId || !ENV_CONFIG.supabaseAnonKey) {
-  throw new Error("No Supabase config present; Supabase stuff won't work.")
+function initSupabaseClient(jwt?: string) {
+  if (!ENV_CONFIG.supabaseInstanceId || !ENV_CONFIG.supabaseAnonKey) {
+    throw new Error("No Supabase config present; Supabase stuff won't work.")
+  }
+  if (jwt == null) {
+    return createClient(
+      ENV_CONFIG.supabaseInstanceId,
+      ENV_CONFIG.supabaseAnonKey
+    )
+  } else {
+    return createClient(
+      ENV_CONFIG.supabaseInstanceId,
+      ENV_CONFIG.supabaseAnonKey,
+      { global: { headers: { Authorization: `Bearer ${jwt}` } } }
+    )
+  }
 }
 
-export const db = createClient(
-  ENV_CONFIG.supabaseInstanceId,
-  ENV_CONFIG.supabaseAnonKey
-)
+export function updateSupabaseAuth(jwt?: string) {
+  db = initSupabaseClient(jwt)
+}
+
+export let db = initSupabaseClient()
