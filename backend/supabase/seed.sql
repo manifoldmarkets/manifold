e5a93ea9594d803355bd0dd67be465c2a3aa15d4
Marshall Polaris <marshall@pol.rs>
2023-03-22 19:44:29 -0700

    Add index for loading activity feed bets

--- backend/supabase/seed.sql
+++ backend/supabase/seed.sql
@@ -206,6 +206,12 @@ create index if not exists contract_bets_bet_id on contract_bets (bet_id);
 create index if not exists contract_bets_created_time_global on contract_bets (
     (to_jsonb(data)->>'createdTime') desc
 );
+/* serving activity feed bets list */
+create index concurrently contract_bets_activity_feed on contract_bets (
+  (to_jsonb(data)->'isAnte'),
+  (to_jsonb(data)->'isRedemption'),
+  (to_jsonb(data)->>'createdTime') desc
+);
 /* serving e.g. the contract page recent bets and the "bets by contract" API */
 create index if not exists contract_bets_created_time on contract_bets (
     contract_id,
