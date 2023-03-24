f00ed6de8fb7e15ec4f80486aa3b42edd87830b6
Marshall Polaris <marshall@pol.rs>
2023-03-24 16:20:17 -0700

    Use new technology in `update-metrics` script

--- backend/scripts/update-metrics.ts
+++ backend/scripts/update-metrics.ts
@@ -1,20 +1,17 @@
-import { initAdmin } from 'shared/init-admin'
-initAdmin()
+import { runScript } from './run-script'
 
 import { log } from 'shared/utils'
 import { updateUserMetricsCore } from 'functions/scheduled/update-user-metrics'
 import { updateContractMetricsCore } from 'functions/scheduled/update-contract-metrics'
 import { updateGroupMetricsCore } from 'functions/scheduled/update-group-metrics'
 
-async function updateMetrics() {
-  log('Updating user metrics...')
-  await updateUserMetricsCore()
-  log('Updating contract metrics...')
-  await updateContractMetricsCore()
-  log('Updating group metrics...')
-  await updateGroupMetricsCore()
-}
-
 if (require.main === module) {
-  updateMetrics().then(() => process.exit())
+  runScript(async () => {
+    log('Updating user metrics...')
+    await updateUserMetricsCore()
+    log('Updating contract metrics...')
+    await updateContractMetricsCore()
+    log('Updating group metrics...')
+    await updateGroupMetricsCore()
+  })
 }
