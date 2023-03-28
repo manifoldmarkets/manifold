f345f81dd0285aaaf1ed919b7b97530c82a7f71d
James Grugett <jahooma@gmail.com>
2023-03-28 14:59:18 -0700

    Increase closing soon to 3 day interval

--- backend/supabase/functions.sql
+++ backend/supabase/functions.sql
@@ -143,19 +143,19 @@ as $$
     select *, row_number() over (order by distance) as row_num
     from available_contracts
     where created_time > (now() - interval '1 day')
-    and close_time > (now() + interval '1 day')
+    and close_time > (now() + interval '3 day')
     order by distance
     limit n / 4
   ), closing_soon_contracts as (
     select *, row_number() over (order by distance) as row_num
     from available_contracts
-    where close_time < (now() + interval '1 day')
+    where close_time < (now() + interval '3 day')
     order by distance
     limit n / 4
   ), trending_contracts as (
     select * from available_contracts
     where created_time < (now() - interval '1 day')
-    and close_time > (now() + interval '1 day')
+    and close_time > (now() + interval '3 day')
   ), results1 as (
     select *, row_number() over (order by freshness_score desc) as row_num
     from trending_contracts
