d5d04dba2b9997288bd97b15ce0ebd69cbf32d0a
mantikoros <sgrugett@gmail.com>
2023-03-29 15:54:21 -0700

    user_topics table

--- backend/supabase/seed.sql
+++ backend/supabase/seed.sql
@@ -465,6 +465,21 @@ create policy "admin write access" on topic_embeddings
   as PERMISSIVE FOR ALL
   to service_role;
 
+
+create table if not exists user_topics (
+    user_id text not null primary key,
+    created_at timestamp not null default now(),
+    topic_embedding vector(1536) not null
+    topics text[] not null
+);
+alter table user_topics enable row level security;
+drop policy if exists "public read" on user_topics;
+create policy "public read" on user_topics for select using (true);
+drop policy if exists "admin write access" on user_topics;
+create policy "admin write access" on user_topics
+  as PERMISSIVE FOR ALL
+  to service_role;
+
 begin;
   drop publication if exists supabase_realtime;
   create publication supabase_realtime;
