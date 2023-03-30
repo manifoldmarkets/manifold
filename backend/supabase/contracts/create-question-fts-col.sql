e34bc095b6140642a151f4157c982a2e00c8cfe9
ingawei <ingawei@gmail.com>
2023-03-30 16:34:15 -0700

    function fix

--- backend/supabase/contracts/create-question-fts-col.sql
+++ backend/supabase/contracts/create-question-fts-col.sql
@@ -1,6 +1,4 @@
 alter table public.contracts
-add column question_fts tsvector generated always as (
-        to_tsvector('english', description || ' ' || title)
-    ) stored;
+add column question_fts tsvector generated always as (to_tsvector('english', question)) stored;
 create index if not exists question_fts on contracts using gin (question_fts);
 -- generate the index
