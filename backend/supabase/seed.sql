3520f6cf70d31d3e7d58a3092cbbf307a7538b91
ingawei <ingawei@gmail.com>
2023-03-30 16:10:14 -0700

    get rid of repeat things

--- backend/supabase/seed.sql
+++ backend/supabase/seed.sql
@@ -453,8 +453,6 @@ create policy "public read" on contract_embeddings for
 select using (true);
 drop policy if exists "admin write access" on contract_embeddings;
 create policy "admin write access" on contract_embeddings as PERMISSIVE FOR ALL to service_role;
-create index if not exists contract_embeddings_embedding on contract_embeddings using ivfflat (embedding vector_cosine_ops) with (lists = 100);
-create policy "admin write access" on contract_embeddings as PERMISSIVE FOR ALL to service_role;
 SET ivfflat.probes = 5;
 create index if not exists contract_embeddings_embedding on contract_embeddings using ivfflat (embedding vector_cosine_ops) with (lists = 100);
 begin;
