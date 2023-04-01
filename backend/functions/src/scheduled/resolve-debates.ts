366a03a7ea396661fb582e4f6526cd26dcdd1d4e
James Grugett <jahooma@gmail.com>
2023-03-31 17:54:32 -0700

    Resolve debates on scheduled function!

---
+++ backend/functions/src/scheduled/resolve-debates.ts
@@ -0,0 +1,56 @@
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+
+import { getUser, getValues, revalidateStaticProps } from 'shared/utils'
+import { Contract, CPMMBinaryContract } from 'common/contract'
+import { createSupabaseDirectClient } from 'shared/supabase/init'
+import { secrets } from 'shared/secrets'
+import { resolveMarket } from 'api/src/resolve-market'
+import { getProbability } from 'common/calculate'
+
+export const resolveDebates = functions
+  .runWith({
+    secrets,
+  })
+  .pubsub.schedule('every 1 minutes')
+  .onRun(doResolveDebates)
+
+export async function doResolveDebates() {
+  const firestore = admin.firestore()
+  const pg = createSupabaseDirectClient()
+
+  const debateGroupId = '0i8ozKhPq5qJ89DG9tCW'
+  const contractIds = (
+    await getValues<{ contractId: string }>(
+      firestore
+        .collection('groups')
+        .doc(debateGroupId)
+        .collection('groupContracts')
+    )
+  ).map((r) => r.contractId)
+
+  const contracts = await pg.map(
+    `select data from contracts
+          where id = any($1)
+          and close_time < now()`,
+    [contractIds],
+    (r) => r.data as Contract
+  )
+
+  console.log('contracts to resolve', contracts)
+
+  for (const contract of contracts) {
+    const creator = await getUser(contract.creatorId)
+    if (creator) {
+      const prob = getProbability(contract as CPMMBinaryContract)
+      await resolveMarket(contract, creator, {
+        outcome: 'MKT',
+        probabilityInt: Math.round(prob * 100),
+        value: undefined,
+        resolutions: undefined,
+      })
+    }
+  }
+
+  if (contracts.length) await revalidateStaticProps('/debate')
+}
