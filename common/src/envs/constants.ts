b3112b828d6f9c76a171fe8ebac4199b58891c31
mantikoros <sgrugett@gmail.com>
2023-04-02 12:27:01 -0700

    turn off april fool's

--- common/src/envs/constants.ts
+++ common/src/envs/constants.ts
@@ -3,7 +3,7 @@ import { DEV_CONFIG } from './dev'
 import { EnvConfig, PROD_CONFIG } from './prod'
 import { THEOREMONE_CONFIG } from './theoremone'
 
-export const APRIL_FOOLS_ENABLED = true
+export const APRIL_FOOLS_ENABLED = false
 
 export const BACKGROUND_COLOR = 'bg-canvas-50'
 export const ENV = process.env.NEXT_PUBLIC_FIREBASE_ENV ?? 'PROD'
