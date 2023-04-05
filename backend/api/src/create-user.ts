1b37b8a19fab6d0dc302fcd3f6a7ee6b37173e45
James Grugett <jahooma@gmail.com>
2023-04-05 13:34:54 -0700

    Add ip to banned list

--- backend/api/src/create-user.ts
+++ backend/api/src/create-user.ts
@@ -164,4 +164,7 @@ function getStorageBucketId() {
 
 // Automatically ban users with these device tokens or ip addresses.
 const bannedDeviceTokens = ['fa807d664415', 'dcf208a11839', 'bbf18707c15d']
-const bannedIpAddresses: string[] = ['24.176.214.250']
+const bannedIpAddresses: string[] = [
+  '24.176.214.250',
+  '2607:fb90:bd95:dbcd:ac39:6c97:4e35:3fed',
+]
