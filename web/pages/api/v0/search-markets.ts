aac861254a0869c7732e0c7ed31aa72f1eddb8b4
ingawei <ingawei@gmail.com>
2023-04-05 14:24:09 -0700

    changed API, no idea if it works tho lol

--- web/pages/api/v0/search-markets.ts
+++ web/pages/api/v0/search-markets.ts
@@ -2,6 +2,7 @@ import { FullMarket, toFullMarket } from 'common/api-market-types'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { applyCorsHeaders, CORS_UNRESTRICTED } from 'web/lib/api/cors'
 import { searchContracts } from 'web/lib/service/algolia'
+import { searchContract } from 'web/lib/supabase/contracts'
 import { ApiError } from 'web/pages/api/v0/_types'
 
 export default async function handler(
@@ -12,6 +13,18 @@ export default async function handler(
   const { terms } = req.query
   const keywords = terms as string
 
-  const contracts = await searchContracts(keywords, 100)
-  res.status(200).json(contracts.map((c) => toFullMarket(c)))
+  const { fuzzyOffset: _fuzzyOffset, data } = await searchContract({
+    state: {
+      contracts: undefined,
+      fuzzyContractOffset: 0,
+      shouldLoadMore: false,
+      showTime: null,
+    },
+    query: keywords,
+    filter: 'all',
+    sort: 'most-popular',
+    offset: 0,
+    limit: 100,
+  })
+  res.status(200).json(data.map((c) => toFullMarket(c)))
 }
