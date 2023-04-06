b322e5f2416a4b7eb434868af5731a68a4a087c5
Ian Philips <iansphilips@gmail.com>
2023-04-06 13:54:12 -0700

    Simplify, maybe fix comment<>bet logic

--- backend/scripts/test-backend-function.ts
+++ backend/scripts/test-backend-function.ts
@@ -1,25 +1,37 @@
 import { getServiceAccountCredentials, initAdmin } from 'shared/init-admin'
 initAdmin()
 import { loadSecretsToEnv } from 'shared/secrets'
-import { completeReferralsQuest } from 'shared/complete-quest-internal'
-import { getUser } from 'shared/utils'
+import { getContract, getUser } from 'shared/utils'
+import {
+  getMostRecentCommentableBet,
+  getPriorContractBets,
+  getPriorUserComments,
+} from 'functions/triggers/on-create-comment-on-contract'
 
 async function testScheduledFunction() {
   const credentials = getServiceAccountCredentials()
   await loadSecretsToEnv(credentials)
   try {
     // await resetDailyQuestStatsInternal()
-    const user = await getUser('6hHpzvRG0pMq8PNJs7RZj2qlZGn2')
-    if (!user) throw new Error('Could not find user')
-    await completeReferralsQuest(user)
-    // const contract = await getContract('TXqpswOSxT7WtbSq7bKG')
-    // if (!user || !contract) throw new Error('Could not find user or contract')
-    // await completeArchaeologyQuest(
-    //   { isRedemption: false } as Bet,
-    //   user,
-    //   contract,
-    //   'ascafwn45'
-    // )
+    const user = await getUser('AJwLWoo3xue32XIiAVrL5SyR1WB2')
+    // if (!user) throw new Error('Could not find user')
+    // await completeReferralsQuest(user)
+    const contract = await getContract('MCaB4moXujkUGsWCSy76')
+    if (!user || !contract) throw new Error('Could not find user or contract')
+    const now = Date.now()
+    const contractId = contract.id
+    const priorUserBets = await getPriorContractBets(contractId, user.id, now)
+    const priorUserComments = await getPriorUserComments(
+      contractId,
+      user.id,
+      now
+    )
+    const bet = getMostRecentCommentableBet(
+      now,
+      priorUserBets,
+      priorUserComments
+    )
+    console.log(bet)
   } catch (e) {
     console.error(e)
   }
