fbf1363154f5addb3dea5e993d6f39a27fa80e35
ingawei <ingawei@gmail.com>
2023-04-08 01:39:57 -0700

    added indexes for things

--- backend/supabase/contracts/indexes.sql
+++ backend/supabase/contracts/indexes.sql
@@ -6,3 +6,11 @@ create index if not exists contracts_created_time on contracts (created_time des
 create index if not exists contracts_close_time on contracts (close_time desc);
 create index if not exists contracts_unique_bettors on contracts (((data->'uniqueBettors7Days')::int) desc);
 create index if not exists contracts_popularity_score on contracts (((data->>'popularityScore')::real) desc);
+create index if not exists contracts_daily_score on contracts (((data->>'dailyScore')::numeric) desc);
+create index if not exists contracts_volume_24_hours on contracts (((data->>'volume24Hours')::numeric) desc);
+create index if not exists contracts_elasticity on contracts (((data->>'elasticity')::numeric) desc);
+create index if not exists contracts_last_updated_time on contracts (((data->>'lastUpdatedTime')::numeric) desc);
+create index if not exists contracts_unique_bettor_count on contracts (((data->>'uniqueBettorCount')::numeric) desc);
+create index if not exists contracts_resolution_time on contracts (resolution_time desc);
+create index if not exists contracts_visibility_public ON contracts (id)
+where visibility = 'public';
