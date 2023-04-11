8c61eb1b24ae6095deb172313c15ff3755c402e3
Ian Philips <iansphilips@gmail.com>
2023-04-11 09:56:24 -0700

    Use single pg connection to stop warnings

--- backend/shared/src/supabase/init.ts
+++ backend/shared/src/supabase/init.ts
@@ -3,6 +3,8 @@ import { createClient, getInstanceHostname } from 'common/supabase/utils'
 import { DEV_CONFIG } from 'common/envs/dev'
 import { PROD_CONFIG } from 'common/envs/prod'
 import { isProd } from '../utils'
+import { IDatabase } from 'pg-promise'
+import { IClient } from 'pg-promise/typescript/pg-subset'
 
 export const pgp = pgPromise()
 
@@ -24,10 +26,13 @@ export function createSupabaseClient() {
   return createClient(instanceId, key)
 }
 
+// Use one connection to avoid WARNING: Creating a duplicate database object for the same connection.
+let pgpDirect: IDatabase<IClient> | null = null
 export function createSupabaseDirectClient(
   instanceId?: string,
   password?: string
 ) {
+  if (pgpDirect) return pgpDirect
   instanceId =
     instanceId ??
     process.env.SUPABASE_INSTANCE_ID ??
@@ -43,10 +48,11 @@ export function createSupabaseDirectClient(
       "Can't connect to Supabase; no process.env.SUPABASE_PASSWORD."
     )
   }
-  return pgp({
+  pgpDirect = pgp({
     host: `db.${getInstanceHostname(instanceId)}`,
     port: 5432,
     user: 'postgres',
     password: password,
   })
+  return pgpDirect
 }
