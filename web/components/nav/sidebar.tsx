3c9090aaaa4ff2d4aef81837cc6843ab44a28bec
mantikoros <sgrugett@gmail.com>
2023-04-13 11:22:32 -0700

    Revert "No new user knows what groups means"
    This reverts commit f438182aaa0ab1a517b278fe0210be20566d1331.

--- web/components/nav/sidebar.tsx
+++ web/components/nav/sidebar.tsx
@@ -135,7 +135,7 @@ const getDesktopNav = (loggedIn: boolean, openDownloadApp: () => void) => {
         icon: TrophyIcon,
       },
       {
-        name: 'Market groups',
+        name: 'Groups',
         icon: RectangleGroupIcon,
         href: '/groups',
       }
@@ -167,7 +167,7 @@ const getMobileNav = (toggleModal: () => void) => {
       href: '/ad',
     },
     {
-      name: 'Market groups',
+      name: 'Groups',
       icon: RectangleGroupIcon,
       href: '/groups',
     }
