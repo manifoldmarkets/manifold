6ab36e4808e9c00c088562ed35003689cdddaf9e
James Grugett <jahooma@gmail.com>
2023-04-13 23:09:59 -0700

    Sort topics in selector alphabetically

--- web/components/topic-selector.tsx
+++ web/components/topic-selector.tsx
@@ -1,4 +1,4 @@
-import { uniq } from 'lodash'
+import { sortBy, uniq } from 'lodash'
 import { cleanTopic, TOPICS_TO_SUBTOPICS } from 'common/topics'
 import { Select } from './widgets/select'
 import { track } from 'web/lib/service/analytics'
@@ -8,7 +8,10 @@ export function TopicSelector(props: {
   onSetTopic: (topic: string) => void
 }) {
   const { topic, onSetTopic } = props
-  const subtopics = uniq(Object.values(TOPICS_TO_SUBTOPICS).flat())
+  const uniqueTopics = uniq(Object.values(TOPICS_TO_SUBTOPICS).flat())
+  const topics = sortBy(uniqueTopics, (topic) =>
+    cleanTopic(topic).toLowerCase()
+  )
 
   const selectTopic = (topic: string) => {
     onSetTopic(topic)
@@ -22,7 +25,7 @@ export function TopicSelector(props: {
       onChange={(e) => selectTopic(e.target.value)}
     >
       <option value="">🖤 For you</option>
-      {subtopics.map((subtopic) => (
+      {topics.map((subtopic) => (
         <option key={subtopic} value={cleanTopic(subtopic)}>
           {subtopic}
         </option>
