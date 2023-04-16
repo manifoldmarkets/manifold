89c503567f5a04787bedd8e3ea3d8f3473ef4243
ingawei <ingawei@gmail.com>
2023-04-15 20:20:40 -0700

    small bet changes

--- backend/supabase/views.sql
+++ backend/supabase/views.sql
@@ -164,10 +164,3 @@ FROM (
     GROUP BY referrer.id
   ) subquery
 ORDER BY total_referrals DESC;
-create view public.contract_bets_rbac as
-select *
-from contract_bets
-where NOT data @> '{"visibility": "private"}'
-  OR (
-    can_access_private_contract(contract_id, firebase_uid())
-  )
