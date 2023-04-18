4a724170b1416c46337d9c0dfea31822eac075a0
mantikoros <sgrugett@gmail.com>
2023-04-17 18:33:44 -0700

    stuff

--- backend/api/src/create-user.ts
+++ backend/api/src/create-user.ts
@@ -170,10 +170,12 @@ const bannedDeviceTokens = [
   'dcf208a11839',
   'bbf18707c15d',
   '4c2d15a6cc0c',
+  '0da6b4ea79d3',
 ]
 const bannedIpAddresses: string[] = [
   '24.176.214.250',
   '2607:fb90:bd95:dbcd:ac39:6c97:4e35:3fed',
   '2607:fb91:389:ddd0:ac39:8397:4e57:f060',
   '2607:fb90:ed9a:4c8f:ac39:cf57:4edd:4027',
+  '2607:fb90:bd36:517a:ac39:6c91:812c:6328',
 ]
