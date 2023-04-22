52a686709600c67445e3b2f44c06cd4b7995fb4b
ingawei <ingawei@gmail.com>
2023-04-21 23:12:06 -0700

    adding post and contract comment things

---
+++ backend/supabase/comments/triggers.sql
@@ -0,0 +1,26 @@
+alter table contract_comments
+add column visibility text;
+
+create
+or replace function comment_populate_cols () returns trigger language plpgsql as $$ begin 
+    if new.data is not null then new.visibility := (new.data)->>'visibility';
+    end if;
+    return new;
+end $$;
+
+create trigger comment_populate before insert
+or
+update on contract_comments for each row
+execute function comment_populate_cols ();
+
+update contract_comments
+set
+  fs_updated_time = fs_updated_time
+create trigger comment_populate before insert
+or
+update on post_comments for each row
+execute function comment_populate_cols ();
+
+update contract_comments
+set
+  fs_updated_time = fs_updated_time
