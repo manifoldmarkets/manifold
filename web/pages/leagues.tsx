39806704c4e62a0b323de291f50c91d951d83d99
James Grugett <jahooma@gmail.com>
2023-04-27 23:51:11 -0700

    Cohort table

--- web/pages/leagues.tsx
+++ web/pages/leagues.tsx
@@ -1,13 +1,20 @@
+import { useState } from 'react'
+import { groupBy } from 'lodash'
+import clsx from 'clsx'
+
 import { SEASONS, division, getDivisionName, season } from 'common/leagues'
 import { toLabel } from 'common/util/adjective-animal'
-import { groupBy } from 'lodash'
-import { useState } from 'react'
 import { Col } from 'web/components/layout/col'
 import { Page } from 'web/components/layout/page'
 import { Row } from 'web/components/layout/row'
 import { Select } from 'web/components/widgets/select'
 import { Title } from 'web/components/widgets/title'
 import { db } from 'web/lib/supabase/db'
+import { useUsers } from 'web/hooks/use-user-supabase'
+import { User } from 'common/user'
+import { LoadingIndicator } from 'web/components/widgets/loading-indicator'
+import { UserAvatarAndBadge } from 'web/components/widgets/user-link'
+import { formatMoney } from 'common/util/format'
 
 export async function getStaticProps() {
   const { data: rows } = await db.from('leagues').select('*')
@@ -32,49 +39,106 @@ export default function Leagues(props: { rows: any[] }) {
   const [division, setDivision] = useState<division>(1)
   const [cohort, setCohort] = useState(cohortNames[0])
 
+  const onSetDivision = (division: division) => {
+    setDivision(division)
+    setCohort(divisionToCohorts[division][0])
+  }
+
+  const cohortRows = cohorts[cohort]
+
   return (
     <Page>
-      <Col>
-        <Title>Leagues</Title>
+      <Title>Leagues</Title>
 
-        <Row className="gap-2">
-          <Select
-            className="!border-ink-200"
-            value={season}
-            onChange={(e) => setSeason(+e.target.value as season)}
-          >
-            {SEASONS.map((season) => (
-              <option key={season} value={season}>
-                Season {season}
-              </option>
-            ))}
-          </Select>
+      <Row className="flex-wrap gap-2">
+        <Select
+          className="!border-ink-200"
+          value={season}
+          onChange={(e) => setSeason(+e.target.value as season)}
+        >
+          {SEASONS.map((season) => (
+            <option key={season} value={season}>
+              Season {season}
+            </option>
+          ))}
+        </Select>
 
-          <Select
-            className="!border-ink-200"
-            value={division}
-            onChange={(e) => setDivision(+e.target.value as division)}
-          >
-            {Object.keys(divisionToCohorts).map((division) => (
-              <option key={division} value={division}>
-                {getDivisionName(division)}
-              </option>
-            ))}
-          </Select>
+        <Select
+          className="!border-ink-200"
+          value={division}
+          onChange={(e) => onSetDivision(+e.target.value as division)}
+        >
+          {Object.keys(divisionToCohorts).map((division) => (
+            <option key={division} value={division}>
+              {getDivisionName(division)}
+            </option>
+          ))}
+        </Select>
 
-          <Select
-            className="!border-ink-200"
-            value={cohort}
-            onChange={(e) => setCohort(e.target.value)}
-          >
-            {divisionToCohorts[division].map((cohortName) => (
-              <option key={cohortName} value={cohortName}>
-                {toLabel(cohortName)}
-              </option>
-            ))}
-          </Select>
-        </Row>
+        <Select
+          className="!border-ink-200"
+          value={cohort}
+          onChange={(e) => setCohort(e.target.value)}
+        >
+          {divisionToCohorts[division].map((cohortName) => (
+            <option key={cohortName} value={cohortName}>
+              {toLabel(cohortName)}
+            </option>
+          ))}
+        </Select>
+      </Row>
+
+      <Col className="mx-4 mt-4 sm:mx-2">
+        <CohortTable cohort={cohort} rows={cohortRows} />
       </Col>
     </Page>
+  )
+}
+
+const CohortTable = (props: { cohort: string; rows: any[] }) => {
+  const { rows } = props
+  const users = useUsers(rows.map((row) => row.user_id))
+  if (!users) return <LoadingIndicator />
+
+  return (
+    <table className="max-w-sm">
+      <thead className={clsx('text-ink-600 text-left text-sm font-semibold')}>
+        <tr>
+          <th className={clsx('pb-1')}>User</th>
+          <th className={clsx('pb-1 text-right')}>Mana earned</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row, i) => {
+          const user = users[i]
+          if (!user) console.log('no user', row)
+          return (
+            user && (
+              <UserRow key={user.id} {...row} user={users[i]} rank={i + 1} />
+            )
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+const UserRow = (props: { user: User; mana_earned: number; rank: number }) => {
+  const { user, mana_earned, rank } = props
+
+  return (
+    <tr>
+      <td>
+        <Row className="mb-2 items-center gap-4">
+          <div className="w-4 text-right font-semibold">{rank}</div>
+          <UserAvatarAndBadge
+            name={user.name}
+            username={user.username}
+            avatarUrl={user.avatarUrl}
+          />
+        </Row>
+      </td>
+      <td className="text-right">{formatMoney(mana_earned)}</td>
+    </tr>
   )
 }
