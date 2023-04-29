253e8b8a4c48e9d9e2fe946caaae92379749a634
Austin Chen <akrolsmir@gmail.com>
2023-04-28 17:05:02 -0700

    Re-add Hotjar for user session recording

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -140,9 +140,25 @@ function MyApp({ Component, pageProps }: AppProps<ManifoldPageProps>) {
         </div>
       </div>
       <Analytics />
+      {/* Umami, for pageview analytics on https://analytics.umami.is/share/ARwUIC9GWLNyowjq/Manifold%20Markets */}
       <Script
         src="https://analytics.umami.is/script.js"
         data-website-id="ee5d6afd-5009-405b-a69f-04e3e4e3a685"
+      />
+      {/* Hotjar, for recording user sessions */}
+      <Script
+        id="hotjar"
+        dangerouslySetInnerHTML={{
+          __html: `
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2968940,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+        }}
       />
       <GoogleOneTapSetup />
     </>
