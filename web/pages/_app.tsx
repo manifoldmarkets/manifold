54670694268b7407d54ace5962babff2472f4eef
akrolsmir <akrolsmir@users.noreply.github.com>
2023-04-29 00:07:15 +0000

    Auto-prettification

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -157,7 +157,7 @@ function MyApp({ Component, pageProps }: AppProps<ManifoldPageProps>) {
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
         }}
       />
       <GoogleOneTapSetup />
