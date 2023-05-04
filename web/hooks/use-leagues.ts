b7cfa2c4a35ff5bb95c1d23b177a965264857869
James Grugett <jahooma@gmail.com>
2023-05-03 17:34:32 -0700

    Fix fetching league error

--- web/hooks/use-leagues.ts
+++ web/hooks/use-leagues.ts
@@ -6,16 +6,13 @@ import { usePersistentInMemoryState } from './use-persistent-in-memory-state'
 
 export const useLeagueInfo = (userId: string | null | undefined) => {
   const [leagueInfo, setLeagueInfo] = usePersistentInMemoryState<
-    league_user_info | undefined
+    league_user_info | null | undefined
   >(undefined, `league-info-${userId}`)
 
   useEffect(() => {
     if (userId) {
       getLeagueInfo(userId).then((result) => {
-        if (result === null)
-          throw new Error('No league info found for user ' + userId)
-
-        setLeagueInfo(result as league_user_info)
+        setLeagueInfo(result as league_user_info | null)
       })
     }
   }, [userId])
