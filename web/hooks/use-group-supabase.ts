b4ed17b42492eb44549bac0e987fca9ad8b64f2b
Marshall Polaris <marshall@pol.rs>
2023-05-18 02:32:45 -0700

    Write code for basic Supabase realtime functionality (#1750)
    * Clean up some common Supabase code
    
    * Make basic abstractions for fetching stuff over Supabase RT
    
    * Use `useLiveStream` to get changes in live feed code
    
    * Ditch preload from `useSubscription`
    
    * Fix bugs
    
    * Migrate post stuff to use new `useSubscription`
    
    * Improvements to `useLiveStream` (renamed to `useRealtimeRows`)
    
    * Further applications of new realtime stuff
    
    * Remove unnecessary logging

--- web/hooks/use-group-supabase.ts
+++ web/hooks/use-group-supabase.ts
@@ -15,6 +15,7 @@ import { getUser } from 'web/lib/supabase/user'
 import { useAdmin } from './use-admin'
 import { useIsAuthorized, useUser } from './use-user'
 import { useSupabasePolling } from 'web/hooks/use-supabase'
+import { useRealtimeChannel } from 'web/lib/supabase/realtime/use-realtime'
 import { getUserIsGroupMember } from 'web/lib/firebase/api'
 import {
   GroupAndRoleType,
@@ -34,27 +35,14 @@ export function useRealtimeRole(groupId: string | undefined) {
       setTranslatedMemberRole(groupId, isManifoldAdmin, setUserRole, user)
     }
   }, [user, isManifoldAdmin, groupId])
-  useEffect(() => {
-    const channel = db.channel('user-group-role-realtime')
-    channel.on(
-      'postgres_changes',
-      {
-        event: '*',
-        schema: 'public',
-        table: 'group_members',
-        filter: `group_id=eq.${groupId}`,
-      },
-      (payload) => {
-        if ((payload.new as any).member_id === user?.id) {
-          setTranslatedMemberRole(groupId, isManifoldAdmin, setUserRole, user)
-        }
-      }
-    )
-    channel.subscribe(async (status) => {})
-    return () => {
-      db.removeChannel(channel)
+
+  const channelFilter = { k: 'group_id', v: groupId ?? '_' } as const
+  useRealtimeChannel('*', 'group_members', channelFilter, (change) => {
+    if ((change.new as any).member_id === user?.id) {
+      setTranslatedMemberRole(groupId, isManifoldAdmin, setUserRole, user)
     }
-  }, [db, user])
+  })
+
   return userRole
 }
 
@@ -124,25 +112,11 @@ export function useRealtimeGroupMembers(
     }
   }, [hitBottom])
 
-  useEffect(() => {
-    const channel = db.channel('group-members-realtime')
-    channel.on(
-      'postgres_changes',
-      {
-        event: '*',
-        schema: 'public',
-        table: 'group_members',
-        filter: `group_id=eq.${groupId}`,
-      },
-      (payload) => {
-        fetchGroupMembers()
-      }
-    )
-    channel.subscribe(async (status) => {})
-    return () => {
-      db.removeChannel(channel)
-    }
-  }, [db])
+  const channelFilter = { k: 'group_id', v: groupId } as const
+  useRealtimeChannel('*', 'group_members', channelFilter, (_change) => {
+    fetchGroupMembers()
+  })
+
   return { admins, moderators, members, loadMore }
 }
 
---
+++ web/lib/supabase/realtime/use-realtime.ts
@@ -0,0 +1,63 @@
+import { useEffect, useId, useRef, useState } from 'react'
+import { RealtimeChannel } from '@supabase/realtime-js'
+import { TableName, Row } from 'common/supabase/utils'
+import { Change, Event, Filter, buildFilterString } from 'common/supabase/realtime'
+import { useIsPageVisible } from 'web/hooks/use-page-visible'
+import { db } from 'web/lib/supabase/db'
+
+export function useRealtimeRows<T extends TableName>(
+  table: T,
+  filter?: Filter<T>
+) {
+  return useRealtimeChanges('INSERT', table, filter).map(i => i.new)
+}
+
+export function useRealtimeChanges<T extends TableName, E extends Event>(
+  event: E,
+  table: T,
+  filter?: Filter<T>
+) {
+  const [changes, setChanges] = useState<Change<T, E>[]>([])
+  useRealtimeChannel(event, table, filter, (change) => {
+    setChanges((cs) => [...cs, change as any])
+  })
+  return changes
+}
+
+export function useRealtimeChannel<T extends TableName, E extends Event>(
+  event: E,
+  table: T,
+  filter: Filter<T> | null | undefined,
+  callback: (change: Change<T, E>) => void
+) {
+  const filterString = filter ? buildFilterString(filter) : undefined
+  const channelId = `${table}-${useId()}`
+  const channel = useRef<RealtimeChannel | undefined>()
+  const isVisible = useIsPageVisible()
+
+  useEffect(() => {
+    if (isVisible) {
+      const opts = {
+        event,
+        schema: 'public',
+        table,
+        filter: filterString
+      } as const
+      const chan = channel.current = db.channel(channelId)
+      chan.on<Row<T>>('postgres_changes', opts as any, (change) => {
+        // if we got this change over a channel we have recycled, ignore it
+        if (channel.current === chan) {
+          callback(change as any)
+        }
+      }).subscribe((_status, err) => {
+        if (err) {
+          console.error(err)
+        }
+      })
+      return () => {
+        db.removeChannel(chan)
+        channel.current = undefined
+      }
+    }
+  }, [table, filterString, isVisible])
+}
