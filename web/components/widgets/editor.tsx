3a24e17f0b31fa765bfe166dd7e24f1d7665f1a3
Sinclair Chen <abc.sinclair@gmail.com>
2023-05-18 17:28:11 -0700

    Make quotes green

--- web/components/widgets/editor.tsx
+++ web/components/widgets/editor.tsx
@@ -69,7 +69,7 @@ export const proseClass = (size: 'sm' | 'md' | 'lg') =>
     size === 'sm' ? 'prose-sm' : 'text-md',
     size !== 'lg' && 'prose-p:my-0 prose-ul:my-0 prose-ol:my-0 prose-li:my-0',
     '[&>p]:prose-li:my-0',
-    'text-ink-900 prose-blockquote:text-ink-600',
+    'text-ink-900 prose-blockquote:text-teal-700 dark:prose-blockquote:text-teal-100',
     'break-anywhere'
   )
 
