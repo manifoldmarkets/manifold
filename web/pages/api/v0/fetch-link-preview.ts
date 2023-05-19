b7a1bd9b35cf8e07ae79040e796a1429e55c6b0a
Ian Philips <iansphilips@gmail.com>
2023-05-19 15:50:58 -0700

    Clean up api

--- web/pages/api/v0/fetch-link-preview.ts
+++ web/pages/api/v0/fetch-link-preview.ts
@@ -1,5 +1,5 @@
 // pages/api/fetch-link-preview.ts
-import { NextApiRequest, NextApiResponse } from 'next'
+import { Metadata, NextApiRequest, NextApiResponse } from 'next'
 import metascraper from 'metascraper'
 import metascraperAuthor from 'metascraper-author'
 import metascraperDate from 'metascraper-date'
@@ -9,28 +9,23 @@ import metascraperLogo from 'metascraper-logo'
 import metascraperPublisher from 'metascraper-publisher'
 import metascraperTitle from 'metascraper-title'
 import metascraperUrl from 'metascraper-url'
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'POST') {
-    res.status(405).json({ message: 'Method not allowed' })
-    return
-  }
+import { ApiError } from 'web/pages/api/v0/_types'
+import { applyCorsHeaders, CORS_UNRESTRICTED } from 'web/lib/api/cors'
 
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Metadata | ApiError>
+) {
+  await applyCorsHeaders(req, res, CORS_UNRESTRICTED)
   const { url } = req.body
-
-  if (!url) {
-    res.status(400).json({ message: 'URL is required' })
-    return
-  }
-
+  if (!url) return res.status(404).json({ error: 'url required' })
   try {
     const metadata = await fetchLinkPreview(url)
-    res.status(200).json(metadata)
+    return res.status(200).json(metadata)
   } catch (error) {
-    res.status(500).json({ message: 'Error fetching link preview' })
+    return res.status(500).json({ error: 'Error fetching link preview' })
   }
 }
-
-export default handler
 
 export async function fetchLinkPreview(url: string) {
   const scraper = metascraper([
@@ -44,12 +39,10 @@ export async function fetchLinkPreview(url: string) {
     metascraperUrl(),
   ])
 
-  console.log('fetching url', url)
   const response = await fetch(url)
   const html = await response.text()
   const responseUrl = response.url
-  // convert html to string
   const metadata = await scraper({ html, url: responseUrl })
-  console.log('metadata', metadata)
+  console.log('link metadata', metadata)
   return metadata
 }
