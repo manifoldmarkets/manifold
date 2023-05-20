1d2b4e0a0137b0d170f219a3d4e97da295b8e567
ingawei <ingawei@gmail.com>
2023-05-19 17:23:52 -0700

    use realtime on contract follows

--- web/hooks/use-follows-supabase.ts
+++ web/hooks/use-follows-supabase.ts
@@ -2,62 +2,58 @@ import { RealtimeChannel } from '@supabase/realtime-js'
 import { useEffect, useState } from 'react'
 import { db } from 'web/lib/supabase/db'
 import { getContractFollows } from 'web/lib/supabase/follows'
+import { useRealtimeChanges } from 'web/lib/supabase/realtime/use-realtime'
+import { useEffectCheckEquality } from './use-effect-check-equality'
+import { Delete, Insert } from 'common/supabase/realtime'
+import { uniq } from 'lodash'
 
 export function useRealtimeContractFollows(contractId: string | undefined) {
   const [follows, setFollows] = useState<string[] | undefined | null>(undefined)
 
-  useEffect(() => {
-    let channel: RealtimeChannel
-    if (contractId) {
-      getContractFollows(contractId)
-        .then((result) => setFollows(result))
-        .catch((e) => console.log(e))
-      channel = db.channel(`realtime-contract-follows-${contractId}`)
-      channel.on(
-        'postgres_changes',
-        {
-          event: 'INSERT',
-          schema: 'public',
-          table: 'contract_follows',
-          filter: `contract_id=eq.${contractId}`,
-        },
-        (payload) => {
-          setFollows((follows) => {
-            if (follows) {
-              return [...follows, payload.new.follow_id]
-            } else {
-              return [payload.new.follow_id]
-            }
-          })
-        }
-      )
-      channel.on(
-        'postgres_changes',
-        {
-          event: 'DELETE',
-          schema: 'public',
-          table: 'contract_follows',
-          filter: `contract_id=eq.${contractId}`,
-        },
-        (payload) => {
-          setFollows((follows) => {
-            if (follows) {
-              return follows.filter(
-                (follow) => follow !== payload.old.follow_id
-              )
-            } else {
-              return
-            }
-          })
-        }
-      )
-      channel.subscribe(async (status) => {})
+  const deletedFollows: Delete<'contract_follows'>[] = useRealtimeChanges(
+    'DELETE',
+    'contract_follows',
+    {
+      k: 'contract_id',
+      v: contractId ?? '',
     }
-    return () => {
-      if (channel) {
-        db.removeChannel(channel)
-      }
+  )
+  const insertedFollows: Insert<'contract_follows'>[] = useRealtimeChanges(
+    'INSERT',
+    'contract_follows',
+    {
+      k: 'contract_id',
+      v: contractId ?? '',
     }
-  }, [db])
-  return follows
+  )
+
+  useEffectCheckEquality(() => {
+    if (!contractId) {
+      return
+    }
+    getContractFollows(contractId)
+      .then((result) => setFollows(result))
+      .catch((e) => console.log(e))
+  }, [contractId])
+
+  let combinedFollows = [
+    ...(follows ?? []),
+    ...insertedFollows.map((followData) => followData.new.follow_id),
+  ]
+
+  // Get array of follow_ids from deletedFollows
+  let deletedFollowIds = deletedFollows.map(
+    (deletedFollow) => deletedFollow.old.follow_id
+  )
+
+  // For each element in deletedFollows, find its index in combinedFollows.
+  // If it exists (the index is not -1), remove it from combinedFollows.
+  deletedFollowIds.forEach((deletedFollowId) => {
+    let index = combinedFollows.indexOf(deletedFollowId ?? '')
+    if (index !== -1) {
+      combinedFollows.splice(index, 1)
+    }
+  })
+
+  return uniq(combinedFollows)
 }
