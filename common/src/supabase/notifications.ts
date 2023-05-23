392bd0467a3a9b629ad433bf72ac3f308dfe5f68
Ian Philips <iansphilips@gmail.com>
2023-05-23 10:44:53 -0700

    Fix notifications queries

--- common/src/supabase/notifications.ts
+++ common/src/supabase/notifications.ts
@@ -9,7 +9,7 @@ export async function getNotifications(
     .from('user_notifications')
     .select('*')
     .eq('user_id', userId)
-    .order('data->created_time', { ascending: false } as any)
+    .order('data->createdTime', { ascending: false } as any)
     .limit(limit)
   const { data } = await run(q)
   return data
@@ -25,7 +25,7 @@ export async function getUnseenNotifications(
     .select('*')
     .eq('user_id', userId)
     .eq('data->isSeen', false)
-    .order('data->created_time', { ascending: false } as any)
+    .order('data->createdTime', { ascending: false } as any)
     .limit(limit)
   const { data } = await run(q)
   return data
