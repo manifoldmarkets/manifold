679b9c018502e9be807c3fa4d0756114f954b1b1
Marshall Polaris <marshall@pol.rs>
2023-05-23 22:02:26 -0700

    Remove not redundant but unused UCM indexes

--- backend/supabase/user_contract_metrics/indexes.sql
+++ backend/supabase/user_contract_metrics/indexes.sql
@@ -6,12 +6,4 @@ create index if not exists user_contract_metrics_weekly_profit on user_contract_
 where
   (data -> 'from' -> 'week' -> 'profit') is not null;
 
-create index if not exists idx_user_contract_metrics_contract_has_yes_shares on user_contract_metrics (contract_id)
-where
-  has_yes_shares = true;
-
-create index idx_user_contract_metrics_contract_yes_shares on user_contract_metrics (contract_id, has_yes_shares);
-
-create index idx_user_contract_metrics_contract_no_shares on user_contract_metrics (contract_id, has_no_shares);
-
 create index idx_user_contract_metrics_contract_profit on user_contract_metrics (contract_id, profit);
