cf338ba1415c9b4b9483dcf6201d1af472a508cd
Ian Philips <iansphilips@gmail.com>
2023-05-31 18:57:42 -0600

    Load newer or older feed items depending on location in feed

--- web/pages/feed-timeline.tsx
+++ web/pages/feed-timeline.tsx
@@ -13,6 +13,10 @@ import { VisibilityObserver } from 'web/components/widgets/visibility-observer'
 import { SiteLink } from 'web/components/widgets/site-link'
 import { useFeedTimeline } from 'web/hooks/use-feed-timeline'
 import { FeedTimelineItems } from 'web/components/feed/feed-timeline-items'
+import { useIsPageVisible } from 'web/hooks/use-page-visible'
+import { useEffect, useState } from 'react'
+import { usePersistentLocalState } from 'web/hooks/use-persistent-local-state'
+import { MINUTE_MS } from 'common/util/time'
 
 export default function FeedTimeline() {
   const user = useUser()
@@ -27,43 +31,70 @@ export default function FeedTimeline() {
 
         <Col className={clsx('gap-6')}>
           <Col>
-            <MainContent />
+            <FeedTimelineContent />
+            <button
+              type="button"
+              className={clsx(
+                'focus:ring-primary-500 fixed  right-3 z-20 inline-flex items-center rounded-full border  border-transparent  p-4 shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 lg:hidden',
+                'disabled:bg-ink-300 text-ink-0 from-primary-500 hover:from-primary-700 to-blue-500 hover:to-blue-700 enabled:bg-gradient-to-r',
+                'bottom-[64px]'
+              )}
+              onClick={() => {
+                Router.push('/create')
+                track('mobile create button')
+              }}
+            >
+              <PencilAltIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
           </Col>
         </Col>
       </Col>
     </Page>
   )
 }
-
-const MainContent = () => {
-  return (
-    <Col>
-      <FeedTimelineContent />
-      <button
-        type="button"
-        className={clsx(
-          'focus:ring-primary-500 fixed  right-3 z-20 inline-flex items-center rounded-full border  border-transparent  p-4 shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 lg:hidden',
-          'disabled:bg-ink-300 text-ink-0 from-primary-500 hover:from-primary-700 to-blue-500 hover:to-blue-700 enabled:bg-gradient-to-r',
-          'bottom-[64px]'
-        )}
-        onClick={() => {
-          Router.push('/create')
-          track('mobile create button')
-        }}
-      >
-        <PencilAltIcon className="h-6 w-6" aria-hidden="true" />
-      </button>
-    </Col>
-  )
-}
 function FeedTimelineContent() {
   const user = useUser()
-  const { boosts, feedTimelineItems, loadMore } = useFeedTimeline(user, 'feed')
+  const { boosts, feedTimelineItems, loadMoreOlder, loadMoreNewer } =
+    useFeedTimeline(user, 'feed-timeline')
+  const isVisible = useIsPageVisible()
+  const [lastSeen, setLastSeen] = usePersistentLocalState(
+    Date.now(),
+    'last-seen-feed-timeline' + user?.id
+  )
+  const [scrolledDown, setScrolledDown] = useState(false)
+  const [loadingNewer, setLoadingNewer] = useState(false)
+  const checkForNewerFeedItems = () => {
+    console.log(
+      'TODO: a while has elapsed, check for new feed items and show floating button'
+    )
+  }
+  useEffect(() => {
+    const now = Date.now()
+    if (isVisible && now - lastSeen > 5 * MINUTE_MS) checkForNewerFeedItems()
+    if (!isVisible) setLastSeen(now)
+    return () => setLastSeen(Date.now())
+  }, [isVisible])
 
   if (!boosts || !feedTimelineItems) return <LoadingIndicator />
 
   return (
     <Col>
+      <div className="relative">
+        <VisibilityObserver
+          className="pointer-events-none absolute top-0 h-5 w-full select-none "
+          onVisibilityUpdated={(visible) => {
+            if (visible && scrolledDown) {
+              setLoadingNewer(true)
+              loadMoreNewer().then(() => {
+                setLoadingNewer(false)
+                setScrolledDown(false)
+              })
+            }
+            if (!visible) setScrolledDown(true)
+          }}
+        />
+        {loadingNewer && <LoadingIndicator />}
+      </div>
       <FeedTimelineItems
         boosts={boosts}
         user={user}
@@ -72,8 +103,8 @@ function FeedTimelineContent() {
 
       <div className="relative">
         <VisibilityObserver
-          className="pointer-events-none absolute bottom-0 h-screen w-full select-none"
-          onVisibilityUpdated={(visible) => visible && loadMore()}
+          className="pointer-events-none absolute bottom-0 h-5 w-full select-none "
+          onVisibilityUpdated={(visible) => visible && loadMoreOlder()}
         />
       </div>
 
