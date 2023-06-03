084c24018882772bee998998887e55e188958b25
Kevin Burke <kevin@burke.dev>
2023-06-02 20:03:06 -0700

    common/src/envs: add different bot user for Kevin
    I'm going to do automated trades from the "burkebot" account now, and
    use my existing account just for web/UI trades.
    
    I had a "check" account before I got labeled a bot and would be great
    to have that back.

--- common/src/envs/constants.ts
+++ common/src/envs/constants.ts
@@ -93,7 +93,7 @@ export const BOT_USERNAMES = [
   'Turbot',
   'MiraBot',
   'MetaculusBot',
-  'KevinBurke',
+  'burkebot',
   'Botflux',
   '7'
 ]
@@ -149,6 +149,7 @@ export const CHECK_USERNAMES = [
   'KatjaGrace',
   'AndrewG',
   'MarcusAbramovitch',
+  'KevinBurke',
 ]
 
 export const HOUSE_BOT_USERNAME = 'acc'
