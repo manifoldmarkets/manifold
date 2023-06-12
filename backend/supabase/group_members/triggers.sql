3f6e1ead8718efcf4300d85f8a94ae8886158edd
ingawei <ingawei@gmail.com>
2023-06-12 13:45:02 -0700

    fixed group_role access, added role column to group_members

---
+++ backend/supabase/group_members/triggers.sql
@@ -0,0 +1,23 @@
+alter table group_members add role text;
+
+alter table group_members add created_time timestamptz;
+
+create
+or replace function group_member_populate_cols () returns trigger language plpgsql as $$ begin 
+    if new.data is not null then 
+    new.role := (new.data)->>'role';
+    new.created_time := case
+        when new.data ? 'createdTime' then millis_to_ts(((new.data) ->> 'createdTime')::bigint)
+        else null
+        end;
+    end if;
+    return new;
+end $$;
+
+create trigger group_members_populate before insert
+or
+update on group_members for each row
+execute function group_member_populate_cols ();
+
+
+update group_members set fs_updated_time = fs_updated_time;
