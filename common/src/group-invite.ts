339c6190377b97236be02902451226ad9c0ea184
Sinclair Chen <abc.sinclair@gmail.com>
2023-06-19 16:23:38 -0700

    Fix group invite static props date serialization

--- common/src/group-invite.ts
+++ common/src/group-invite.ts
@@ -1,11 +1,3 @@
-export type GroupInvite = {
-  id: string
-  group_id: string
-  uses: number
-  max_uses: number | null
-  created_time: Date
-  duration: unknown
-  expire_time: Date | null
-  is_forever: boolean | null
-  is_max_uses_reached: boolean | null
-}
+import type { Row } from './supabase/utils'
+
+export type GroupInvite = Row<'group_invites'>
