33b66c940b82154c2c82fe7c96361d998e022f0b
ingawei <ingawei@gmail.com>
2023-06-20 16:40:47 -0700

    added James's suggestions

--- web/components/bet/limit-slider.tsx
+++ web/components/bet/limit-slider.tsx
@@ -11,18 +11,36 @@ import { useWindowSize } from 'web/hooks/use-window-size'
 import { Col } from '../layout/col'
 import { Row } from '../layout/row'
 
+export function convertNumberToProb(
+  number: number,
+  isPseudoNumeric: boolean,
+  min_prob: number,
+  max_prob: number
+) {
+  if (isPseudoNumeric) {
+    return ((number - min_prob) / (max_prob - min_prob)) * 100
+  }
+  return number
+}
+
 export function LimitSlider(props: {
+  isPseudoNumeric: boolean
   lowLimitProb: number
   setLowLimitProb: (prob: number) => void
   highLimitProb: number
   setHighLimitProb: (prob: number) => void
+  max_prob: number
+  min_prob: number
   mobileView?: boolean
 }) {
   const {
+    isPseudoNumeric,
     lowLimitProb,
     setLowLimitProb,
     highLimitProb,
     setHighLimitProb,
+    max_prob,
+    min_prob,
     mobileView,
   } = props
 
@@ -42,15 +60,21 @@ export function LimitSlider(props: {
     }
   }, [window.width])
 
-  const lowLimitPosition = sliderWidth * ((lowLimitProb ?? 0) / 100)
-  const highLimitPosition = sliderWidth * ((highLimitProb ?? 0) / 100)
+  const lowLimitPosition =
+    sliderWidth *
+    (convertNumberToProb(lowLimitProb, isPseudoNumeric, min_prob, max_prob) /
+      100)
+  const highLimitPosition =
+    sliderWidth *
+    (convertNumberToProb(highLimitProb, isPseudoNumeric, min_prob, max_prob) /
+      100)
   const LIMIT_LABEL_MIN_DISTANCE = 130
   return (
     <Col className="mb-8">
       <Row className="mt-1 mb-4 gap-4">
         <Col className="w-full">
           <div className="text-ink-800 text-sm">
-            Attempt to keep between probabilities:
+            Attempt to keep between {isPseudoNumeric ? '' : 'probabilities'}:
           </div>
           <Row
             className="relative h-12 grow items-center gap-4"
@@ -58,45 +82,74 @@ export function LimitSlider(props: {
           >
             <div
               className={clsx(
-                'absolute bottom-0 w-16 select-none rounded p-1 px-2 text-lg',
-                lowLimitProb === undefined || lowLimitProb === 0
-                  ? 'bg-ink-300 text-ink-600'
+                'absolute bottom-0 w-16 select-none rounded p-1 px-2 text-center text-lg',
+                lowLimitProb === undefined || lowLimitProb === min_prob
+                  ? 'bg-ink-300 text-ink-600 opacity-40'
+                  : lowLimitProb == highLimitProb
+                  ? 'bg-red-400 bg-opacity-20 text-red-500'
                   : 'bg-canvas-0'
               )}
               style={{
                 left: `${
                   highLimitPosition - lowLimitPosition <
                   LIMIT_LABEL_MIN_DISTANCE
-                    ? `calc(${lowLimitProb}-${
+                    ? `calc(${convertNumberToProb(
+                        lowLimitProb,
+                        isPseudoNumeric,
+                        min_prob,
+                        max_prob
+                      )}%-${
                         LIMIT_LABEL_MIN_DISTANCE -
                         (highLimitPosition - lowLimitPosition)
                       }px)`
-                    : `${lowLimitProb}%`
+                    : `${convertNumberToProb(
+                        lowLimitProb,
+                        isPseudoNumeric,
+                        min_prob,
+                        max_prob
+                      )}%`
                 }`,
               }}
             >
-              {lowLimitProb} <span className="text-sm">%</span>
+              {lowLimitProb}{' '}
+              {!isPseudoNumeric && <span className="text-sm">%</span>}
             </div>
             <div
               className={clsx(
-                'absolute bottom-0 w-16 select-none rounded p-1 px-2 text-lg',
-                highLimitProb === undefined || highLimitProb === 100
-                  ? 'bg-ink-300 text-ink-600'
+                'absolute bottom-0 w-16 select-none rounded p-1 px-2 text-center text-lg',
+                highLimitProb === undefined || highLimitProb === max_prob
+                  ? 'bg-ink-300 text-ink-600 opacity-40'
+                  : lowLimitProb == highLimitProb
+                  ? 'bg-red-400 bg-opacity-20 text-red-500'
                   : 'bg-canvas-0'
               )}
               style={{
                 right: `${
                   highLimitPosition - lowLimitPosition <
                   LIMIT_LABEL_MIN_DISTANCE
-                    ? `calc((100% - ${highLimitProb}%)-${
+                    ? `calc((100% - ${convertNumberToProb(
+                        highLimitProb,
+                        isPseudoNumeric,
+                        min_prob,
+                        max_prob
+                      )}%)-${
                         LIMIT_LABEL_MIN_DISTANCE -
                         (highLimitPosition - lowLimitPosition)
                       }px)`
-                    : `${100 - (highLimitProb ?? 0)}%`
+                    : `${
+                        100 -
+                        (convertNumberToProb(
+                          highLimitProb,
+                          isPseudoNumeric,
+                          min_prob,
+                          max_prob
+                        ) ?? 0)
+                      }%`
                 }`,
               }}
             >
-              {highLimitProb} <span className="text-sm">%</span>
+              {highLimitProb}
+              {!isPseudoNumeric && <span className="text-sm">%</span>}
             </div>
           </Row>
         </Col>
@@ -110,12 +163,18 @@ export function LimitSlider(props: {
       <Row>
         <Slider
           range
-          marks={{
-            '0': mark(0),
-            '50': mark(50),
-            '100': mark(100),
-          }}
+          marks={
+            isPseudoNumeric
+              ? undefined
+              : {
+                  0: mark(min_prob),
+                  50: mark(50),
+                  100: mark(max_prob),
+                }
+          }
           value={[lowLimitProb, highLimitProb]}
+          min={min_prob}
+          max={max_prob}
           onChange={(value) => {
             if (value && Array.isArray(value) && value.length > 1) {
               setLowLimitProb(value[0])
