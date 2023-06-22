785dd10167eb82afce30854ef52217319d0d10d1
Sinclair Chen <abc.sinclair@gmail.com>
2023-06-22 15:05:38 -0700

    Fix update-group and restrict to admins/creator

--- backend/api/src/update-group.ts
+++ backend/api/src/update-group.ts
@@ -1,7 +1,10 @@
+import * as admin from 'firebase-admin'
+
 import { createSupabaseDirectClient } from 'shared/supabase/init'
 import { updateData } from 'shared/supabase/utils'
 import { z } from 'zod'
-import { authEndpoint, validate } from './helpers'
+import { APIError, authEndpoint, validate } from './helpers'
+import { isAdmin } from 'common/envs/constants'
 
 const schema = z.object({
   id: z.string(),
@@ -17,9 +20,29 @@ const schema = z.object({
   bannerUrl: z.string().optional(),
 })
 
-export const updategroup = authEndpoint(async (req) => {
+export const updategroup = authEndpoint(async (req, auth) => {
   const { id, ...data } = validate(schema, req.body)
   const db = createSupabaseDirectClient()
-  await updateData(db, 'posts', id, data)
+
+  const requester = await db.oneOrNone(
+    'select role from group_members where group_id = $1 and member_id = $2',
+    [id, auth.uid]
+  )
+
+  // TODO: remove after migrating all creators to be admins
+  const creator = await db.oneOrNone(
+    'select creator_id from groups where id = $1',
+    [id]
+  )
+
+  if (
+    requester?.role !== 'admin' &&
+    creator?.creator_id !== auth.uid &&
+    !isAdmin((await admin.auth().getUser(auth.uid)).email)
+  ) {
+    throw new APIError(403, 'You do not have permission to update this group')
+  }
+
+  await updateData(db, 'groups', id, data)
   return { satus: 'success' }
 })
