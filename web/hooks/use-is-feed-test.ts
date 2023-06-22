f4d2a892c65250a09f5c005000d92b66a48e06d7
mantikoros <sgrugett@gmail.com>
2023-06-22 14:12:28 -0700

    test feed homepage 2

--- web/hooks/use-is-feed-test.ts
+++ web/hooks/use-is-feed-test.ts
@@ -1,7 +1,7 @@
 import { useABTest } from './use-ab-test'
 
 export const useIsFeedTest = () => {
-  return useABTest('test feed homepage', {
+  return useABTest('test feed homepage 2', {
     markets: false,
     feed: true,
   })
