dd46d6f82c5cf9a122c443fdb620aba98b8c25a7
James Grugett <jahooma@gmail.com>
2023-06-27 00:27:22 -0700

    Turn off liquidity notifications from house

--- backend/functions/src/triggers/on-create-liquidity-provision.ts
+++ backend/functions/src/triggers/on-create-liquidity-provision.ts
@@ -3,11 +3,9 @@ import { getContract, getUser, log } from 'shared/utils'
 import { createFollowOrMarketSubsidizedNotification } from 'shared/create-notification'
 import { LiquidityProvision } from 'common/liquidity-provision'
 import { addUserToContractFollowers } from 'shared/follow-market'
-import { FIXED_ANTE } from 'common/economy'
 import {
   DEV_HOUSE_LIQUIDITY_PROVIDER_ID,
   HOUSE_LIQUIDITY_PROVIDER_ID,
-  UNIQUE_BETTOR_LIQUIDITY_AMOUNT,
 } from 'common/antes'
 import { secrets } from 'common/secrets'
 import { addContractToFeed } from 'shared/create-feed'
@@ -23,10 +21,8 @@ export const onCreateLiquidityProvision = functions
     // Ignore Manifold Markets liquidity for now - users see a notification for free market liquidity provision
     if (
       liquidity.isAnte ||
-      ((liquidity.userId === HOUSE_LIQUIDITY_PROVIDER_ID ||
-        liquidity.userId === DEV_HOUSE_LIQUIDITY_PROVIDER_ID) &&
-        (liquidity.amount === FIXED_ANTE ||
-          liquidity.amount === UNIQUE_BETTOR_LIQUIDITY_AMOUNT))
+      liquidity.userId === HOUSE_LIQUIDITY_PROVIDER_ID ||
+      liquidity.userId === DEV_HOUSE_LIQUIDITY_PROVIDER_ID
     )
       return
 
