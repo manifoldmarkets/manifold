73326080bc0798f991d821763a246875a65f06d0
Ian Philips <iansphilips@gmail.com>
2023-07-04 15:04:47 -0700

    Increase non-predictive threshold slightly

--- backend/shared/src/supabase/contracts.ts
+++ backend/shared/src/supabase/contracts.ts
@@ -240,7 +240,7 @@ export const isContractLikelyNonPredictive = async (
          <=>
         (select embedding from topic_embedding)) as distance`,
       [contractId],
-      (row) => row.distance < 0.075
+      (row) => row.distance < 0.1
     )
   )[0]
 }
