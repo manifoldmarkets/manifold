bcb7b5fae58f315988fe36e49bf0f977d5abccd2
mantikoros <sgrugett@gmail.com>
2023-07-12 16:58:34 -0700

    save metadata

--- backend/api/src/manachan-tweet.ts
+++ backend/api/src/manachan-tweet.ts
@@ -5,7 +5,6 @@ import { User } from 'common/user'
 import { APIError, authEndpoint, validate } from './helpers'
 import { FieldValue } from 'firebase-admin/firestore'
 import { postTweet } from 'shared/twitter'
-import { runTxn } from 'shared/txn/run-txn'
 
 const bodySchema = z.object({
   tweet: z.string().trim().min(1).max(280),
@@ -17,7 +16,8 @@ export const manachantweet = authEndpoint(async (req, auth) => {
   const { tweet } = validate(bodySchema, req.body)
 
   const firestore = admin.firestore()
-  await firestore.runTransaction(async (transaction) => {
+
+  const user = await firestore.runTransaction(async (transaction) => {
     const userDoc = firestore.doc(`users/${auth.uid}`)
     const userSnap = await transaction.get(userDoc)
     if (!userSnap.exists) throw new APIError(400, 'User not found')
@@ -30,7 +30,20 @@ export const manachantweet = authEndpoint(async (req, auth) => {
       balance: FieldValue.increment(-MANACHAN_TWEET_COST),
       totalDeposits: FieldValue.increment(-MANACHAN_TWEET_COST),
     })
+
+    return user
   })
 
-  return await postTweet(tweet)
+  const result = await postTweet(tweet)
+
+  firestore.collection('manachan-tweets').add({
+    userId: auth.uid,
+    username: user.name,
+    tweetId: result.id,
+    tweet: result.text,
+    createdTime: Date.now(),
+    cost: MANACHAN_TWEET_COST,
+  })
+
+  return { success: true, tweetId: result.id }
 })
