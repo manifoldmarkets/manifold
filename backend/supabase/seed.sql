6c471dbb98c6256a7d1ee7912018a913a0f8084c
mantikoros <sgrugett@gmail.com>
2023-07-14 11:29:02 -0700

    run calibration on server

--- backend/supabase/seed.sql
+++ backend/supabase/seed.sql
@@ -615,6 +615,24 @@ create index if not exists contract_bets_unexpired_limit_orders on contract_bets
 alter table contract_bets
 cluster on contract_bets_created_time;
 
+
+
+create table if not exists
+  platform_calibration (
+    id bigint generated always as identity primary key,
+    created_time timestamptz not null default now(),
+    data jsonb not null
+  );
+
+alter table platform_calibration enable row level security;
+
+drop policy if exists "public read" on platform_calibration;
+
+create policy "public read" on platform_calibration for
+select
+  using (true);
+
+
 create table if not exists
   contract_comments (
     contract_id text not null,
