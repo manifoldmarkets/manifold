33d75331426a948cfd0a5fa0f33ae8cd4a3de797
mantikoros <mantikoros@users.noreply.github.com>
2023-07-16 22:50:46 +0000

    Auto-remove unused imports

--- backend/functions/src/scheduled/update-stats.ts
+++ backend/functions/src/scheduled/update-stats.ts
@@ -27,7 +27,6 @@ import {
 } from 'shared/supabase/init'
 import { secrets } from 'common/secrets'
 import { bulkUpsert } from 'shared/supabase/utils'
-import { saveCalibrationData } from './calculate-calibration'
 
 const firestore = admin.firestore()
 
