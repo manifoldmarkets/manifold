32220b315168a6c20e7fa3df692efc43ad3db722
Ian Philips <iansphilips@gmail.com>
2023-07-15 17:57:02 -0700

    Write contract metrics columns in bulkUpdate

--- backend/shared/src/helpers/user-contract-metrics.ts
+++ backend/shared/src/helpers/user-contract-metrics.ts
@@ -5,6 +5,8 @@ import { calculateUserMetrics } from 'common/calculate-metrics'
 import { bulkUpsert } from 'shared/supabase/utils'
 import { createSupabaseDirectClient } from 'shared/supabase/init'
 import { ContractMetric } from 'common/contract-metric'
+import { Row } from 'common/supabase/utils'
+import { removeUndefinedProps } from 'common/util/object'
 
 export async function updateContractMetricsForUsers(
   contract: Contract,
@@ -22,17 +24,25 @@ export async function updateContractMetricsForUsers(
 
 export async function bulkUpdateContractMetrics(metrics: ContractMetric[]) {
   const pg = createSupabaseDirectClient()
+  const updatedTime = new Date().toISOString()
   bulkUpsert(
     pg,
     'user_contract_metrics',
     ['user_id', 'contract_id'],
     metrics.map(
       (m) =>
-        ({
+        removeUndefinedProps({
           contract_id: m.contractId,
           user_id: m.userId,
           data: m,
-        } as any)
+          fs_updated_time: updatedTime,
+          has_no_shares: m.hasNoShares,
+          has_shares: m.hasShares,
+          profit: m.profit,
+          has_yes_shares: m.hasYesShares,
+          total_shares_no: m.totalShares['NO'],
+          total_shares_yes: m.totalShares['YES'],
+        }) as Row<'user_contract_metrics'>
     )
   )
 }
