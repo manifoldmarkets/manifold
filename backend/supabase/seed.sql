1a7e9345a3ef916a9c370aff22aa13fb6d144add
James Grugett <jahooma@gmail.com>
2023-07-16 13:49:58 -0700

    Add back supabase real time

--- backend/supabase/seed.sql
+++ backend/supabase/seed.sql
@@ -1243,36 +1243,34 @@ drop publication if exists supabase_realtime;
 
 create publication supabase_realtime;
 
--- Disable real time except for bets to judge performance.
--- alter publication supabase_realtime
--- add table contracts;
-
+alter publication supabase_realtime
+add table contracts;
 alter publication supabase_realtime
 add table contract_bets;
 
--- alter publication supabase_realtime
--- add table contract_comments;
+alter publication supabase_realtime
+add table contract_comments;
 
--- alter publication supabase_realtime
--- add table group_members;
+alter publication supabase_realtime
+add table group_members;
 
--- alter publication supabase_realtime
--- add table posts;
+alter publication supabase_realtime
+add table posts;
 
--- alter publication supabase_realtime
--- add table post_comments;
+alter publication supabase_realtime
+add table post_comments;
 
--- alter publication supabase_realtime
--- add table group_contracts;
+alter publication supabase_realtime
+add table group_contracts;
 
--- alter publication supabase_realtime
--- add table contract_follows;
+alter publication supabase_realtime
+add table contract_follows;
 
--- alter publication supabase_realtime
--- add table chat_messages;
+alter publication supabase_realtime
+add table chat_messages;
 
--- alter publication supabase_realtime
--- add table user_notifications;
+alter publication supabase_realtime
+add table user_notifications;
 
 commit;
 
