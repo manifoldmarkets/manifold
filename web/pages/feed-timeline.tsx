54f09d127373103bdf21077a5dc983a18ecda78a
Ian Philips <iansphilips@gmail.com>
2023-07-18 14:23:19 -0700

    remove max-w

--- web/pages/feed-timeline.tsx
+++ web/pages/feed-timeline.tsx
@@ -200,7 +200,7 @@ const NewActivityButton = (props: {
     <button
       className={clsx(
         'bg-canvas-50 border-ink-200 hover:bg-ink-200 rounded-full border-2 py-2 pr-3 pl-2 text-sm transition-colors',
-        'sticky top-7 z-20 max-w-[12rem] self-center'
+        'sticky top-7 z-20 self-center'
       )}
       onClick={scrollToTop}
     >
