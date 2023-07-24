7f44c7b4df481e0ad29bf719234beedfe3bd14b0
ingawei <ingawei@users.noreply.github.com>
2023-07-24 21:35:54 +0000

    Auto-remove unused imports

--- web/pages/group/[...slugs]/index.tsx
+++ web/pages/group/[...slugs]/index.tsx
@@ -1,4 +1,4 @@
-import Router, { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import { useEffect, useRef, useState } from 'react'
 
 import { Group, groupPath, PrivacyStatusType } from 'common/group'
