db5de924aaf8e4f62bdaafbc60358398b76de43f
ingawei <ingawei@gmail.com>
2023-07-26 19:40:56 -0700

    added seeing things

--- web/components/poll/poll-panel.tsx
+++ web/components/poll/poll-panel.tsx
@@ -11,6 +11,14 @@ import { Col } from '../layout/col'
 import { sumBy } from 'lodash'
 import Link from 'next/link'
 import { ArrowRightIcon } from '@heroicons/react/solid'
+import { MODAL_CLASS, Modal, SCROLLABLE_MODAL_CLASS } from '../layout/modal'
+import clsx from 'clsx'
+import { useOptionVoters } from 'web/hooks/use-votes'
+import { LoadingIndicator } from '../widgets/loading-indicator'
+import { Row } from '../layout/row'
+import { Avatar } from '../widgets/avatar'
+import { UserLink } from '../widgets/user-link'
+import { Spacer } from '../layout/spacer'
 
 export function PollPanel(props: {
   contract: PollContract
@@ -65,10 +73,7 @@ export function PollPanel(props: {
             end={
               <>
                 {hasVoted && (
-                  <span className="whitespace-nowrap">
-                    {option.votes}{' '}
-                    <span className="text-ink-500 text-xs">votes</span>
-                  </span>
+                  <SeeVotesButton option={option} contractId={contract.id} />
                 )}
                 {!hasVoted && votingOpen && (
                   <VoteButton
@@ -94,6 +99,78 @@ export function PollPanel(props: {
         </Link>
       )}
     </Col>
+  )
+}
+
+export function SeeVotesButton(props: {
+  option: PollOption
+  contractId: string
+}) {
+  const { option, contractId } = props
+  const [open, setOpen] = useState(false)
+  const disabled = option.votes === 0
+  return (
+    <>
+      <button
+        className="disabled:text-ink-500 disabled:pointer-none group whitespace-nowrap transition-colors hover:text-indigo-400 disabled:cursor-not-allowed"
+        onClick={() => setOpen(true)}
+        disabled={disabled}
+      >
+        <span>{option.votes}</span>{' '}
+        <span
+          className={clsx(
+            'text-ink-500 text-xs transition-colors',
+            disabled ? '' : 'group-hover:text-indigo-400'
+          )}
+        >
+          votes
+        </span>
+      </button>
+      <SeeVotesModal
+        option={option}
+        contractId={contractId}
+        open={open}
+        setOpen={setOpen}
+      />
+    </>
+  )
+}
+
+export function SeeVotesModal(props: {
+  option: PollOption
+  contractId: string
+  open: boolean
+  setOpen: (open: boolean) => void
+}) {
+  const { option, contractId, open, setOpen } = props
+  const voters = useOptionVoters(contractId, option.id)
+  return (
+    <Modal open={open} setOpen={setOpen}>
+      <Col className={clsx(MODAL_CLASS, SCROLLABLE_MODAL_CLASS)}>
+        <div className="bg-canvas-0 fixed inset-x-0 top-0 z-40 w-full rounded-t-md py-2 px-8">
+          Votes on <b>{option.text}</b>
+        </div>
+        <Spacer h={2} />
+        {!voters ? (
+          <LoadingIndicator />
+        ) : voters.length == 0 ? (
+          'No votes yet...'
+        ) : (
+          voters.map((voter) => {
+            return (
+              <Row className="w-full items-center gap-2">
+                <Avatar
+                  username={voter.username}
+                  avatarUrl={voter.avatarUrl}
+                  size={'sm'}
+                />
+                <UserLink name={voter.name} username={voter.username} />
+              </Row>
+            )
+          })
+        )}
+      </Col>
+    </Modal>
   )
 }
 
--- web/hooks/use-votes.ts
+++ web/hooks/use-votes.ts
@@ -1,6 +1,6 @@
 import { User } from 'common/user'
 import { useEffect, useState } from 'react'
-import { getContractVoters } from 'web/lib/supabase/polls'
+import { getContractVoters, getOptionVoters } from 'web/lib/supabase/polls'
 
 export function useContractVoters(contractId: string) {
   const [contractVoters, setContractVoters] = useState<User[] | undefined>(
@@ -12,4 +12,16 @@ export function useContractVoters(contractId: string) {
     })
   }, [contractId])
   return contractVoters
+}
+
+export function useOptionVoters(contractId: string, optionId: string) {
+  const [optionVoters, setOptionVoters] = useState<User[] | undefined>(
+    undefined
+  )
+  useEffect(() => {
+    getOptionVoters(contractId, optionId).then((voters) => {
+      setOptionVoters(voters)
+    })
+  }, [contractId, optionId])
+  return optionVoters
 }
