e7b8939dd79bc2d63f332adfd8b05a55ccc66934
ingawei <ingawei@users.noreply.github.com>
2023-07-31 20:53:46 +0000

    Auto-remove unused imports

--- web/components/contract/contracts-table.tsx
+++ web/components/contract/contracts-table.tsx
@@ -19,8 +19,6 @@ import { Action } from './contract-table-action'
 import { useFirebasePublicAndRealtimePrivateContract } from 'web/hooks/use-contract-supabase'
 import { Col } from '../layout/col'
 import { useNumContractComments } from 'web/hooks/use-comments-supabase'
-import { Tooltip } from '../widgets/tooltip'
-import { RiBarChartFill } from 'react-icons/ri'
 
 const lastItemClassName = 'rounded-r pr-2'
 const firstItemClassName = 'rounded-l pl-2 pr-4'
