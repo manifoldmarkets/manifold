7cc81b8608da7da4f8f5790c8e185f8e84ad4eef
James Grugett <jahooma@gmail.com>
2023-08-01 00:54:38 -0400

    Fix build

--- web/pages/embed/analytics.tsx
+++ web/pages/embed/analytics.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Col } from 'web/components/layout/col'
-import { CustomAnalytics } from '../stats'
+// import { CustomAnalytics } from '../stats'
 import { getStats } from 'web/lib/supabase/stats'
 import { Stats } from 'common/stats'
 
@@ -14,7 +14,7 @@ export default function AnalyticsEmbed() {
   }
   return (
     <Col className="bg-canvas-0 w-full px-2">
-      <CustomAnalytics {...stats} />
+      {/* <CustomAnalytics {...stats} /> */}
     </Col>
   )
 }
