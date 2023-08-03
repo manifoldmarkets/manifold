c335e912cefabb3641dfd92f0ca75c20aa37134d
mantikoros <sgrugett@gmail.com>
2023-08-03 11:24:11 -0400

    link preview vercel caching

--- web/pages/api/v0/fetch-link-preview.ts
+++ web/pages/api/v0/fetch-link-preview.ts
@@ -5,16 +5,26 @@ import { getLinkPreview } from 'link-preview-js'
 
 import { first } from 'lodash'
 
+const CACHE = new Map<string, Awaited<ReturnType<typeof getLinkPreview>>>()
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Awaited<ReturnType<typeof getLinkPreview>> | ApiError>
 ) {
   await applyCorsHeaders(req, res, CORS_UNRESTRICTED)
+  res.setHeader('Cache-Control', 's-maxage=86400')
+
   const { url } = req.body
   if (!url) return res.status(404).json({ error: 'url required' })
+
+  if (CACHE.has(url))
+    return res
+      .status(200)
+      .json(CACHE.get(url) ?? { error: 'Error fetching link preview' })
+
   try {
     const metadata = await fetchLinkPreview(url)
-    res.setHeader('Cache-Control', 's-maxage=86400')
+    CACHE.set(url, metadata)
     return res.status(200).json(metadata)
   } catch (error) {
     return res.status(500).json({ error: 'Error fetching link preview' })
