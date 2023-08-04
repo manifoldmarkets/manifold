81b0e4e1bffe053dcfd8c96c6f27260b3ab84f95
Ian Philips <iansphilips@gmail.com>
2023-08-04 09:55:28 -0400

    Add class to news article, remove broken 'All Articles'

--- web/components/feed/feed-timeline-items.tsx
+++ web/components/feed/feed-timeline-items.tsx
@@ -106,6 +106,7 @@ export const FeedTimelineItems = (props: {
                 author={(news as any)?.author}
                 published_time={(news as any)?.published_time}
                 {...news}
+                className={'h-24 object-cover'}
               />
               {item.contracts && item.contracts.length > 0 && (
                 <Col className="px-4 pt-2 pb-3">
--- web/components/layout/tabs.tsx
+++ web/components/layout/tabs.tsx
@@ -7,7 +7,7 @@ import { Tooltip } from 'web/components/widgets/tooltip'
 import { Row } from 'web/components/layout/row'
 import { Carousel } from 'web/components/widgets/carousel'
 
-type Tab = {
+export type Tab = {
   title: string
   content: ReactNode
   stackedTabIcon?: ReactNode
