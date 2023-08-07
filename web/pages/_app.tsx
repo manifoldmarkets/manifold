15e975ba89619627c90cb5f810c8d5369fe0a8c9
mantikoros <sgrugett@gmail.com>
2023-08-07 13:44:43 -0400

    fb px

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -161,6 +161,22 @@ function MyApp({ Component, pageProps }: AppProps<ManifoldPageProps>) {
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
         }}
       />
+      <Script
+        id="fbpx"
+        dangerouslySetInnerHTML={{
+          __html: `
+          !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '254770557407697');
+  fbq('track', 'PageView');`,
+        }}
+      />
       <GoogleOneTapSetup />
     </>
   )
