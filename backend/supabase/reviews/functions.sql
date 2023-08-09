dc5dc2ba3d09a91dc1496d667a117b30fdc8ee6f
mantikoros <sgrugett@gmail.com>
2023-08-09 15:16:09 -0400

    get_rating: use total review count

--- backend/supabase/reviews/functions.sql
+++ backend/supabase/reviews/functions.sql
@@ -2,23 +2,30 @@ CREATE OR REPLACE FUNCTION get_rating(user_id TEXT)
 RETURNS TABLE (count BIGINT, rating NUMERIC)
 IMMUTABLE PARALLEL SAFE LANGUAGE SQL
 AS $$
-  WITH 
+  WITH
+
   -- find average of each user's reviews
-  rating_counts AS (
+  avg_ratings AS (
     SELECT AVG(rating) AS avg_rating
     FROM reviews
     WHERE vendor_id = user_id
     GROUP BY reviewer_id
-  ), 
-  
+  ),
+
+  total_count AS (
+    SELECT COUNT(*) AS count
+    FROM reviews
+    WHERE vendor_id = user_id
+  ),
+
   positive_counts AS (
-    SELECT 5 + COUNT(*) AS count FROM rating_counts WHERE avg_rating >= 4.0
-  ), 
-  
+    SELECT 5 + COUNT(*) AS count FROM avg_ratings WHERE avg_rating >= 4.0
+  ),
+
   negative_counts AS (
-    SELECT 2 * COUNT(*) AS count FROM rating_counts WHERE avg_rating < 4.0
-  ), 
-  
+    SELECT 2 * COUNT(*) AS count FROM avg_ratings WHERE avg_rating < 4.0
+  ),
+
   -- calculate lower bound of 95th percentile confidence interval: https://www.evanmiller.org/how-not-to-sort-by-average-rating.html
   rating AS (
     SELECT (positive_counts.count + negative_counts.count) AS count,
@@ -30,8 +37,8 @@ AS $$
     FROM positive_counts, negative_counts
   )
 
-  SELECT rating.count - 5, -- subtract added positive reviews
+  SELECT total_count.count as count,
          -- squash with sigmoid, multiply by 5
          5 / (1 + POW(2.71828, -10*(rating.rating-0.5))) AS rating
-  FROM rating;
+  FROM total_count, rating;
 $$;
