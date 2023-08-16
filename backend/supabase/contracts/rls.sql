e7bfaff6eb10a14eafe9b57951e3ca26904c0638
ingawei <ingawei@gmail.com>
2023-08-16 13:25:59 -0700

    getting rid of private group rls, private contracts can no longer be accessed through RLS

--- backend/supabase/contracts/rls.sql
+++ backend/supabase/contracts/rls.sql
@@ -10,14 +10,3 @@ drop policy if exists "Enable read access for admins" on public.contracts;
 create policy "Enable read access for admins" on public.contracts for
 select
   to service_role using (true);
-
-drop policy if exists "Enable read access for private group markets if user is member" on public.contracts;
-
-create policy "Enable read access for private group markets if user is member" on public.contracts for
-select
-  using (
-    (
-      visibility = 'private'::text
-      and can_access_private_contract (id, firebase_uid ())
-    )
-  );
