d1f3d4c32f5271e7234eb487b2f21bf21aa29f92
ingawei <ingawei@gmail.com>
2023-08-17 18:07:26 -0700

    truncated description

---
+++ web/components/feed/feed-contract-card-description.tsx
@@ -0,0 +1,42 @@
+import { Contract } from 'common/contract'
+import { useEffect, useRef, useState } from 'react'
+import { Content } from '../widgets/editor'
+import { ArrowRightIcon } from '@heroicons/react/solid'
+import { Row } from '../layout/row'
+import { Col } from '../layout/col'
+import { useEffectCheckEquality } from 'web/hooks/use-effect-check-equality'
+export const MAX_HEIGHT = 250
+
+export default function FeedContractCardDescription(props: {
+  contract: Contract
+}) {
+  const { contract } = props
+  const [isOverflowing, setIsOverflowing] = useState(false)
+  const contentRef = useRef<HTMLDivElement>(null)
+
+  useEffectCheckEquality(() => {
+    if (contentRef.current && contentRef.current.scrollHeight > MAX_HEIGHT) {
+      setIsOverflowing(true)
+    }
+  }, [contract.description])
+  return (
+    <div className="relative">
+      <div
+        ref={contentRef}
+        className={`overflow-hidden`}
+        style={{ maxHeight: `${MAX_HEIGHT}px` }}
+      >
+        <Content content={contract.description} />
+      </div>
+      {isOverflowing && (
+        <Col className="from-canvas-0 via-canvas-0 via-30% absolute bottom-0 right-0 left-0 h-12 justify-end bg-gradient-to-t to-transparent">
+          <Row className="w-full justify-end">
+            <span className=" text-ink-500 hover:text-primary-500 text-sm">
+              Read more <ArrowRightIcon className="inline h-4 w-4" />
+            </span>
+          </Row>
+        </Col>
+      )}
+    </div>
+  )
+}
