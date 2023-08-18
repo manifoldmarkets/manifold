1e7be831ad9f7c8c0765c6dcfee0db1390816ca5
Sinclair Chen <abc.sinclair@gmail.com>
2023-08-18 08:49:19 -0700

    Add new user market page

--- backend/supabase/contracts/functions.sql
+++ backend/supabase/contracts/functions.sql
@@ -30,10 +30,22 @@ or replace function get_user_bet_contracts (this_user_id text, this_limit intege
 $$;
 
 create
-or replace function get_unique_bettors_since(this_contract_id text, since bigint) returns bigint
-language sql as $$
+or replace function get_unique_bettors_since (this_contract_id text, since bigint) returns bigint language sql as $$
   select count(distinct user_id)
   from contract_bets
   where contract_id = this_contract_id
     and created_time >= millis_to_ts(since);
+$$;
+
+create
+or replace function get_noob_questions () returns setof contracts language sql as $$
+  with newbs as (
+    select id
+    from users
+    where (data->>'createdTime')::bigint > ts_to_millis(now() - interval '2 weeks')
+  )
+  select * from contracts
+  where creator_id in (select * from newbs)
+  and visibility = 'public'
+  order by created_time desc
 $$;
