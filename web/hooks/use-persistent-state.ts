5ebaff9a9d691e39b964c222bcbfcfbe1c564fe7
Sinclair Chen <abc.sinclair@gmail.com>
2023-08-23 18:11:17 -0700

    delete usePersistentRevalidatedState
    makes daily profit's contracts more out of date
    but we revalidate on open anyways

--- web/hooks/use-persistent-state.ts
+++ web/hooks/use-persistent-state.ts
@@ -3,47 +3,11 @@ import { useStateCheckEquality } from './use-state-check-equality'
 import { useHasLoaded } from './use-has-loaded'
 
 export type PersistenceOptions<T> = { key: string; store: PersistentStore<T> }
-export type RevalidationOptions<T> = {
-  every: number
-  // callback is async with results of the revalidation
-  callback: () => Promise<T>
-}
-
-export type Backend = Pick<Storage, 'getItem' | 'setItem' | 'removeItem'>
 
 export interface PersistentStore<T> {
   get: (k: string) => T | undefined
   set: (k: string, v: T | undefined) => void
 }
-
-export const storageStore = <T>(
-  storage: Backend | undefined
-): PersistentStore<T> => ({
-  get: (k: string) => {
-    if (!storage) {
-      return undefined
-    }
-    const saved = storage.getItem(k)
-    if (typeof saved === 'string') {
-      try {
-        return JSON.parse(saved) as T
-      } catch (e) {
-        console.error(e)
-      }
-    } else {
-      return undefined
-    }
-  },
-  set: (k: string, v: T | undefined) => {
-    if (storage) {
-      if (v === undefined) {
-        storage.removeItem(k)
-      } else {
-        storage.setItem(k, JSON.stringify(v))
-      }
-    }
-  },
-})
 
 export const historyStore = <T>(prefix = '__manifold'): PersistentStore<T> => ({
   get: (k: string) => {
@@ -104,45 +68,4 @@ export const usePersistentState = <T>(
   }, [key, state, hasLoaded])
 
   return [state, setState] as const
-}
-
-/** @deprecated  - use usePersistentLocalState or write new hook */
-export const usePersistentRevalidatedState = <T>(
-  initial: T,
-  persist: PersistenceOptions<T>,
-  revalidation: RevalidationOptions<T>
-) => {
-  const { key, store } = persist
-  // Is there any reason why a persistent store couldn't write a string?
-  const stringStore = store as PersistentStore<any>
-  const lastUpdateTimeKey = `${key}-last-update-time`
-  const hasLoaded = useHasLoaded()
-  const { every, callback } = revalidation
-
-  // Note that it's important in some cases to get the state correct during the
-  // first render, or scroll restoration won't take into account the saved state.
-  // However, if this is the first server render, we don't want to read from the store,
-  // because it could cause a hydration error.
-  const savedValue =
-    hasLoaded && key != null && store != null ? store.get(key) : undefined
-
-  const [state, setState] = useStateCheckEquality(savedValue ?? initial)
-
-  useEffect(() => {
-    if (!hasLoaded || key === null || store === null) return
-    const lastUpdateTime = parseInt(stringStore.get(lastUpdateTimeKey) ?? 0)
-    const now = Date.now()
-    if (lastUpdateTime + every < now) {
-      stringStore.set(lastUpdateTimeKey, now.toString())
-      callback().then(setState)
-    }
-  }, [])
-
-  useEffect(() => {
-    if (hasLoaded && key != null && store != null && state !== undefined) {
-      store.set(key, state)
-    }
-  }, [key, state, hasLoaded])
-
-  return [state ?? savedValue, setState] as const
 }
