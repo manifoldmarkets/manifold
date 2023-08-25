5b476af5c43a301134e918b95f2252150eb909dd
mantikoros <sgrugett@gmail.com>
2023-08-25 10:24:40 -0700

    useTrendingGroupsSearchResults: fb token

--- web/components/search/query-groups.ts
+++ web/components/search/query-groups.ts
@@ -2,6 +2,7 @@ import { debounce, uniqBy } from 'lodash'
 import { useEffect, useMemo } from 'react'
 import { SearchGroupInfo, searchGroups } from 'web/lib/supabase/groups'
 import { usePersistentInMemoryState } from 'web/hooks/use-persistent-in-memory-state'
+import { auth } from 'web/lib/firebase/users'
 
 export function useTrendingGroupsSearchResults(
   query: string,
@@ -13,20 +14,24 @@ export function useTrendingGroupsSearchResults(
     'trending-groups-market-search'
   )
 
-  const debouncedOnSearch = useMemo(
-    () =>
-      debounce(async (query: string) => {
-        searchGroups({ term: query, limit }).then((results) =>
-          setResults(uniqBy(results.data, 'id'))
-        )
-      }, 50),
-    [limit]
-  )
+  const search = async (query: string) => {
+    searchGroups({ term: query, limit }).then((results) =>
+      setResults(uniqBy(results.data, 'id'))
+    )
+  }
+  const debouncedOnSearch = useMemo(() => debounce(search, 50), [limit])
 
   useEffect(() => {
     if (lockSearch) return
-    debouncedOnSearch(query)
-  }, [query, debouncedOnSearch, lockSearch])
+    if (query) debouncedOnSearch(query)
+    else search('') // don't debounce on page load
+  }, [
+    query,
+    debouncedOnSearch,
+    lockSearch,
+    // need to pass FB user to get auth token to request customized groups
+    auth.currentUser?.uid,
+  ])
 
   return results
 }
