ef7b77db8367422a85710e775bc6ca9a832b6019
ingawei <ingawei@gmail.com>
2023-08-31 15:50:45 -0700

    add back to right

--- web/components/feed/feed-timeline-items.tsx
+++ web/components/feed/feed-timeline-items.tsx
@@ -138,13 +138,13 @@ export function GroupTags(props: {
   const { groups, className, maxGroups = 3 } = props
   if (!groups || groups.length <= 0) return null
   return (
-    // <Row className="w-full justify-end">
-    <Row className={(clsx('gap-0.5 overflow-hidden'), className)}>
-      {groups.slice(0, maxGroups).map((group) => (
-        <GroupTag key={group.slug} group={group} />
-      ))}
+    <Row className="w-full justify-end">
+      <Row className={(clsx('gap-0.5 overflow-hidden'), className)}>
+        {groups.slice(0, maxGroups).map((group) => (
+          <GroupTag key={group.slug} group={group} />
+        ))}
+      </Row>
     </Row>
-    // </Row>
   )
 }
 
