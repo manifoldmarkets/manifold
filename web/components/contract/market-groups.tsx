73895f054143069d6c8d606d40215d2781a679ba
Sinclair Chen <abc.sinclair@gmail.com>
2023-09-06 18:54:17 -0700

    Fix breadcrumb spacing for 🖥️ Technology

--- web/components/contract/market-groups.tsx
+++ web/components/contract/market-groups.tsx
@@ -132,8 +132,6 @@ export function PublicMarketGroups(props: { contract: Contract }) {
 }
 
 export function removeEmojis(input: string): string {
-  const emojiRegex =
-    /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{2300}-\u{23FF}\u{2B50}\u{1F004}\u{1F3F4}\u{E0067}-\u{E007F}\u{1F1E6}-\u{1F1FF}]/gu
-
-  return replace(input, emojiRegex, '')
+  const emojiRegex = /[\p{Extended_Pictographic}\u{FE00}-\u{FE0F}]/gu
+  return input.replace(emojiRegex, '').trim()
 }
