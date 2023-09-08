932019c181bbd4b0979f20198d1e87caa8ece2a2
ingawei <ingawei@gmail.com>
2023-09-07 17:42:22 -0700

    basic cardless functionality

---
+++ web/components/dashboard/dashboard-sidebar.tsx
@@ -0,0 +1,31 @@
+import { JSONContent } from '@tiptap/core'
+import { JSONEmpty } from '../contract/contract-description'
+import { Col } from '../layout/col'
+import { Content } from '../widgets/editor'
+import clsx from 'clsx'
+
+export const DashboardSidebar = (props: {
+  description?: JSONContent
+  inSidebar?: boolean
+}) => {
+  const { description, inSidebar } = props
+
+  if (!description || JSONEmpty(description)) return <></>
+
+  return (
+    <Col className={clsx(inSidebar ? 'hidden xl:inline-flex' : 'xl:hidden')}>
+      {description && (
+        <Col className=" text-primary-700 mb-2 hidden xl:inline-flex">
+          Additional Context
+        </Col>
+      )}
+      {description && (
+        <>
+          <Col className="bg-canvas-0 mb-4 gap-2 py-2 px-4 xl:px-6 xl:py-4">
+            <Content content={description} />
+          </Col>
+        </>
+      )}
+    </Col>
+  )
+}
---
+++ web/pages/dashboard/[dashboardSlug].tsx
@@ -0,0 +1,49 @@
+import { Dashboard } from 'common/dashboard'
+import { getDashboardFromSlug } from 'common/supabase/dashboard'
+import { DashboardSidebar } from 'web/components/dashboard/dashboard-sidebar'
+import { Col } from 'web/components/layout/col'
+import { Page } from 'web/components/layout/page'
+import { NewsSidebar } from 'web/components/news/news-dashboard'
+import { Content } from 'web/components/widgets/editor'
+import { Title } from 'web/components/widgets/title'
+import { db } from 'web/lib/supabase/db'
+
+export async function getStaticProps(ctx: {
+  params: { dashboardSlug: string }
+}) {
+  const { dashboardSlug } = ctx.params
+
+  try {
+    const dashboard: Dashboard = await getDashboardFromSlug(dashboardSlug, db)
+    return { props: { dashboard } }
+  } catch (e) {
+    if (typeof e === 'object' && e !== null && 'code' in e && e.code === 404) {
+      return {
+        props: { state: 'not found' },
+        revalidate: 60,
+      }
+    }
+    throw e
+  }
+}
+
+export async function getStaticPaths() {
+  return { paths: [], fallback: 'blocking' }
+}
+
+export default function DashboardPage(props: { dashboard: Dashboard }) {
+  const { dashboard } = props
+  console.log(dashboard)
+  return (
+    <Page
+      rightSidebar={
+        <DashboardSidebar description={dashboard.description} inSidebar />
+      }
+    >
+      <Col>
+        <Title>{dashboard.title}</Title>
+        <DashboardSidebar description={dashboard.description} />
+      </Col>
+    </Page>
+  )
+}
