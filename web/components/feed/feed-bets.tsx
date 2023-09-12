e8d906f91b3594c1cf8a5310467dbbe33ad747d8
Sinclair Chen <abc.sinclair@gmail.com>
2023-09-11 19:07:27 -0700

    delete unused feed bet stuff

--- web/components/feed/feed-bets.tsx
+++ web/components/feed/feed-bets.tsx
@@ -20,9 +20,6 @@ import { floatingEqual, floatingLesserEqual } from 'common/util/math'
 import { Col } from 'web/components/layout/col'
 import { ReplyIcon } from '@heroicons/react/solid'
 import { track } from 'web/lib/service/analytics'
-import { groupBy, maxBy, partition, sumBy } from 'lodash'
-import { MINUTE_MS } from 'common/util/time'
-import { sort } from 'd3-array'
 import { Tooltip } from 'web/components/widgets/tooltip'
 import { InfoTooltip } from '../widgets/info-tooltip'
 import Link from 'next/link'
@@ -66,112 +63,6 @@ export const FeedBet = memo(function FeedBet(props: {
         />
       </Row>
     </Col>
-  )
-})
-export function groupBetsByCreatedTimeAndUserId(bets: Bet[]) {
-  const DISTANCE_IN_MINUTES = 60
-  const betsByUserId = groupBy(
-    bets,
-    // Don't combine limit order creations and bets that actually fill
-    (bet) => `${bet.userId}-${bet.amount === 0 ? 'limit' : 'market'}`
-  )
-  const betsByTime: {
-    [key: number]: Bet[]
-  } = {}
-  Object.keys(betsByUserId).forEach((userIdAndBetType) => {
-    const userBets = betsByUserId[userIdAndBetType]
-    userBets.forEach((bet) => {
-      const { createdTime } = bet
-      const timestamps = Object.keys(betsByTime)
-      let foundTimestamp = 0
-      timestamps.forEach((key) => {
-        const timestamp = key ? parseInt(key) : createdTime
-        const differenceInMinutes = Math.abs(
-          (createdTime - timestamp) / MINUTE_MS
-        )
-        if (
-          differenceInMinutes <= DISTANCE_IN_MINUTES &&
-          betsByTime[timestamp][0].userId === userIdAndBetType.split('-')[0]
-        ) {
-          foundTimestamp = timestamp
-        }
-      })
-      if (foundTimestamp) {
-        betsByTime[foundTimestamp].push(bet)
-      } else {
-        betsByTime[createdTime] = [bet]
-      }
-    })
-  })
-  const sortedBetsByTime: Bet[][] = []
-  sort(Object.keys(betsByTime), (a, b) => parseInt(b) - parseInt(a)).forEach(
-    (key) => {
-      sortedBetsByTime.push(betsByTime[parseInt(key)])
-    }
-  )
-  return sortedBetsByTime
-}
-export const SummarizeBets = memo(function SummarizeBets(props: {
-  contract: Contract
-  betsBySameUser: Bet[]
-  avatarSize?: AvatarSizeType
-  inTimeline?: boolean
-}) {
-  const { contract, betsBySameUser, avatarSize, inTimeline } = props
-  let bet = betsBySameUser[0]
-  // for simplicity, we should just show buys of yes or buys of no
-  if (betsBySameUser.length > 1) {
-    // get the bet with the highest amount
-    const maxBet = maxBy(betsBySameUser, (bet) => Math.abs(bet.amount))
-    bet = maxBet ?? bet
-    const [yesBets, noBets] = partition(
-      betsBySameUser,
-      (bet) => bet.outcome === 'YES' || (bet.outcome === 'NO' && bet.amount < 0)
-    )
-    const totalYesAmount = sumBy(yesBets, (bet) => Math.abs(bet.amount))
-    const totalNoAmount = sumBy(noBets, (bet) => Math.abs(bet.amount))
-    const showYes = totalYesAmount > totalNoAmount
-    bet = {
-      ...bet,
-      outcome: showYes ? 'YES' : 'NO',
-      amount: Math.abs(totalYesAmount - totalNoAmount),
-    }
-  }
-  const { userAvatarUrl, userUsername, createdTime } = bet
-  const showUser = dayjs(createdTime).isAfter('2022-06-01')
-
-  // group bets by userid made within 30 minutes of each other
-  return (
-    <Row className={'w-full gap-2'}>
-      {showUser ? (
-        <Avatar
-          size={avatarSize}
-          avatarUrl={userAvatarUrl}
-          username={userUsername}
-          className="z-10"
-        />
-      ) : (
-        <EmptyAvatar className="mx-1" />
-      )}
-      <Col>
-        <span>
-          <UserLink
-            name={bet.userName}
-            username={bet.userUsername}
-            className="text-ink-1000 font-semibold"
-          />{' '}
-          bet
-          <RelativeTimestamp time={createdTime} shortened={true} />
-        </span>
-        <BetStatusText
-          bet={bet}
-          contract={contract}
-          hideUser={!showUser}
-          className="flex-1"
-          inTimeline={inTimeline}
-        />
-      </Col>
-    </Row>
   )
 })
 
