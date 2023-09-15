28345d2c7f41408754f0ee826387783aa4a8336e
ingawei <ingawei@gmail.com>
2023-09-15 16:32:57 -0700

    fix typo

--- backend/api/src/update-dashboard.ts
+++ backend/api/src/update-dashboard.ts
@@ -1,10 +1,8 @@
 import { createSupabaseDirectClient } from 'shared/supabase/init'
 import { log } from 'shared/utils'
+import { DashboardItemSchema } from 'shared/zod-types'
 import { z } from 'zod'
-import { APIError, authEndpoint, validate } from './helpers'
-import { DashboardItemSchema, contentSchema } from 'shared/zod-types'
-import { slugify } from 'common/util/slugify'
-import { randomString } from 'common/util/random'
+import { authEndpoint, validate } from './helpers'
 
 const schema = z.object({
   dashboardId: z.string(),
@@ -19,7 +17,7 @@ export const updatedashboard = authEndpoint(async (req, auth) => {
 
   const updatedDashboard = await pg.one(
     `update dashboards
-      items = $1
+      set items = $1
       where id = $2 and creator_id = $3
       returning *`,
     [JSON.stringify(items), dashboardId, auth.uid]
