b810efeb8bf62a61af410a11a7ab2c5694f35a4d
Ian Philips <iansphilips@gmail.com>
2023-09-18 12:16:52 -0700

    Fix destiny and yc pages

--- web/pages/yc-s23.tsx
+++ web/pages/yc-s23.tsx
@@ -18,7 +18,7 @@ import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/outline'
 export async function getStaticProps() {
   const contracts = await supabaseSearchContractsWithDefaults({
     // yc-s23
-    groupId: '46258e3a-ae43-47d6-9a51-fc150a487f7e',
+    groupId: 'yc-s23',
     // Manifold
     creatorId: 'IPTOzEqrpkWmEzh6hwvAyY9PqFb2',
   })
