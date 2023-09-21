8df24eba1911caff6d84897aa70f50ef7c76e897
Ian Philips <iansphilips@gmail.com>
2023-09-21 10:22:56 -0700

    Add ian to dev admins

--- common/src/envs/dev.ts
+++ common/src/envs/dev.ts
@@ -29,5 +29,5 @@ export const DEV_CONFIG: EnvConfig = {
     androidClientId:
       '134303100058-mu6dbubhks8khpqi3dq0fokqnkbputiq.apps.googleusercontent.com',
   },
-  adminIds: ['pfKxvtgSEua5DxoIfiPXxR4fAWd2'],
+  adminIds: ['pfKxvtgSEua5DxoIfiPXxR4fAWd2', '6hHpzvRG0pMq8PNJs7RZj2qlZGn2'],
 }
