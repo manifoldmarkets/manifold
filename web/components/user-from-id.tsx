97da59a1688ccde8eefb0aaf1ccbcdbefd388b9a
Ian Philips <iansphilips@gmail.com>
2023-10-04 15:26:30 -0600

    Remove unused var

--- web/components/user-from-id.tsx
+++ web/components/user-from-id.tsx
@@ -3,12 +3,8 @@ import { Row } from 'web/components/layout/row'
 import { UserAvatarAndBadge } from 'web/components/widgets/user-link'
 import { LoadingIndicator } from 'web/components/widgets/loading-indicator'
 
-export const UserFromId = (props: {
-  userId: string
-  className?: string
-  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
-}) => {
-  const { userId, className, size = 'md' } = props
+export const UserFromId = (props: { userId: string; className?: string }) => {
+  const { userId, className } = props
   const user = useUserById(userId)
   return (
     <Row className={className}>
