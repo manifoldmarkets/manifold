5c1334a3f162caf5235c63b8766b688f27a6e671
ProgramCrafter <82749242+ProgramCrafter@users.noreply.github.com>
2023-10-13 00:13:30 +0500

    Better placeholders for /love page (#2066)
    * A better placeholder for /love page
    
    * Summoned Dating service from graveyard

--- web/pages/love/index.tsx
+++ web/pages/love/index.tsx
@@ -1,3 +1,54 @@
+import { Page } from 'web/components/layout/page'
+import { Col } from 'web/components/layout/col'
+import { Row } from 'web/components/layout/row'
+import { Button } from 'web/components/buttons/button'
+import Link from 'next/link'
+
+import { ManifoldLogo } from 'web/components/nav/manifold-logo' // TODO: maybe separate logo?
+import { LogoSEO } from 'web/components/LogoSEO'
+import { PrivacyAndTerms } from 'web/components/privacy-terms'  // TODO: new ToS
+
 export default function ManifoldLove() {
-  return <div>Welcome to Manifold.love</div>
+  return (
+    <Page trackPageView={'signed out home page'} hideSidebar>
+      <Col className="mx-auto mb-8 w-full gap-8 px-4">
+        <Col className="gap-4">
+          <Row className="items-center justify-between">
+            <ManifoldLogo />
+            <LogoSEO />
+          </Row>
+
+          <Row className="justify-between rounded-lg p-4">
+            <Col className="max-w-2xl gap-2">
+              <h1 className="mb-4 text-3xl">Choose the future. Predict what happens</h1>
+              <h1 className="text-2xl">
+                Play-money markets. Real-world improvements
+              </h1>
+              <h1 className="text-lg">
+                Create a question on what will your relationship be like, and bet on your friends' and other people's dates!
+              </h1>
+              <h1 className="text-lg">
+                Predictions are subsidized by Manifold. One-click sign up.
+              </h1>
+              
+              <Link href="https://manifoldmarkets.notion.site/Manifold-love-app-details-1c46d8984f794e2db7dc06cd126d32bb">
+                <Button
+                  color="gradient"
+                  size="2xl"
+                  className="mt-8"
+                >
+                  Coming soon!
+                </Button>
+              </Link>
+            </Col>
+            <Col className="hidden sm:flex">
+              <img src="/welcome/manipurple.png" width={210} />
+            </Col>
+          </Row>
+        </Col>
+
+        <PrivacyAndTerms />
+      </Col>
+    </Page>
+  )
 }
