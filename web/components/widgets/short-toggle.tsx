95bf416ccd92ce27b192da68346aa21f4365e1a8
Sinclair Chen <abc.sinclair@gmail.com>
2023-10-18 15:54:04 -0700

    Update switch style and use for notifs

--- web/components/widgets/short-toggle.tsx
+++ web/components/widgets/short-toggle.tsx
@@ -6,41 +6,29 @@ export default function ShortToggle(props: {
   on: boolean
   setOn: (enabled: boolean) => void
   disabled?: boolean
-  onChange?: (enabled: boolean) => void
   className?: string
 }) {
-  const { on, setOn, disabled, onChange, className } = props
+  const { on, setOn, disabled, className } = props
 
   return (
     <Switch
       disabled={disabled}
       checked={on}
-      onChange={(e: boolean) => {
-        setOn(e)
-        onChange?.(e)
-      }}
+      onChange={setOn}
       className={clsx(
-        'ring-primary-500 ring-offset-canvas-50 group relative inline-flex h-5 w-10 flex-shrink-0 items-center justify-center rounded-full !outline-none ring-offset-2 focus:ring-2',
+        'ring-primary-500 ring-offset-canvas-50 group relative inline-flex h-6 w-11 flex-shrink-0 rounded-full border-2 border-transparent ring-offset-2 transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2',
+        on ? 'bg-primary-500' : 'bg-ink-300',
         !disabled ? 'cursor-pointer' : 'cursor-not-allowed opacity-50',
         className
       )}
     >
       <span
         aria-hidden="true"
-        className="pointer-events-none absolute h-full w-full rounded-md bg-inherit"
-      />
-      <span
-        aria-hidden="true"
         className={clsx(
-          on ? 'bg-primary-600' : 'bg-ink-300',
-          'pointer-events-none absolute mx-auto h-4 w-9 rounded-full transition-colors duration-200 ease-in-out'
-        )}
-      />
-      <span
-        aria-hidden="true"
-        className={clsx(
-          on ? 'translate-x-5' : 'translate-x-0',
-          'border-ink-300 bg-canvas-0 dark:bg-ink-600 pointer-events-none absolute left-0 inline-block h-5 w-5 transform rounded-full border ring-0 transition-transform duration-200 ease-in-out'
+          on
+            ? 'dark:bg-primary-50 translate-x-5'
+            : 'dark:bg-ink-600 translate-x-0',
+          'bg-canvas-0 pointer-events-none inline-block h-5 w-5 rounded-full ring-0 transition duration-200 ease-in-out'
         )}
       />
     </Switch>
