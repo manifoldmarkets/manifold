1a743b6849251d9838f62969903c529e870c56c0
ingawei <ingawei@gmail.com>
2023-10-18 17:01:36 -0700

    playing with numbers till things aren't ugly

--- web/pages/messages/[channelId].tsx
+++ web/pages/messages/[channelId].tsx
@@ -164,8 +164,8 @@ export const PrivateChat = (props: { user: User; channelId: number }) => {
       <Col
         className={clsx(
           'gap-2 overflow-y-scroll py-2 ',
-          'max-h-[calc(100vh-14rem)] min-h-[calc(100vh-14rem)]',
-          'lg:max-h-[calc(100vh-12rem)] lg:min-h-[calc(100vh-12rem)]'
+          'max-h-[calc(100vh-216px)] min-h-[calc(100vh-216px)]',
+          'lg:max-h-[calc(100vh-184px)] lg:min-h-[calc(100vh-184px)]'
         )}
       >
         {realtimeMessages === undefined ? (
