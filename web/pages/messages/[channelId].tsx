375cf640e66b5f715347cc14ec3e01b5b52c1275
ingawei <ingawei@users.noreply.github.com>
2023-10-19 00:19:29 +0000

    Auto-remove unused imports

--- web/pages/messages/[channelId].tsx
+++ web/pages/messages/[channelId].tsx
@@ -1,5 +1,4 @@
 import { Page } from 'web/components/layout/page'
-import { Title } from 'web/components/widgets/title'
 import { useRouter } from 'next/router'
 import {
   useOtherUserIdsInPrivateMessageChannelIds,
