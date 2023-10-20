b01923236d18bd8a5fd6ce969abcacff8a8d0cbd
Marshall Polaris <marshall@pol.rs>
2023-10-19 23:21:29 -0700

    Add private_users table and RLS

--- backend/supabase/seed.sql
+++ backend/supabase/seed.sql
@@ -41,6 +41,21 @@ $$;
 /******************************************/
 /* 1. tables containing firestore content */
 /******************************************/
+
+create table if not exists private_users (
+  id text not null primary key,
+  data jsonb not null,
+  fs_updated_time timestamp not null,
+);
+
+alter table private_users enable row level security;
+
+drop policy if exists "private read" on private_users;
+create policy "private read" on private_users for select
+  using (firebase_uid() = id);
+
+alter table private_users cluster on private_users_pkey;
+
 create table if not exists
   user_portfolio_history (
     user_id text not null,
