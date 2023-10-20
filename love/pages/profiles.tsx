9fef33f4743e03fdd22c48a88f59f15bccf00772
ingawei <ingawei@gmail.com>
2023-10-20 14:59:34 -0700

    clean up profile look

--- love/pages/profiles.tsx
+++ love/pages/profiles.tsx
@@ -14,6 +14,10 @@ import { buildArray } from 'common/util/array'
 import { PhotosModal } from 'love/components/photos-modal'
 import { calculateAge } from 'love/components/calculate-age'
 import { Title } from 'web/components/widgets/title'
+import { User } from 'common/user'
+import { Row } from 'web/components/layout/row'
+import { shortenedFromNow } from 'web/lib/util/shortenedFromNow'
+import { Row as SupabaseRow } from 'common/supabase/utils'
 
 export default function ProfilesPage() {
   const allLovers = useLovers()
@@ -27,74 +31,23 @@ export default function ProfilesPage() {
   return (
     <LovePage trackPageView={'user profiles'}>
       <Col className="items-center">
-        <Col className={'bg-canvas-0 w-full px-6 py-4 rounded'}>
+        <Col className={'bg-canvas-0 w-full rounded px-6 py-4'}>
           <Title className="!mb-2 text-3xl">Profiles</Title>
           <Filters allLovers={allLovers} setLovers={setLovers} />
 
-          <Col className=" grid-cols-6 gap-4 overflow-x-scroll">
-            {lovers === undefined ? (
-              <LoadingIndicator />
-            ) : (
-              <Table>
-                <thead>
-                  <tr>
-                    <th>Photo</th>
-                    <th>Name</th>
-                    <th>Gender</th>
-                    <th>Age</th>
-                    <th>Location</th>
-                    <th>Last online</th>
-                  </tr>
-                </thead>
-                {lovers.map(
-                  ({
-                    id,
-                    user,
-                    photo_urls,
-                    gender,
-                    birthdate,
-                    pinned_url,
-                    city,
-                    last_online_time,
-                  }) => (
-                    <tr key={id} className={clsx()}>
-                      <td>
-                        {pinned_url && (
-                          <div
-                            className={clsx(
-                              'relative cursor-pointer rounded-md  p-2',
-                              'hover:border-teal-900'
-                            )}
-                            onClick={() => {
-                              setSelectedPhotos(
-                                buildArray(pinned_url, photo_urls)
-                              )
-                              setShowPhotosModal(true)
-                            }}
-                          >
-                            <Image
-                              className="h-5 w-5 rounded-full"
-                              height={20}
-                              width={20}
-                              alt={pinned_url}
-                              src={pinned_url}
-                            />
-                          </div>
-                        )}
-                      </td>
-                      <td>
-                        <UserLink name={user.name} username={user.username} />
-                      </td>
-                      <td>{gender}</td>
-                      <td>{calculateAge(birthdate)}</td>
-                      <td>{city}</td>
-                      <td>{fromNow(new Date(last_online_time).getTime())}</td>
-                    </tr>
-                  )
-                )}
-              </Table>
-            )}
-          </Col>
+          {lovers === undefined ? (
+            <LoadingIndicator />
+          ) : (
+            <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4">
+              {lovers.map((lover) => (
+                <ProfilePreview
+                  lover={lover}
+                  setSelectedPhotos={setSelectedPhotos}
+                  setShowPhotosModal={setShowPhotosModal}
+                />
+              ))}
+            </div>
+          )}
         </Col>
         <PhotosModal
           photos={selectedPhotos ?? []}
@@ -103,5 +56,56 @@ export default function ProfilesPage() {
         />
       </Col>
     </LovePage>
+  )
+}
+
+function ProfilePreview(props: {
+  lover: SupabaseRow<'lovers'> & { user: User }
+  setSelectedPhotos: (selectedPhotos: string[]) => void
+  setShowPhotosModal: (showPhotosModal: boolean) => void
+}) {
+  const { lover, setSelectedPhotos, setShowPhotosModal } = props
+  const {
+    id,
+    user,
+    photo_urls,
+    gender,
+    birthdate,
+    pinned_url,
+    city,
+    last_online_time,
+  } = props.lover
+  return (
+    <Col
+      className="relative h-60 w-full overflow-hidden rounded text-white transition-all hover:z-40 hover:scale-110 hover:drop-shadow"
+      onClick={() => {
+        setSelectedPhotos(buildArray(pinned_url, photo_urls))
+        setShowPhotosModal(true)
+      }}
+    >
+      <Image
+        src={pinned_url ?? ''}
+        fill
+        alt={`preview`}
+        className="h-full w-full object-cover"
+      />
+      <Col className="absolute inset-x-0 bottom-0 h-20 bg-gradient-to-t from-black/70 via-black/70 to-transparent px-4 pt-6">
+        <Row>
+          <div className="line-clamp-1 max-w-[calc(100%-2rem)] font-semibold">
+            {user.name}
+          </div>
+          , {calculateAge(birthdate)}
+        </Row>
+        <Row className="gap-1 text-xs">
+          {gender} • {city}
+        </Row>
+      </Col>
+
+      <Row className="absolute inset-x-0 top-0 justify-end px-4 pt-2 text-xs">
+        <span className="rounded-full bg-gray-600 bg-opacity-50 px-2 py-0.5">
+          Active <b>{shortenedFromNow(new Date(last_online_time).getTime())}</b>
+        </span>
+      </Row>
+    </Col>
   )
 }
