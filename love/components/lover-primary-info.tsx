735ed2dd1b83b38ca7851fb03572f7b31ee76bad
ingawei <46611122+ingawei@users.noreply.github.com>
2023-10-23 18:27:51 -0700

    add most important profile thing to top (#2092)

---
+++ love/components/lover-primary-info.tsx
@@ -0,0 +1,42 @@
+import { Lover } from 'love/hooks/use-lover'
+import { ReactNode } from 'react'
+import { FaLocationDot } from 'react-icons/fa6'
+import { Row } from 'web/components/layout/row'
+import { Gender } from './gender-icon'
+import GenderIcon from './GenderIcon'
+import { capitalizeFirstLetter } from 'web/lib/util/capitalize-first-letter'
+import { formatLoverValue } from 'love/pages/[username]'
+import { MdHeight } from 'react-icons/md'
+import { IoLocationOutline } from 'react-icons/io5'
+
+export default function LoverPrimaryInfo(props: { lover: Lover }) {
+  const { lover } = props
+  return (
+    <Row className="text-ink-700 gap-4 text-sm">
+      <IconWithInfo
+        text={lover.city}
+        icon={<IoLocationOutline className="h-4 w-4" />}
+      />
+      <IconWithInfo
+        text={capitalizeFirstLetter(lover.gender)}
+        icon={
+          <GenderIcon gender={lover.gender as Gender} className="h-4 w-4 " />
+        }
+      />
+      <IconWithInfo
+        text={formatLoverValue('height_in_inches', lover.height_in_inches)}
+        icon={<MdHeight className="h-4 w-4 " />}
+      />
+    </Row>
+  )
+}
+
+function IconWithInfo(props: { text: string; icon: ReactNode }) {
+  const { text, icon } = props
+  return (
+    <Row className="items-center gap-0.5">
+      <div className="text-ink-500">{icon}</div>
+      {text}
+    </Row>
+  )
+}
--- love/components/lover-profile-header.tsx
+++ love/components/lover-profile-header.tsx
@@ -13,6 +13,7 @@ import { SendMessageButton } from 'web/components/messaging/send-message-button'
 import { Linkify } from 'web/components/widgets/linkify'
 import { useIsMobile } from 'web/hooks/use-is-mobile'
 import { calculateAge } from './calculate-age'
+import LoverPrimaryInfo from './lover-primary-info'
 export default function LoverProfileHeader(props: {
   isCurrentUser: boolean
   currentUser: User | null | undefined
@@ -25,45 +26,41 @@ export default function LoverProfileHeader(props: {
   return (
     <Col className="w-full">
       <Row className={clsx('flex-wrap justify-between gap-2 py-1')}>
-        <Col>
+        <Col className="gap-1">
           <div className="text-xl">
             <span className="font-semibold">{user.name}</span>,{' '}
             {calculateAge(lover.birthdate)}
           </div>
-          <div className="text-ink-500 text-sm">@{user.username}</div>
+          <LoverPrimaryInfo lover={lover} />
         </Col>
         {isCurrentUser ? (
           <Row className={'items-center gap-1 sm:gap-2'}>
             {lover && (
               <Button
                 color={'gray-outline'}
-                className={'h-12'}
                 onClick={() => router.push('profile')}
+                size="sm"
               >
-                <PencilIcon className="mr-2 h-4 w-4" />
-                Edit
+                <PencilIcon className=" h-4 w-4" />
               </Button>
             )}
           </Row>
         ) : (
           <Row className="items-center gap-1 sm:gap-2">
-            <SendMessageButton
-              toUser={user}
-              currentUser={currentUser}
-              includeLabel
-            />
+            <SendMessageButton toUser={user} currentUser={currentUser} />
             <MoreOptionsUserButton user={user} />
           </Row>
         )}
       </Row>
-      <Col className={'mt-1'}>
-        {lover.bio && (
-          <div className="sm:text-md mt-1 text-sm">
-            <Linkify text={lover.bio}></Linkify>
+      <Col className={'mt-1 gap-2'}>
+        {user.bio && (
+          <div className="text-sm">
+            <Linkify text={user.bio}></Linkify>
           </div>
         )}
-        <Row className="text-ink-400 mt-2 flex-wrap items-center gap-2 sm:gap-4">
-          {lover.website && (
+        {/* TODO: add this to more info, not that important */}
+        {/* <Row className="text-ink-400 mt-2 flex-wrap items-center gap-2 sm:gap-4">
+          {user.website && (
             <a
               href={
                 'https://' +
@@ -97,7 +94,7 @@ export default function LoverProfileHeader(props: {
               </Row>
             </a>
           )}
-        </Row>
+        </Row> */}
       </Col>
     </Col>
   )
