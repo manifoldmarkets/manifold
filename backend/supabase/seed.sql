9456e90cbffca18c38abeca7b0428ab5e27426a5
Marshall Polaris <marshall@pol.rs>
2023-10-26 23:21:28 -0700

    Populate `manalink_claims` table from claims in manalink data (#2120)

--- backend/supabase/seed.sql
+++ backend/supabase/seed.sql
@@ -855,6 +855,11 @@ as $$ begin
     new.creator_id := (new.data)->>'fromId';
     new.max_uses := ((new.data)->>'maxUses')::numeric;
     new.message := (new.data)->>'message';
+    if (new.data)->'claims' is not null then
+        delete from manalink_claims where manalink_id = new.id;
+        with claims as (select new.id, jsonb_array_elements((new.data)->'claims') as cdata)
+        insert into manalink_claims (manalink_id, txn_id) select id, cdata->>'txnId' from claims;
+    end if;
   end if;
   return new;
 end $$;
@@ -862,6 +867,15 @@ end $$;
 drop trigger manalinks_populate on manalinks;
 create trigger manalinks_populate before insert or update on manalinks
 for each row execute function manalinks_populate_cols();
+
+create table if not exists
+  manalink_claims (
+    manalink_id text not null,
+    txn_id text not null,
+    primary key (manalink_id, txn_id)
+  );
+
+alter table manalink_claims cluster on manalink_claims_pkey;
 
 create table if not exists
   posts (
