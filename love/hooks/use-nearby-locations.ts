426e3472e0bbd824180181bb367f2c131b56b5ff
ingawei <ingawei@gmail.com>
2023-10-31 15:45:56 -0700

    nearby location

--- love/hooks/use-nearby-locations.ts
+++ love/hooks/use-nearby-locations.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { usePersistentInMemoryState } from 'web/hooks/use-persistent-in-memory-state'
 import { searchNearCity } from 'web/lib/firebase/api'
 
@@ -6,22 +6,27 @@ export function useNearbyCities(
   referenceCityId: string | null | undefined,
   radius: number
 ) {
+  const searchCount = useRef(0)
   const [nearbyCities, setNearbyCities] = usePersistentInMemoryState<
     string[] | undefined | null
   >(undefined, `nearby-cities-${referenceCityId}-${radius}`)
   useEffect(() => {
+    searchCount.current++
+    const thisSearchCount = searchCount.current
     if (referenceCityId) {
       searchNearCity({
         cityId: referenceCityId,
         radius,
       }).then((result) => {
-        if (result.status === 'failure') {
-          setNearbyCities(null)
-          console.log('ERROR:', result.data)
-        } else {
-          setNearbyCities(
-            (result.data.data as any[]).map((city) => city.id.toString())
-          )
+        if (thisSearchCount == searchCount.current) {
+          if (result.status === 'failure') {
+            setNearbyCities(null)
+            console.log('ERROR:', result.data)
+          } else {
+            setNearbyCities(
+              (result.data.data as any[]).map((city) => city.id.toString())
+            )
+          }
         }
       })
     }
