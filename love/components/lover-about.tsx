a6cac818999cb5404325fd4a2c99221a26bafed5
ingawei <ingawei@gmail.com>
2023-10-31 12:46:00 -0700

    add links to profile

--- love/components/lover-about.tsx
+++ love/components/lover-about.tsx
@@ -27,6 +27,7 @@ import { Row } from 'web/components/layout/row'
 import { fromNow } from 'web/lib/util/time'
 import { Gender, convertGenderPlural } from './gender-icon'
 import { HiOutlineGlobe } from 'react-icons/hi'
+import { UserHandles } from 'web/components/user/user-handles'
 
 export function AboutRow(props: {
   icon: ReactNode
@@ -82,6 +83,7 @@ export default function LoverAbout(props: { lover: Lover }) {
         text={lover.is_vegetarian_or_vegan ? 'Vegetarian/Vegan' : null}
       />
       <WantsKids lover={lover} />
+      <UserHandles website={lover.website} twitterHandle={lover.twitter} />
     </Col>
   )
 }
