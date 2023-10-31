0e30980084a66deeff36348869ad004de9f6ad1f
mantikoros <sgrugett@gmail.com>
2023-10-30 19:55:17 -0700

    fix purchase options

--- web/pages/add-funds.tsx
+++ web/pages/add-funds.tsx
@@ -10,8 +10,9 @@ export const WEB_PRICES = {
   [formatMoney(1000)]: 1000,
   [formatMoney(2500)]: 2500,
   [formatMoney(10000)]: 10000,
-  [formatMoney(20000)]: 20000,
+  [formatMoney(100000)]: 100000,
 }
+
 export const IOS_PRICES = {
   [formatMoney(1000)]: 1199,
   [formatMoney(2500)]: 2999,
