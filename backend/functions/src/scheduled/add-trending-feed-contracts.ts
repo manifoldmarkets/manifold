74ab5cae775da6c48a07d35c5e894715fbb795a5
Marshall Polaris <marshall@pol.rs>
2023-11-05 02:21:51 -0800

    Fix apparent bug in `addcontractstofeed` calculation time

--- backend/functions/src/scheduled/add-trending-feed-contracts.ts
+++ backend/functions/src/scheduled/add-trending-feed-contracts.ts
@@ -26,7 +26,7 @@ export const addcontractstofeed = onSchedule(
       // TODO: we should just turn this into a docker container
       // Keep running with the users cached (we refresh for new users) until we've just 10 minutes left,
       // on the next run we'll do a full refresh (picking a new random sample of old users)
-      while (Date.now() < startTime + (MINUTE_INTERVAL * 60 - 10) * 1000) {
+      while (Date.now() < startTime + (MINUTE_INTERVAL - 10) * 60 * 1000) {
         await addInterestingContractsToFeed(db, pg)
       }
 
