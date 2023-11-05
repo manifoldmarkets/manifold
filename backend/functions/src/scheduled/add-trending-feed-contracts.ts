84596b0481efd80c7f7af05cedb9615a6cdeee1f
Marshall Polaris <marshall@pol.rs>
2023-11-05 01:22:37 -0700

    Implement locking system for `addcontractstofeed` (#2174)

--- backend/functions/src/scheduled/add-trending-feed-contracts.ts
+++ backend/functions/src/scheduled/add-trending-feed-contracts.ts
@@ -8,6 +8,7 @@ import {
   createSupabaseClient,
   createSupabaseDirectClient,
 } from 'shared/supabase/init'
+import { runSingleton } from 'shared/firestore-lock'
 
 export const addcontractstofeed = onSchedule(
   {
@@ -18,14 +19,17 @@ export const addcontractstofeed = onSchedule(
     cpu: 2
   },
   async () => {
-    const db = createSupabaseClient()
-    const pg = createSupabaseDirectClient()
-    const startTime = Date.now()
-    // TODO: we should just turn this into a docker container
-    // Keep running with the users cached (we refresh for new users) until we've just 10 minutes left,
-    // on the next run we'll do a full refresh (picking a new random sample of old users)
-    while (Date.now() < startTime + (MINUTE_INTERVAL * 60 - 10) * 1000) {
-      await addInterestingContractsToFeed(db, pg)
-    }
+    runSingleton('add-contracts-to-feed', async () => {
+      const db = createSupabaseClient()
+      const pg = createSupabaseDirectClient()
+      const startTime = Date.now()
+      // TODO: we should just turn this into a docker container
+      // Keep running with the users cached (we refresh for new users) until we've just 10 minutes left,
+      // on the next run we'll do a full refresh (picking a new random sample of old users)
+      while (Date.now() < startTime + (MINUTE_INTERVAL * 60 - 10) * 1000) {
+        await addInterestingContractsToFeed(db, pg)
+      }
+
+    })
   }
 )
