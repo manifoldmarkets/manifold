bf03b6bf1e04cb2719aeff9013089915b06aa231
James Grugett <jahooma@gmail.com>
2023-11-09 22:48:50 -0800

    Note on markets reset

--- love/components/matches.tsx
+++ love/components/matches.tsx
@@ -136,7 +136,14 @@ export const Matches = (props: { userId: string }) => {
           )}
         </Col>
       ) : (
-        <span className={'text-ink-500 text-sm'}>No matches yet.</span>
+        <Col>
+          <div className="mb-4 bg-canvas-50 px-2 py-1.5">
+            Hello manifold.love users — we've overhauled matches to include
+            markets on 1st date, 2nd date and more! Sadly, the old matches have
+            been reset and will need to be recreated. Enjoy!
+          </div>
+          <span className={'text-ink-500 text-sm'}>No matches yet.</span>
+        </Col>
       )}
 
       {lover && (
