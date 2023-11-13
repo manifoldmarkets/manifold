008edb90ff90bf96a0baf8ec0e23d52e36908487
Ian Philips <iansphilips@gmail.com>
2023-11-13 11:45:49 -0700

    Refactor answers sql

---
+++ backend/supabase/answers.sql
@@ -0,0 +1,52 @@
+
+create table if not exists
+    answers (
+                id text not null primary key,
+                index int, -- Order of the answer in the list
+                contract_id text, -- Associated contract
+                user_id text, -- Creator of the answer
+                text text,
+                created_time timestamptz default now(),
+    -- Mechanism props
+                pool_yes numeric, -- YES shares in the pool
+                pool_no numeric, -- NO shares in the pool
+                prob numeric, -- Probability of YES computed from pool_yes and pool_no
+                total_liquidity numeric default 0, -- for historical reasons, this the total subsidy amount added in M
+                subsidy_pool numeric default 0, -- current value of subsidy pool in M
+                data jsonb not null,
+                fs_updated_time timestamp not null
+);
+
+alter table answers enable row level security;
+
+drop policy if exists "public read" on answers;
+
+create policy "public read" on answers for
+    select
+    using (true);
+
+create
+    or replace function answers_populate_cols () returns trigger language plpgsql as $$
+begin
+    if new.data is not null then
+        new.index := ((new.data) ->> 'index')::int;
+        new.contract_id := (new.data) ->> 'contractId';
+        new.user_id := (new.data) ->> 'userId';
+        new.text := ((new.data) ->> 'text')::text;
+        new.created_time :=
+                case when new.data ? 'createdTime' then millis_to_ts(((new.data) ->> 'createdTime')::bigint) else null end;
+
+        new.pool_yes := ((new.data) ->> 'poolYes')::numeric;
+        new.pool_no := ((new.data) ->> 'poolNo')::numeric;
+        new.prob := ((new.data) ->> 'prob')::numeric;
+        new.total_liquidity := ((new.data) ->> 'totalLiquidity')::numeric;
+        new.subsidy_pool := ((new.data) ->> 'subsidyPool')::numeric;
+    end if;
+    return new;
+end
+$$;
+
+create trigger answers_populate before insert
+    or
+    update on answers for each row
+execute function answers_populate_cols ();
