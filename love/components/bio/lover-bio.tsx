4c87d633e333b21aa6e744f1f61a561e31a294a5
ingawei <ingawei@gmail.com>
2023-11-13 16:44:26 -0800

    thingz

--- love/components/bio/lover-bio.tsx
+++ love/components/bio/lover-bio.tsx
@@ -14,6 +14,8 @@ import { MAX_DESCRIPTION_LENGTH } from 'common/contract'
 import { Row } from 'web/components/layout/row'
 import { updateLover } from 'web/lib/firebase/love/api'
 import { track } from 'web/lib/service/analytics'
+import { EditableBio } from './editable-bio'
+import { BioBlock } from './lover-bio-block'
 
 export function LoverBio(props: {
   isCurrentUser: boolean
@@ -26,7 +28,11 @@ export function LoverBio(props: {
     <Col>
       <Subtitle>Bio</Subtitle>
       {lover.bio ? (
-        <Content content={lover.bio as JSONContent} />
+        <BioBlock
+          isCurrentUser={isCurrentUser}
+          lover={lover}
+          refreshLover={refreshLover}
+        />
       ) : (
         isCurrentUser && (
           <AddBioButton lover={lover} refreshLover={refreshLover} />
@@ -37,7 +43,7 @@ export function LoverBio(props: {
 }
 
 function AddBioButton(props: { lover: Lover; refreshLover: () => void }) {
-  const { lover } = props
+  const { lover, refreshLover } = props
   const [open, setOpen] = useState(false)
   return (
     <>
@@ -54,56 +60,13 @@ function AddBioButton(props: { lover: Lover; refreshLover: () => void }) {
           <EditableBio
             lover={lover}
             onCancel={() => setOpen(false)}
-            onSave={() => setOpen(false)}
+            onSave={() => {
+              refreshLover()
+              setOpen(false)
+            }}
           />
         </Col>
       </Modal>
     </>
-  )
-}
-
-function EditableBio(props: {
-  lover: Lover
-  onCancel: () => void
-  onSave: () => void
-}) {
-  const { lover, onCancel, onSave } = props
-  const editor = useTextEditor({
-    max: MAX_DESCRIPTION_LENGTH,
-    defaultValue: (lover.bio as JSONContent) ?? '',
-    placeholder: 'Tell us about yourself!',
-  })
-
-  const saveBio = async () => {
-    if (!editor) return
-    const res = await updateLover({
-      ...lover,
-      bio: editor.getJSON(),
-    }).catch((e) => {
-      console.error(e)
-      return false
-    })
-    if (res) {
-      console.log('success')
-      track('edited lover bio')
-    }
-  }
-  return (
-    <Col className="w-full">
-      <TextEditor editor={editor} />
-      <Row className="my-2 justify-between gap-2">
-        <Button color="gray-outline" onClick={onCancel}>
-          Cancel
-        </Button>
-        <Button
-          onClick={async () => {
-            await saveBio()
-            onSave()
-          }}
-        >
-          Save
-        </Button>
-      </Row>
-    </Col>
   )
 }
