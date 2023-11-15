b4bd9679e9d478ec767be696069fb57419708035
Sinclair Chen <abc.sinclair@gmail.com>
2023-11-14 16:16:21 -0800

    fix issue with admin/mod check

--- backend/shared/src/helpers/auth.ts
+++ backend/shared/src/helpers/auth.ts
@@ -2,7 +2,7 @@ import { APIError } from 'common/api'
 import { isAdminId, isModId } from 'common/envs/constants'
 
 export const throwErrorIfNotMod = async (userId: string) => {
-  if (!isAdminId(userId) || !isModId(userId)) {
+  if (!isAdminId(userId) && !isModId(userId)) {
     throw new APIError(
       403,
       `User ${userId} must be an admin or trusted to perform this action.`
