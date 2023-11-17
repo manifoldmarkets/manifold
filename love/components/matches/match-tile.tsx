da645e6e798a1498311fd5225fbef10121e1a4d3
James Grugett <jahooma@gmail.com>
2023-11-16 17:27:40 -0800

    Drop drop-shadow b/c it breaks ios sofari

--- love/components/matches/match-tile.tsx
+++ love/components/matches/match-tile.tsx
@@ -63,7 +63,7 @@ export const MatchTile = (props: {
     .trim()
 
   return (
-    <Col className="mb-2 w-[220px] shrink-0 overflow-hidden rounded drop-shadow-lg">
+    <Col className="w-[220px] shrink-0 overflow-hidden rounded">
       <div className="bg-canvas-0 w-full bg-gradient-to-b px-4 py-2">
         <UserLink
           className={
