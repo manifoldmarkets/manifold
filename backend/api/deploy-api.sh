f48f6aeefeee0a080ddc8dd481087df0ab584a3b
Marshall Polaris <marshall@pol.rs>
2023-11-20 20:37:28 -0800

    Make deploy scripts use rolling updates on ALB

--- backend/api/deploy-api.sh
+++ backend/api/deploy-api.sh
@@ -10,6 +10,7 @@ fi
 INITIALIZE=false
 
 SERVICE_NAME="api"
+SERVICE_GROUP="${SERVICE_NAME}-group"
 REGION="us-central1"
 ZONE="us-central1-a"
 ENV=${1:-dev}
@@ -31,12 +32,26 @@ IMAGE_URL="gcr.io/${GCLOUD_PROJECT}/${SERVICE_NAME}"
 yarn build
 gcloud builds submit . --tag ${IMAGE_URL} --project ${GCLOUD_PROJECT}
 
-echo "Looking up instance IDs to update..."
-INSTANCE_IDS=$(gcloud compute instances list --project ${GCLOUD_PROJECT} --filter NAME~"api-group" --format="value(NAME)")
-for i in $INSTANCE_IDS; do
-    echo "Pulling new image onto instance: $i..."
-    gcloud compute instances update-container $i \
-           --project ${GCLOUD_PROJECT} \
-           --zone ${ZONE} \
-           --container-image ${IMAGE_URL}
-done
+TIMESTAMP=$(date +"%s")
+TEMPLATE_NAME=${SERVICE_NAME}-${TIMESTAMP}
+
+gcloud compute instance-templates create-with-container ${TEMPLATE_NAME} \
+       --project ${GCLOUD_PROJECT} \
+       --container-image ${IMAGE_URL} \
+       --machine-type n2-standard-4 \
+       --container-env ENVIRONMENT=${ENVIRONMENT},GOOGLE_CLOUD_PROJECT=${GCLOUD_PROJECT} \
+       --scopes default,cloud-platform
+
+gcloud compute instance-groups managed rolling-action start-update ${SERVICE_GROUP} \
+       --project ${GCLOUD_PROJECT} \
+       --zone ${ZONE} \
+       --version="template=${TEMPLATE_NAME}" \
+       --max-unavailable=0 # don't kill old one until new one is healthy
+
+DASHBOARD_URL="https://console.cloud.google.com/compute/instanceGroups/details/${ZONE}/${SERVICE_GROUP}?project=${GCLOUD_PROJECT}"
+
+echo "Waiting for update to roll out... Dashboard: ${DASHBOARD_URL}"
+gcloud compute instance-groups managed wait-until ${SERVICE_GROUP} \
+       --project ${GCLOUD_PROJECT} \
+       --zone ${ZONE} \
+       --stable
