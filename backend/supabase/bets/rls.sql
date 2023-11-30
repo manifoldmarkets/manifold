24b4f9903b99534424757f0f8108ba8399aad621
Ian Philips <iansphilips@gmail.com>
2023-11-30 13:29:42 -0800

    Refactor contract_bets sql to own file

--- backend/supabase/bets/rls.sql
+++
@@ -1,5 +0,0 @@
-drop policy if exists "Enable read access for non private bets" on public.contract_bets;
-
-create policy "Enable read access for non private bets" on public.contract_bets for
-select
-  using ((visibility <> 'private'::text));
