f06a81a348c0b9c2882ce3e1a9aedb1d13a9d982
Sinclair Chen <abc.sinclair@gmail.com>
2023-11-30 11:31:53 -0800

    Add labels to zoom slider

--- web/components/charts/zoom-slider.tsx
+++ web/components/charts/zoom-slider.tsx
@@ -4,6 +4,7 @@ import clsx from 'clsx'
 import { useMemo } from 'react'
 import { Col } from '../layout/col'
 import { Row } from '../layout/row'
+import { clamp } from 'lodash'
 
 // assumes x is time and we're zooming over a time period
 export const ZoomSlider = (props: {
@@ -15,8 +16,16 @@ export const ZoomSlider = (props: {
   const { xScale, viewXScale, rescaleBetween } = zoomParams
   const [min, max] = xScale.domain()
   const [low, hi] = viewXScale.domain()
+  const maxPx = xScale(max)
+  const lowPx = clamp(xScale(low), 50, maxPx - 48)
+  const hiPx = clamp(xScale(hi), 50, maxPx - 48)
 
   const now = useMemo(() => new Date(), [])
+
+  const ttColor =
+    color === 'light-green'
+      ? 'bg-emerald-200 dark:bg-teal-200'
+      : 'bg-primary-200'
 
   return (
     <Col className={clsx('w-full items-stretch', className)}>
@@ -28,10 +37,42 @@ export const ZoomSlider = (props: {
         setValues={rescaleBetween}
         color={color}
       />
-      <Row className="justify-between text-xs">
-        <div>{formatDateInRange(min, min, max)}</div>
-        <div>{formatDateInRange(max, min, now)}</div>
-      </Row>
+      <div className="relative -top-1 text-xs">
+        <span
+          className={clsx(
+            'absolute left-0 py-1 transition-opacity',
+            lowPx < 100 && 'opacity-0'
+          )}
+        >
+          {formatDateInRange(min, min, max)}
+        </span>
+        <div
+          className={clsx(
+            'absolute right-0 py-1 transition-opacity',
+            hiPx > maxPx - 100 && 'opacity-0'
+          )}
+        >
+          {formatDateInRange(max, min, now)}
+        </div>
+        <div
+          className={clsx(
+            ttColor,
+            'absolute -translate-x-full transform whitespace-nowrap rounded px-2 py-1'
+          )}
+          style={{ left: lowPx }}
+        >
+          {formatDateInRange(low, min, max)}
+        </div>
+        <div
+          className={clsx(
+            ttColor,
+            'absolute whitespace-nowrap rounded px-2 py-1'
+          )}
+          style={{ left: hiPx }}
+        >
+          {formatDateInRange(hi, min, max)}
+        </div>
+      </div>
     </Col>
   )
 }
