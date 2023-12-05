804cae6694a4036d6a9658c71bebea6bfed9f46b
ingawei <ingawei@gmail.com>
2023-12-04 20:06:51 -0800

    ze final touch

--- web/pages/[username]/index.tsx
+++ web/pages/[username]/index.tsx
@@ -217,7 +217,7 @@ function UserProfile(props: {
       )}
       {showConfetti && <FullscreenConfetti />}
 
-      <Col className="mt-1">
+      <Col className="relative mt-1">
         {isMobile && (
           <Row
             className={
@@ -367,6 +367,16 @@ function UserProfile(props: {
             ]}
           />
         </Col>
+        {isCurrentUser && (
+          <Row className="fixed bottom-16 left-0 right-0 max-w-7xl select-none justify-end sm:bottom-8">
+            <Link
+              href={`/${user.username}/wrapped2023`}
+              className="from-primary-400 hover:from-primary-500 mr-4 w-fit rounded-full bg-gradient-to-br to-pink-400 px-4 py-2 hover:to-pink-500"
+            >
+              🎁 Your Manifold Wrapped is here!
+            </Link>
+          </Row>
+        )}
       </Col>
     </Page>
   )
