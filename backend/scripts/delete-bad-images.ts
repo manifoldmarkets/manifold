24f9fc56a2e79abab7806e04723810f7c35e731f
mantikoros <sgrugett@gmail.com>
2023-12-08 16:37:04 -0800

    remove debug

--- backend/scripts/delete-bad-images.ts
+++ backend/scripts/delete-bad-images.ts
@@ -4,7 +4,7 @@ import { FieldValue } from 'firebase-admin/firestore'
 if (require.main === module) {
   runScript(async ({ pg, firestore }) => {
     const contractIds = await pg.map(
-      `select distinct contract_id, ts from user_events where name = 'image error on contract' order by ts desc limit 1000`,
+      `select distinct contract_id from user_events where name = 'image error on contract'`,
       [],
       (r) => r.contract_id
     )
@@ -16,7 +16,6 @@ if (require.main === module) {
       writer.update(firestore.doc(`contracts/${contractId}`), {
         coverImageUrl: FieldValue.delete(),
       })
-      break
     }
 
     console.log('Deleting bad images...')
