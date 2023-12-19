d761c48fbb315e4e489b1d7e5b2d28a7ae6efdec
Marshall Polaris <marshall@pol.rs>
2023-12-19 01:52:00 -0800

    Filter out more sensitive tokens from tsqueries

--- backend/shared/src/helpers/search.ts
+++ backend/shared/src/helpers/search.ts
@@ -1,7 +1,7 @@
 export const constructPrefixTsQuery = (term: string) => {
   const sanitized = term
     .replace(/'/g, "''")
-    .replace(/[!&|():*]/g, '')
+    .replace(/[!&|():*<>]/g, '')
     .trim()
   console.log(`Term: "${sanitized}"`)
   if (sanitized === '') return ''
