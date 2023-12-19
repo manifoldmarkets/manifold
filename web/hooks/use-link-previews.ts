945baaf379b570fd43c7c46349620fc720ccb537
Marshall Polaris <marshall@pol.rs>
2023-12-19 01:19:03 -0800

    Debounce dashboard add link input so it sucks less

--- web/hooks/use-link-previews.ts
+++ web/hooks/use-link-previews.ts
@@ -1,17 +1,22 @@
 import { LinkPreview } from 'common/link-preview'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { useDebouncedEffect } from 'web/hooks/use-debounced-effect'
 import { api } from 'web/lib/firebase/api'
 import { safeLocalStorage } from 'web/lib/util/local'
 
 export const useLinkPreview = (url: string) => {
   const [preview, setPreview] = useState<LinkPreview | undefined>(undefined)
-  useEffect(() => {
-    if (url) {
-      cachedLinkPreview(url.trim()).then((p) => setPreview(p))
-    } else {
-      setPreview(undefined)
-    }
-  }, [url])
+  useDebouncedEffect(
+    () => {
+      if (url) {
+        cachedLinkPreview(url.trim()).then((p) => setPreview(p))
+      } else {
+        setPreview(undefined)
+      }
+    },
+    100,
+    [url]
+  )
   return preview
 }
 
@@ -19,9 +24,14 @@ export const cachedLinkPreview = async (url: string) => {
   const cacheKey = `link-preview-${url}`
   const cached = safeLocalStorage?.getItem(cacheKey)
   if (cached) return JSON.parse(cached) as LinkPreview
-  const preview = await api('fetch-link-preview', { url })
-  if (preview) {
-    safeLocalStorage?.setItem(cacheKey, JSON.stringify(preview))
+  try {
+    const preview = await api('fetch-link-preview', { url })
+    if (preview) {
+      safeLocalStorage?.setItem(cacheKey, JSON.stringify(preview))
+    }
+    return preview
+  } catch (e) {
+    // likely just it's not a valid URL
+    console.warn(e)
   }
-  return preview
 }
