2c90a0827320abe7504b2b2fe7b547f1fa7fc020
Sinclair Chen <abc.sinclair@gmail.com>
2023-12-26 23:45:24 -0800

    Don't pass my dashboards thru static props

--- backend/api/src/get-headlines.ts
+++ backend/api/src/get-headlines.ts
@@ -1,8 +1,7 @@
 import { createSupabaseClient } from 'shared/supabase/init'
 import { APIError, APIHandler } from './helpers'
-import { uniqBy } from 'lodash'
 
-export const getHeadlines: APIHandler<'headlines'> = async (props, auth) => {
+export const getHeadlines: APIHandler<'headlines'> = async () => {
   const db = createSupabaseClient()
 
   const all = await db
@@ -12,20 +11,7 @@ export const getHeadlines: APIHandler<'headlines'> = async (props, auth) => {
     .order('importance_score', { ascending: false })
 
   if (all.error)
-    throw new APIError(500, 'Error fetching headlines' + all.error.message)
+    throw new APIError(500, 'Error fetching headlines ' + all.error.message)
 
-  if (!auth) {
-    return all.data
-  } else {
-    const mine = await db
-      .from('dashboards')
-      .select('id, slug, title')
-      .eq('dashboard_follows (follower_id)', auth.uid)
-      .order('importance_score', { ascending: false })
-
-    if (mine.error)
-      throw new APIError(500, 'Error fetching headlines' + mine.error.message)
-
-    return uniqBy([...mine.data, ...all.data], 'id')
-  }
+  return all.data
 }
