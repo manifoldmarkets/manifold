cb9221791423004d7a562b0cafe6a8547e920fc5
James Grugett <jahooma@gmail.com>
2023-12-30 11:12:21 -0800

    Require countdown to claim an ad

--- common/src/boost.ts
+++ common/src/boost.ts
@@ -1,6 +1,8 @@
 // number normal markets to show between boosted markets
 export const AD_PERIOD = 5
 
+export const AD_WAIT_SECONDS = 9 // Time with ad visible before allowing the user to claim.
+
 export const AD_RATE_LIMIT = 10 * 1000 // 10 seconds
 
 // cost in mana
