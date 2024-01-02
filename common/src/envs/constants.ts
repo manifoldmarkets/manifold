a796b62df6a2ea9413eedb2be1ec2cdeeb79ce5b
tspoerri <95598269+tspoerri@users.noreply.github.com>
2024-01-01 22:25:29 -0500

    Update constants.ts
    Added TenShino to bots

--- common/src/envs/constants.ts
+++ common/src/envs/constants.ts
@@ -55,6 +55,7 @@ export const CORS_ORIGIN_LOCALHOST = /^http:\/\/localhost:\d+$/
 
 // TODO: These should maybe be part of the env config?
 export const BOT_USERNAMES = [
+  'TenShino',
   'pos',
   'v',
   'acc',
