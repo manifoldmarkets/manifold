cc0e850f6cc123e656fff8d629b43a197cfefcf5
James Grugett <jahooma@gmail.com>
2024-01-02 14:37:12 -0800

    Revert "Disable get-related-markets"
    This reverts commit 37a030292720936549cf7ec008cb404c552fa4c4.

--- common/src/contract-params.ts
+++ common/src/contract-params.ts
@@ -73,12 +73,10 @@ export async function getContractParams(
       : {},
     contract.resolution ? getTopContractMetrics(contract.id, 10, db) : [],
     isCpmm1 || isMulti ? getContractMetricsCount(contract.id, db) : 0,
-    // TEMPORARILY DISABLED, TODO: Re-enable!
-    // unauthedApi('get-related-markets', {
-    //   contractId: contract.id,
-    //   limit: 4,
-    // }),
-    [],
+    unauthedApi('get-related-markets', {
+      contractId: contract.id,
+      limit: 4,
+    }),
     // getRelatedContracts(contract, 20, db),
     // TODO: Should only send bets that are replies to comments we're sending, and load the rest client side
     isCpmm1
