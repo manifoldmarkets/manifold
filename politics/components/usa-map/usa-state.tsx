51eb04d72c163185a9a3757c795ada96aa9be0a0
ingawei <46611122+ingawei@users.noreply.github.com>
2024-01-02 19:05:32 -0800

    Ingawei/map tweaks (#2314)
    * highlight state

--- politics/components/usa-map/usa-state.tsx
+++ politics/components/usa-map/usa-state.tsx
@@ -8,6 +8,7 @@ type USAStateProps = {
   onClickState?: ClickHandler
   stateName: string
   hideStateTitle?: boolean
+  selected?: boolean
 }
 export const USAState = ({
   state,
@@ -16,19 +17,24 @@ export const USAState = ({
   onClickState,
   stateName,
   hideStateTitle,
+  selected,
 }: USAStateProps) => {
   return (
-    <path
-      d={dimensions}
-      fill={fill}
-      data-name={state}
-      className={clsx(
-        !!onClickState && 'hover:cursor-pointer hover:contrast-125'
-      )}
-      onClick={onClickState}
-      id={state}
-    >
-      {hideStateTitle ? null : <title>{stateName}</title>}
-    </path>
+    <>
+      <path
+        d={dimensions}
+        fill={fill}
+        data-name={state}
+        className={clsx(
+          !!onClickState && 'hover:cursor-pointer hover:contrast-125'
+        )}
+        onClick={onClickState}
+        id={state}
+        stroke={selected ? '#000' : undefined}
+        strokeWidth={selected ? 2 : undefined}
+      >
+        {hideStateTitle ? null : <title>{stateName}</title>}
+      </path>
+    </>
   )
 }
