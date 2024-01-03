621e967f454bd52dac30c0e3ff9f156874b263a6
ingawei <46611122+ingawei@users.noreply.github.com>
2024-01-03 15:14:42 -0800

    Ingawei/map tweaks (#2317)
    * highlight state
    * colors and correct market connections

---
+++ politics/components/usa-map/election-contract-data.ts
@@ -0,0 +1,32 @@
+import { StateElectionMarket } from './state-election-map'
+
+export const presidency2024: StateElectionMarket[] = [
+  {
+    state: 'AZ',
+    slug: 'which-party-will-win-the-us-preside-c1307cf9f69a',
+  },
+  {
+    state: 'WI',
+    slug: 'which-party-will-win-the-us-preside-ee07598f45ea',
+  },
+  {
+    state: 'FL',
+    slug: 'which-party-will-win-the-us-preside-a0c0e217efb2',
+  },
+  {
+    state: 'PA',
+    slug: 'which-party-will-win-the-us-preside',
+  },
+  {
+    state: 'GA',
+    slug: 'which-party-will-win-the-us-preside-9d5b554982a7',
+  },
+  {
+    state: 'NV',
+    slug: 'which-party-will-win-the-us-preside-5777ea10ce2a',
+  },
+  {
+    state: 'MI',
+    slug: 'which-party-will-win-the-us-preside-7e7362326c95',
+  },
+]
