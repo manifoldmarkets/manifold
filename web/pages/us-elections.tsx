2eeafd0530fddecccd1fe82358168c5f4fc76c23
ingawei <ingawei@gmail.com>
2024-01-09 13:41:42 -0800

    added markets

--- web/pages/us-elections.tsx
+++ web/pages/us-elections.tsx
@@ -24,13 +24,38 @@ export async function getStaticProps() {
     })
   )
 
+  const electionPartyContract = await getContractFromSlug(
+    'which-party-will-win-the-2024-us-pr-f4158bf9278a',
+    adminDb
+  )
+
+  const electionCandidateContract = await getContractFromSlug(
+    'who-will-win-the-2024-us-presidenti-8c1c8b2f8964',
+    adminDb
+  )
+
+  const republicanCandidateContract = await getContractFromSlug(
+    'who-will-win-the-2024-republican-pr-e1332cf40e59',
+    adminDb
+  )
+
+  const democratCandidateContract = await getContractFromSlug(
+    'who-will-win-the-2024-democratic-pr-47576e90fa38',
+    adminDb
+  )
+
   // Wait for all promises to resolve
   const mapContracts = await Promise.all(mapContractsPromises)
 
   return {
     props: {
       mapContracts: mapContracts,
+      electionPartyContract: electionPartyContract,
+      electionCandidateContract: electionCandidateContract,
+      republicanCandidateContract: republicanCandidateContract,
+      democratCandidateContract: democratCandidateContract,
     },
+    revalidate: 60,
   }
 }
 
@@ -38,8 +63,18 @@ export type MapContracts = { state: string; contract: Contract | null }
 
 export default function USElectionsPage(props: {
   mapContracts: MapContracts[]
+  electionPartyContract: Contract
+  electionCandidateContract: Contract
+  republicanCandidateContract: Contract
+  democratCandidateContract: Contract
 }) {
-  const { mapContracts } = props
+  const {
+    mapContracts,
+    electionCandidateContract,
+    electionPartyContract,
+    republicanCandidateContract,
+    democratCandidateContract,
+  } = props
   const [targetContract, setTargetContract] = useState<
     Contract | undefined | null
   >(undefined)
@@ -95,15 +130,23 @@ export default function USElectionsPage(props: {
   return (
     <Page trackPageView="us elections page 2024">
       <Col className="gap-3">
-        <div className="mx-auto font-semibold sm:text-xl">
-          Which party will win the US Presidency?
-        </div>
-        <USAMap customize={stateContractMap} />
-        {(hoveredContract || targetContract) && (
-          <StateContract
-            targetContract={(hoveredContract ?? targetContract) as Contract}
-          />
-        )}
+        <PoliticsContractCard contract={electionPartyContract} />
+        <PoliticsContractCard contract={electionCandidateContract} />
+        <PoliticsContractCard contract={republicanCandidateContract} />
+        <PoliticsContractCard contract={democratCandidateContract} />
+        <Col className="bg-canvas-0 rounded-xl p-4">
+          <div className="mx-auto font-semibold sm:text-xl">
+            Which party will win the US Presidency?
+          </div>
+          <USAMap customize={stateContractMap} />
+          {hoveredContract || targetContract ? (
+            <StateContract
+              targetContract={(hoveredContract ?? targetContract) as Contract}
+            />
+          ) : (
+            <div className="bg-canvas-100 h-[224px] w-full animate-pulse" />
+          )}
+        </Col>
       </Col>
     </Page>
   )
