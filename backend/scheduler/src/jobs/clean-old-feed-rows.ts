5c2d3173fb72d7bf3306183e1d97c27244ebb4f6
Marshall Polaris <marshall@pol.rs>
2024-01-08 16:34:20 -0800

    Use much more efficient version of `clean-old-feed-rows`

--- backend/scheduler/src/jobs/clean-old-feed-rows.ts
+++ backend/scheduler/src/jobs/clean-old-feed-rows.ts
@@ -1,22 +1,23 @@
-import { chunk } from 'lodash'
 import { createSupabaseDirectClient } from 'shared/supabase/init'
 import { JobContext } from 'shared/utils'
 
 export async function cleanOldFeedRows({ log }: JobContext) {
   log('Running clean old feed rows...')
   const pg = createSupabaseDirectClient()
-  const userIds = await pg.map(
-    'select distinct id from users',
-    [],
-    (r) => r.id as string
-  )
-  const chunks = chunk(userIds, 500)
-  for (const batch of chunks) {
-    await pg.none(
-      `delete from user_feed
-         where user_id in ($1:list) and created_time < now() - interval '2 weeks'`,
-      [batch]
+  const BATCH_SIZE = 1000000
+  while (true) {
+    const result = await pg.one(
+      `with deleted as (
+      delete from user_feed where id in
+        (select id from user_feed where created_time < now() - interval '2 weeks' limit $1)
+      returning 1
+     )
+     select count(*) as n from deleted`,
+      [BATCH_SIZE]
     )
-    log(`Deleted rows from ${batch.length} users`)
+    log(`Deleted ${result.n} old feed rows.`)
+    if (result.n < BATCH_SIZE) {
+      return
+    }
   }
 }
