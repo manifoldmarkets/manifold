fc50c14e3e18d5acc7dba252d0ad72e0dd0310d4
Calvin Liang <me@calvin.sh>
2024-01-09 17:04:06 -0800

    Revert "Add show more button to topic list"
    This reverts commit e54dc6b42148b2223089e523bc6a2dceb041bc13.

--- web/components/contract/market-topics.tsx
+++ web/components/contract/market-topics.tsx
@@ -1,22 +1,21 @@
-import { PencilIcon, PlusIcon } from '@heroicons/react/solid'
-import clsx from 'clsx'
 import { Contract } from 'common/contract'
-import { Group, Topic, groupPath } from 'common/group'
-import { removeEmojis } from 'common/topics'
-import { filterDefined } from 'common/util/array'
-import Link from 'next/link'
-import { useEffect, useRef, useState } from 'react'
-import { ShowMoreLessButton } from 'web/components/buttons/show-more-less-button'
-import { Col } from 'web/components/layout/col'
-import { Modal } from 'web/components/layout/modal'
 import { Row } from 'web/components/layout/row'
-import { ContractTopicsList } from 'web/components/topics/contract-topics-list'
-import { TopicTag } from 'web/components/topics/topic-tag'
-import { linkClass } from 'web/components/widgets/site-link'
-import { useAdminOrTrusted } from 'web/hooks/use-admin'
+import clsx from 'clsx'
 import { useGroupsWhereUserHasRole } from 'web/hooks/use-group-supabase'
+import Link from 'next/link'
+import { linkClass } from 'web/components/widgets/site-link'
+import { useState } from 'react'
 import { useUser } from 'web/hooks/use-user'
+import { PencilIcon, PlusIcon } from '@heroicons/react/solid'
+import { Modal } from 'web/components/layout/modal'
+import { Col } from 'web/components/layout/col'
+import { ContractTopicsList } from 'web/components/topics/contract-topics-list'
+import { useAdminOrTrusted } from 'web/hooks/use-admin'
+import { filterDefined } from 'common/util/array'
+import { Group, groupPath, Topic } from 'common/group'
 import { track } from 'web/lib/service/analytics'
+import { removeEmojis } from 'common/topics'
+import { TopicTag } from 'web/components/topics/topic-tag'
 
 export function MarketTopics(props: { contract: Contract; topics: Topic[] }) {
   const { contract, topics } = props
@@ -49,53 +48,26 @@ const ContractTopicBreadcrumbs = (props: {
 }) => {
   const { contract, topics } = props
 
-  const spanRef = useRef<HTMLSpanElement>(null)
-  const [isClamped, setClamped] = useState(false)
-  const [showMore, setShowMore] = useState(false)
-
-  useEffect(() => {
-    function handleResize() {
-      if (spanRef.current) {
-        setClamped(spanRef.current.scrollHeight > spanRef.current.clientHeight)
-      }
-    }
-
-    window.addEventListener('resize', handleResize)
-
-    return () => window.removeEventListener('resize', handleResize)
-  }, [topics])
-
   return (
-    <Col>
-      <span
-        ref={spanRef}
-        className={clsx(['min-h-[24px]', { 'line-clamp-1': !showMore }])}
-      >
-        {topics.map((topic, i) => (
-          <span key={topic.id} className={'text-primary-700 text-sm'}>
-            <Link
-              className={linkClass}
-              href={groupPath(topic.slug)}
-              onClick={() => {
-                track('click category pill on market', {
-                  contractId: contract.id,
-                  categoryName: topic.name,
-                })
-              }}
-            >
-              {removeEmojis(topic.name)}
-            </Link>
-            {i !== topics.length - 1 && <span className="mx-1.5">{'•'}</span>}
-          </span>
-        ))}
-      </span>
-      {isClamped && (
-        <ShowMoreLessButton
-          showMore={showMore}
-          onClick={() => setShowMore(!showMore)}
-        />
-      )}
-    </Col>
+    <span className="min-h-[24px]">
+      {topics.map((topic, i) => (
+        <span key={topic.id} className={'text-primary-700 text-sm'}>
+          <Link
+            className={linkClass}
+            href={groupPath(topic.slug)}
+            onClick={() => {
+              track('click category pill on market', {
+                contractId: contract.id,
+                categoryName: topic.name,
+              })
+            }}
+          >
+            {removeEmojis(topic.name)}
+          </Link>
+          {i !== topics.length - 1 && <span className="mx-1.5">{'•'}</span>}
+        </span>
+      ))}
+    </span>
   )
 }
 
