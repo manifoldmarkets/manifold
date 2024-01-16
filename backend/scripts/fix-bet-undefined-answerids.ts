50fcd0dbc507ae6e52bb3991c3c767d4406dc68c
Ian Philips <iansphilips@gmail.com>
2024-01-16 23:05:33 +0900

    Script to remove all 'undefined' answerIds on bets

---
+++ backend/scripts/fix-bet-undefined-answerids.ts
@@ -0,0 +1,28 @@
+import { runScript } from './run-script'
+import { FieldValue } from 'firebase-admin/firestore'
+import { Bet } from 'common/bet'
+import { log } from 'shared/utils'
+
+runScript(async ({ firestore }) => {
+  const writer = firestore.bulkWriter({ throttling: false })
+  let updated = 0
+  const limitBets = firestore
+    .collectionGroup('bets')
+    .where('answerId', '==', 'undefined')
+    .orderBy('id', 'asc')
+    .limit(100000)
+  const limitBetsSnap = await limitBets.get()
+  const limitBetsDocs = limitBetsSnap.docs
+  console.log(`Found ${limitBetsDocs.length} bets with 'undefined' answerIds`)
+  for (const doc of limitBetsDocs) {
+    const data = doc.data() as Bet
+    if (data.answerId !== 'undefined') continue
+    const update = { answerId: FieldValue.delete() }
+    updated++
+    writer.update(doc.ref, update)
+  }
+
+  log(`Committing ${updated} updates...`)
+  await writer.close()
+  return updated
+})
