7f4eb026c02a5fa65493a8eee2d542a45cdf2a18
ingawei <46611122+ingawei@users.noreply.github.com>
2024-01-19 17:40:28 -0800

    Ingawei/nh stuff (#2394)
    * nh stuf

--- web/pages/elections.tsx
+++ web/pages/elections.tsx
@@ -152,8 +152,10 @@ export default function USElectionsPage(props: {
 
   if (
     !electionPartyContract ||
+    !electionCandidateContract ||
     !republicanCandidateContract ||
-    !democratCandidateContract
+    !democratCandidateContract ||
+    !newHampshireContract
   ) {
     return <Custom404 />
   }
@@ -183,7 +185,7 @@ export default function USElectionsPage(props: {
                   'text-ink-700 grow items-start font-semibold transition-colors sm:text-lg'
                 )}
               >
-                NH Primaries
+                New Hampshire Primaries
               </div>
 
               <Carousel>
@@ -198,10 +200,10 @@ export default function USElectionsPage(props: {
             <div className="relative">
               <div
                 className={clsx(
-                  'text-ink-700 absolute -top-4 left-[calc(50%-50px)] mx-auto grow items-start font-semibold transition-colors sm:text-lg'
+                  'text-ink-700 absolute -top-4 left-[calc(50%-120px)] mx-auto grow items-start font-semibold transition-colors sm:text-lg'
                 )}
               >
-                <div className="bg-canvas-50 px-4">NH Primaries</div>
+                <div className="bg-canvas-50 px-4">New Hampshire Primaries</div>
               </div>
               <Row className="border-ink-300 w-full gap-4 rounded-xl border-2 p-4">
                 <NHPrimaries
