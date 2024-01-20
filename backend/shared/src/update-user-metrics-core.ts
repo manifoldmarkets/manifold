a9b77e076adda5bcc6539250ecb3f2455fffef5a
Ian Philips <iansphilips@gmail.com>
2024-01-20 09:36:06 +0900

    Handle user profit vs portfolio cache updates

--- backend/shared/src/update-user-metrics-core.ts
+++ backend/shared/src/update-user-metrics-core.ts
@@ -81,10 +81,10 @@ export async function updateUserMetricsCore({ log }: JobContext) {
         Object.keys(userToPortfolioMetrics).length
       } users.`
     )
-    log('Loading current portfolio snapshot...')
     const userIdsMissingPortfolio = users
       .filter((u) => !userToPortfolioMetrics[u.id]?.currentPortfolio)
       .map((u) => u.id)
+    log(`Loading current portfolio snapshot for ${userIdsMissingPortfolio.length} users...`)
     // We recalculate the portfolio every run per user, so we don't need to ever query it more than once
     const currentPortfolios = await getPortfolioSnapshot(
       pg,
@@ -312,6 +312,9 @@ const getPortfolioSnapshot = async (
   pg: SupabaseDirectClient,
   userIds: string[]
 ) => {
+  if (userIds.length === 0) {
+    return {}
+  }
   return Object.fromEntries(
     await pg.map(
       `select distinct on (user_id) user_id, investment_value, balance, total_deposits, loan_total
