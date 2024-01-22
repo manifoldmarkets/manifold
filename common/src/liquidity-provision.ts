2856418f82a422843c6ee42860e34043ea9f7633
Ian Philips <iansphilips@gmail.com>
2024-01-22 10:43:04 +0900

    Add answerId note on LiquidityProvision

--- common/src/liquidity-provision.ts
+++ common/src/liquidity-provision.ts
@@ -4,7 +4,10 @@ export type LiquidityProvision = {
   contractId: string
   createdTime: number
   isAnte?: boolean
-  answerId?: string // This is only set for house subsidies
+  // WARNING: answerId is not properly set on most LP's. It is not set on initial MC LP's even if the
+  // contract has multiple answers. Furthermore, it's only set on answers added after the question was created
+  // (after this commit), and house subsidies.
+  answerId?: string
   amount: number // Ṁ quantity
 
   liquidity: number // change in constant k after provision
