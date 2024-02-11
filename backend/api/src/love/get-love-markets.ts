99e7aea70400e8116dda5b9027f67c04c6d15bbf
James Grugett <jahooma@gmail.com>
2024-02-10 17:16:16 -0800

    love: Fully fix mutual message bug

--- backend/api/src/love/get-love-markets.ts
+++ backend/api/src/love/get-love-markets.ts
@@ -2,7 +2,8 @@ import { type APIHandler } from 'api/helpers/endpoint'
 import { CPMMMultiContract } from 'common/contract'
 import { Lover } from 'common/love/lover'
 import { filterDefined } from 'common/util/array'
-import { groupBy, mapValues, uniq } from 'lodash'
+import { uniq } from 'lodash'
+import { getCreatorMutuallyMessagedUserIds } from 'shared/love/love-markets'
 import { createSupabaseDirectClient } from 'shared/supabase/init'
 
 export const getLoveMarkets: APIHandler<'get-love-markets'> = async () => {
@@ -56,32 +57,8 @@ export const getLoveMarketsMain = async () => {
 
   console.log('got contracts', contracts.length, 'and lovers', lovers.length)
 
-  const mutualMessageData = await pg.manyOrNone<{
-    creator_id: string
-    mutual_id: string
-    channel_id: string
-  }>(
-    `
-    SELECT
-        p1.user_id AS creator_id,
-        p2.user_id AS mutual_id,
-        p1.channel_id
-    FROM
-        private_user_messages p1
-    JOIN
-        private_user_messages p2 ON p1.channel_id = p2.channel_id AND p1.user_id != p2.user_id
-    WHERE
-        p1.user_id = any($1)
-    GROUP BY
-        p1.user_id, p2.user_id, p1.channel_id
-    `,
-    [creatorIds]
-  )
-
-  const creatorMutuallyMessagedUserIds = mapValues(
-    groupBy(mutualMessageData, (r) => r.creator_id),
-    (v) => v.map((r) => r.mutual_id)
-  )
+  const creatorMutuallyMessagedUserIds =
+    await getCreatorMutuallyMessagedUserIds(creatorIds)
 
   console.log('creatorMutuallyMessagedUserIds', creatorMutuallyMessagedUserIds)
 
