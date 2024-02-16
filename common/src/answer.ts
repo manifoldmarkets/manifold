7428a48aa216d94a49e62b0a0b747dbf6642229b
James Grugett <jahooma@gmail.com>
2024-02-15 17:27:37 -0800

    Tooltip typo

--- common/src/answer.ts
+++ common/src/answer.ts
@@ -60,7 +60,7 @@ export const getMaximumAnswers = (shouldAnswersSumToOne: boolean) =>
   shouldAnswersSumToOne ? MAX_ANSWERS : MAX_INDEPENDENT_ANSWERS
 
 export const OTHER_TOOLTIP_TEXT =
-  "Bet on all answers that aren't listed yet. A bet on Other automatically includes any answer added on the future."
+  "Bet on all answers that aren't listed yet. A bet on Other automatically includes any answer added in the future."
 
 export type MultiSort =
   | 'prob-desc'
