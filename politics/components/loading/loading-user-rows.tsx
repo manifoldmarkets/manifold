7a9278e64dc416223d6682a6c44d6f5849348828
Ian Philips <iansphilips@gmail.com>
2024-02-27 14:29:15 -0800

    Loading user rows animation

--- politics/components/loading/loading-user-rows.tsx
+++ politics/components/loading/loading-user-rows.tsx
@@ -4,7 +4,9 @@ import { Col } from 'web/components/layout/col'
 
 export default function LoadingUserRows() {
   return (
-    <Col className="min-h-screen w-full">
+    <Col className="min-h-screen w-full gap-2">
+      <LoadingUserRow />
+      <LoadingUserRow />
       <LoadingUserRow />
       <LoadingUserRow />
       <LoadingUserRow />
@@ -18,6 +20,7 @@ export function LoadingUserRow() {
     <div className="border-ink-200 flex w-full animate-pulse border-b p-2 last:border-none sm:rounded-md sm:border-none">
       <Row className="w-full  justify-between gap-1 sm:gap-4">
         <Row className={clsx('sm:w-[calc(100%-12rem] w-full gap-2 sm:gap-4')}>
+          <span className={'mr-6'}>#</span>
           <div className="h-6 w-6 rounded-full bg-gray-500" />
           <div className="h-5 w-32 rounded-full bg-gray-500" />
         </Row>
