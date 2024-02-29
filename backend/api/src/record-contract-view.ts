1a50836b9eb0090eb213adbddc10d6b142ec2442
Marshall Polaris <marshall@pol.rs>
2024-02-29 03:03:50 -0800

    Add some log messages to `record-contract-view` for debugging

--- backend/api/src/record-contract-view.ts
+++ backend/api/src/record-contract-view.ts
@@ -1,5 +1,6 @@
 import { APIError, APIHandler } from 'api/helpers/endpoint'
 import { createSupabaseDirectClient } from 'shared/supabase/init'
+import { log } from 'shared/utils'
 
 export const recordContractView: APIHandler<'record-contract-view'> = async (
   body,
@@ -11,6 +12,7 @@ export const recordContractView: APIHandler<'record-contract-view'> = async (
   }
   const pg = createSupabaseDirectClient()
   if (userId != null) {
+    log('Inserting USM entry for view.', { userId, contractId, kind })
     await pg.none(
       `insert into user_seen_markets (user_id, contract_id, is_promoted, type)
        values ($1, $2, $3, $4)`,
@@ -22,6 +24,7 @@ export const recordContractView: APIHandler<'record-contract-view'> = async (
       ]
     )
   }
+  log('Inserting UCV entry for view.', { userId, contractId, kind })
   await pg.none(
     `insert into user_contract_views as ucv (user_id, contract_id, promoted_views, card_views, page_views)
      values ($1, $2, $3, $4, $5)
@@ -39,6 +42,7 @@ export const recordContractView: APIHandler<'record-contract-view'> = async (
       kind === 'card' ? 1 : 0,
     ]
   )
+  log('Done inserting view entries.')
 
   return { status: 'success' }
 }
