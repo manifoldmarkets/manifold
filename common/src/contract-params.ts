ea248d0d7309ae1cb3be3779b987fd795261cd57
Ian Philips <iansphilips@gmail.com>
2024-03-01 13:38:11 -0800

    Convert get-related-markets to cached, GET endpoint

--- common/src/contract-params.ts
+++ common/src/contract-params.ts
@@ -80,7 +80,7 @@ export async function getContractParams(
       : {},
     contract.resolution ? getTopContractMetrics(contract.id, 10, db) : [],
     isCpmm1 || isMulti ? getContractMetricsCount(contract.id, db) : 0,
-    unauthedApi('get-related-markets', {
+    unauthedApi('get-related-markets-cache', {
       contractId: contract.id,
       limit: 4,
       limitTopics: 4,
