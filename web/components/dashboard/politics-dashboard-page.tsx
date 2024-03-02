2db4401506b42f407103d11fca8dab3435f94fe4
ingawei <ingawei@gmail.com>
2024-03-01 22:13:44 -0800

    oopsies

--- web/components/dashboard/politics-dashboard-page.tsx
+++ web/components/dashboard/politics-dashboard-page.tsx
@@ -32,7 +32,7 @@ import { Headline } from 'common/news'
 import { type Contract } from 'common/contract'
 import { UserHovercard } from '../user/user-hovercard'
 export type DashboardEndpoints = 'news' | 'politics'
-export function DashboardPage(props: {
+export function PoliticsDashboardPage(props: {
   initialDashboard: Dashboard
   previews: LinkPreviews
   initialContracts: Contract[]
