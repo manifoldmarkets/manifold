81633b13dab2f2f4294c1e7aaa57e8a152cd7f4a
SirSaltyy <d4vidchee@gmail.com>
2024-03-01 16:08:06 -0800

    bet panel improvements

--- politics/components/answers/answer-components.tsx
+++ politics/components/answers/answer-components.tsx
@@ -230,9 +230,7 @@ export const MultiBettor = (props: {
   contract: CPMMMultiContract
 }) => {
   const { answer, contract } = props
-  const [outcome, setOutcome] = useState<'YES' | 'NO' | 'LIMIT' | undefined>(
-    undefined
-  )
+  const [outcome, setOutcome] = useState<'YES' | 'NO' | undefined>(undefined)
 
   const user = useUser()
 
@@ -272,9 +270,7 @@ export const YesNoBetButtons = (props: {
   fillColor?: string
 }) => {
   const { answer, contract, fillColor } = props
-  const [outcome, setOutcome] = useState<'YES' | 'NO' | 'LIMIT' | undefined>(
-    undefined
-  )
+  const [outcome, setOutcome] = useState<'YES' | 'NO' | undefined>(undefined)
 
   const user = useUser()
 
