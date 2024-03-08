9964472c118e5c4e57252888904cf692315e6522
James Grugett <jahooma@gmail.com>
2024-03-07 16:16:05 -0800

    Refactor home content into home page.

--- web/components/feed-timeline.tsx
+++ web/components/feed-timeline.tsx
@@ -1,11 +1,9 @@
-import { useIsAuthorized, useShouldBlockDestiny } from 'web/hooks/use-user'
+import { useIsAuthorized } from 'web/hooks/use-user'
 import { Col } from 'web/components/layout/col'
 import { Row } from 'web/components/layout/row'
 import { LoadingIndicator } from 'web/components/widgets/loading-indicator'
 import clsx from 'clsx'
-import { track } from 'web/lib/service/analytics'
-import Router from 'next/router'
-import { ArrowUpIcon, PencilAltIcon } from '@heroicons/react/solid'
+import { ArrowUpIcon } from '@heroicons/react/solid'
 import { VisibilityObserver } from 'web/components/widgets/visibility-observer'
 import Link from 'next/link'
 import { FeedTimelineItem, useFeedTimeline } from 'web/hooks/use-feed-timeline'
@@ -15,84 +13,11 @@ import { useEffect, useState } from 'react'
 import { usePersistentLocalState } from 'web/hooks/use-persistent-local-state'
 import { Avatar } from 'web/components/widgets/avatar'
 import { range, uniq, uniqBy } from 'lodash'
-import { buildArray, filterDefined } from 'common/util/array'
-import { DAY_MS, MINUTE_MS } from 'common/util/time'
-import {
-  DAYS_TO_USE_FREE_QUESTIONS,
-  freeQuestionRemaining,
-  PrivateUser,
-  User,
-} from 'common/user'
-import { CreateQuestionButton } from 'web/components/buttons/create-question-button'
-import { shortenedFromNow } from 'web/lib/util/shortenedFromNow'
-import { Tabs } from './layout/tabs'
-import { SupabaseSearch } from './supabase-search'
-import { BrowseTopicPills } from './topics/browse-topic-pills'
-import { useTrendingTopics, useUserTrendingTopics } from './search/query-topics'
-import { Group } from 'common/group'
-import { DESTINY_GROUP_SLUGS } from 'common/envs/constants'
-import { usePersistentInMemoryState } from 'web/hooks/use-persistent-in-memory-state'
+import { filterDefined } from 'common/util/array'
+import { MINUTE_MS } from 'common/util/time'
+import { PrivateUser, User } from 'common/user'
 
 export function FeedTimeline(props: {
-  user: User | undefined | null
-  privateUser: PrivateUser | undefined | null
-}) {
-  const { user, privateUser } = props
-  const remaining = freeQuestionRemaining(
-    user?.freeQuestionsCreated,
-    user?.createdTime
-  )
-  return (
-    <Col className="w-full pb-4 sm:px-2">
-      {user && remaining > 0 && (
-        <Row className="text-md mb-2 items-center justify-between gap-2 self-center rounded-md border-2 border-indigo-500 p-2">
-          <span>
-            🎉 You've got{' '}
-            <span className="font-semibold">{remaining} free questions</span>!
-            Use them before they expire in{' '}
-            {shortenedFromNow(
-              user.createdTime + DAY_MS * DAYS_TO_USE_FREE_QUESTIONS
-            )}
-            .
-          </span>
-          <CreateQuestionButton className={'max-w-[10rem]'} />
-        </Row>
-      )}
-      <Tabs
-        className="bg-canvas-50 sticky top-6 z-10 mb-1 px-1"
-        tabs={[
-          {
-            title: 'Feed',
-            content: privateUser && (
-              <FeedTimelineContent user={user} privateUser={privateUser} />
-            ),
-            prerender: true,
-          },
-          {
-            title: 'Browse',
-            content: <BrowseSection privateUser={privateUser} user={user} />,
-            prerender: true,
-          },
-        ]}
-      />
-      <button
-        type="button"
-        className={clsx(
-          'focus:ring-primary-500 fixed  right-3 z-20 inline-flex items-center rounded-full border  border-transparent  p-4 shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 lg:hidden',
-          'disabled:bg-ink-300 text-ink-0 from-primary-500 hover:from-primary-700 to-blue-500 hover:to-blue-700 enabled:bg-gradient-to-r',
-          'bottom-[64px]'
-        )}
-        onClick={() => {
-          Router.push('/create')
-          track('mobile create button')
-        }}
-      >
-        <PencilAltIcon className="h-6 w-6" aria-hidden="true" />
-      </button>
-    </Col>
-  )
-}
-export function FeedTimelineContent(props: {
   privateUser: PrivateUser
   user: User | undefined | null
 }) {
@@ -242,56 +167,5 @@ const NewActivityButton = (props: {
         <div className="ml-1">New updates</div>
       </Row>
     </button>
-  )
-}
-
-const BrowseSection = (props: {
-  privateUser: PrivateUser | null | undefined
-  user: User | undefined | null
-}) => {
-  const { privateUser, user } = props
-
-  const [topicSlug, setTopicSlug] = usePersistentInMemoryState(
-    '',
-    'home-browse'
-  )
-  const shouldFilterDestiny = useShouldBlockDestiny(user?.id)
-  const userTrendingTopics = useUserTrendingTopics(user, 25)
-  const trendingTopics = useTrendingTopics(
-    50,
-    'home-page-trending-topics'
-  ) as Group[]
-  const topics = uniqBy(
-    [...(userTrendingTopics ?? []), ...trendingTopics],
-    'slug'
-  )
-
-  return (
-    <Col>
-      <BrowseTopicPills
-        className={'relative w-full py-1 pl-1'}
-        topics={topics}
-        currentTopicSlug={topicSlug}
-        setTopicSlug={(slug) => setTopicSlug(slug === topicSlug ? '' : slug)}
-      />
-      <SupabaseSearch
-        persistPrefix="browse-home"
-        autoFocus={false}
-        additionalFilter={{
-          excludeContractIds: privateUser?.blockedContractIds,
-          excludeGroupSlugs: buildArray(
-            privateUser?.blockedGroupSlugs,
-            shouldFilterDestiny &&
-              !DESTINY_GROUP_SLUGS.includes(topicSlug ?? '') &&
-              DESTINY_GROUP_SLUGS
-          ),
-          excludeUserIds: privateUser?.blockedUserIds,
-        }}
-        hideSearch
-        hideContractFilters
-        topicSlug={topicSlug}
-        contractsOnly
-      />
-    </Col>
   )
 }
--- web/pages/home/index.tsx
+++ web/pages/home/index.tsx
@@ -1,3 +1,8 @@
+import { track } from '@amplitude/analytics-browser'
+import { PencilAltIcon } from '@heroicons/react/solid'
+import clsx from 'clsx'
+import { uniqBy } from 'lodash'
+
 import { DailyStats } from 'web/components/home/daily-stats'
 import { Page } from 'web/components/layout/page'
 import { Row } from 'web/components/layout/row'
@@ -6,7 +11,11 @@ import { Title } from 'web/components/widgets/title'
 import { useIsClient } from 'web/hooks/use-is-client'
 import { useRedirectIfSignedOut } from 'web/hooks/use-redirect-if-signed-out'
 import { useSaveReferral } from 'web/hooks/use-save-referral'
-import { usePrivateUser, useUser } from 'web/hooks/use-user'
+import {
+  usePrivateUser,
+  useShouldBlockDestiny,
+  useUser,
+} from 'web/hooks/use-user'
 import { FeedTimeline } from 'web/components/feed-timeline'
 import { api } from 'web/lib/firebase/api'
 import { Headline } from 'common/news'
@@ -16,6 +25,27 @@ import { useNewUserMemberTopicsAndContracts } from 'web/hooks/use-group-supabase
 import { LoadingIndicator } from 'web/components/widgets/loading-indicator'
 import { DAY_MS } from 'common/util/time'
 import { useSaveScroll } from 'web/hooks/use-save-scroll'
+import { CreateQuestionButton } from 'web/components/buttons/create-question-button'
+import { shortenedFromNow } from 'web/lib/util/shortenedFromNow'
+import { DESTINY_GROUP_SLUGS } from 'common/envs/constants'
+import {
+  PrivateUser,
+  freeQuestionRemaining,
+  DAYS_TO_USE_FREE_QUESTIONS,
+} from 'common/user'
+import { buildArray } from 'common/util/array'
+import Router from 'next/router'
+import { Col } from 'web/components/layout/col'
+import { Tabs } from 'web/components/layout/tabs'
+import {
+  useUserTrendingTopics,
+  useTrendingTopics,
+} from 'web/components/search/query-topics'
+import { SupabaseSearch } from 'web/components/supabase-search'
+import { BrowseTopicPills } from 'web/components/topics/browse-topic-pills'
+import { usePersistentInMemoryState } from 'web/hooks/use-persistent-in-memory-state'
+import { User } from 'common/user'
+import { Group } from 'common/group'
 
 export async function getStaticProps() {
   try {
@@ -66,7 +96,7 @@ export default function Home(props: { headlines: Headline[] }) {
           <LoadingIndicator />
         ) : !createdRecently ? (
           isClient ? (
-            <FeedTimeline user={user} privateUser={privateUser} />
+            <HomeContent user={user} privateUser={privateUser} />
           ) : null
         ) : !memberTopicsWithContracts ? (
           <LoadingIndicator />
@@ -75,10 +105,121 @@ export default function Home(props: { headlines: Headline[] }) {
             <WelcomeTopicSections
               memberTopicsWithContracts={memberTopicsWithContracts}
             />
-            {isClient && <FeedTimeline user={user} privateUser={privateUser} />}
+            {isClient && <HomeContent user={user} privateUser={privateUser} />}
           </>
         )}
       </Page>
     </>
+  )
+}
+
+export function HomeContent(props: {
+  user: User | undefined | null
+  privateUser: PrivateUser | undefined | null
+}) {
+  const { user, privateUser } = props
+  const remaining = freeQuestionRemaining(
+    user?.freeQuestionsCreated,
+    user?.createdTime
+  )
+  return (
+    <Col className="w-full pb-4 sm:px-2">
+      {user && remaining > 0 && (
+        <Row className="text-md mb-2 items-center justify-between gap-2 self-center rounded-md border-2 border-indigo-500 p-2">
+          <span>
+            🎉 You've got{' '}
+            <span className="font-semibold">{remaining} free questions</span>!
+            Use them before they expire in{' '}
+            {shortenedFromNow(
+              user.createdTime + DAY_MS * DAYS_TO_USE_FREE_QUESTIONS
+            )}
+            .
+          </span>
+          <CreateQuestionButton className={'max-w-[10rem]'} />
+        </Row>
+      )}
+      <Tabs
+        className="bg-canvas-50 sticky top-6 z-10 mb-1 px-1"
+        tabs={[
+          {
+            title: 'Feed',
+            content: privateUser && (
+              <FeedTimeline user={user} privateUser={privateUser} />
+            ),
+            prerender: true,
+          },
+          {
+            title: 'Browse',
+            content: <BrowseSection privateUser={privateUser} user={user} />,
+            prerender: true,
+          },
+        ]}
+      />
+      <button
+        type="button"
+        className={clsx(
+          'focus:ring-primary-500 fixed  right-3 z-20 inline-flex items-center rounded-full border  border-transparent  p-4 shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 lg:hidden',
+          'disabled:bg-ink-300 text-ink-0 from-primary-500 hover:from-primary-700 to-blue-500 hover:to-blue-700 enabled:bg-gradient-to-r',
+          'bottom-[64px]'
+        )}
+        onClick={() => {
+          Router.push('/create')
+          track('mobile create button')
+        }}
+      >
+        <PencilAltIcon className="h-6 w-6" aria-hidden="true" />
+      </button>
+    </Col>
+  )
+}
+
+const BrowseSection = (props: {
+  privateUser: PrivateUser | null | undefined
+  user: User | undefined | null
+}) => {
+  const { privateUser, user } = props
+
+  const [topicSlug, setTopicSlug] = usePersistentInMemoryState(
+    '',
+    'home-browse'
+  )
+  const shouldFilterDestiny = useShouldBlockDestiny(user?.id)
+  const userTrendingTopics = useUserTrendingTopics(user, 25)
+  const trendingTopics = useTrendingTopics(
+    50,
+    'home-page-trending-topics'
+  ) as Group[]
+  const topics = uniqBy(
+    [...(userTrendingTopics ?? []), ...trendingTopics],
+    'slug'
+  )
+
+  return (
+    <Col>
+      <BrowseTopicPills
+        className={'relative w-full py-1 pl-1'}
+        topics={topics}
+        currentTopicSlug={topicSlug}
+        setTopicSlug={(slug) => setTopicSlug(slug === topicSlug ? '' : slug)}
+      />
+      <SupabaseSearch
+        persistPrefix="browse-home"
+        autoFocus={false}
+        additionalFilter={{
+          excludeContractIds: privateUser?.blockedContractIds,
+          excludeGroupSlugs: buildArray(
+            privateUser?.blockedGroupSlugs,
+            shouldFilterDestiny &&
+              !DESTINY_GROUP_SLUGS.includes(topicSlug ?? '') &&
+              DESTINY_GROUP_SLUGS
+          ),
+          excludeUserIds: privateUser?.blockedUserIds,
+        }}
+        hideSearch
+        hideContractFilters
+        topicSlug={topicSlug}
+        contractsOnly
+      />
+    </Col>
   )
 }
