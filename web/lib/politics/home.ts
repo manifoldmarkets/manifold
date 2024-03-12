d29b3099f7c8d9907c658736d574746bb4d7fc07
ingawei <ingawei@gmail.com>
2024-03-12 14:57:57 -0700

    republican

--- web/lib/politics/home.ts
+++ web/lib/politics/home.ts
@@ -49,9 +49,9 @@ export async function getElectionsPageProps() {
     'who-will-win-the-2024-democratic-pr-47576e90fa38',
     'who-will-win-the-new-hampshire-repu',
     'who-will-be-the-republican-nominee-8a36dedc6445',
-    'who-will-be-the-democratic-nominee-9d4a78f63ce1',
-    'who-would-win-the-us-presidential-e-e43c62c31980',
-    'who-would-win-the-us-presidential-e-2f4e0b318013',
+    // 'who-will-be-the-democratic-nominee-9d4a78f63ce1',
+    // 'who-would-win-the-us-presidential-e-e43c62c31980',
+    // 'who-would-win-the-us-presidential-e-2f4e0b318013',
   ]
   const contractsPromises = specialContractSlugs.map(async (slug) =>
     getContract(slug)
@@ -64,9 +64,9 @@ export async function getElectionsPageProps() {
     democratCandidateContract,
     newHampshireContract,
     republicanVPContract,
-    democraticVPContract,
-    democraticElectability,
-    republicanElectability,
+    // democraticVPContract,
+    // democraticElectability,
+    // republicanElectability,
   ] = await Promise.all(contractsPromises)
 
   const linkPreviews = await fetchLinkPreviews([NH_LINK])
@@ -103,9 +103,9 @@ export async function getElectionsPageProps() {
     democratCandidateContract,
     newHampshireContract,
     republicanVPContract,
-    democraticVPContract,
-    democraticElectability,
-    republicanElectability,
+    // democraticVPContract,
+    // democraticElectability,
+    // republicanElectability,
     linkPreviews,
     newsDashboards,
     headlines,
