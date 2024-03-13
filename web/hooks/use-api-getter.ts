7e153b360d4101cc1cf59a62ed5aa6309405c27b
James Grugett <jahooma@gmail.com>
2024-03-13 15:39:08 -0700

    Dedupe api promises in cache

--- web/hooks/use-api-getter.ts
+++ web/hooks/use-api-getter.ts
@@ -5,6 +5,8 @@ import { usePersistentInMemoryState } from './use-persistent-in-memory-state'
 import { APIError, api } from 'web/lib/firebase/api'
 import { useEvent } from './use-event'
 
+const promiseCache: Record<string, Promise<any> | undefined> = {}
+
 export const useAPIGetter = <P extends APIPath>(
   path: P,
   props: APIParams<P> | undefined
@@ -15,16 +17,24 @@ export const useAPIGetter = <P extends APIPath>(
     APIResponse<P> | undefined
   >(undefined, `${path}-${propsString}`)
 
+  const key = `${path}-${propsString}-error`
   const [error, setError] = usePersistentInMemoryState<APIError | undefined>(
     undefined,
-    `${path}-${propsString}-error`
+    key
   )
 
   const refresh = useEvent(async () => {
     if (!props) return
-    await api(path, props)
-      .then(setData)
-      .catch((e) => setError(e))
+
+    const cachedPromise = promiseCache[key]
+    if (cachedPromise) {
+      await cachedPromise.then(setData).catch(setError)
+    } else {
+      const promise = api(path, props)
+      promiseCache[key] = promise
+      await promise.then(setData).catch(setError)
+      promiseCache[key] = undefined
+    }
   })
 
   useEffect(() => {
