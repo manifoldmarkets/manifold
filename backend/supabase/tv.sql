440374db3f67cfbf39e7a606f0230c5346bf5e60
mantikoros <sgrugett@gmail.com>
2024-03-15 17:31:43 -0700

    sql

--- backend/supabase/tv.sql
+++ backend/supabase/tv.sql
@@ -2,11 +2,13 @@ create table if not exists
   tv_schedule (
     id serial primary key,
     schedule_created_time timestamptz default now(),
+    creator_id text not null,
     contract_id text not null,
     stream_id text not null,
     source text not null,
-    start_time timestamptz,
-    end_time timestamptz,
+    start_time timestamptz not null,
+    end_time timestamptz not null,
+    is_featured boolean default false,
   );
 
 alter publication supabase_realtime add table tv_schedule;
