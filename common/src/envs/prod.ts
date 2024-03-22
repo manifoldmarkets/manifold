b97c543f915b48d0da7f4b07c970870ee74a8c40
mantikoros <sgrugett@gmail.com>
2024-03-21 18:43:45 -0700

    make ai acct admin

--- common/src/envs/prod.ts
+++ common/src/envs/prod.ts
@@ -84,6 +84,7 @@ export const PROD_CONFIG: EnvConfig = {
     'z0cH5XmIM9XgWFOBAILQWt0fTHr1', // Rachel W
     'cA1JupYR5AR8btHUs2xvkui7jA93', // Genzy
     'vuI5upWB8yU00rP7yxj95J2zd952', // Manifold Politics
+    '8lZo8X5lewh4hnCoreI7iSc0GxK2', // Manifold AI
   ],
   visibility: 'PUBLIC',
 
