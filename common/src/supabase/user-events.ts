c406365a8eba844ad6ae5de09a7d8bf498c753b5
Ian Philips <iansphilips@gmail.com>
2024-04-08 23:42:03 -0600

    Substitute faster queries for user_events in backend

--- common/src/supabase/user-events.ts
+++ common/src/supabase/user-events.ts
@@ -10,7 +10,16 @@ export async function getUserShareEventsCount(
     .select('*', { head: true, count: 'exact' })
     .eq('user_id', userId)
     .gt('ts', startTs)
-    .eq('data->>type', 'copy sharing link')
+    .in('name', [
+      'copy market link',
+      'copy referral link',
+      'copy comment link',
+      'copy group link',
+      'share home news item',
+      'share user page',
+      'copy questions page link',
+      'copy dashboard link',
+    ])
 
   const { count } = await run(q)
   return count
