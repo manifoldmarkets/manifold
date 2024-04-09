5d10466a96f7efa367fb8fc3a13a9ee0abc305c9
Ian Philips <iansphilips@gmail.com>
2024-04-09 16:31:58 -0600

    Restrict manalinks

--- backend/api/src/create-manalink.ts
+++ backend/api/src/create-manalink.ts
@@ -1,10 +1,16 @@
 import { createSupabaseClient } from 'shared/supabase/init'
 import { APIError, APIHandler } from './helpers/endpoint'
 import { millisToTs } from 'common/supabase/utils'
+import { isAdminId } from 'common/envs/constants'
 
 export const createManalink: APIHandler<'manalink'> = async (props, auth) => {
   const { amount, expiresTime, maxUses, message } = props
-
+  if (!isAdminId(auth.uid)) {
+    throw new APIError(
+      403,
+      `User ${auth.uid} must be an admin to perform this action.`
+    )
+  }
   const db = createSupabaseClient()
   const { data, error } = await db
     .from('manalinks')
