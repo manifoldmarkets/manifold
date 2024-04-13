b9539604b9668522fa9235b21bdb24fd586b05bf
Ian Philips <iansphilips@gmail.com>
2024-04-12 17:24:38 -0700

    Time out hanging scheduler jobs w/ bulkWriter

--- backend/scheduler/src/jobs/helpers.ts
+++ backend/scheduler/src/jobs/helpers.ts
@@ -28,7 +28,8 @@ const DEFAULT_OPTS: CronOptions = {
 export function createJob(
   name: string,
   schedule: string | null,
-  fn: (ctx: JobContext) => Promise<void>
+  fn: (ctx: JobContext) => Promise<void>,
+  timeout?: number
 ) {
   const opts = { name, ...DEFAULT_OPTS }
   return Cron(schedule ?? new Date(0), opts, async () => {
@@ -55,13 +56,25 @@ export function createJob(
         )
       log(`Last end time: ${lastEndTimeStamp ?? 'never'}`)
 
-      // Run job
-      await fn({
+      const jobPromise = fn({
         lastEndTime: lastEndTimeStamp
           ? new Date(lastEndTimeStamp).valueOf()
           : undefined,
       })
 
+      if (timeout) {
+        // This doesn't actually stop execution of the hung job, it just allows another to start.
+        // We'll have to see if the hung job ever causes a problem.
+        const timeoutPromise = new Promise((_, reject) => {
+          setTimeout(() => {
+            reject(new Error(`Job ${name} timed out after ${timeout}ms`))
+          }, timeout)
+        })
+
+        await Promise.race([jobPromise, timeoutPromise])
+      } else {
+        await jobPromise
+      }
       // Update last end time
       await db
         .from('scheduler_info')
