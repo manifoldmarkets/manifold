9520e273bbbb1c1da4e1b9b65e88374be931959b
Sinclair Chen <abc.sinclair@gmail.com>
2024-05-02 12:46:54 -0600

    Use txns for Manachan, bot fee

--- backend/api/src/manachan-tweet.ts
+++ backend/api/src/manachan-tweet.ts
@@ -1,12 +1,11 @@
-import * as admin from 'firebase-admin'
 import { z } from 'zod'
-
-import { User } from 'common/user'
 import { APIError, authEndpoint, validate } from './helpers/endpoint'
-import { FieldValue } from 'firebase-admin/firestore'
 import { postTweet } from 'shared/twitter'
 import { MANACHAN_TWEET_COST } from 'common/economy'
-import { createSupabaseClient } from 'shared/supabase/init'
+import { createSupabaseDirectClient } from 'shared/supabase/init'
+import { getUser } from 'shared/utils'
+import { runTxn } from 'shared/txn/run-txn'
+import { insert } from 'shared/supabase/utils'
 
 const bodySchema = z
   .object({
@@ -17,42 +16,35 @@ const bodySchema = z
 export const manachantweet = authEndpoint(async (req, auth) => {
   const { tweet } = validate(bodySchema, req.body)
 
-  const firestore = admin.firestore()
-  const db = createSupabaseClient()
+  const pg = createSupabaseDirectClient()
 
-  const user = await firestore.runTransaction(async (transaction) => {
-    const userDoc = firestore.doc(`users/${auth.uid}`)
-    const userSnap = await transaction.get(userDoc)
-    if (!userSnap.exists) throw new APIError(401, 'Your account was not found')
-    const user = userSnap.data() as User
+  const user = await getUser(auth.uid)
+  if (!user) throw new APIError(401, 'Your account was not found')
 
-    if (user.balance < MANACHAN_TWEET_COST)
-      throw new APIError(403, 'Insufficient balance')
-
-    transaction.update(userDoc, {
-      balance: FieldValue.increment(-MANACHAN_TWEET_COST),
-      totalDeposits: FieldValue.increment(-MANACHAN_TWEET_COST),
+  const result = await pg.tx(async (tx) => {
+    await runTxn(tx, {
+      category: 'MANACHAN_TWEET',
+      token: 'M$',
+      fromType: 'USER',
+      fromId: auth.uid,
+      toType: 'BANK',
+      toId: 'BANK',
+      amount: MANACHAN_TWEET_COST,
     })
 
-    return user
+    const result = await postTweet(tweet)
+
+    await insert(tx, 'manachan_tweets', {
+      user_id: auth.uid,
+      username: user.name,
+      tweet_id: result.id,
+      tweet: result.text,
+      created_time: Date.now(),
+      cost: MANACHAN_TWEET_COST,
+    })
+
+    return result
   })
-
-  const result = await postTweet(tweet)
-
-  const { error } = await db.from('manachan_tweets').insert({
-    user_id: auth.uid,
-    username: user.name,
-    tweet_id: result.id,
-    tweet: result.text,
-    created_time: Date.now(),
-    cost: MANACHAN_TWEET_COST,
-  })
-
-  if (error)
-    throw new APIError(
-      500,
-      'Tweet succeeded but to log tweet to database: ' + error.message
-    )
 
   return { success: true, tweetId: result.id }
 })
