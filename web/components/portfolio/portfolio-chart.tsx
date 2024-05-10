561c87ddb0700c694384c958acc89762bd54a346
ingawei <ingawei@gmail.com>
2024-05-10 12:39:26 -0700

    ya

--- web/components/portfolio/portfolio-chart.tsx
+++ web/components/portfolio/portfolio-chart.tsx
@@ -157,16 +157,17 @@ export const PortfolioChart = <P extends HistoryPoint>(props: {
     })
 
     unstackedPs.forEach((p, i) => {
-      if (i == SPICE_IDX && !!p.prev?.y) {
+      if (i == SPICE_IDX) {
+        console.log(p.prev?.y)
         updateGraphValues({
-          spice: p.prev.y / SPICE_TO_MANA_CONVERSION_RATE,
+          spice: (p.prev?.y ?? 0) / SPICE_TO_MANA_CONVERSION_RATE,
         })
       }
-      if (i == BALANCE_IDX && !!p.prev?.y) {
-        updateGraphValues({ balance: p.prev.y })
+      if (i == BALANCE_IDX) {
+        updateGraphValues({ balance: p.prev?.y })
       }
-      if (i == INVESTED_IDX && !!p.prev?.y) {
-        updateGraphValues({ invested: p.prev.y })
+      if (i == INVESTED_IDX) {
+        updateGraphValues({ invested: p.prev?.y })
       }
     })
 
