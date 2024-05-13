0e8520b521c2eb04c75f269d015fd217e690cb62
mantikoros <sgrugett@gmail.com>
2024-05-13 15:12:17 -0600

    remove tv

--- web/components/nav/sidebar.tsx
+++ web/components/nav/sidebar.tsx
@@ -144,16 +144,16 @@ const getDesktopNav = (
         href: '/politics',
         icon: GiCapitol,
       },
-      // {
-      //   name: 'AI',
-      //   href: '/ai',
-      //   icon: PiRobotBold,
-      // },
       {
-        name: 'TV',
-        href: '/tv',
-        icon: PiTelevisionSimpleBold,
+        name: 'AI',
+        href: '/ai',
+        icon: PiRobotBold,
       },
+      // {
+      //   name: 'TV',
+      //   href: '/tv',
+      //   icon: PiTelevisionSimpleBold,
+      // },
       {
         name: 'Messages',
         href: '/messages',
