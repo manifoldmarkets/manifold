6db124af72b329dcbef02d91b08fd5c48419019d
ingawei <ingawei@gmail.com>
2024-05-13 20:11:54 -0700

    just users no filters

--- web/components/search/user-results.tsx
+++ web/components/search/user-results.tsx
@@ -1,62 +1,60 @@
 import { FullUser } from 'common/api/user-types'
-import Link from 'next/link'
-import { FollowButton } from '../buttons/follow-button'
 import { Col } from '../layout/col'
 import { Row } from '../layout/row'
 import { Avatar } from '../widgets/avatar'
-import { StackedUserNames } from '../widgets/user-link'
+import Link from 'next/link'
+import { useState } from 'react'
+import { useIsMobile } from 'web/hooks/use-is-mobile'
 
-export const UserResults = (props: { users: FullUser[] }) => {
-  const { users } = props
+const MAX_SHOWN = 15
+const MAX_SHOWN_MOBILE = 8
 
+export function UserResults(props: { userResults: FullUser[] }) {
+  const { userResults } = props
+  const [expanded, setExpanded] = useState(false)
+  const isMobile = useIsMobile()
+  const MAX_INIT_USERS = isMobile ? MAX_SHOWN_MOBILE : MAX_SHOWN
+  const shownUsers = expanded
+    ? userResults
+    : userResults.slice(0, MAX_INIT_USERS)
   return (
-    <Col className={'mt-1 w-full gap-1'}>
-      {users.map((user) => {
-        return (
-          <Link key={user.id} href={`/${user.username}`}>
-            <UserResult user={user} />
-          </Link>
-        )
-      })}
-    </Col>
-  )
-}
-
-function UserResult(props: { user: FullUser }) {
-  const {
-    id,
-    name,
-    username,
-    avatarUrl,
-    bio,
-    createdTime,
-    isBannedFromPosting,
-  } = props.user
-
-  return (
-    <Row className={'hover:bg-primary-100 p-1'}>
-      <Col className={'w-full'}>
-        <Row className={'justify-between'}>
-          <Row className={'gap-1'}>
+    <Col className="mb-4 px-2 sm:px-0">
+      <Row className="text-ink-500 items-center gap-1 text-sm">
+        <hr className="border-ink-300 ml-2 grow sm:ml-0" />
+        <span>
+          {userResults.length} user{userResults.length > 1 && 's'}
+        </span>
+        <hr className="border-ink-300 mr-2 grow sm:mr-0" />
+      </Row>
+      <Row className="flex-wrap gap-1 text-sm">
+        {shownUsers.map((u) => (
+          <Link
+            key={u.id}
+            href={`/${u.username}`}
+            className="bg-ink-200 hover:bg-ink-300 dark:bg-canvas-0 hover:dark:bg-canvas-100 flex flex-row items-center gap-1 rounded-full py-1 pl-1 pr-2 transition-colors"
+          >
             <Avatar
-              username={username}
-              avatarUrl={avatarUrl}
-              className={'mt-1'}
+              key={u.id}
+              username={u.username}
+              avatarUrl={u.avatarUrl}
+              size="2xs"
+              className="-my-0.5 -mr-1 last:mr-0"
+              noLink
             />
-            <StackedUserNames
-              user={{ id, name, username, createdTime, isBannedFromPosting }}
-              className={'font-normal sm:text-lg'}
-              usernameClassName={'sm:text-sm font-normal'}
-            />
-          </Row>
-          <Row className="gap-1">
-            <FollowButton size={'xs'} userId={id} />
-          </Row>
-        </Row>
-        <div className={'text-ink-500 ml-1 line-clamp-2 text-sm'}>
-          <span>{bio}</span>
-        </div>
-      </Col>
-    </Row>
+            {u.username}
+          </Link>
+        ))}
+        {userResults.length > MAX_INIT_USERS && (
+          <button
+            onClick={() => setExpanded(!expanded)}
+            className="text-primary-700 bg-ink-200 hover:bg-ink-300 dark:bg-canvas-0 hover:dark:bg-canvas-100 flex flex-row items-center gap-1 rounded-full p-2 py-1"
+          >
+            {expanded
+              ? `Show less`
+              : `Show ${userResults.length - MAX_INIT_USERS} more`}
+          </button>
+        )}
+      </Row>
+    </Col>
   )
 }
