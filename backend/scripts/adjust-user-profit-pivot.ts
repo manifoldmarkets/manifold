dc9eb5c090b27e325e7b70e95b02ae6381f028a9
Ian Philips <iansphilips@gmail.com>
2024-05-16 15:28:30 -0600

    Set dpm, unranked, or unlisted profit adjustments to null

--- backend/scripts/adjust-user-profit-pivot.ts
+++ backend/scripts/adjust-user-profit-pivot.ts
@@ -1,106 +1,120 @@
 import { runScript } from './run-script'
 import { chunk } from 'lodash'
 import { log } from 'shared/utils'
-import { updateUserMetricsCore } from 'shared/update-user-metrics-core'
 const PROFIT_CUTOFF_TIME = 1715805887741
-const UNRANK_AND_ZERO_PROFITS_ONLY = true
+const DPM_CUTOFF_TIMESTAMP = '2023-08-01 18:06:58.813000 +00:00'
+const UNRANK_AND_ZERO_PROFITS = true
+const ONLY_UNRANK_AND_ZERO_PROFITS = false
+
 if (require.main === module) {
-  runScript(async ({ pg, firestore }) => {
-    const userIdsToZero = await pg.map(
-      `select distinct users.id
-      from users
-      join contract_bets cb on cb.user_id = users.id
-      join contracts on cb.contract_id = contracts.id
-        where 
-            (coalesce((contracts.data -> 'isRanked')::boolean, true) = false or
-             contracts.visibility!='public')
-        and contracts.resolution_time is not null
-        and contracts.resolution_time <= millis_to_ts($1)
-        and contracts.deleted = false
-      `,
-      [PROFIT_CUTOFF_TIME],
-      (row) => row.id
-    )
-    log(`Found ${userIdsToZero.length} users to update`)
-    const chunksToZero = chunk(userIdsToZero, 500)
-    let totalProcessed = 0
-    for (const chunk of chunksToZero) {
-      await Promise.all(
-        chunk.map(async (userId) => {
-          await pg.none(
-            `
-                with filtered_metrics as (
-                    select ucm.profit, ucm.id as ucm_id
+  runScript(async ({ pg }) => {
+    const chunkSize = 100
+    if (UNRANK_AND_ZERO_PROFITS) {
+      const userIdsToZero = await pg.map(
+        `select distinct users.id
+         from users
+          join contract_bets cb on cb.user_id = users.id
+          join contracts on cb.contract_id = contracts.id
+         where (
+             -- unranked
+             coalesce((contracts.data -> 'isRanked')::boolean, true) = false or
+             -- or unlisted
+             contracts.visibility != 'public' or
+             -- or formerly dpm
+              (contracts.mechanism = 'cpmm-multi-1' and contracts.created_time < $2::timestamptz)
+             )
+           and contracts.resolution_time is not null
+           and contracts.resolution_time <= millis_to_ts($1)
+        `,
+        [PROFIT_CUTOFF_TIME, DPM_CUTOFF_TIMESTAMP],
+        (row) => row.id
+      )
+      log(`Found ${userIdsToZero.length} users to update`)
+      const chunksToZero = chunk(userIdsToZero, chunkSize)
+      let totalProcessed = 0
+      for (const chunk of chunksToZero) {
+        await Promise.all(
+          chunk.map(async (userId) => {
+            await pg.none(
+              `
+                  with filtered_metrics as (select ucm.profit, ucm.id as ucm_id
                     from user_contract_metrics ucm
                     join contracts on contracts.id = ucm.contract_id
-                    where ucm.user_id = $1
-                      and contracts.resolution_time is not null
-                      and contracts.resolution_time <= millis_to_ts($2)
-                      and contracts.deleted = false
+                      where ucm.user_id = $1
                       and ucm.answer_id is null
-                      and (coalesce((contracts.data -> 'isRanked')::boolean, true) = false or
-                            contracts.visibility!='public')
-                )
-                update user_contract_metrics
-                set profit_adjustment = null
-                from filtered_metrics
-                where user_contract_metrics.id = filtered_metrics.ucm_id;
-            `,
-            [userId, PROFIT_CUTOFF_TIME]
-          )
-          await pg.none(
-            `update users
-             set profit_adjustment = (
-                 select sum(ucm.profit_adjustment)
-                 from user_contract_metrics ucm
-                 where ucm.user_id = users.id
-             )
-             where id = $1`,
-            [userId]
-          )
-        })
-      )
-      totalProcessed += chunk.length
-      log(
-        `Updated ${chunk.length} users, total ${totalProcessed} users updated`
-      )
-    }
-
-    if (UNRANK_AND_ZERO_PROFITS_ONLY) {
-      return
+                      and (
+                        -- unranked    
+                       coalesce((contracts.data -> 'isRanked')::boolean, true) = false or
+                       -- or unlisted
+                       contracts.visibility != 'public' or
+                       -- or formerly dpm
+                        (contracts.mechanism = 'cpmm-multi-1' and contracts.created_time < $3::timestamptz)
+                       )
+                     and contracts.resolution_time is not null
+                     and contracts.resolution_time <= millis_to_ts($2)
+                    )
+                  update user_contract_metrics
+                  set profit_adjustment = null
+                  from filtered_metrics
+                  where user_contract_metrics.id = filtered_metrics.ucm_id;
+              `,
+              [userId, PROFIT_CUTOFF_TIME, DPM_CUTOFF_TIMESTAMP]
+            )
+            await pg.none(
+              `update users
+               set profit_adjustment = (select sum(ucm.profit_adjustment)
+                                        from user_contract_metrics ucm
+                                        where ucm.user_id = users.id)
+               where id = $1`,
+              [userId]
+            )
+          })
+        )
+        totalProcessed += chunk.length
+        log(
+          `Updated ${chunk.length} users, total ${totalProcessed} users updated`
+        )
+      }
+      if (ONLY_UNRANK_AND_ZERO_PROFITS)
+        return log('Only unranked and zero profit users updated')
     }
 
     log('Computing non-zero profit adjustment for users')
-    // Compute dpm-converts only
+
+    // Recalculate dpm-convert contract metrics
+    // const userIdsToUpdate = await pg.map(
+    //   `select distinct users.id
+    //   from users
+    //   join contract_bets cb on cb.user_id = users.id
+    //   join contracts c on cb.contract_id = c.id
+    //   where
+    //      c.mechanism = 'cpmm-multi-1' and c.created_time < $2::timestamptz
+    //     and c.resolution_time is not null
+    //     and c.resolution_time <= millis_to_ts($1)
+    //     and c.deleted = false
+    //   `,
+    //   [PROFIT_CUTOFF_TIME, DPM_CUTOFF_TIMESTAMP],
+    //   (row) => row.id
+    // )
+    // let i = 0
+    // while (i < userIdsToUpdate.length / chunkSize) {
+    //   await updateUserMetricsCore(
+    //     userIdsToUpdate.slice(i * chunkSize, (i + 1) * chunkSize)
+    //   )
+    //   i++
+    //   console.log(`Updated ${i} times`)
+    // }
+
+    // Calculate all profit adjustments
     const userIdsToUpdate = await pg.map(
-      `select distinct users.id
-      from users
-      join contract_bets cb on cb.user_id = users.id
-      join contracts c on cb.contract_id = c.id
-      where c.mechanism = 'cpmm-multi-1' and c.created_time < '2023-08-01 18:06:58.813000 +00:00'::timestamptz
-      `,
+      `
+        select id from users where
+        data->>'lastBetTime' is not null
+        `,
       [],
       (row) => row.id
     )
-    let i = 0
-    while (i < userIdsToUpdate.length / 1000) {
-      await updateUserMetricsCore(
-        userIdsToUpdate.slice(i * 1000, (i + 1) * 1000)
-      )
-      i++
-      console.log(`Updated ${i} times`)
-    }
-    // Compute all profit adjustments
-    // const userIds = await pg.map(
-    //   `
-    //               select id from users where
-    //     (data->'profitCached'->'allTime')::numeric !=0 and
-    //     not coalesce((data->'isBannedFromPosting')::boolean,false) = true
-    //     `,
-    //   [],
-    //   (row) => row.id
-    // )
-    const chunks = chunk(userIdsToUpdate, 500)
+    const chunks = chunk(userIdsToUpdate, chunkSize)
     let total = 0
     for (const chunk of chunks) {
       await Promise.all(
@@ -118,21 +132,28 @@ if (require.main === module) {
                       and ucm.answer_id is null
                       and (ucm.profit != 0 or ucm.profit_adjustment is not null) 
                       and coalesce((contracts.data -> 'isRanked')::boolean, true) = true
+                      and contracts.visibility = 'public'
+                      and (contracts.mechanism != 'cpmm-multi-1' or contracts.created_time > $3::timestamptz)
                 )
                 update user_contract_metrics
                 set profit_adjustment = filtered_metrics.profit * 9
                 from filtered_metrics
                 where user_contract_metrics.id = filtered_metrics.ucm_id;
             `,
-            [userId, PROFIT_CUTOFF_TIME]
+            [userId, PROFIT_CUTOFF_TIME, DPM_CUTOFF_TIMESTAMP]
           )
           await pg.none(
             `update users
              set profit_adjustment = (
-                 select sum(ucm.profit_adjustment)
-                 from user_contract_metrics ucm
-                 where ucm.user_id = users.id
-             )
+                 case
+                 when users.data->>'isBannedFromPosting' = 'true' then 0
+                 else (
+                     select sum(ucm.profit_adjustment)
+                     from user_contract_metrics ucm
+                     where ucm.user_id = users.id
+                 )
+                 end
+            )
              where id = $1`,
             [userId]
           )
