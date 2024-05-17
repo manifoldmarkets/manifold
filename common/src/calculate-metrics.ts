0c2b9d99d0ded03e6451c4eec39069c14267c12a
mantikoros <sgrugett@gmail.com>
2024-05-17 15:50:13 -0700

    adjust elasticity

--- common/src/calculate-metrics.ts
+++ common/src/calculate-metrics.ts
@@ -65,7 +65,7 @@ const getLoanTotal = (
   })
 }
 
-export const ELASTICITY_BET_AMOUNT = 100
+export const ELASTICITY_BET_AMOUNT = 1000 // readjust with platform volume
 
 export const computeElasticity = (
   unfilledBets: LimitBet[],
