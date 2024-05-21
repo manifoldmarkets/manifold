12a0b926da1a6d1e09e3d1228e074b359d96a54c
Ian Philips <iansphilips@gmail.com>
2024-05-21 15:07:14 -0600

    Use bulk writer to backfill

--- backend/scripts/backfill-answer-resolution-times.ts
+++ backend/scripts/backfill-answer-resolution-times.ts
@@ -1,13 +1,16 @@
 import { runScript } from 'run-script'
 import { tsToMillis } from 'common/supabase/utils'
 import { removeUndefinedProps } from 'common/util/object'
+import { SafeBulkWriter } from 'shared/safe-bulk-writer'
+import { log } from 'shared/monitoring/log'
 
 if (require.main === module) {
   runScript(async ({ firestore, pg }) => {
     const resolvedAnswers = await pg.map(
       `select answers.id, answers.contract_id, contracts.resolution_time, contracts.data->>'resolverId' as resolver_id from answers
              join contracts on answers.contract_id = contracts.id
-       where resolution_time is not null
+         where resolution_time is not null
+         and answers.data->>'resolutionTime' is null
          and mechanism = 'cpmm-multi-1'
       `,
       [],
@@ -20,17 +23,23 @@ if (require.main === module) {
     )
     const toUpdate = resolvedAnswers.length
     let total = 0
+    const writer = new SafeBulkWriter(undefined, firestore)
     for (const answer of resolvedAnswers) {
-      await firestore
-        .doc(`contracts/${answer.contractId}/answersCpmm/${answer.id}`)
-        .update(
-          removeUndefinedProps({
-            resolutionTime: answer.resolutionTime,
-            resolverId: answer.resolverId,
-          })
-        )
+      writer.update(
+        firestore.doc(
+          `contracts/${answer.contractId}/answersCpmm/${answer.id}`
+        ),
+        removeUndefinedProps({
+          resolutionTime: answer.resolutionTime,
+          resolverId: answer.resolverId,
+        })
+      )
       total++
       console.log(`Updated ${total}/${toUpdate} answers`)
     }
+    await writer
+      .close()
+      .catch((e) => log.error('Error bulk writing answer updates', e))
+      .then(() => log('Committed Firestore writes.'))
   })
 }
