d1df369a467f541c0176cef30d2d0ae49898509a
Sinclair Chen <abc.sinclair@gmail.com>
2024-05-24 14:46:12 -0700

    live update liquidity with websocket not firebase

--- web/hooks/use-liquidity.ts
+++ web/hooks/use-liquidity.ts
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react'
 import { LiquidityProvision } from 'common/liquidity-provision'
-import { listenForLiquidity } from 'web/lib/firebase/liquidity'
+import { useEffect, useState } from 'react'
+import { getLiquidtyDocs } from 'web/lib/supabase/liquidity'
+import { useApiSubscription } from './use-api-subscription'
 
 export const useLiquidity = (contractId: string) => {
   const [liquidities, setLiquidities] = useState<
@@ -8,8 +9,15 @@ export const useLiquidity = (contractId: string) => {
   >(undefined)
 
   useEffect(() => {
-    return listenForLiquidity(contractId, setLiquidities)
+    getLiquidtyDocs(contractId).then(setLiquidities)
   }, [contractId])
+
+  useApiSubscription({
+    topics: [`contract/${contractId}/new-subsidy`],
+    onBroadcast: () => {
+      getLiquidtyDocs(contractId).then(setLiquidities)
+    },
+  })
 
   return liquidities
 }
