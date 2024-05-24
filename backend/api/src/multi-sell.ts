fbe60c6095c5b86c84d8c35badb70536ce48f195
James Grugett <jahooma@gmail.com>
2024-05-23 17:38:01 -0700

    Revert "Auto-remove unused imports"
    This reverts commit 3b4ca99e8d124d59c15b3302c2fb0c1f31c31ca0.

--- backend/api/src/multi-sell.ts
+++ backend/api/src/multi-sell.ts
@@ -1,6 +1,18 @@
+import * as admin from 'firebase-admin'
 import { APIError, type APIHandler } from './helpers/endpoint'
-
-
+import { Answer } from 'common/answer'
+import { onCreateBets } from 'api/on-create-bet'
+import {
+  getUnfilledBetsAndUserBalances,
+  processNewBetResult,
+} from 'api/place-bet'
+import { getContractSupabase, getUser, log } from 'shared/utils'
+import * as crypto from 'crypto'
+import { groupBy, mapValues, sum, sumBy } from 'lodash'
+import { getCpmmMultiSellSharesInfo } from 'common/sell-bet'
+import { incrementBalance } from 'shared/supabase/users'
+import { runEvilTransaction } from 'shared/evil-transaction'
+import { convertBet } from 'common/supabase/bets'
 
 export const multiSell: APIHandler<'multi-sell'> = async (props, auth) => {
   throw new APIError(500, 'This endpoint is disabled.')
--- backend/api/src/sell-shares.ts
+++ backend/api/src/sell-shares.ts
@@ -1,4 +1,22 @@
+import { mapValues, groupBy, sumBy } from 'lodash'
+import * as admin from 'firebase-admin'
 import { APIError, type APIHandler } from './helpers/endpoint'
+import { Contract, CPMM_MIN_POOL_QTY } from 'common/contract'
+import { getCpmmMultiSellBetInfo, getCpmmSellBetInfo } from 'common/sell-bet'
+import { removeUndefinedProps } from 'common/util/object'
+import { floatingEqual, floatingLesserEqual } from 'common/util/math'
+import { getUnfilledBetsAndUserBalances, updateMakers } from './place-bet'
+import { removeUserFromContractFollowers } from 'shared/follow-market'
+import { Answer } from 'common/answer'
+import { getCpmmProbability } from 'common/calculate-cpmm'
+import { onCreateBets } from 'api/on-create-bet'
+import { getUser, log } from 'shared/utils'
+import * as crypto from 'crypto'
+import { formatMoneyWithDecimals } from 'common/util/format'
+import { incrementBalance } from 'shared/supabase/users'
+import { runEvilTransaction } from 'shared/evil-transaction'
+import { cancelLimitOrders, insertBet } from 'shared/supabase/bets'
+import { convertBet } from 'common/supabase/bets'
 
 export const sellShares: APIHandler<'market/:contractId/sell'> = async (
   props,
