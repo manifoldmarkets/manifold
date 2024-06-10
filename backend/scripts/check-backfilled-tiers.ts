68f25482c567cac1a7d86dda4d6a2ae00a23223d
ingawei <ingawei@gmail.com>
2024-06-10 14:09:50 -0700

    scriptz

--- backend/scripts/check-backfilled-tiers.ts
+++ backend/scripts/check-backfilled-tiers.ts
@@ -12,24 +12,18 @@ if (require.main === module) {
   const contractsRef = firestore.collection('contracts')
   contractsRef.get().then(async (contractsSnaps) => {
     console.log(`Loaded ${contractsSnaps.size} contracts.`)
-    const needsFilling = contractsSnaps.docs.filter((ct) => {
-      return (
-        !('marketTier' in ct.data()) &&
-        'totalLiquidity' in ct.data()
-      )
+    const shouldCheck = contractsSnaps.docs.filter((ct) => {
+      return 'totalLiquidity' in ct.data() && ct.data().isResolved
     })
-    console.log(`Found ${needsFilling.length} contracts to update.`)
+    console.log(`Found ${shouldCheck.length} contracts to check.`)
     await Promise.all(
-      needsFilling.map((ct) => {
-        ct.ref.update({
-          marketTier: getTierFromLiquidity(
-            ct.data() as MarketContract,
-            ct.data().totalLiquidity
-          ),
-        })
+      shouldCheck.map((ct) => {
         console.log(ct.data().marketTier, ct.data().totalLiquidity)
+        if (ct.data().answers) {
+          console.log(ct.data().answers.length)
+        }
       })
     )
-    console.log(`Updated all contracts.`)
+    console.log(`Checked all contracts`)
   })
 }
