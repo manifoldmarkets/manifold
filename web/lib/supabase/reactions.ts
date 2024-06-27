4430b1d13e976ee70ac9818c2f60257a4204e35d
James Grugett <jahooma@gmail.com>
2024-06-26 21:06:19 -0700

    Move some files out of firebase directory

--- web/lib/supabase/reactions.ts
+++ web/lib/supabase/reactions.ts
@@ -1,5 +1,30 @@
 import { run } from 'common/supabase/utils'
 import { db } from 'web/lib/supabase/db'
+import { api } from '../api/api'
+import { ReactionContentTypes } from 'common/reaction'
+
+export const unLike = async (
+  contentId: string,
+  contentType: ReactionContentTypes
+) => {
+  api('react', {
+    remove: true,
+    contentId,
+    contentType,
+  })
+}
+
+export const like = async (
+  contentId: string,
+  contentType: ReactionContentTypes
+) => {
+  api('react', {
+    remove: false,
+    contentId,
+    contentType,
+  })
+}
+
 
 export async function getLikedContracts(userId: string) {
   // TODO: The best way to do this would be to join the matching table via contentId and type
