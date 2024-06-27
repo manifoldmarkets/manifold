1fcdb07ed567b727a05ea4291b2e8ce4ae73b9e6
Sinclair Chen <abc.sinclair@gmail.com>
2024-06-27 14:16:21 -0700

    Make reset betting single query and not in mem (#2685)

--- backend/functions/src/scheduled/reset-betting-streaks.ts
+++ backend/functions/src/scheduled/reset-betting-streaks.ts
@@ -1,19 +1,11 @@
 // check every day if the user has created a bet since 4pm UTC, and if not, reset their streak
-
-import * as functions from 'firebase-functions'
-import { User } from 'common/user'
-import { DAY_MS } from 'common/util/time'
 import { BETTING_STREAK_RESET_HOUR } from 'common/economy'
-import { updateUser } from 'shared/supabase/users'
-import {
-  SupabaseDirectClient,
-  createSupabaseDirectClient,
-} from 'shared/supabase/init'
-import { convertUser } from 'common/supabase/users'
 import { secrets } from 'common/secrets'
+import * as functions from 'firebase-functions'
+import { createSupabaseDirectClient } from 'shared/supabase/init'
 
 export const resetBettingStreaksForUsers = functions
-  .runWith({ timeoutSeconds: 540, memory: '4GB', secrets })
+  .runWith({ timeoutSeconds: 540, secrets })
   .pubsub.schedule(`0 ${BETTING_STREAK_RESET_HOUR} * * *`)
   .timeZone('Etc/UTC')
   .onRun(async () => {
@@ -23,40 +15,15 @@ export const resetBettingStreaksForUsers = functions
 const resetBettingStreaksInternal = async () => {
   const pg = createSupabaseDirectClient()
 
-  const users = await pg.map(
-    `select * from users
-    where (data->'currentBettingStreak')::numeric > 0`,
-    [],
-    convertUser
+  await pg.none(
+    `update users
+    set data = data || 
+      case when (data->'streakForgiveness')::numeric > 0 then
+        jsonb_build_object('streakForgiveness', (data->'streakForgiveness')::numeric - 1)
+      else
+        jsonb_build_object('currentBettingStreak', 0)
+      end
+    where (data->'currentBettingStreak')::numeric > 0
+    and (data->'lastBetTime')::numeric < ts_to_millis(now() - interval '1 day')`
   )
-
-  const betStreakResetTime = Date.now() - DAY_MS
-  await Promise.all(
-    users.map((user) => resetBettingStreakForUser(pg, user, betStreakResetTime))
-  )
-}
-
-const resetBettingStreakForUser = async (
-  db: SupabaseDirectClient,
-  user: User,
-  betStreakResetTime: number
-) => {
-  // if they made a bet within the last day, don't reset their streak
-  if (
-    (user?.lastBetTime ?? 0) > betStreakResetTime ||
-    !user.currentBettingStreak ||
-    user.currentBettingStreak === 0
-  )
-    return
-
-  if (user.streakForgiveness > 0) {
-    await updateUser(db, user.id, {
-      streakForgiveness: user.streakForgiveness - 1,
-    })
-    // Should we send a notification to the user?
-  } else {
-    await updateUser(db, user.id, {
-      currentBettingStreak: 0,
-    })
-  }
 }
