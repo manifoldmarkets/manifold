0762254603ebae13084599ed5d2782072285bec7
mantikoros <sgrugett@gmail.com>
2024-06-28 13:56:04 -0700

    sidebar: remove tv

--- web/components/nav/sidebar.tsx
+++ web/components/nav/sidebar.tsx
@@ -157,11 +157,11 @@ const getDesktopNav = (
       //   href: '/ai',
       //   icon: PiRobotBold,
       // },
-      {
-        name: 'TV',
-        href: '/tv',
-        icon: PiTelevisionSimpleBold,
-      },
+      // {
+      //   name: 'TV',
+      //   href: '/tv',
+      //   icon: PiTelevisionSimpleBold,
+      // },
       {
         name: 'Messages',
         href: '/messages',
