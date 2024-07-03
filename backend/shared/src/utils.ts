5ec90ed0890cdafd6ba01bdb262280af540d6887
Sinclair Chen <abc.sinclair@gmail.com>
2024-07-02 19:19:44 -0700

    Make api watch faster

--- backend/shared/src/utils.ts
+++ backend/shared/src/utils.ts
@@ -18,11 +18,11 @@ import {
 import { convertPrivateUser, convertUser } from 'common/supabase/users'
 import { convertContract } from 'common/supabase/contracts'
 import { Row } from 'common/supabase/utils'
-import { log, Logger } from 'shared/monitoring/log'
+import { log } from 'shared/monitoring/log'
 import { metrics } from 'shared/monitoring/metrics'
 
 export { metrics }
-export { log, Logger }
+export { log }
 
 export const logMemory = () => {
   const used = process.memoryUsage()
