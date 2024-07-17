80591dbd0d060beb367437c9a70187074dce88fa
sipec <sipec@users.noreply.github.com>
2024-07-17 18:52:12 +0000

    Auto-remove unused imports

--- web/lib/supabase/stats.ts
+++ web/lib/supabase/stats.ts
@@ -1,4 +1,4 @@
-import { Row, run } from 'common/supabase/utils'
+import { run } from 'common/supabase/utils'
 import { db } from './db'
 
 // export type Stats = { [key in keyof Row<'daily_stats'>]: Row<'daily_stats'>[key][] }
