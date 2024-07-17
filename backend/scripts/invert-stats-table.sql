d7bf55cd9773337a5fd6015cbc69cdf1778ba221
Sinclair Chen <abc.sinclair@gmail.com>
2024-07-17 11:20:43 -0700

    Simplify create table script

--- backend/scripts/invert-stats-table.sql
+++ backend/scripts/invert-stats-table.sql
@@ -1,19 +1,21 @@
-create table daily_stats as
-WITH unnested AS (
-  SELECT 
-    title,
-    unnest(daily_values) AS value,
-    generate_series(0, array_length(daily_values, 1)) AS day_number
-  FROM stats
-),
-days AS (
-  SELECT generate_series(
-    (SELECT MIN(day_number) FROM unnested),
-    (SELECT MAX(day_number) FROM unnested)
-  ) AS day_number
-)
-SELECT 
-  days.day_number AS day,
+-- select millis_to_ts(daily_values[1]::bigint)::date + 365 as today
+-- from stats
+-- where title = 'startDate';
+-- result: 2024-07-16
+
+create table
+  daily_stats as
+with
+  days as (
+    select
+      title,
+      unnest(daily_values) as value,
+      generate_series(array_length(daily_values, 1) - 1, 0, -1) as days_ago
+    from
+      stats
+  )
+select
+ '2024-07-16'::date - days_ago::integer - 1 as start_date,
   max(case when title = 'dailyActiveUsers' then value end) as dau,
   max(case when title = 'weeklyActiveUsers' then value end) as wau,
   max(case when title = 'monthlyActiveUsers' then value end) as mau,
@@ -24,7 +26,7 @@ SELECT
   max(case when title = 'nd1' then value end) as nd1,
   max(case when title = 'nw1' then value end) as nw1,
   max(case when title = 'fracDaysActiveD1ToD3' then value end) as active_d1_to_d3,
-  max(case when title = 'dailyBetCounts' then value end) bet_count,
+  max(case when title = 'dailyBetCounts' then value end) as bet_count,
   max(case when title = 'dailyContractCounts' then value end) as contract_count,
   max(case when title = 'dailyCommentCounts' then value end) as comment_count,
   max(case when title = 'dailySignups' then value end) as signups,
@@ -36,29 +38,19 @@ SELECT
   max(case when title = 'd1BetAverage' then value end) as d1_bet_average,
   max(case when title = 'd1Bet3DayAverage' then value end) as d1_bet_3_day_average,
   max(case when title = 'feedConversionScores' then value end) as feed_conversion
-FROM 
+from
   days
-LEFT JOIN unnested ON days.day_number = unnested.day_number
-GROUP BY days.day_number
-ORDER BY days.day_number;
-
--- put finishing touches on table
-
-alter table daily_stats
-add column start_date date;
-
--- select (daily_values) from stats
--- where title = 'startDate'
--- > 1688886000000
-update daily_stats
-set start_date = date (millis_to_ts (1688886000000)) + day;
-
-alter table daily_stats
-drop column day;
+group by
+  days.days_ago
+order by
+  days.days_ago asc;
 
 alter table daily_stats
 add primary key (start_date),
 alter column start_date set not null;
 
-create policy "public read" on daily_stats
-  for select using (true);
+alter table daily_stats enable row level security;
+
+create policy "public read" on daily_stats for
+select
+  using (true);
