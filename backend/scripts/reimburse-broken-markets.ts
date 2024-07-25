8da5d01492e745a26c7533e90b8996d47a6754d5
Sinclair Chen <abc.sinclair@gmail.com>
2024-07-25 14:37:37 -0700

    Include indie multi which have added answers
    okay, so if you add answers to an indie question, the new answers will
    insert liquidity docs with isAnte = true. to remedy we find answers with
    no liquidity, find their contracts, and of the indie ones we generate
    ante liquidity docs

--- backend/scripts/reimburse-broken-markets.ts
+++ backend/scripts/reimburse-broken-markets.ts
@@ -5,9 +5,10 @@ import { generateAntes } from 'api/create-market'
 import { Contract } from 'common/contract'
 import { Answer } from 'common/answer'
 import { log } from 'shared/utils'
+import { uniqBy } from 'lodash'
 
 runScript(async ({ pg }) => {
-  const contractAntes = await pg.many<{
+  const contractAntes = await pg.manyOrNone<{
     amount: number
     user_id: string
     contract_id: string
@@ -39,7 +40,54 @@ runScript(async ({ pg }) => {
     and t.category = 'CREATE_CONTRACT_ANTE'`
   )
 
-  for (const c of contractAntes) {
+  // some contracts have answers added later which do have the correct ante liquidities
+  const answerAntes = await pg.manyOrNone<{
+    amount: number
+    user_id: string
+    contract_id: string
+    question: string
+    slug: string
+    resolution: string | null
+    outcome_type: 'BINARY' | 'MULTIPLE_CHOICE'
+    sums_to_one: boolean | null
+    answers: Answer[] | null
+  }>(
+    `with affected_answers as (
+      select a.id, a.contract_id
+      from answers a
+      left join (select * from contract_liquidity where (data->'isAnte')::boolean = true) l
+      on a.id = l.data->>'answerId'
+      where a.id is not null
+      and l.liquidity_id is null
+      and a.created_time > '2024-07-23 16:46-0800'
+    ),
+    affected_contracts as (
+      select distinct contract_id from affected_answers
+    )
+    select
+      t.amount,
+      t.from_id as user_id,
+      c.id as contract_id,
+      c.question,
+      c.slug,
+      c.resolution,
+      c.outcome_type,
+      c.data->'shouldAnswersSumToOne' as sums_to_one,
+      c.data->'answers' as answers
+    from contracts c
+    join txns t
+    on c.id = t.to_id
+    where c.id in (select * from affected_contracts)
+    and (c.data->'shouldAnswersSumToOne')::boolean = false
+    and t.category = 'CREATE_CONTRACT_ANTE'`
+  )
+
+  log(
+    `found ${contractAntes.length} contracts and ${answerAntes.length} indie multi contracts`
+  )
+
+  const allAntes = uniqBy([...contractAntes, ...answerAntes], 'contract_id')
+  for (const c of allAntes) {
     log(`fixing [${c.question}](${c.slug})`)
 
     await pg.tx(async (tx) => {
