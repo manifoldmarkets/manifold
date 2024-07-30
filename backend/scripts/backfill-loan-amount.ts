14c8bb66b630f115ecde95d5647b640f81bbf8f3
Ian Philips <iansphilips@gmail.com>
2024-07-29 18:39:43 -0600

    update loan_amounts via bet_id

--- backend/scripts/backfill-loan-amount.ts
+++ backend/scripts/backfill-loan-amount.ts
@@ -20,37 +20,24 @@ if (require.main === module) {
 
   runScript(async () => {
     let retries = 0
-    let converted = 0
     const runBackfill = async () => {
       const pg = createSupabaseDirectClient()
 
-      while (true) {
-        log(`fetching bets to convert`)
-        const betIds = await pg.timeout(argv.timeout, (t) =>
-          t.map(
-            `select bet_id
-       from contract_bets
-       where loan_amount is null
-       and data->>'loanAmount' is not null
-       limit 5000`,
-            [converted],
-            (row) => row.bet_id as string
+      for (const range of ranges) {
+        log(`Processing range ${range.start} to ${range.end}`)
+        await pg.timeout(argv.timeout, async (t) => {
+          await t.none(
+            // Works bc contract_bet_populate_cols reads from the data blob and sets the column
+            `update contract_bets
+             set bet_id = bet_id
+             where bet_id >= $1
+               and ($2 is null or bet_id < $2)
+               and loan_amount is null
+               and data ->> 'loanAmount' is not null`,
+            [range.start, range.end]
           )
-        )
-        if (betIds.length < 1) {
-          log(`no more bets to convert`)
-          break
-        }
-        log(`converting bets`, betIds.length)
-        await pg.none(
-          `update contract_bets
-       set loan_amount = (data ->> 'loanAmount')::numeric
-       where bet_id in ($1:list)`,
-          [betIds]
-        )
-        converted += betIds.length
-        log(`converted ${betIds.length} bets`)
-        log(`total converted ${converted} bets`)
+          log(`Converted ${range.start} to ${range.end}`)
+        })
       }
     }
 
@@ -71,3 +58,68 @@ if (require.main === module) {
     }
   })
 }
+
+const ranges = [
+  { start: '0', end: '1' },
+  { start: '1', end: '2' },
+  { start: '2', end: '3' },
+  { start: '3', end: '4' },
+  { start: '4', end: '5' },
+  { start: '5', end: '6' },
+  { start: '6', end: '7' },
+  { start: '7', end: '8' },
+  { start: '8', end: '9' },
+  { start: '9', end: 'A' },
+  { start: 'A', end: 'B' },
+  { start: 'B', end: 'C' },
+  { start: 'C', end: 'D' },
+  { start: 'D', end: 'E' },
+  { start: 'E', end: 'F' },
+  { start: 'F', end: 'G' },
+  { start: 'G', end: 'H' },
+  { start: 'H', end: 'I' },
+  { start: 'I', end: 'J' },
+  { start: 'J', end: 'K' },
+  { start: 'K', end: 'L' },
+  { start: 'L', end: 'M' },
+  { start: 'M', end: 'N' },
+  { start: 'N', end: 'O' },
+  { start: 'O', end: 'P' },
+  { start: 'P', end: 'Q' },
+  { start: 'Q', end: 'R' },
+  { start: 'R', end: 'S' },
+  { start: 'S', end: 'T' },
+  { start: 'T', end: 'U' },
+  { start: 'U', end: 'V' },
+  { start: 'V', end: 'W' },
+  { start: 'W', end: 'X' },
+  { start: 'X', end: 'Y' },
+  { start: 'Y', end: 'Z' },
+  { start: 'Z', end: 'a' },
+  { start: 'a', end: 'b' },
+  { start: 'b', end: 'c' },
+  { start: 'c', end: 'd' },
+  { start: 'd', end: 'e' },
+  { start: 'e', end: 'f' },
+  { start: 'f', end: 'g' },
+  { start: 'g', end: 'h' },
+  { start: 'h', end: 'i' },
+  { start: 'i', end: 'j' },
+  { start: 'j', end: 'k' },
+  { start: 'k', end: 'l' },
+  { start: 'l', end: 'm' },
+  { start: 'm', end: 'n' },
+  { start: 'n', end: 'o' },
+  { start: 'o', end: 'p' },
+  { start: 'p', end: 'q' },
+  { start: 'q', end: 'r' },
+  { start: 'r', end: 's' },
+  { start: 's', end: 't' },
+  { start: 't', end: 'u' },
+  { start: 'u', end: 'v' },
+  { start: 'v', end: 'w' },
+  { start: 'w', end: 'x' },
+  { start: 'x', end: 'y' },
+  { start: 'y', end: 'z' },
+  { start: 'z', end: null },
+]
