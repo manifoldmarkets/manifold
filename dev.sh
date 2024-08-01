8b7ef7e779a4b4a31439cec5c8db98c0498ff19f
Ian Philips <iansphilips@gmail.com>
2024-08-01 16:11:59 -0700

    Add debug option for debugger

--- dev.sh
+++ dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ENV=${1:-dev}
-PROJECT=$2
+DEBUG=${2:-false}
 case $ENV in
     dev)
       FIREBASE_PROJECT=dev
@@ -16,11 +16,16 @@ esac
 
 firebase use $FIREBASE_PROJECT
 
+API_COMMAND="dev"
+if [ "$DEBUG" = "true" ]; then
+    API_COMMAND="debug"
+fi
+
 npx concurrently \
     -n API,NEXT,TS \
     -c white,magenta,cyan \
     "cross-env NEXT_PUBLIC_FIREBASE_ENV=${NEXT_ENV} \
-                      yarn --cwd=backend/api dev" \
+                      yarn --cwd=backend/api $API_COMMAND" \
     "cross-env NEXT_PUBLIC_API_URL=localhost:8088 \
               NEXT_PUBLIC_FIREBASE_ENV=${NEXT_ENV} \
               yarn --cwd=web serve" \
