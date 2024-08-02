5fe984106d59b9f23f3688f9537311f0623b1756
Ian Philips <iansphilips@gmail.com>
2024-08-02 11:18:35 -0700

    Chunk by created_time

--- backend/scripts/migrate-data-from-contract-bets.ts
+++ backend/scripts/migrate-data-from-contract-bets.ts
@@ -1,27 +1,36 @@
 import { runScript } from 'run-script'
 import { log } from 'shared/utils'
 import { createSupabaseDirectClient } from 'shared/supabase/init'
-import { ranges } from './backfill-loan-amount'
 
 if (require.main === module) {
   runScript(async () => {
     const pg = createSupabaseDirectClient()
-    for (const range of ranges) {
-      log(`Processing range ${range.start} to ${range.end}`)
+    const startDate = new Date('2023-01-12')
+    const endDate = new Date()
+    const diff = 2
+    while (startDate < endDate) {
+      const periodStart = new Date(startDate)
+      periodStart.setDate(periodStart.getDate() + diff)
+
+      log(
+        `Processing from ${startDate.toISOString()} to ${periodStart.toISOString()}`
+      )
+
       await pg.task(async (t) => {
         await t.none(
-          // Works bc contract_bet_populate_cols reads from the data blob and sets the column
           `update contract_bets
              set bet_id = bet_id
-             where bet_id >= $1
-               and ($2 is null or bet_id < $2)
-               and (is_filled is null and data ->> 'isFilled' is not null) or
-                  (is_cancelled is null and data ->> 'isCancelled' is not null)
-               `,
-          [range.start, range.end]
+             where created_time >= $1
+               and created_time < $2
+          `,
+          [startDate.toISOString(), periodStart.toISOString()]
         )
-        log(`Converted ${range.start} to ${range.end}`)
+        log(
+          `Converted from ${startDate.toISOString()} to ${periodStart.toISOString()}`
+        )
       })
+
+      startDate.setDate(startDate.getDate() + diff)
     }
   })
 }
