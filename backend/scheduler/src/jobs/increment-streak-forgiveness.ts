9cabf8bb09d6e187d6518d171f5be33f0dd9bda4
Ian Philips <iansphilips@gmail.com>
2024-08-07 13:10:26 -0700

    Increment streak forgiveness partitioned by created_time

--- backend/scheduler/src/jobs/increment-streak-forgiveness.ts
+++ backend/scheduler/src/jobs/increment-streak-forgiveness.ts
@@ -1,11 +1,43 @@
 import { createSupabaseDirectClient } from 'shared/supabase/init'
+import { DAY_MS } from 'common/util/time'
+import { log } from 'shared/utils'
+
+const bulkUpdateUsersByChunk = async (
+  pg: ReturnType<typeof createSupabaseDirectClient>,
+  daysInterval: number
+) => {
+  const earliestUserResult = await pg.one(`
+    select min(created_time) as earliest_time
+    from users
+  `)
+  const earliestTime = earliestUserResult.earliest_time
+
+  const now = new Date()
+  let chunkStart = new Date(earliestTime)
+  let chunkEnd = new Date(chunkStart.getTime() + daysInterval * DAY_MS)
+
+  while (chunkStart < now) {
+    await pg.none(
+      `
+      update users
+      set data = data || jsonb_build_object('streakForgiveness', coalesce((data->>'streakForgiveness')::numeric, 0) + 1)
+      where created_time >= $1 and created_time < $2
+    `,
+      [chunkStart.toISOString(), chunkEnd.toISOString()]
+    )
+    log(
+      'Updated streak forgiveness for users created between',
+      chunkStart,
+      'and',
+      chunkEnd
+    )
+
+    chunkStart = chunkEnd
+    chunkEnd = new Date(chunkStart.getTime() + daysInterval * DAY_MS)
+  }
+}
 
 export const incrementStreakForgiveness = async () => {
   const pg = createSupabaseDirectClient()
-  await pg.none(`
-    update users set data = data || 
-      jsonb_build_object('streakForgiveness', 
-        coalesce((data->'streakForgiveness')::numeric, 0) + 1
-      )
-  `)
+  await bulkUpdateUsersByChunk(pg, 15)
 }
