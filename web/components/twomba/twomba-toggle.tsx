1956d676101b1a9d7a093333fc1abf89e7923f1c
ingawei <ingawei@gmail.com>
2024-08-09 15:05:52 -0700

    tweak

--- web/components/twomba/twomba-toggle.tsx
+++ web/components/twomba/twomba-toggle.tsx
@@ -11,26 +11,31 @@ export function TwombaToggle(props: {
   const { mode, onClick } = props
   return (
     <button
-      className="bg-ink-200 dark:bg-canvas-50 relative flex h-fit w-fit shrink-0 flex-row items-center gap-1 rounded-full p-0.5 text-2xl"
+      className={clsx(
+        'bg-ink-200 dark:bg-canvas-50 relative flex h-fit w-fit shrink-0 flex-row items-center gap-1 rounded-full border-[1.5px] p-0.5 text-2xl transition-colors',
+        mode === 'mana'
+          ? 'dark:border-primary-700 border-primary-500'
+          : 'border-lime-500 dark:border-lime-200'
+      )}
       onClick={onClick}
     >
       {/* Add a moving circle behind the active coin */}
       <div
         className={clsx(
           'dark:bg-ink-300 bg-canvas-0 absolute h-[28px] w-[28px] rounded-full drop-shadow transition-all',
-          mode === 'sweepies' ? 'left-0' : 'left-[calc(100%-28px)]'
-        )}
-      />
-      <SweepiesFlatCoin
-        className={clsx(
-          'z-10 h-8 transition-opacity',
-          mode === 'sweepies' ? 'opacity-100' : 'opacity-20'
+          mode === 'mana' ? 'left-0' : 'left-[calc(100%-28px)]'
         )}
       />
       <ManaFlatCoin
         className={clsx(
           'z-10 h-8 transition-opacity',
           mode === 'mana' ? 'opacity-100' : 'opacity-20'
+        )}
+      />
+      <SweepiesFlatCoin
+        className={clsx(
+          'z-10 h-8 transition-opacity',
+          mode === 'sweepies' ? 'opacity-100' : 'opacity-20'
         )}
       />
     </button>
