dbe773dd4b9d921aace3160720760ef704b3705e
Ian Philips <iansphilips@gmail.com>
2024-08-18 16:18:36 -0700

    Remove supabase js client from managrams endpoint

--- backend/api/src/get-managrams.ts
+++ backend/api/src/get-managrams.ts
@@ -1,25 +1,41 @@
-import { millisToTs, run } from 'common/supabase/utils'
+import { millisToTs } from 'common/supabase/utils'
 import { APIError, type APIHandler } from './helpers/endpoint'
-import { createSupabaseClient } from 'shared/supabase/init'
+import { createSupabaseDirectClient } from 'shared/supabase/init'
 import { convertTxn } from 'common/supabase/txns'
 import { ManaPayTxn } from 'common/txn'
+import { buildArray } from 'common/util/array'
+import {
+  from,
+  limit,
+  orderBy,
+  renderSql,
+  select,
+  where,
+} from 'shared/supabase/sql-builder'
 
 export const getManagrams: APIHandler<'managrams'> = async (props) => {
-  const { limit, toId, fromId, before, after } = props
+  const { limit: limitValue, toId, fromId, before, after } = props
 
-  const db = createSupabaseClient()
-  let query = db
-    .from('txns')
-    .select()
-    .eq('category', 'MANA_PAYMENT')
-    .order('created_time', { ascending: false } as any)
-    .limit(limit)
-  if (before) query = query.lt('created_time', millisToTs(before))
-  if (after) query = query.gt('created_time', millisToTs(after))
-  if (toId) query = query.eq('to_id', toId)
-  if (fromId) query = query.eq('from_id', fromId)
+  const pg = createSupabaseDirectClient()
+
+  const conditions = buildArray(
+    where('category = ${category}', { category: 'MANA_PAYMENT' }),
+    before && where('created_time < ${before}', { before: millisToTs(before) }),
+    after && where('created_time > ${after}', { after: millisToTs(after) }),
+    toId && where('to_id = ${toId}', { toId }),
+    fromId && where('from_id = ${fromId}', { fromId })
+  )
+
+  const query = renderSql(
+    select('*'),
+    from('txns'),
+    ...conditions,
+    orderBy('created_time desc'),
+    limitValue && limit(limitValue)
+  )
+
   try {
-    const { data } = await run(query)
+    const data = await pg.manyOrNone(query)
     return (data.map(convertTxn) as ManaPayTxn[]) ?? []
   } catch (e) {
     throw new APIError(500, `Error while fetching managrams: ${e}`)
