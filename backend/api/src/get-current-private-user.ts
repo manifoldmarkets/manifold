5fcc859ab83bcdaeb46a35418747f28da62be213
Ian Philips <iansphilips@gmail.com>
2024-08-18 01:03:12 -0400

    Remove supabase js client from me endpoints

--- backend/api/src/get-current-private-user.ts
+++ backend/api/src/get-current-private-user.ts
@@ -1,27 +1,17 @@
-import { createSupabaseClient } from 'shared/supabase/init'
+import { createSupabaseDirectClient } from 'shared/supabase/init'
 import { APIError, APIHandler } from './helpers/endpoint'
-import { PrivateUser } from 'common/user'
 
 export const getCurrentPrivateUser: APIHandler<'me/private'> = async (
   _,
   auth
 ) => {
-  const db = createSupabaseClient()
-
-  const q = db.from('private_users').select('data').eq('id', auth.uid)
-
-  const { data, error } = await q
-
-  if (error) {
-    throw new APIError(
-      500,
-      'Error fetching private user data: ' + error.message
-    )
-  }
-
-  if (!data.length) {
-    throw new APIError(401, 'Your account was not found')
-  }
-
-  return data[0].data as PrivateUser
+  const pg = createSupabaseDirectClient()
+  const privateUser = await pg.oneOrNone(
+    `select data from private_users
+            where id = $1`,
+    [auth.uid],
+    (r) => r?.data
+  )
+  if (!privateUser) throw new APIError(404, 'Your account was not found')
+  return privateUser
 }
--- backend/api/src/get-me.ts
+++ backend/api/src/get-me.ts
@@ -1,15 +1,6 @@
-import { APIError, type APIHandler } from './helpers/endpoint'
-import { createSupabaseClient } from 'shared/supabase/init'
-import { toUserAPIResponse } from 'common/api/user-types'
-import { convertUser } from 'common/supabase/users'
+import { type APIHandler } from './helpers/endpoint'
+import { getUser } from 'api/get-user'
 
 export const getMe: APIHandler<'me'> = async (_, auth) => {
-  const db = createSupabaseClient()
-  const { data, error } = await db.from('users').select().eq('id', auth.uid)
-  if (error)
-    throw new APIError(500, 'Error fetching user data: ' + error.message)
-
-  if (!data.length) throw new APIError(401, 'Your account was not found')
-
-  return toUserAPIResponse(convertUser(data[0]))
+  return getUser({ id: auth.uid })
 }
