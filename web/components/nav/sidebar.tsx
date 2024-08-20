92cd6510748ac4cca461fee405d45ebcc618bb5e
mantikoros <sgrugett@gmail.com>
2024-08-20 16:43:02 -0400

    tv

--- web/components/nav/sidebar.tsx
+++ web/components/nav/sidebar.tsx
@@ -153,11 +153,11 @@ const getDesktopNav = (
         href: '/election',
         icon: GiCapitol,
       },
-      // {
-      //   name: 'TV',
-      //   href: '/tv',
-      //   icon: PiTelevisionSimpleBold,
-      // },
+      {
+        name: 'TV',
+        href: '/tv',
+        icon: PiTelevisionSimpleBold,
+      },
       {
         name: 'Messages',
         href: '/messages',
