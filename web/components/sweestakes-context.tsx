558bb8107a071c982595da0e4e98142d1098b344
sipec <sipec@users.noreply.github.com>
2024-08-22 23:26:44 +0000

    Auto-remove unused imports

--- web/components/sweestakes-context.tsx
+++ web/components/sweestakes-context.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext } from 'react'
 import { usePersistentQueryState } from 'web/hooks/use-persistent-query-state'
 
 type SweepstakesContextType = {
