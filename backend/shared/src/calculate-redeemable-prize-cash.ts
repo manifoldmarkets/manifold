8cddb8ee9efcb94aa6710b9d8ca24a7ae31f9b0a
Sinclair Chen <abc.sinclair@gmail.com>
2024-08-26 11:35:11 -0700

    function to calculate redeemable cash balance

---
+++ backend/shared/src/calculate-redeemable-prize-cash.ts
@@ -0,0 +1,25 @@
+import { SupabaseDirectClient } from 'shared/supabase/init'
+
+// Gets the total amount of prize cash that a user can redeem for usd, before fees
+// You still have to Math.min with user cash balance btw
+export async function calculateRedeemablePrizeCash(
+  userId: string,
+  pg: SupabaseDirectClient
+) {
+  // TODO: add cash out cancels
+  const result = await pg.oneOrNone<{ total: number }>(
+    `select sum(case
+      when category = 'CONTRACT_RESOLUTION_PAYOUT' then amount
+      when category = 'CONTRACT_UNDO_RESOLUTION_PAYOUT' then -amount
+      when category = 'CASH_OUT_PENDING' then -amount
+      else 0
+    end) as total
+    from txns
+    where token = 'CASH'
+    and (to_id = $1 or from_id = $1)
+    and category in ('CONTRACT_RESOLUTION_PAYOUT', 'CONTRACT_UNDO_RESOLUTION_PAYOUT', 'CASH_OUT_PENDING')`,
+    [userId]
+  )
+
+  return result?.total ?? 0
+}
