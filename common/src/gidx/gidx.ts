96c726787669249470a1bd901b6ad6524c7e64dd
Ian Philips <iansphilips@gmail.com>
2024-09-02 19:01:55 -0700

    Disable fake customer

--- common/src/gidx/gidx.ts
+++ common/src/gidx/gidx.ts
@@ -289,7 +289,7 @@ export type CheckoutSessionResponse = {
 export const ID_ERROR_MSG =
   'Registration failed, identity error. Check your identifying information.'
 
-export const ENABLE_FAKE_CUSTOMER = true
+export const ENABLE_FAKE_CUSTOMER = false
 export const exampleCustomers = [
   {
     EmailAddress: 'mradamgibbs@gmail.com',
