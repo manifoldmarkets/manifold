cdf2ee8fe206d4e94912e9254e3e6e7337c7e980
ingawei <ingawei@gmail.com>
2024-09-03 13:56:12 -0700

    bets

--- web/components/bet/bet-slider.tsx
+++ web/components/bet/bet-slider.tsx
@@ -1,4 +1,3 @@
-import { buildArray } from 'common/util/array'
 import { formatWithToken, InputTokenType } from 'common/util/format'
 import { BinaryOutcomes } from 'web/components/bet/bet-panel'
 import { Slider } from 'web/components/widgets/slider'
@@ -8,10 +7,14 @@ export const LARGE_SLIDER_VALUES = [
   3000, 3500, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 15000, 20000, 25000,
 ]
 
+export const LARGE_SLIDER_VALUE_LABELS = [100, 1000, 10000]
+
 export const SMALL_SLIDER_VALUES = [
   1, 2, 5, 7, 10, 15, 20, 25, 30, 40, 50, 75, 100, 200, 300, 400, 500, 750,
   1000,
 ]
+
+export const SMALL_SLIDER_VALUE_LABELS = [1, 100, 1000]
 
 export const MAX_CASH_SMALL_SLIDER_VALUE = 10000
 export const MAX_CASH_LARGE_SLIDER_VALUE = 50000
@@ -20,9 +23,13 @@ export const CASH_SMALL_SLIDER_VALUES = SMALL_SLIDER_VALUES.map((a) => {
   return a * 100
 }).filter((a) => a <= MAX_CASH_SMALL_SLIDER_VALUE)
 
+export const CASH_SMALL_SLIDER_VALUE_LABELS = [100, 1000, 10000]
+
 export const CASH_LARGE_SLIDER_VALUES = LARGE_SLIDER_VALUES.map((a) => {
   return a * 100
 }).filter((a) => a <= MAX_CASH_LARGE_SLIDER_VALUE)
+
+export const CASH_LARGE_SLIDER_VALUE_LABELS = [100, 10000, 50000]
 
 export const BetSlider = (props: {
   amount: number | undefined
@@ -42,6 +49,8 @@ export const BetSlider = (props: {
     className,
   } = props
 
+  console.log('smallAmounts', smallAmounts)
+
   const token = props.token ?? 'M$'
 
   const sliderAmounts =
@@ -53,6 +62,26 @@ export const BetSlider = (props: {
       ? SMALL_SLIDER_VALUES
       : LARGE_SLIDER_VALUES
 
+  const sliderMarkArray =
+    token == 'CASH'
+      ? smallAmounts
+        ? CASH_SMALL_SLIDER_VALUE_LABELS
+        : CASH_LARGE_SLIDER_VALUE_LABELS
+      : smallAmounts
+      ? SMALL_SLIDER_VALUE_LABELS
+      : LARGE_SLIDER_VALUE_LABELS
+
+  const marks = sliderMarkArray.map((a) => {
+    return {
+      value: sliderAmounts.findIndex((amount) => amount === a),
+      label: formatWithToken({
+        amount: a,
+        token: token,
+        short: token === 'CASH' ? false : true,
+      }),
+    }
+  })
+
   const maxSliderIndex = sliderAmounts.length - 1
   const amountToSliderIndex = (amount: number) => {
     const index = sliderAmounts.findLastIndex((a) => amount >= a)
@@ -60,55 +89,13 @@ export const BetSlider = (props: {
   }
 
   const sliderIndex = amountToSliderIndex(amount ?? 0)
-  const hundredIndex = sliderAmounts.findIndex((a) => a === 100)
-  const thousandIndex = sliderAmounts.findIndex((a) => a === 1000)
-  const tenThousandIndex = sliderAmounts.findIndex((a) => a === 10000)
 
   return (
     <Slider
       className={className}
       min={0}
       max={maxSliderIndex}
-      marks={buildArray(
-        smallAmounts && {
-          value: 0,
-          label: formatWithToken({
-            amount: sliderAmounts[0],
-            token: token,
-          }),
-        },
-        {
-          value: hundredIndex,
-          label: formatWithToken({
-            amount: sliderAmounts[hundredIndex],
-            token: token,
-          }),
-        },
-        !smallAmounts && {
-          value: thousandIndex,
-          label: formatWithToken({
-            amount: sliderAmounts[thousandIndex],
-            token: token,
-            short: true,
-          }),
-        },
-        !smallAmounts && {
-          value: tenThousandIndex,
-          label: formatWithToken({
-            amount: sliderAmounts[tenThousandIndex],
-            token: token,
-            short: true,
-          }),
-        },
-        smallAmounts && {
-          value: maxSliderIndex,
-          label: formatWithToken({
-            amount: sliderAmounts[maxSliderIndex],
-            token: token,
-            short: true,
-          }),
-        }
-      )}
+      marks={marks}
       color={
         binaryOutcome === 'YES'
           ? 'green'
