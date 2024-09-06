1f64a3393225b85995229d192546fae8ccce9f73
ingawei <ingawei@gmail.com>
2024-09-06 13:36:11 -0700

    just go to cashout page

--- web/components/profile/redeem-sweeps-buttons.tsx
+++ web/components/profile/redeem-sweeps-buttons.tsx
@@ -1,28 +1,8 @@
-import {
-  blockFromSweepstakes,
-  getVerificationStatus,
-  IDENTIFICATION_FAILED_MESSAGE,
-  LOCATION_BLOCKED_MESSAGE,
-  PHONE_NOT_VERIFIED_MESSAGE,
-  User,
-  USER_BLOCKED_MESSAGE,
-  USER_NOT_REGISTERED_MESSAGE,
-} from 'common/user'
-import { Button } from '../buttons/button'
-import { useAPIGetter } from 'web/hooks/use-api-getter'
+import { blockFromSweepstakes, User } from 'common/user'
 import { useRouter } from 'next/router'
+import { useAPIGetter } from 'web/hooks/use-api-getter'
+import { Button } from '../buttons/button'
 import { CoinNumber } from '../widgets/coin-number'
-import { useState } from 'react'
-import { Modal, MODAL_CLASS } from '../layout/modal'
-import { Col } from '../layout/col'
-import Link from 'next/link'
-import { Row } from '../layout/row'
-import { KYC_VERIFICATION_BONUS } from 'common/economy'
-import { RegisterIcon } from 'web/public/custom-components/registerIcon'
-import { LocationBlockedIcon } from 'web/public/custom-components/locationBlockedIcon'
-import { RiUserForbidLine } from 'react-icons/ri'
-import { MdOutlineNotInterested } from 'react-icons/md'
-import { SWEEPIES_NAME } from 'common/envs/constants'
 
 export function RedeemSweepsButtons(props: { user: User; className?: string }) {
   const { user, className } = props
