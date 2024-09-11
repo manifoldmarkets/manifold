6e40ceed9e2a37dd9d526282e63dc99365d98eee
ingawei <ingawei@gmail.com>
2024-09-10 18:44:00 -0700

    yuh

--- web/components/sweeps-explainer.tsx
+++ web/components/sweeps-explainer.tsx
@@ -19,20 +19,20 @@ export function SweepsExplainer() {
     <>
       <div
         className={clsx(
-          'flex h-64 w-full flex-col overflow-hidden drop-shadow-sm sm:mt-4 sm:h-48 sm:flex-row sm:text-lg '
+          'flex h-60 w-full flex-col overflow-hidden text-lg drop-shadow-sm sm:mt-4 sm:h-48 sm:flex-row '
         )}
       >
         <div
           className={clsx(
-            'relative z-30 h-[37%] w-full rounded-t-xl bg-indigo-200 p-4 sm:h-full sm:w-[50%] sm:rounded-l-xl sm:rounded-r-none sm:p-8 '
+            'relative z-30 h-[40%] w-full rounded-t-xl bg-indigo-200 p-4 sm:h-full sm:w-[50%] sm:rounded-l-xl sm:rounded-r-none sm:p-8 '
           )}
         >
-          <Row className="h-full w-full items-center gap-4 pt-7 text-black sm:pt-0">
-            <ManaCoin className="text-8xl" />
+          <Row className="h-full w-full items-center gap-4 pt-6 text-black sm:pt-0">
+            <ManaCoin className="text-7xl md:text-8xl" />
             Compete with your friends by {TRADING_TERM} with play money
           </Row>
         </div>
-        <div className="relative h-[63%] w-full rounded-b-xl bg-indigo-700 p-4 align-bottom sm:h-full sm:w-[50%] sm:rounded-l-none sm:rounded-r-xl sm:p-8 sm:pl-16">
+        <div className="relative h-[60%] w-full rounded-b-xl bg-indigo-700 p-4 align-bottom sm:h-full sm:w-[50%] sm:rounded-l-none sm:rounded-r-xl sm:p-8 sm:pl-16">
           {!isMobile && (
             <div className="absolute -left-0.5 bottom-0 z-20 h-full">
               <SquiggleVertical className={clsx('h-full text-indigo-200')} />
@@ -41,7 +41,7 @@ export function SweepsExplainer() {
           {isMobile && (
             <div className="absolute -top-0.5 right-0 z-10 w-full items-center">
               <SquiggleHorizontal
-                className={clsx('h-16 w-full object-fill text-indigo-200')}
+                className={clsx('h-12 w-full object-fill text-indigo-200')}
                 preserveAspectRatio="none"
               />
             </div>
@@ -49,7 +49,7 @@ export function SweepsExplainer() {
           <Row className=" h-full w-full items-end gap-4  text-white sm:items-center">
             Or {TRADE_TERM} with {SWEEPIES_NAME} for a chance to win real
             prizes!
-            <SweepiesCoin className="text-8xl" />
+            <SweepiesCoin className="text-7xl md:text-8xl" />
           </Row>
         </div>
       </div>
