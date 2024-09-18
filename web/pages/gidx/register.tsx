7742f69baf8fd525ac1c02f81b20995579e5644e
ingawei <46611122+ingawei@users.noreply.github.com>
2024-09-18 13:22:00 -0700

    Ingawei/man 1718 put us only on sweeps registration page (#2885)

--- web/pages/gidx/register.tsx
+++ web/pages/gidx/register.tsx
@@ -5,6 +5,10 @@ import { RegisterUserForm } from 'web/components/gidx/register-user-form'
 
 import { TWOMBA_ENABLED } from 'common/envs/constants'
 import { Col } from 'web/components/layout/col'
+import { Tooltip } from 'web/components/widgets/tooltip'
+import { Row } from 'web/components/layout/row'
+import { InfoTooltip } from 'web/components/widgets/info-tooltip'
+import { LiaFlagUsaSolid } from 'react-icons/lia'
 
 const HomePage = () => {
   const user = useUser()
@@ -12,9 +16,17 @@ const HomePage = () => {
   if (!TWOMBA_ENABLED) return null
   return (
     <Page trackPageView={'register user gidx'}>
-      <Col className="bg-canvas-0 mx-auto w-full max-w-lg gap-4 px-6 py-4">
+      <Col className="bg-canvas-0 relative mx-auto w-full max-w-lg gap-4 px-6 py-4">
+        <Row className="text-ink-600 w-full select-none  justify-end gap-1 rounded">
+          <Tooltip
+            className="flex flex-row items-center gap-1  "
+            text="Sweepstakes are limited to 18+ in all US states except WA, MI, ID, DE"
+          >
+            <LiaFlagUsaSolid className="h-6 w-6" /> US only
+          </Tooltip>
+        </Row>
         {!user || !privateUser ? (
-          <LoadingIndicator />
+          <LoadingIndicator className="mx-auto my-auto h-80" />
         ) : (
           <RegisterUserForm user={user} privateUser={privateUser} />
         )}
