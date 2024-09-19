09d1fc2cbdb39bb84a28ead7195cfa8a1d5ddda0
mantikoros <sgrugett@gmail.com>
2024-09-19 16:33:26 -0700

    gtm

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -231,17 +231,14 @@ function MyApp({ Component, pageProps }: AppProps<ManifoldPageProps>) {
       />
 
       <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${ENV_CONFIG.googleAnalyticsId}`}
-      />
-      <Script
-        id="gaw"
+        id="gtm"
         dangerouslySetInnerHTML={{
           __html: `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', '${ENV_CONFIG.googleAnalyticsId}');`,
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${ENV_CONFIG.googleAnalyticsId}');`,
         }}
       />
     </>
