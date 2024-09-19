d4b230af60d63d4ce237cbd44926b7c6a75c736d
Ian Philips <iansphilips@gmail.com>
2024-09-18 18:41:03 -0700

    Remove cast as URLSearchParams

--- web/hooks/use-defined-search-params.ts
+++ web/hooks/use-defined-search-params.ts
@@ -1,12 +1,12 @@
 import { useSearchParams } from 'next/navigation'
 import { useCallback } from 'react'
-import { URLSearchParams } from 'url'
 export const useDefinedSearchParams = () => {
   // Note: useSearchParams() must be used inside a <Suspense> component if page is SSR
   const searchParams = useSearchParams()!
   const createQueryString = useCallback(
     (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams as URLSearchParams)
+      // Note: don't cast searchParams as URLSearchParams or you'll break this instantiation
+      const params = new URLSearchParams(searchParams as any)
       params.set(name, value)
 
       return params.toString()
