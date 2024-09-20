98491b3786e09eadb96108cdba1a3d70222b4d8a
David Chee <d4vidchee@gmail.com>
2024-09-19 23:07:05 -0700

    replace redeem button copy with claim copy if applicable

--- web/components/profile/redeem-sweeps-buttons.tsx
+++ web/components/profile/redeem-sweeps-buttons.tsx
@@ -3,6 +3,8 @@ import { useRouter } from 'next/router'
 import { useAPIGetter } from 'web/hooks/use-api-getter'
 import { Button } from '../buttons/button'
 import { CoinNumber } from '../widgets/coin-number'
+import { KYC_VERIFICATION_BONUS_CASH } from 'common/economy'
+import { useKYCGiftAmount } from '../twomba/toggle-verify-callout'
 
 export function RedeemSweepsButtons(props: { user: User; className?: string }) {
   const { user, className } = props
@@ -12,24 +14,37 @@ export function RedeemSweepsButtons(props: { user: User; className?: string }) {
 
   const canRedeem = redeemableCash > 0 && !blockFromSweepstakes(user)
 
+  const amount = useKYCGiftAmount(user)
+
   const onClick = () => {
     router.push('/redeem')
   }
 
   return (
     <>
-      <Button
-        onClick={onClick}
-        color={canRedeem ? 'amber' : 'gray'}
-        className={className}
-      >
-        Redeem
-        <CoinNumber
-          amount={redeemableCash}
-          className={'ml-1'}
-          coinType={'sweepies'}
-        />
-      </Button>
+      {canRedeem ? (
+        <Button onClick={onClick} color={'amber'} className={className}>
+          Redeem
+          <CoinNumber
+            amount={redeemableCash}
+            className={'ml-1'}
+            coinType={'sweepies'}
+          />
+        </Button>
+      ) : (
+        <Button onClick={onClick} color={'amber'} className={className}>
+          Claim
+          {amount == undefined ? (
+            <CoinNumber
+              amount={KYC_VERIFICATION_BONUS_CASH}
+              coinType="CASH"
+              className="ml-1"
+            />
+          ) : (
+            <CoinNumber amount={amount} coinType="CASH" className="ml-1" />
+          )}
+        </Button>
+      )}
     </>
   )
 }
