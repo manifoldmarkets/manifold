a9a2ff2081d1a081bde13538f36d2eb5ec4b7eea
mantikoros <sgrugett@gmail.com>
2024-09-24 00:27:39 -0700

    redeem button: make distinct from claim button

--- web/components/profile/redeem-sweeps-buttons.tsx
+++ web/components/profile/redeem-sweeps-buttons.tsx
@@ -3,20 +3,13 @@ import { useRouter } from 'next/router'
 import { useAPIGetter } from 'web/hooks/use-api-getter'
 import { Button } from '../buttons/button'
 import { CoinNumber } from '../widgets/coin-number'
-import { KYC_VERIFICATION_BONUS_CASH } from 'common/economy'
-import { useKYCGiftAmount } from '../twomba/sweep-verify-section'
 import { SweepiesCoin } from 'web/public/custom-components/sweepiesCoin'
-import { blockFromSweepstakes } from 'common/gidx/user'
 
 export function RedeemSweepsButtons(props: { user: User; className?: string }) {
-  const { user, className } = props
+  const { className } = props
   const { data: redeemable } = useAPIGetter('get-redeemable-prize-cash', {})
   const redeemableCash = redeemable?.redeemablePrizeCash ?? 0
   const router = useRouter()
-
-  const canRedeem = user.sweepstakesVerified && !blockFromSweepstakes(user)
-
-  const kycGift = useKYCGiftAmount(user)
 
   const onClick = () => {
     router.push('/redeem')
@@ -24,33 +17,18 @@ export function RedeemSweepsButtons(props: { user: User; className?: string }) {
 
   return (
     <>
-      {canRedeem ? (
-        <Button onClick={onClick} color={'amber'} className={className}>
-          Redeem
-          {redeemableCash > 0 ? (
-            <CoinNumber
-              amount={redeemableCash}
-              className="ml-1"
-              coinType="sweepies"
-            />
-          ) : (
-            <SweepiesCoin className="ml-1" />
-          )}
-        </Button>
-      ) : (
-        <Button onClick={onClick} color={'amber'} className={className}>
-          Claim
-          {kycGift == undefined ? (
-            <CoinNumber
-              amount={KYC_VERIFICATION_BONUS_CASH}
-              coinType="CASH"
-              className="ml-1"
-            />
-          ) : (
-            <CoinNumber amount={kycGift} coinType="CASH" className="ml-1" />
-          )}
-        </Button>
-      )}
+      <Button onClick={onClick} color={'amber'} className={className}>
+        Redeem
+        {redeemableCash > 0 ? (
+          <CoinNumber
+            amount={redeemableCash}
+            className="ml-1"
+            coinType="sweepies"
+          />
+        ) : (
+          <SweepiesCoin className="ml-1" />
+        )}
+      </Button>
     </>
   )
 }
