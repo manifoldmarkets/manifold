e48568fdcbc36340d11cb93fc7efde2381542f70
David Chee <d4vidchee@gmail.com>
2024-09-25 12:21:30 -0700

    Show twomba tooltip on all markets

--- web/public/custom-components/sweepiesFlatCoinX.tsx
+++ web/public/custom-components/sweepiesFlatCoinX.tsx
@@ -1,11 +1,11 @@
 import clsx from 'clsx'
 import { ENV_CONFIG } from 'common/envs/constants'
 
-export function SweepiesFlatCoin(props: { className?: string }) {
+export function SweepiesFlatCoinX(props: { className?: string }) {
   const { className } = props
   return (
     <img
-      src="/SweepiesFlat.svg"
+      src="/SweepiesFlatX.svg"
       alt={ENV_CONFIG.moneyMoniker}
       className={clsx('inline-block', className)}
       style={{
