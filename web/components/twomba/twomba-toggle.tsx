76c1659b0072cca7a5b61a8aaab686c7f5af4e7f
David Chee <104849031+SirSaltyy@users.noreply.github.com>
2024-09-26 16:07:01 -0700

    Twombatoggle update 2 (#2901)
    * Revert "Revert "Show twomba tooltip on all markets""
    
    This reverts commit 74d4db19cf6c3adc5137fc9891f217739eb5b28f.
    
    * fix politics page, portfolio and browse page

--- web/components/twomba/twomba-toggle.tsx
+++ web/components/twomba/twomba-toggle.tsx
@@ -2,39 +2,75 @@ import clsx from 'clsx'
 import { ManaFlatCoin } from 'web/public/custom-components/manaFlatCoin'
 import { SweepiesFlatCoin } from 'web/public/custom-components/sweepiesFlatCoin'
 import { useSweepstakes } from 'web/components/sweestakes-context'
+import { SweepiesFlatCoinX } from 'web/public/custom-components/sweepiesFlatCoinX'
+import { Tooltip } from 'web/components/widgets/tooltip'
 
-export function TwombaToggle() {
-  const { isPlay, setIsPlay } = useSweepstakes()
+export function TwombaToggle({
+  sweepsEnabled = false,
+  isPlay: isPlayProp,
+  onClick,
+}: {
+  sweepsEnabled?: boolean
+  isPlay?: boolean
+  onClick?: () => void
+}) {
+  const { isPlay: contextIsPlay, setIsPlay: contextSetIsPlay } =
+    useSweepstakes()
+  const isPlay = isPlayProp !== undefined ? isPlayProp : contextIsPlay
+
+  const handleClick = () => {
+    if (sweepsEnabled) {
+      if (onClick) {
+        onClick()
+      } else {
+        contextSetIsPlay(!isPlay)
+      }
+    }
+  }
+
+  const SweepiesCoin = sweepsEnabled ? SweepiesFlatCoin : SweepiesFlatCoinX
 
   return (
-    <button
-      className={clsx(
-        'bg-ink-200 dark:bg-canvas-50 relative flex h-fit w-fit shrink-0 flex-row items-center gap-1 rounded-full border-[1.5px] p-0.5 text-2xl transition-colors',
-        isPlay
-          ? 'border-violet-600 dark:border-violet-400'
-          : 'border-amber-500 dark:border-amber-200'
-      )}
-      onClick={() => setIsPlay(!isPlay)}
+    <Tooltip
+      text={
+        sweepsEnabled
+          ? null
+          : 'This question does not have sweepstakes enabled.'
+      }
     >
-      {/* Add a moving circle behind the active coin */}
-      <div
+      <button
         className={clsx(
-          'dark:bg-ink-300 bg-canvas-0 absolute h-[28px] w-[28px] rounded-full drop-shadow transition-all',
-          isPlay ? 'left-0' : 'left-[calc(100%-28px)]'
+          'bg-ink-200 dark:bg-canvas-50 relative flex h-fit w-fit shrink-0 flex-row items-center gap-1 rounded-full border-[1.5px] p-0.5 text-2xl transition-colors',
+          isPlay
+            ? 'border-violet-600 dark:border-violet-400'
+            : 'border-amber-500 dark:border-amber-200',
+          sweepsEnabled
+            ? ''
+            : 'cursor-not-allowed border-gray-400 opacity-60 dark:border-gray-400' // Greys out the button when disabled
         )}
-      />
-      <ManaFlatCoin
-        className={clsx(
-          'z-10 h-8 transition-opacity',
-          isPlay ? 'opacity-100' : 'opacity-20'
-        )}
-      />
-      <SweepiesFlatCoin
-        className={clsx(
-          'z-10 h-8 transition-opacity',
-          !isPlay ? 'opacity-100' : 'opacity-20'
-        )}
-      />
-    </button>
+        onClick={handleClick}
+      >
+        <div
+          className={clsx(
+            'dark:bg-ink-300 bg-canvas-0 absolute h-[28px] w-[28px] rounded-full drop-shadow transition-all',
+            isPlay ? 'left-0' : 'left-[calc(100%-28px)]'
+          )}
+        />
+        <ManaFlatCoin
+          className={clsx(
+            'z-10 h-8 transition-opacity',
+            isPlay ? 'opacity-100' : 'opacity-20',
+            sweepsEnabled ? '' : 'grayscale filter'
+          )}
+        />
+        <SweepiesCoin
+          className={clsx(
+            'z-10 h-8 transition-opacity',
+            !isPlay ? 'opacity-100' : 'opacity-20',
+            sweepsEnabled ? '' : 'grayscale filter'
+          )}
+        />
+      </button>
+    </Tooltip>
   )
 }
