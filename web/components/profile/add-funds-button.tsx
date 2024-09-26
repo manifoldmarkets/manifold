c889da6ea74887ee7b88bc2d6474b4b4fb377477
Ian Philips <iansphilips@gmail.com>
2024-09-25 18:10:25 -0700

    Add note about special offer

--- web/components/profile/add-funds-button.tsx
+++ web/components/profile/add-funds-button.tsx
@@ -6,6 +6,9 @@ import { TWOMBA_ENABLED } from 'common/envs/constants'
 import { useRouter } from 'next/router'
 import { ManaCoin } from 'web/public/custom-components/manaCoin'
 import { SweepiesCoin } from 'web/public/custom-components/sweepiesCoin'
+import { introductoryTimeWindow } from 'common/user'
+import { RelativeTimestamp } from '../relative-timestamp'
+import Link from 'next/link'
 
 export function AddFundsButton(props: {
   userId?: string
@@ -17,6 +20,11 @@ export function AddFundsButton(props: {
   const user = useUser()
   const router = useRouter()
   if (!userId || user?.id !== userId) return null
+  const expirationStart = user
+    ? new Date(introductoryTimeWindow(user))
+    : new Date()
+  const eligibleForNewUserOffer =
+    user && Date.now() < expirationStart.valueOf() && !user.purchasedSweepcash
   return (
     <>
       <Button
@@ -33,6 +41,17 @@ export function AddFundsButton(props: {
       >
         Get <ManaCoin className="ml-1" /> and <SweepiesCoin className="ml-1" />
       </Button>
+      {eligibleForNewUserOffer && (
+        <Link href="/checkout" className="text-center text-sm text-amber-500">
+          ✨ Special offer expires in
+          <RelativeTimestamp
+            className="text-amber-500 "
+            time={expirationStart.valueOf()}
+            shortened
+          />{' '}
+          ✨
+        </Link>
+      )}
       <AddFundsModal open={open} setOpen={setOpen} />
     </>
   )
