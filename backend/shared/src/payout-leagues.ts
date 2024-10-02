bd0044eae1bf9a2b27f2c016abf9229f1ecd3ad1
David Chee <104849031+SirSaltyy@users.noreply.github.com>
2024-10-01 18:14:32 -0700

    Update payout-leagues.ts (#2915)

--- backend/shared/src/payout-leagues.ts
+++ backend/shared/src/payout-leagues.ts
@@ -5,7 +5,6 @@ import { runTxnFromBank } from './txn/run-txn'
 
 import { LeaguePrizeTxn } from 'common/txn'
 import { chunk } from 'lodash'
-import { TWOMBA_ENABLED } from 'common/envs/constants'
 
 export const sendEndOfSeasonNotificationsAndBonuses = async (
   pg: SupabaseDirectClient,
@@ -62,7 +61,7 @@ const sendEndOfSeasonNotificationAndBonus = async (
     toId: userId,
     toType: 'USER',
     amount: prize,
-    token: TWOMBA_ENABLED ? 'CASH' : 'SPICE',
+    token: 'M$',
     category: 'LEAGUE_PRIZE',
     data: prevRow,
   }
