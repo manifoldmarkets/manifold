c16acb38c86bbf990bfdc6e7ccc2cb308703fcac
Ian Philips <iansphilips@gmail.com>
2024-10-08 06:51:34 -0600

    Once per minute

--- backend/scheduler/src/jobs/index.ts
+++ backend/scheduler/src/jobs/index.ts
@@ -86,7 +86,7 @@ export function createJobs() {
     ),
     createJob(
       'update-user-portfolio-histories',
-      '0 */2 * * * *', // every other minute
+      '0 * * * * *', // every minute
       () => updateUserPortfolioHistoriesCore()
     ),
     createJob(
