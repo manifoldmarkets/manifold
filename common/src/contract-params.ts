d70cce22efae8de825533dc897a2032569903ec0
Ian Philips <iansphilips@gmail.com>
2024-10-10 09:28:37 -0600

    Fix reference

--- common/src/contract-params.ts
+++ common/src/contract-params.ts
@@ -31,7 +31,7 @@ export async function getContractParams(
   const isNumber = market.outcomeType === 'NUMBER'
   const numberContractBetCount = async () =>
     unauthedApi('unique-bet-group-count', {
-      contractId: contract.id,
+      contractId: market.id,
     }).then((res) => res.count)
 
   const [
