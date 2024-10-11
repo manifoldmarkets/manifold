80dbc6809c33704dbe4940d2b75708457b1c8fc8
Bohaska <73286691+Bohaska@users.noreply.github.com>
2024-10-11 10:25:35 +0800

    use embed page as market preview image

--- web/pages/api/og/market.tsx
+++ web/pages/api/og/market.tsx
@@ -1,9 +1,12 @@
 import { ImageResponse } from '@vercel/og'
 import { ImageResponseOptions } from '@vercel/og/dist/types'
 import { NextRequest } from 'next/server'
-import { OgMarket } from 'web/components/og/og-market'
+import { ContractEmbedPage } from 'web/pages/[username]/[contractSlug]'
 import { classToTw } from 'web/components/og/utils'
 import { OgCardProps } from 'common/contract-seo'
+import { unauthedApi } from 'common/util/api'
+import { base64toPoints } from 'common/edge/og'
+import { getCardOptions } from './market'
 
 export const config = { runtime: 'edge' }
 export const getCardOptions = async () => {
@@ -39,17 +42,44 @@ const figtreeMediumData = fetch(FIGTREE_MED_URL).then((res) =>
 export default async function handler(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url)
+
+    const contractSlug = searchParams.get('contractSlug')
+    const username = searchParams.get('username')
+
+    const contract = await unauthedApi('slug/:slug', {
+      slug: contractSlug,
+    }).catch((e) => {
+      throw new Error('Error fetching contract')
+    })
+
+    if (!contract) {
+      throw new Error('Contract not found')
+    }
+
+    const cashContract = contract.siblingContractId
+      ? await unauthedApi('market/:id', { id: contract.siblingContractId })
+      : null
+
+    const points = base64toPoints(contract.pointsString ?? '')
+    const cashPoints = cashContract ? base64toPoints(cashContract.pointsString ?? '') : null
+
     const options = await getCardOptions()
-    const OgMarketProps = Object.fromEntries(
-      searchParams.entries()
-    ) as OgCardProps
-    const image = OgMarket(OgMarketProps)
+
+    const image = (
+      <ContractEmbedPage
+        contract={contract}
+        points={points}
+        cashContract={cashContract}
+        cashPoints={cashPoints}
+      />
+    )
 
     return new ImageResponse(classToTw(image), options as ImageResponseOptions)
   } catch (e: any) {
-    console.log(`${e.message}`)
+    console.error(e.message)
     return new Response(`Failed to generate the image`, {
       status: 500,
     })
   }
 }
+
