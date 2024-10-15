3f1a97fab529482e799a242a997615e664eeb4a0
Ian Philips <iansphilips@gmail.com>
2024-10-15 13:22:57 -0700

    Revert "Revert "Update mc metadata in place-bet, too""
    This reverts commit ef0b8a4fbf29733a12db17807318fc2b7b7fcaf0.

--- backend/api/src/place-bet.ts
+++ backend/api/src/place-bet.ts
@@ -742,6 +742,17 @@ export const executeNewBetResult = async (
     )
     betsToInsert.push(...otherBetsToInsert)
   }
+  const isUniqueBettor =
+    (contract.outcomeType !== 'NUMBER' || firstBetInMultiBet) &&
+    !contractMetrics.find((m) => m.userId === user.id)
+  const lastBetTime =
+    maxBy(betsToInsert, (b) => b.createdTime)?.createdTime ?? Date.now()
+  const sharedContractUpdates: Partial<MarketContract> = removeUndefinedProps({
+    lastBetTime,
+    volume: contract.volume + sumBy(betsToInsert, (b) => Math.abs(b.amount)),
+    lastUpdatedTime: lastBetTime,
+    uniqueBettorCount: contract.uniqueBettorCount + (isUniqueBettor ? 1 : 0),
+  })
 
   if (newBet.answerId) {
     // Multi-cpmm-1 contract
@@ -755,9 +766,8 @@ export const executeNewBetResult = async (
         prob,
       })
     }
+    await updateContract(pgTrans, contract.id, sharedContractUpdates)
   } else {
-    const lastBetTime =
-      maxBy(betsToInsert, (b) => b.createdTime)?.createdTime ?? Date.now()
     await updateContract(
       pgTrans,
       contract.id,
@@ -766,11 +776,7 @@ export const executeNewBetResult = async (
         p: newP,
         totalLiquidity: newTotalLiquidity,
         prob: newPool && newP ? getCpmmProbability(newPool, newP) : undefined,
-        lastBetTime,
-        volume:
-          contract.volume + sumBy(betsToInsert, (b) => Math.abs(b.amount)),
-        lastUpdatedTime: lastBetTime,
-        uniqueBettorCount: contract.uniqueBettorCount + (bonuxTxn ? 1 : 0),
+        ...sharedContractUpdates,
       })
     )
   }
