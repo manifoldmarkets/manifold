cf7e2c7a0377f32fb68a895c5114042ccdc66e2d
Ian Philips <iansphilips@gmail.com>
2024-10-15 17:10:10 -0700

    Add explicit user ids

--- backend/scripts/top-up-manifold.ts
+++ backend/scripts/top-up-manifold.ts
@@ -1,16 +1,25 @@
+import {
+  DEV_HOUSE_LIQUIDITY_PROVIDER_ID,
+  HOUSE_LIQUIDITY_PROVIDER_ID,
+} from 'common/antes'
 import { runScript } from 'run-script'
+import { isProd } from 'shared/utils'
 import { runTxnFromBank } from 'shared/txn/run-txn'
+
+const userId = isProd()
+  ? HOUSE_LIQUIDITY_PROVIDER_ID
+  : DEV_HOUSE_LIQUIDITY_PROVIDER_ID
 
 if (require.main === module) {
   runScript(async ({ pg }) => {
     await pg.tx(async (tx) => {
       await runTxnFromBank(tx, {
-        amount: 25_000,
+        amount: 1_000_000,
         description: 'Top up Manifold account',
         category: 'MANIFOLD_TOP_UP',
         fromType: 'BANK',
         toType: 'USER',
-        toId: 'IPTOzEqrpkWmEzh6hwvAyY9PqFb2', // Manifold account user id
+        toId: userId,
         token: 'M$',
       })
     })
