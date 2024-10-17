e62db71de98f5f1b86b801fb49b53d882ad361fc
Sinclair Chen <abc.sinclair@gmail.com>
2024-10-16 17:36:12 -0700

    use the sqlbuilder

--- backend/api/src/get-leaderboard.ts
+++ backend/api/src/get-leaderboard.ts
@@ -1,9 +1,19 @@
 import { APIError, APIHandler } from 'api/helpers/endpoint'
 import { createSupabaseDirectClient } from 'shared/supabase/init'
+import {
+  renderSql,
+  select,
+  from,
+  where,
+  orderBy,
+  limit,
+  join,
+  groupBy,
+} from 'shared/supabase/sql-builder'
 
 export const getLeaderboard: APIHandler<'leaderboard'> = async ({
   groupId,
-  limit,
+  limit: limitValue,
   token,
   kind,
 }) => {
@@ -12,50 +22,47 @@ export const getLeaderboard: APIHandler<'leaderboard'> = async ({
 
   const pg = createSupabaseDirectClient()
 
-  let query = ''
-  if (kind === 'creator') {
-    query = `
-      SELECT c.creator_id as user_id, COUNT(*) as score
-      FROM contracts c
-      JOIN user_contract_metrics ucm ON ucm.contract_id = c.id
-      WHERE ucm.answer_id IS NULL
-      ${
-        groupId
-          ? 'AND c.id IN (SELECT contract_id FROM group_contracts WHERE group_id = $1)'
-          : ''
-      }
-      GROUP BY c.creator_id
-      ORDER BY score DESC
-      LIMIT $2
-    `
-  } else if (kind === 'profit') {
-    query = `
-      SELECT user_id, SUM(profit) as score
-      FROM user_contract_metrics
-      WHERE answer_id IS NULL
-      ${
-        groupId
-          ? 'AND contract_id IN (SELECT contract_id FROM group_contracts WHERE group_id = $1)'
-          : ''
-      }
-      GROUP BY user_id
-      ORDER BY score DESC
-      LIMIT $2
-    `
-  } else if (kind === 'referral') {
-    query = `
-      SELECT referred_by_user_id as user_id, COUNT(*) as score
-      FROM users
-      WHERE referred_by_user_id IS NOT NULL
-      GROUP BY referred_by_user_id
-      ORDER BY score DESC
-      LIMIT $2
-    `
+  if (kind === 'referral') {
+    const data = await pg.any(
+      `select id, total_referrals, total_referred_profit
+      from user_referrals_profit limit $1`,
+      [limitValue]
+    )
+
+    return data.map((r) => ({
+      userId: r.id,
+      score: r.totalReferrals,
+      totalReferredProfit: r.totalReferredProfit,
+    }))
   }
 
-  const result = await pg.manyOrNone(
-    query,
-    groupId ? [groupId, limit] : [limit]
+  const query = renderSql(
+    from('contracts c'),
+
+    kind === 'creator' && [
+      select('c.creator_id as user_id, count(*) as score'),
+      groupBy('c.creator_id'),
+    ],
+
+    kind === 'profit' && [
+      join('user_contract_metrics ucm on ucm.contract_id = c.id'),
+      where('ucm.answer_id is null'),
+      select('user_id, sum(profit) as score'),
+      groupBy('user_id'),
+    ],
+
+    where('c.token = ${token}', { token }),
+    groupId &&
+      where(
+        'c.id in (select contract_id from group_contracts where group_id = ${groupId})',
+        { groupId }
+      ),
+    orderBy('score desc'),
+    limit(limitValue)
   )
-  return result.map((r) => ({ userId: r.user_id, score: parseFloat(r.score) }))
+
+  return await pg.any(query, (r: any) => ({
+    userId: r.user_id,
+    score: parseFloat(r.score),
+  }))
 }
