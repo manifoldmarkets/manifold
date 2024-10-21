40dc5fb0615519987342cc8e50dbfa54b4d2f6e5
Ian Philips <iansphilips@gmail.com>
2024-10-21 09:15:19 -0700

    Remove nanoid from common

--- common/src/bet.ts
+++ common/src/bet.ts
@@ -1,7 +1,6 @@
 import { groupBy, mapValues } from 'lodash'
 import { Fees } from './fees'
 import { maxMinBin } from './chart'
-import { nanoid } from 'nanoid'
 
 /************************************************
 
@@ -102,4 +101,12 @@ export type maker = {
   timestamp: number
 }
 
-export const getNewBetId = () => nanoid(12)
+export const getNewBetId = () => {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let result = ''
+  for (let i = 0; i < 12; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length))
+  }
+  return result
+}
