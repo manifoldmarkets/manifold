935b9a32637db3296c67186b95e1307dfe45534b
Sinclair Chen <abc.sinclair@gmail.com>
2024-10-21 17:16:04 -0700

    Drop weekly_update. delete notif and page code

--- common/src/supabase/schema.ts
+++ common/src/supabase/schema.ts
@@ -3152,48 +3152,6 @@ export type Database = {
         }
         Relationships: []
       }
-      weekly_update: {
-        Row: {
-          contract_metrics: Json
-          created_time: string
-          id: string
-          profit: number
-          range_end: string
-          user_id: string
-        }
-        Insert: {
-          contract_metrics: Json
-          created_time?: string
-          id?: string
-          profit: number
-          range_end: string
-          user_id: string
-        }
-        Update: {
-          contract_metrics?: Json
-          created_time?: string
-          id?: string
-          profit?: number
-          range_end?: string
-          user_id?: string
-        }
-        Relationships: [
-          {
-            foreignKeyName: 'weekly_update_user_id_fkey'
-            columns: ['user_id']
-            isOneToOne: false
-            referencedRelation: 'user_referrals_profit'
-            referencedColumns: ['id']
-          },
-          {
-            foreignKeyName: 'weekly_update_user_id_fkey'
-            columns: ['user_id']
-            isOneToOne: false
-            referencedRelation: 'users'
-            referencedColumns: ['id']
-          }
-        ]
-      }
     }
     Views: {
       final_pp_balances: {
