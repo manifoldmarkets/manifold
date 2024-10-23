ea3f5bcffc32350a50daa6e778b58b369297372a
Ian Philips <iansphilips@gmail.com>
2024-10-23 10:38:17 -0700

    Restart server at 2/3am every day

--- backend/api/ecosystem.config.js
+++ backend/api/ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: 'serve',
       script: 'backend/api/lib/serve.js',
-      // Restart the app every hour
-      // cron_restart: '0 * * * *',
+      // Restart every day at 2/3 AM LA time (UTC-7/8)
+      cron_restart: '0 10 * * *',
       instances: 1,
       autorestart: true,
       watch: false,
