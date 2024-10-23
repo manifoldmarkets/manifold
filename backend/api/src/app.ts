0568556ae75aa6272e02dee8a1cc307e19b64b1a
Ian Philips <iansphilips@gmail.com>
2024-10-23 11:17:24 -0700

    Try adding express's ip address

--- backend/api/src/app.ts
+++ backend/api/src/app.ts
@@ -215,12 +215,20 @@ const ignoredEndpoints = [
   '/get-channel-seen-time',
 ]
 
+const getCleanIp = (ip: string) => {
+  return ip.startsWith('::ffff:') ? ip.substring(7) : ip
+}
+
 const requestMonitoring: RequestHandler = (req, res, next) => {
   const traceContext = req.get('X-Cloud-Trace-Context')
   const traceId = traceContext ? traceContext.split('/')[0] : randomString(12)
   const { method, path: endpoint, url } = req
   const baseEndpoint = getBaseName(endpoint)
-  const context = { endpoint, traceId, baseEndpoint }
+
+  // Get clean IP address
+  const ip = getCleanIp(req.ip)
+
+  const context = { endpoint, traceId, baseEndpoint, ip }
   withMonitoringContext(context, () => {
     if (method == 'OPTIONS') {
       next()
@@ -234,7 +242,7 @@ const requestMonitoring: RequestHandler = (req, res, next) => {
     ) {
       log(`${method} ${url}`)
     }
-    metrics.inc('http/request_count', { endpoint, baseEndpoint, method })
+    metrics.inc('http/request_count', { endpoint, baseEndpoint, method, ip })
     res.on('close', () => {
       const endTs = hrtime.bigint()
       const latencyMs = Number(endTs - startTs) / 1e6 // Convert to milliseconds
