0d0e96461577fa2e3e3c0387b0066a611b876bcd
mantikoros <sgrugett@gmail.com>
2024-10-23 11:23:37 -0700

    top up: make less scary

--- backend/scripts/top-up-manifold.ts
+++ backend/scripts/top-up-manifold.ts
@@ -10,18 +10,53 @@ const userId = isProd()
   ? HOUSE_LIQUIDITY_PROVIDER_ID
   : DEV_HOUSE_LIQUIDITY_PROVIDER_ID
 
+const askQuestion = (query: string): Promise<string> => {
+  return new Promise((resolve) => {
+    process.stdout.write(query)
+    process.stdin.once('data', (data) => {
+      resolve(data.toString().trim())
+    })
+  })
+}
+
 if (require.main === module) {
   runScript(async ({ pg }) => {
-    await pg.tx(async (tx) => {
-      await runTxnFromBank(tx, {
-        amount: 1_000_000,
-        description: 'Top up Manifold account',
-        category: 'MANIFOLD_TOP_UP',
-        fromType: 'BANK',
-        toType: 'USER',
-        toId: userId,
-        token: 'M$',
+    const amount = await askQuestion(
+      'Enter the amount to top up: '
+    )
+    const token = await askQuestion('Enter the token (M$ / CASH): ')
+
+    const confirmedAmount = amount ? parseInt(amount) : 0
+    if (isNaN(confirmedAmount) || !Number.isInteger(confirmedAmount) || confirmedAmount < 1 || confirmedAmount > 1000000) {
+      console.error('Invalid amount. Please enter an integer between 1 and 1,000,000.');
+      process.exit(1);
+    }
+    const confirmedToken = token.toUpperCase()
+    if (confirmedToken !== 'M$' && confirmedToken !== 'CASH') {
+      console.error('Invalid token. Please enter either M$ or CASH.')
+      process.exit(1)
+    }
+
+    console.log(`Confirming: Top up ${confirmedAmount} ${token}`)
+    const confirm = await askQuestion('Proceed? (y/n): ')
+
+    if (confirm.toLowerCase() === 'y') {
+      await pg.tx(async (tx) => {
+        await runTxnFromBank(tx, {
+          amount: confirmedAmount,
+          description: 'Top up Manifold account',
+          category: 'MANIFOLD_TOP_UP',
+          fromType: 'BANK',
+          toType: 'USER',
+          toId: userId,
+          token: confirmedToken as 'M$' | 'SHARE' | 'SPICE' | 'CASH',
+        })
       })
-    })
+      console.log('Transaction completed.')
+    } else {
+      console.log('Transaction cancelled.')
+    }
+
+    process.exit(0)
   })
 }
