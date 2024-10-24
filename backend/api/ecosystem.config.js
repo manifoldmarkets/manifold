b413b8468981cc4b1ff225702bece8f2bb976376
Ian Philips <iansphilips@gmail.com>
2024-10-24 00:18:10 -0700

    Restart every hour w/ reduced cache size

--- backend/api/ecosystem.config.js
+++ backend/api/ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: 'serve',
       script: 'backend/api/lib/serve.js',
-      // Restart every day at 2/3 AM LA time (UTC-7/8)
-      cron_restart: '0 10 * * *',
+      // Restart every hour
+      cron_restart: '0 * * * *',
       instances: 1,
       autorestart: true,
       watch: false,
