e846e0765615d561d49dc7948ead9241847965a7
Ian Philips <iansphilips@gmail.com>
2024-10-23 17:04:09 -0700

    Revert "Test ip address solely in metrics"
    This reverts commit 5931c1aee15e4da03c3cb6e63b6d9108492d2b35.

--- backend/api/src/app.ts
+++ backend/api/src/app.ts
@@ -214,16 +214,12 @@ const ignoredEndpoints = [
   '/v0/get-mod-reports',
   '/get-channel-seen-time',
 ]
-const getCleanIp = (ip: string) => {
-  return ip.startsWith('::ffff:') ? ip.substring(7) : ip
-}
+
 const requestMonitoring: RequestHandler = (req, res, next) => {
   const traceContext = req.get('X-Cloud-Trace-Context')
   const traceId = traceContext ? traceContext.split('/')[0] : randomString(12)
   const { method, path: endpoint, url } = req
   const baseEndpoint = getBaseName(endpoint)
-  const ip = getCleanIp(req.ip)
-
   const context = { endpoint, traceId, baseEndpoint }
   withMonitoringContext(context, () => {
     if (method == 'OPTIONS') {
@@ -238,7 +234,7 @@ const requestMonitoring: RequestHandler = (req, res, next) => {
     ) {
       log(`${method} ${url}`)
     }
-    metrics.inc('http/request_count', { endpoint, baseEndpoint, method, ip })
+    metrics.inc('http/request_count', { endpoint, baseEndpoint, method })
     res.on('close', () => {
       const endTs = hrtime.bigint()
       const latencyMs = Number(endTs - startTs) / 1e6 // Convert to milliseconds
