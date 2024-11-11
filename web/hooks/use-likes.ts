d9642a0145608c786aed272333155f5424abae78
Ian Philips <iansphilips@gmail.com>
2024-11-11 10:35:13 -0800

    Debounce likes queries

--- web/hooks/use-likes.ts
+++ web/hooks/use-likes.ts
@@ -3,6 +3,36 @@ import { db } from 'web/lib/supabase/db'
 import { useEffect } from 'react'
 import { run } from 'common/supabase/utils'
 import { usePersistentInMemoryState } from './use-persistent-in-memory-state'
+import { debounce } from 'lodash'
+
+const pendingRequests: Map<ReactionContentTypes, Set<string>> = new Map()
+const pendingCallbacks: Map<string, ((data: Reaction[]) => void)[]> = new Map()
+
+const executeBatchLikesQuery = debounce(async () => {
+  for (const [contentType, contentIds] of pendingRequests.entries()) {
+    if (!contentIds.size) continue
+
+    const { data } = await run(
+      db
+        .from('user_reactions')
+        .select()
+        .eq('content_type', contentType)
+        .in('content_id', Array.from(contentIds))
+    )
+
+    contentIds.forEach((contentId) => {
+      const key = `${contentType}-${contentId}`
+      const callbacks = pendingCallbacks.get(key) || []
+      const filteredData = (data || []).filter(
+        (item) => item.content_id === contentId
+      )
+      callbacks.forEach((callback) => callback(filteredData))
+      pendingCallbacks.delete(key)
+    })
+
+    pendingRequests.delete(contentType)
+  }
+}, 50)
 
 export const useLikesOnContent = (
   contentType: ReactionContentTypes,
@@ -14,13 +44,31 @@ export const useLikesOnContent = (
   )
 
   useEffect(() => {
-    run(
-      db
-        .from('user_reactions')
-        .select()
-        .eq('content_type', contentType)
-        .eq('content_id', contentId)
-    ).then(({ data }) => setLikes(data))
+    if (!pendingRequests.has(contentType)) {
+      pendingRequests.set(contentType, new Set())
+    }
+    pendingRequests.get(contentType)!.add(contentId)
+
+    const key = `${contentType}-${contentId}`
+    if (!pendingCallbacks.has(key)) {
+      pendingCallbacks.set(key, [])
+    }
+    pendingCallbacks.get(key)!.push(setLikes)
+
+    executeBatchLikesQuery()
+
+    return () => {
+      const callbacks = pendingCallbacks.get(key)
+      if (callbacks) {
+        const index = callbacks.indexOf(setLikes)
+        if (index > -1) {
+          callbacks.splice(index, 1)
+        }
+        if (callbacks.length === 0) {
+          pendingCallbacks.delete(key)
+        }
+      }
+    }
   }, [contentType, contentId])
 
   return likes
