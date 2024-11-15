7938f4419e0c6fac864c2fd856ce927217a23cc0
ingawei <ingawei@gmail.com>
2024-11-15 12:18:46 -0800

    no log

--- web/components/contract/creator-share-panel.tsx
+++ web/components/contract/creator-share-panel.tsx
@@ -19,7 +19,6 @@ import { UNIQUE_BETTOR_BONUS_AMOUNT } from 'common/economy'
 
 export function CreatorShareBoostPanel(props: { contract: Contract }) {
   const { contract } = props
-  console.log(contract.mechanism)
 
   return (
     <GradientContainer className="mb-8 flex w-full">
