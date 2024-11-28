52033fa3429cb2cd4629a676395010d6b7ba1cba
mantikoros <sgrugett@gmail.com>
2024-11-28 01:45:04 -0800

    low-effort thanksgiving

--- web/components/nav/manifold-logo.tsx
+++ web/components/nav/manifold-logo.tsx
@@ -24,13 +24,13 @@ export function ManifoldLogo(props: { className?: string; twoLine?: boolean }) {
         )}
       >
         <LogoIcon
-          className="h-10 w-10 shrink-0 stroke-indigo-700 transition-transform group-hover:rotate-12 dark:stroke-white"
+          className="h-10 w-10 shrink-0 stroke-amber-700 transition-transform group-hover:rotate-12"
           aria-hidden
         />
         <div
-          className={clsx('text-xl font-thin text-indigo-700 dark:text-white')}
+          className={clsx('text-xl font-thin text-amber-700')}
         >
-          {ENV == 'DEV' ? 'DEVIFO️LD' : 'MANIFOLD'}
+          {ENV == 'DEV' ? 'DEVIFO️LD' : 'MANIF🦃LD'}
         </div>
       </Link>
     </>
