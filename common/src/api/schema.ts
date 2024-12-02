2823f6a3eb8416037c255b5cdc8069c0b7287213
Ian Philips <iansphilips@gmail.com>
2024-12-02 07:50:58 -0800

    Try preferAuth on search-markets

--- common/src/api/schema.ts
+++ common/src/api/schema.ts
@@ -84,6 +84,8 @@ type APIGenericSchema = {
   returns?: Record<string, any>
   // Cache-Control header. like, 'max-age=60'
   cache?: string
+  // whether the endpoint should prefer authentication even if not required
+  preferAuth?: boolean
 }
 
 let _apiTypeCheck: { [x: string]: APIGenericSchema }
@@ -756,6 +758,7 @@ export const API = (_apiTypeCheck = {
     method: 'GET',
     visibility: 'undocumented',
     authed: false,
+    preferAuth: true,
     cache: DEFAULT_CACHE_STRATEGY,
     returns: [] as Contract[],
     props: searchProps,
