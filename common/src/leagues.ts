d99759ae9c9e2a95fae530d7f04de364a4643fc4
David Chee <d4vidchee@gmail.com>
2024-12-02 10:51:44 -0800

    Generate leagues s20

--- common/src/leagues.ts
+++ common/src/leagues.ts
@@ -3,7 +3,7 @@ import { Row } from './supabase/utils'
 export type season = (typeof SEASONS)[number]
 
 export const SEASONS = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 ] as const
 export const CURRENT_SEASON = SEASONS[SEASONS.length - 1]
 
