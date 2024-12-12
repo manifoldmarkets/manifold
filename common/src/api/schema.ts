f612c15c78721e530c1e8ab3bd495159c277a808
mantikoros <95266179+mantikoros@users.noreply.github.com>
2024-12-11 21:45:02 -0600

    New feed (#3212)
    * site activity
    
    * get-site-activity
    
    * fix contracts issue
    
    * pass params
    
    * remove comments
    
    * adjust limits
    
    * styling
    
    * feed images
    
    * contract mention: don't open in new tab
    
    * conditionally show description
    
    * move description outside hovercard
    
    * todos
    
    * include sales
    
    * more todos

--- common/src/api/schema.ts
+++ common/src/api/schema.ts
@@ -1913,6 +1913,23 @@ export const API = (_apiTypeCheck = {
       userId: z.string(),
     }),
   },
+  'get-site-activity': {
+    method: 'GET',
+    visibility: 'public',
+    authed: false,
+    returns: {} as {
+      bets: Bet[]
+      comments: ContractComment[]
+      newContracts: Contract[]
+      relatedContracts: Contract[]
+    },
+    props: z.object({
+      limit: z.coerce.number().default(10),
+      blockedUserIds: z.array(z.string()).optional(),
+      blockedGroupSlugs: z.array(z.string()).optional(),
+      blockedContractIds: z.array(z.string()).optional(),
+    }).strict(),
+  },
 } as const)
 
 export type APIPath = keyof typeof API
