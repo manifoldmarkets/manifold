5dc1518e3481340cdf40254cb1ace97e3b061b2b
ingawei <46611122+ingawei@users.noreply.github.com>
2024-12-18 15:17:22 -0800

    formatting (#3249)

--- mani/app/[contractId].tsx
+++ mani/app/[contractId].tsx
@@ -9,11 +9,15 @@ import { BinaryBetButtons } from 'components/contract/bet/BinaryBetButtons'
 import { MultiBinaryBetButtons } from 'components/contract/bet/MultiBinaryBetButtons'
 import { BinaryOverview } from 'components/contract/overview/BinaryOverview'
 import { MultiOverview } from 'components/contract/overview/MultiOverview'
+import { Col } from 'components/layout/col'
 import Page from 'components/Page'
 import { ThemedText } from 'components/ThemedText'
 import { EXAMPLE_CONTRACTS } from 'constants/examples/ExampleContracts'
 import { useLocalSearchParams } from 'expo-router'
 import { useColor } from 'hooks/useColor'
+
+export const LARGE_QUESTION_LENGTH = 95
+
 export default function ContractPage() {
   const { contractId } = useLocalSearchParams()
   const color = useColor()
@@ -34,28 +38,34 @@ export default function ContractPage() {
 
   return (
     <Page>
-      <ThemedText size="2xl" weight="semibold" style={{ paddingTop: 16 }}>
-        {contract.question}
-      </ThemedText>
+      <Col style={{ gap: 16 }}>
+        <ThemedText
+          size={contract.question.length > LARGE_QUESTION_LENGTH ? 'xl' : '2xl'}
+          weight="semibold"
+          style={{ paddingTop: 16 }}
+        >
+          {contract.question}
+        </ThemedText>
 
-      {isBinary && <BinaryOverview contract={contract as BinaryContract} />}
+        {isBinary && <BinaryOverview contract={contract as BinaryContract} />}
 
-      {isBinaryMc ? (
-        <MultiBinaryBetButtons
-          contract={contract as CPMMMultiContract}
-          size="lg"
-        />
-      ) : isMultipleChoice ? (
-        <MultiOverview contract={contract as MultiContract} />
-      ) : (
-        <BinaryBetButtons contract={contract} size="lg" />
-      )}
+        {isBinaryMc ? (
+          <MultiBinaryBetButtons
+            contract={contract as CPMMMultiContract}
+            size="lg"
+          />
+        ) : isMultipleChoice ? (
+          <MultiOverview contract={contract as MultiContract} />
+        ) : (
+          <BinaryBetButtons contract={contract} size="lg" />
+        )}
 
-      {/* {contract.description && (
+        {/* {contract.description && (
           <ThemedText color={color.textSecondary}>
             {contract.description}
           </ThemedText>
         )} */}
+      </Col>
     </Page>
   )
 }
