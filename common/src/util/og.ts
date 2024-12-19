7463fafbf7df3d694749b0f1271fcf7d4b7f35ee
Sinclair Chen <abc.sinclair@gmail.com>
2024-12-19 14:59:54 -0800

    Add topic og:image (social preview)
    Resolves MAN-1925

--- common/src/util/og.ts
+++ common/src/util/og.ts
@@ -1,15 +1,20 @@
 import { SerializedPoint } from 'common/chart'
 import { DOMAIN } from 'common/envs/constants'
+import { mapValues } from 'lodash'
 
 // opengraph functions that run in static props or client-side, but not in the edge (in image creation)
 
-export function buildOgUrl<P extends Record<string, string>>(
+export function buildOgUrl<P extends Record<string, string | string[]>>(
   props: P,
   endpoint: string,
   domain?: string
 ) {
   const generateUrlParams = (params: P) =>
-    new URLSearchParams(params).toString()
+    new URLSearchParams(
+      mapValues(params, (v) =>
+        typeof v === 'string' ? v : v.length === 0 ? '' : v.join(',')
+      )
+    ).toString()
 
   // Change to localhost:3000 for local testing
   const url =
