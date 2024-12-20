0a1c9b5fbfde28a50e1c7c216b8f1e06ec81b7ae
ingawei <ingawei@gmail.com>
2024-12-20 14:05:55 -0800

    fix

--- mani/components/contract/comments/parent-comment.tsx
+++ mani/components/contract/comments/parent-comment.tsx
@@ -2,11 +2,11 @@ import { ContractComment } from 'common/comment'
 import { Col } from 'components/layout/col'
 import { useEvent } from 'hooks/use-event'
 import { useState } from 'react'
-import { Comment } from './comment'
 import { TouchableOpacity } from 'react-native'
-import { Modal } from 'components/layout/modal'
 import { ThemedText } from 'components/themed-text'
-import { useColor } from 'hooks/useColor'
+import { useColor } from 'hooks/use-color'
+import { Modal } from 'components/layout/modal'
+import { Comment } from './comment'
 
 export function ParentComment({
   parentComment,
