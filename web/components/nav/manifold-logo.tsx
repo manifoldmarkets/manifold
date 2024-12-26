f5cbc1f3fdc88699fd225ade6bbfe654b50a3fde
mantikoros <sgrugett@gmail.com>
2024-12-26 10:27:14 -0700

    xmas

--- web/components/nav/manifold-logo.tsx
+++ web/components/nav/manifold-logo.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import clsx from 'clsx'
 import { useUser } from 'web/hooks/use-user'
 import { ENV } from 'common/envs/constants'
-import { LogoIcon } from '../icons/logo-icon'
 
 export function ManifoldLogo(props: { className?: string; twoLine?: boolean }) {
   const { className } = props
@@ -23,7 +22,13 @@ export function ManifoldLogo(props: { className?: string; twoLine?: boolean }) {
           className
         )}
       >
-        <LogoIcon
+        {/* <LogoIcon
+          className="h-10 w-10 shrink-0 stroke-indigo-700 transition-transform group-hover:rotate-12 dark:stroke-white"
+          aria-hidden
+        /> */}
+        <img
+          src="/christmas_manifold_logo.png"
+          alt="Manifold Christmas logo"
           className="h-10 w-10 shrink-0 stroke-indigo-700 transition-transform group-hover:rotate-12 dark:stroke-white"
           aria-hidden
         />
