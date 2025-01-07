1f80a22ba0838bcd72509bd399e468605ca49aa6
ingawei <ingawei@gmail.com>
2025-01-06 17:19:46 -0800

    different grays

--- mani/constants/colors.ts
+++ mani/constants/colors.ts
@@ -25,17 +25,17 @@ export const withOpacity = (hexColor: string, opacity: number) => {
 }
 
 const gray = {
-  50: '#f8fafc',
-  100: '#f1f5f9',
-  200: '#e2e8f0',
-  300: '#cbd5e1',
-  400: '#94a3b8',
-  500: '#64748b',
-  600: '#475569',
-  700: '#334155',
-  800: '#1e293b',
-  900: '#0f172a',
-  950: '#020617',
+  50: '#fafafa',
+  100: '#f4f4f5',
+  200: '#e4e4e7',
+  300: '#d4d4d8',
+  400: '#a1a1aa',
+  500: '#71717a',
+  600: '#52525b',
+  700: '#3f3f46',
+  800: '#27272a',
+  900: '#18181b',
+  950: '#09090b',
 } as const
 
 // Tailwind emerald colors
