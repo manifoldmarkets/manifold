6f0ef442b0f55d4058bbccd6fcc920e93f970776
ingawei <46611122+ingawei@users.noreply.github.com>
2025-01-09 18:41:13 -0800

    Ingawei/man 2205 fancy sports header (#3341)

--- mani/components/ui/icon-symbol.tsx
+++ mani/components/ui/icon-symbol.tsx
@@ -13,6 +13,7 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'account.balance': 'account-balance',
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
