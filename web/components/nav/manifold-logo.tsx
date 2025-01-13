1459fbaa8d7c7c000a1883022867bbd7e3d56123
Austin Chen <akrolsmir@gmail.com>
2025-01-13 15:48:14 -0800

    🪦 Max Chiswick

--- web/components/nav/manifold-logo.tsx
+++ web/components/nav/manifold-logo.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx'
 import { useUser } from 'web/hooks/use-user'
 import { ENV } from 'common/envs/constants'
 import { LogoIcon } from 'web/components/icons/logo-icon'
+import { Tooltip } from '../widgets/tooltip'
 
 export function ManifoldLogo(props: { className?: string; twoLine?: boolean }) {
   const { className } = props
@@ -33,5 +34,29 @@ export function ManifoldLogo(props: { className?: string; twoLine?: boolean }) {
         </div>
       </Link>
     </>
+  )
+}
+
+export function MemorialLogo(props: { className?: string; twoLine?: boolean }) {
+  const { className } = props
+
+  return (
+    <Tooltip text="RIP Max Chiswick">
+      <Link
+        href="https://blog.rossry.net/chisness/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={clsx(
+          'group flex w-full flex-row items-center justify-center gap-0.5 bg-black px-1 outline-none',
+          className
+        )}
+      >
+        <LogoIcon
+          className="h-10 w-10 shrink-0 stroke-white transition-transform group-hover:-rotate-3"
+          aria-hidden
+        />
+        <div className={clsx('text-xl font-thin text-white')}>MANIFOLD</div>
+      </Link>
+    </Tooltip>
   )
 }
