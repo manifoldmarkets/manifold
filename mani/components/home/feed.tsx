24ec07b67a82fc8bfc02737fd9aa41062431316d
Ian Philips <iansphilips@gmail.com>
2025-01-16 10:06:04 -0800

    Bump versions and add groups ids for tabs

--- mani/components/home/feed.tsx
+++ mani/components/home/feed.tsx
@@ -8,7 +8,21 @@ import { useAPIGetter } from 'hooks/use-api-getter'
 import { getDefinedContract, pairContracts } from 'lib/contracts'
 import { isProd } from 'lib/firebase/init'
 
+const NFL_ID = isProd
+  ? 'TNQwmbE5p6dnKx2e6Qlp'
+  : '437ba66d-e96c-436e-9227-a5552ea6c555'
+const NBA_ID = isProd
+  ? 'i0v3cXwuxmO9fpcInVYb'
+  : 'a458e68d-d098-46aa-adaa-e6db90e51f34'
+const EPL_ID = isProd
+  ? '5gsW3dPR3ySBRZCodrgm'
+  : '40b1c6e9-4c8f-40df-8908-c03bafaafa2f'
 const SPORTS_ID = isProd ? '2hGlgVhIyvVaFyQAREPi' : 'IOffGO7C9c0dfDura9Yn'
+const MLB_ID = 'RFwfANk54JSXOwj4qwsW,786nRQzgVyUnuUtaLTGW' // MLB, Baseball
+const NHL_ID = isProd
+  ? 'lccgApXa1l7O5ZH3XfhH,tYP9jmPPjoX29KfzE4l5' // NHL, Hockey
+  : '63cb2aac-11ce-40e5-b2a1-6980d8b21fce' // NHL
+const ALL_IDS = [NFL_ID, SPORTS_ID, EPL_ID, NBA_ID, MLB_ID].join(',')
 
 function MarketsList(props: { fetchProps: APIParams<'search-markets-full'> }) {
   const { fetchProps } = props
@@ -58,6 +72,12 @@ export function Feed({ tab }: { tab: string }) {
         <MLBContent />
       ) : tab == 'NHL' ? (
         <NHLContent />
+      ) : tab == 'news' ? (
+        <NewsContent />
+      ) : tab == 'entertainment' ? (
+        <EntertainmentContent />
+      ) : tab == 'politics' ? (
+        <PoliticsContent />
       ) : (
         <ForecastsContent />
       )}
@@ -84,8 +104,8 @@ function ForecastsContent() {
       fetchProps={{
         term: '',
         filter: 'open',
-        sort: 'newest',
-        // gids: ALL_IDS,
+        sort: 'score',
+        gids: ALL_IDS,
       }}
     />
   )
@@ -98,7 +118,7 @@ function NFLContent() {
         term: '',
         filter: 'open',
         sort: 'score',
-        // gids: NFL_ID,
+        gids: NFL_ID,
       }}
     />
   )
@@ -111,7 +131,7 @@ function NBAContent() {
         term: '',
         filter: 'open',
         sort: 'score',
-        // gids: NBA_ID,
+        gids: NBA_ID,
       }}
     />
   )
@@ -124,7 +144,7 @@ function EPLContent() {
         term: '',
         filter: 'open',
         sort: 'score',
-        // gids: EPL_ID,
+        gids: EPL_ID,
       }}
     />
   )
@@ -137,7 +157,7 @@ function MLBContent() {
         term: '',
         filter: 'open',
         sort: 'score',
-        // gids: MLB_ID,
+        gids: MLB_ID,
       }}
     />
   )
@@ -150,7 +170,58 @@ function NHLContent() {
         term: '',
         filter: 'open',
         sort: 'score',
-        // gids: NHL_ID,
+        gids: NHL_ID,
+      }}
+    />
+  )
+}
+
+function NewsContent() {
+  return (
+    <MarketsList
+      fetchProps={{
+        term: '',
+        filter: 'open',
+        sort: 'score',
+        // TODO: add an ignoreGids to ignore sports, fun, etc.?
+      }}
+    />
+  )
+}
+
+const entertainmentGroupIds = [
+  'KSeNIu7AWgiBBM5FqVuB', // Movies
+  '4QIcUOfCSSha0JZHAg9X', // Celebrities
+  'EUSEngFk1dGGBfaMeAmh', // TV and Film
+  'eJZecx6r22G2NriYYXcC', // Culture
+  'XU1fOYURSnb58lgsqaly', // Entertainment & Pop culture
+]
+
+function EntertainmentContent() {
+  return (
+    <MarketsList
+      fetchProps={{
+        term: '',
+        filter: 'open',
+        sort: 'score',
+        gids: isProd
+          ? entertainmentGroupIds.join(',')
+          : '42730330-3904-476f-861f-c4ba083d7199',
+      }}
+    />
+  )
+}
+
+function PoliticsContent() {
+  return (
+    <MarketsList
+      fetchProps={{
+        term: '',
+        filter: 'open',
+        sort: 'score',
+        gids: isProd
+          ? 'AjxQR8JMpNyDqtiqoA96' // US Politics
+          : 'iSWHIw6Xo1RkPq4Zh77m',
       }}
     />
   )
