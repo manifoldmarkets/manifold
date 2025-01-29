9a9fde19750c1c06786f872746c63b45efdf6c8d
Ian Philips <iansphilips@gmail.com>
2025-01-29 08:25:41 -0800

    Add mod task completion reward

--- backend/api/src/update-mod-report.ts
+++ backend/api/src/update-mod-report.ts
@@ -1,15 +1,40 @@
 import { ModReport } from 'common/mod-report'
 import { APIError, type APIHandler } from './helpers/endpoint'
-import { createSupabaseClient } from 'shared/supabase/init'
+import {
+  createSupabaseClient,
+  createSupabaseDirectClient,
+} from 'shared/supabase/init'
 import { removeUndefinedProps } from 'common/util/object'
+import { isAdminId, isModId } from 'common/envs/constants'
+import { runTxnOutsideBetQueue } from 'shared/txn/run-txn'
+import { log } from 'shared/utils'
+import { AdminRewardTxn } from 'common/txn'
+
+const MOD_REWARD_AMOUNT = 25
 
 export const updateModReport: APIHandler<'update-mod-report'> = async (
-  props
+  props,
+  auth
 ) => {
+  if (!isModId(auth.uid) && !isAdminId(auth.uid)) {
+    throw new APIError(403, 'You are not authorized to update mod reports')
+  }
   const { reportId, updates } = props
   const db = createSupabaseClient()
+  const pg = createSupabaseDirectClient()
 
   const updateData = removeUndefinedProps(updates)
+
+  // Check if this update has already been rewarded
+  const existingReward = await pg.oneOrNone(
+    `select 1 from txns where
+     category = $1 and
+     to_id = $2 and 
+     from_id = 'BANK' and
+     (data->'data'->'reportId')::int = $3 and 
+     data->'data'->>'updateType' = $4`,
+    ['ADMIN_REWARD', auth.uid, reportId, updates.status]
+  )
 
   const { data, error } = await db
     .from('mod_reports')
@@ -18,12 +43,30 @@ export const updateModReport: APIHandler<'update-mod-report'> = async (
     .select()
 
   if (error) {
-    console.error('Error updating report:', error)
+    log.error('Error updating report:', error)
     throw new APIError(500, 'Error updating report', { error })
   }
   if (!data || data.length === 0) {
-    console.error('Report not found for ID:', reportId)
+    log.error('Report not found for ID:', { reportId })
     throw new APIError(404, 'Report not found')
+  }
+
+  if (!existingReward && updates.status !== 'new') {
+    const txn = {
+      fromId: 'BANK',
+      fromType: 'BANK',
+      toId: auth.uid,
+      toType: 'USER',
+      amount: MOD_REWARD_AMOUNT,
+      token: 'M$',
+      category: 'ADMIN_REWARD',
+      data: {
+        reportId,
+        updateType: updates.status ?? '',
+      },
+      description: `Reward for updating mod report ${reportId}`,
+    } as AdminRewardTxn
+    await pg.tx(async (tx) => runTxnOutsideBetQueue(tx, txn))
   }
 
   return { status: 'success', report: data[0] as ModReport }
