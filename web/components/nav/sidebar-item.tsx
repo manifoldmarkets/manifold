acc0f16c76dd730ad09cd6f7856127d9c5dc3af5
mantikoros <mantikoros@users.noreply.github.com>
2025-02-06 07:52:53 +0000

    Auto-prettification

--- web/components/nav/sidebar-item.tsx
+++ web/components/nav/sidebar-item.tsx
@@ -26,7 +26,8 @@ export function SidebarItem(props: { item: NavItem; currentPage?: string }) {
     queryCleanedHref.split('/').length > 2
       ? '/' + queryCleanedHref.split('/')[1]
       : queryCleanedHref
-  const isCurrentPage = currentBasePath === segmentCleanedHref && !item.href?.startsWith('https://')
+  const isCurrentPage =
+    currentBasePath === segmentCleanedHref && !item.href?.startsWith('https://')
 
   const onClick = () => {
     track('sidebar: ' + item.name)
