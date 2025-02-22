b23d9b3d7bac3f2a83131677bbc331f779061b26
Sinclair Chen <abc.sinclair@gmail.com>
2025-02-21 21:37:49 -0800

    fix errors. also get user create times

--- backend/scripts/dump-lovers.ts
+++ backend/scripts/dump-lovers.ts
@@ -4,9 +4,9 @@ import * as fs from 'fs'
 
 // get all users of manifold love but ONLY the data relevant to manifold love
 
-runScript(({ pg }) => {
-  dumpUsers(pg)
-  dumpChats(pg)
+runScript(async ({ pg }) => {
+  await dumpUsers(pg)
+  await dumpChats(pg)
 })
 
 const dumpUsers = async (pg: SupabaseDirectClient) => {
@@ -15,9 +15,10 @@ const dumpUsers = async (pg: SupabaseDirectClient) => {
       u.id,
       u.username,
       u.name,
+      u.created_time,
       
       -- select some keys from user data
-      u.data - array(select jsonb_object_keys(u.data)
+      u.data - array(select jsonb_object_keys(u.data
         - 'isBannedFromPosting'
         - 'userDeleted'
         - 'bio'
@@ -28,25 +29,21 @@ const dumpUsers = async (pg: SupabaseDirectClient) => {
         - 'sweestakesVerified'
         - 'verifiedPhone'
         - 'idVerified'
-      ) as user_data,
+      )) as user_data,
       
       -- select some keys from private user data
-      pu.data - array(select jsonb_object_keys(pu.data)
+      pu.data - array(select jsonb_object_keys(pu.data
         - 'email'
         - 'notificationPreferences'
-      ) as private_user_data
+      )) as private_user_data
       
     FROM lovers l
     JOIN users u ON u.id = l.user_id
     LEFT JOIN private_users pu ON u.id = pu.id
   `)
 
-  const keys = Object.keys(user_results[0])
-  const csv = user_results
-    .map((row) => keys.map((k) => row.data[k]).join(','))
-    .join('\n')
-
-  fs.writeFileSync('users.csv', keys.join(',') + '\n' + csv)
+  const csv = resultsToCSV(user_results)
+  fs.writeFileSync('users.csv', csv)
 }
 
 const dumpChats = async (pg: SupabaseDirectClient) => {
@@ -56,22 +53,38 @@ const dumpChats = async (pg: SupabaseDirectClient) => {
       from private_user_message_channels mc
       where not exists (
         -- Check if any member is NOT a love user
-        select unnest(mc.member_ids) as user_id
+        select user_id from private_user_message_channel_members mcm
+          where mc.id = mcm.channel_id
         except
         select user_id from lovers
       )
-      and mc.member_count > 0
     )
     select 
       pum.*
     from love_channels lc
     left join private_user_messages pum
-    on pum.channel_id = lc.channel_id
-    order by lc.channel_id, pum.created_time
+    on pum.channel_id = lc.id
+    order by pum.channel_id, pum.created_time
   `)
 
-  const keys = Object.keys(results[0])
-  const csv = results.map((row) => keys.map((k) => row[k]).join(',')).join('\n')
-
-  fs.writeFileSync('chats.csv', keys.join(',') + '\n' + csv)
+  const csv = resultsToCSV(results)
+  fs.writeFileSync('chats.csv', csv)
 }
+
+const resultsToCSV = (results: any[]) => {
+  const keys = Object.keys(results[0])
+  const csv = results
+    .map((row) =>
+      keys
+        .map((k) =>
+          typeof row[k] === 'object' ? csvJSONStringify(row[k]) : row[k]
+        )
+        .join(',')
+    )
+    .join('\n')
+
+  return keys.join(',') + '\n' + csv
+}
+
+const csvJSONStringify = (o: Record<string, any>) =>
+  `"${JSON.stringify(o).replace(/"/g, '""')}"`
