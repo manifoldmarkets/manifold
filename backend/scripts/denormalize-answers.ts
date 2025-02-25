b3c9720a2f8daf5a99ea9d8c9bf0802df49c42e0
Ian Philips <iansphilips@gmail.com>
2025-02-25 15:40:58 -0800

    Fix script

--- backend/scripts/denormalize-answers.ts
+++ backend/scripts/denormalize-answers.ts
@@ -1,29 +1,8 @@
 import { runScript } from 'run-script'
-import { denormalizeContractAnswers } from 'functions/scheduled/denormalize-answers'
-import { chunk } from 'lodash'
-import { DAY_MS } from 'common/util/time'
+import { denormalizeAnswers } from 'scheduler/jobs/denormalize-answers'
 
 if (require.main === module) {
-  runScript(async ({ db, pg }) => {
-    // await denormalizeContractAnswers(db, ['ieywZhEmvi6ynaFSywX5'])
-    // Fetch answers modified since we broke denormalization
-    const timeAgo = new Date(Date.now() - 2 * DAY_MS)
-    const contractIds = await pg.map(
-      `
-        select id from contracts
-        where last_updated_time > $1
-        and mechanism = 'cpmm-multi-1'
-        `,
-      [timeAgo.toISOString()],
-      (r) => r.id as string
-    )
-    console.log('Denormalizing answers for contracts: ', contractIds)
-
-    const contractIdChunks = chunk(contractIds, 20)
-    for (const contractIdChunk of contractIdChunks) {
-      await denormalizeContractAnswers(db, contractIdChunk)
-    }
-
-    console.log('Done.')
+  runScript(async () => {
+    await denormalizeAnswers()
   })
 }
