9b96ef0e6f93e43d1541da39fd0d0bd4739c08ca
David Chee <d4vidchee@gmail.com>
2025-04-02 12:49:38 +0900

    generate leagues season 24

--- common/src/leagues.ts
+++ common/src/leagues.ts
@@ -4,7 +4,7 @@ export type season = (typeof SEASONS)[number]
 
 export const SEASONS = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23,
+  23, 24,
 ] as const
 export const CURRENT_SEASON = SEASONS[SEASONS.length - 1]
 
