1423f96c3760d5695aedd608001fc38df3102582
Ian Philips <iansphilips@gmail.com>
2025-05-14 10:10:05 -0700

    Tweak clarifications definition

--- backend/api/src/on-create-comment-on-contract.ts
+++ backend/api/src/on-create-comment-on-contract.ts
@@ -294,8 +294,12 @@ ${richTextToString(comment.content)}
 SYSTEM: Please analyze if the creator's latest comment ${
     commentsContext ? '(in context of the comment thread)' : ''
   } is clarifying or adding important details about how the market will be resolved, that is not already covered by the market's description/question title. 
-Only choose to issue a clarification if the creator's comment is unambiguously changing the resolution criteria as outlined in the description/question.
-If the creator says that they're going to update the description themselves, or they indicate their comment ${
+
+  ONLY choose to issue a clarification if you are CERTAIN that the creator's comment is unambiguously changing the resolution criteria as outlined in the description/question.
+  Do not issue clarifications for everything the creator says, only clarifications on how the market will resolve.
+  A clarification should very likely be a response to a question from a user about how the market will resolve in x case. Ignore lighthearted commentary and banter.
+
+  If the creator says that they're going to update the description themselves, or they indicate their comment ${
     commentsContext ? '(or their comments in the thread)' : ''
   } shouldn't be used to update the description, do not issue a clarification.
 
