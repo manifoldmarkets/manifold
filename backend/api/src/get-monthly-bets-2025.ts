3867a78b21a9bdc2d0c83141f4131398cfd744a9
Ian Philips <iansphilips@gmail.com>
2025-12-16 09:51:13 -0800

    Fix monthly bets

--- backend/api/src/get-monthly-bets-2025.ts
+++ backend/api/src/get-monthly-bets-2025.ts
@@ -1,9 +1,9 @@
 import { APIHandler } from 'api/helpers/endpoint'
 import { createSupabaseDirectClient } from 'shared/supabase/init'
 
-export const getmonthlybets2025: APIHandler<
-  'get-monthly-bets-2025'
-> = async (props) => {
+export const getmonthlybets2025: APIHandler<'get-monthly-bets-2025'> = async (
+  props
+) => {
   const { userId } = props
   const pg = createSupabaseDirectClient()
 
@@ -17,6 +17,7 @@ export const getmonthlybets2025: APIHandler<
     WHERE user_id = $1
       AND created_time >= '2025-01-01'::timestamp
       AND created_time <= '2025-12-31 23:59:59'::timestamp
+      and not is_redemption
     GROUP BY date_trunc('month', created_time)
     ORDER BY month
     `,
@@ -25,4 +26,3 @@ export const getmonthlybets2025: APIHandler<
 
   return data
 }
-
