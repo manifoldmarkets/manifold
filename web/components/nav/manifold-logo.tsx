a5d80cb3cb3eb371401c3e7a1079119ba47c3ed6
mantikoros <sgrugett@gmail.com>
2026-01-04 23:12:12 -0800

    use logoicon

--- web/components/nav/manifold-logo.tsx
+++ web/components/nav/manifold-logo.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx'
 import Image from 'next/image'
 import { useUser } from 'web/hooks/use-user'
 import { ENV } from 'common/envs/constants'
+import { LogoIcon } from '../icons/logo-icon'
 
 export function ManifoldLogo(props: { className?: string; twoLine?: boolean }) {
   const { className } = props
@@ -17,12 +18,9 @@ export function ManifoldLogo(props: { className?: string; twoLine?: boolean }) {
           className
         )}
       >
-        <Image
-          src="/logo.png"
-          alt="Manifold"
-          width={40}
-          height={40}
-          className="h-10 w-10 shrink-0 transition-transform group-hover:rotate-12"
+        <LogoIcon
+          className="h-10 w-10 shrink-0 stroke-indigo-700 transition-transform group-hover:rotate-12 dark:stroke-white"
+          aria-hidden
         />
         <div
           className={clsx('text-xl font-thin text-indigo-700 dark:text-white')}
