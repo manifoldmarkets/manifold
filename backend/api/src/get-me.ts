f32e88923ba9c00562d0e8569742cde994b765c8
todwadd <tod@wadd.com.au>
2026-01-13 13:19:41 +1030

    change getUserWithVisibility to getUser

--- backend/api/src/get-me.ts
+++ backend/api/src/get-me.ts
@@ -1,7 +1,7 @@
 import { type APIHandler } from './helpers/endpoint'
-import { getUserWithVisibility } from 'api/get-user'
+import { getUser } from 'api/get-user'
 
 export const getMe: APIHandler<'me'> = async (_, auth) => {
   // Use 'self' visibility so user can see their own modAlert (without mod identity)
-  return getUserWithVisibility({ id: auth.uid }, { visibility: 'self' })
+  return getUser({ id: auth.uid }, { visibility: 'self' })
 }
