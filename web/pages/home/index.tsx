14845fa10dbd4df6b44d0467d1146cc958c20541
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2026-05-02 13:51:23 -0400

    Revert banner to Manifest

--- web/pages/home/index.tsx
+++ web/pages/home/index.tsx
@@ -4,7 +4,7 @@ import Router from 'next/router'
 import { SEO } from 'web/components/SEO'
 import { DailyStats } from 'web/components/home/daily-stats'
 import { Page } from 'web/components/layout/page'
-import { DowntimeBanner, PrizeDrawing2Banner } from 'web/components/nav/banner'
+import { DowntimeBanner, Manifest2026Banner } from 'web/components/nav/banner'
 import { Welcome } from 'web/components/onboarding/welcome'
 import { VerificationResultModal } from 'web/components/onboarding/verification-result-modal'
 import { useRedirectIfSignedOut } from 'web/hooks/use-redirect-if-signed-out'
@@ -24,7 +24,7 @@ export default function Home() {
       <VerificationResultModal />
       <SEO title={`Home`} description={`Browse all questions`} url={`/home`} />
       <DowntimeBanner />
-      <PrizeDrawing2Banner />
+      <Manifest2026Banner />
       <DailyStats className="z-50 mb-1 w-full px-2 py-2" user={user} />
       <BrowsePageContent />
       {user && (
